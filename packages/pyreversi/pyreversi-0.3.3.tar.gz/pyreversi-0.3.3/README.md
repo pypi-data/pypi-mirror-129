9ff0c69f92f985768a449c6abbfcfd8ff1c9353c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:27 +0000

    Add package pyreversi/0.3.3/pyreversi-0.3.3.tar.gz

---
+++ packages/pyreversi/pyreversi-0.3.3.tar.gz/pyreversi-0.3.3/README.md
@@ -0,0 +1,47 @@
+# :black_circle: :white_circle: pyreversi :black_circle: :white_circle: 
+
+pyreversi is a reversi game in your terminal with IA available.
+
+# Démo
+
+![image](./demo_01.png)
+
+
+# 🚀 How to use **pyreversi**
+
+``pyreversi``
+
+## IA options
+
+- ``-b --blackbot [int]`` : black player is a bot, with level [int]
+- ``-w --whitebot [int]`` : white player is a bot, with level [int]
+
+- level 0 : one in available choice
+- level 1 : max increase score
+- level 2 : max increase score + target corner + not offer corner
+## Other options
+
+- ``-u/--update`` update the package
+- ``-v/--verbose`` verbose mode
+- ``-a/--auto`` auto mode (disable "press any key")
+- ``-r/--random`` random choice in the set of best choices for IA
+# ⚙️ Install
+
+See [this page](INSTALL.md)
+
+# :construction_worker: Contribution
+
+See [this page](CONTRIBUTING.md)
+
+# :package: Changelog
+
+See [this page](CHANGELOG.md)
+
+
+# License
+
+MIT License
+
+Copyright (c) 2021 [thib1984](https://github.com/thib1984)
+
+See [this page](LICENSE.txt) for details
---
+++ packages/pyreversi/pyreversi-0.3.3.tar.gz/pyreversi-0.3.3/pyreversi/args.py
@@ -0,0 +1,89 @@
+"""
+pygitscrum argparse gestion
+"""
+
+import argparse
+import sys
+
+
+class CustomHelpFormatter(argparse.HelpFormatter):
+    def _format_action_invocation(self, action):
+        if not action.option_strings or action.nargs == 0:
+            return super()._format_action_invocation(action)
+        default = self._get_default_metavar_for_optional(action)
+        args_string = self._format_args(action, default)
+        return ", ".join(action.option_strings) + " " + args_string
+
+    def _format_args(self, action, default_metavar):
+        get_metavar = self._metavar_formatter(action, default_metavar)
+        if action.nargs == argparse.ONE_OR_MORE:
+            return "%s" % get_metavar(1)
+        else:
+            return super(CustomHelpFormatter, self)._format_args(
+                action, default_metavar
+            )
+
+def compute_args():
+    """
+    check args and return them
+    """
+    my_parser = argparse.ArgumentParser(
+        description="pyreversi is a reversi game in your terminal with IA available.",
+        epilog="""
+        Full documentation at: <https://github.com/thib1984/pyreversi>.
+        Report bugs to <https://github.com/thib1984/pyreversi/issues>.
+        MIT Licence.
+        Copyright (c) 2021 thib1984.
+        This is free software: you are free to change and redistribute it.
+        There is NO WARRANTY, to the extent permitted by law.
+        Written by thib1984.""",
+        formatter_class=CustomHelpFormatter,
+    )
+
+    my_parser.add_argument(
+        "-v",
+        "--verbose",
+        action="store_true",
+        help="mode verbeux",
+    )
+    my_parser.add_argument(
+        "-a",
+        "--auto",
+        action="store_true",
+        help="mode auto",
+    )                     
+    my_parser.add_argument(
+        "-u",
+        "--update",
+        action="store_true",
+        help="mise à jour de pyreversi",
+    ),
+    my_parser.add_argument(
+        "-r",
+        "--random",
+        action="store_true",
+        help="add random in IA",
+    ),    
+    my_parser.add_argument(
+        "-w",
+        "--whitebot",
+        metavar="X",
+        action="store",
+        type=int,
+        default=-1,
+        choices=range(0, 3),
+        help="white player is a bot",
+    )    
+    my_parser.add_argument(
+        "-b",
+        "--blackbot",
+        metavar="X",
+        action="store",
+        type=int,
+        default=-1,
+        choices=range(0, 3),
+        help="black player is a bot",
+    )        
+
+    args = my_parser.parse_args()
+    return args
