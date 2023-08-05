fda4fa916f0ae759b2492eab288a9f0ed9879708
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:21 +0000

    Add package hikari-lightbulb/2.0.2/hikari_lightbulb-2.0.2-py3-none-any.whl

---
+++ packages/hikari-lightbulb/hikari_lightbulb-2.0.2-py3-none-any.whl/lightbulb/__init__.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+# Copyright © tandemdude 2020-present
+#
+# This file is part of Lightbulb.
+#
+# Lightbulb is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Lightbulb is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with Lightbulb. If not, see <https://www.gnu.org/licenses/>.
+from lightbulb import app
+from lightbulb import checks
+from lightbulb import commands
+from lightbulb import context
+from lightbulb import converters
+from lightbulb import cooldowns
+from lightbulb import decorators
+from lightbulb import errors
+from lightbulb import events
+from lightbulb import help_command
+from lightbulb import plugins
+from lightbulb import utils
+from lightbulb.app import *
+from lightbulb.checks import *
+from lightbulb.cooldowns import *
+from lightbulb.decorators import *
+from lightbulb.errors import *
+from lightbulb.events import *
+from lightbulb.help_command import *
+from lightbulb.plugins import *
+
+__version__ = "2.0.2"
