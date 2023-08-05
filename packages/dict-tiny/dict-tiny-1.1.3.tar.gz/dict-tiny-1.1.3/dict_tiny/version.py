b1b1c3b50297a8229d5491eff63651c184f547ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:25 +0000

    Add package dict-tiny/1.1.3/dict-tiny-1.1.3.tar.gz

---
+++ packages/dict-tiny/dict-tiny-1.1.3.tar.gz/dict-tiny-1.1.3/dict_tiny/version.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+
+name = "dict-tiny"
+__version__ = '1.1.3'
+DESCRIPTION = "A command-line translator was implemented in Plumbum. Google Translate and DeepL Translator and Youdao. Just for fun :)"
