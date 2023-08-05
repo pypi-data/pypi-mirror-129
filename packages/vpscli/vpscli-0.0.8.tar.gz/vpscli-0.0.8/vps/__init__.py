45f2641815d57b45a661ff7745986a09a5c11365
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:51 +0000

    Add package vpscli/0.0.8/vpscli-0.0.8.tar.gz

---
+++ packages/vpscli/vpscli-0.0.8.tar.gz/vpscli-0.0.8/vps/__init__.py
@@ -0,0 +1,19 @@
+#!/usr/local/env python3
+from .configer import (CalculatePrime, EndlessConfig, GostConfig,
+                       MonitorConfig, QbittorrentConfig, SystemConfig)
+from .installer import AutoRemove, CoreAppInstaller, StatAppInstaller
+from .piper import PiperContext
+
+
+def main():
+    exec_classes = [
+        CoreAppInstaller, StatAppInstaller, AutoRemove, SystemConfig,
+        EndlessConfig, QbittorrentConfig, MonitorConfig, GostConfig,
+        CalculatePrime, PiperContext
+    ]
+    for ec in exec_classes:
+        ec().run()
+
+
+if __name__ == '__main__':
+    main()
