00b3c17cabc0f369749638457efe6f68bdeee856
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:54 +0000

    Add package linkauto/0.0.6/linkauto-0.0.6.tar.gz

---
+++ packages/linkauto/linkauto-0.0.6.tar.gz/linkauto-0.0.6/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+from distutils.core import setup
+
+setup(
+  name='linkauto',
+  packages=setuptools.find_packages(),
+  version='0.0.6',
+  license='MIT',
+  description='Python wrapper for the private LinkedIn API',
+  author='Stan van Rooy',
+  author_email='stan@rooy.dev',
+  url='https://github.com/stanvanrooy/linkauto',
+  download_url='https://github.com/stanvanrooy/linkauto/archive/0.0.6.tar.gz',
+  keywords=['linkedin api', 'private linkedin api'],
+  install_requires=[
+    'asyncio'
+    'orjson',
+    'aiohttp'
+    'aioscheduler'
+  ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Libraries :: Python Modules',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3.8',
+  ],
+)
