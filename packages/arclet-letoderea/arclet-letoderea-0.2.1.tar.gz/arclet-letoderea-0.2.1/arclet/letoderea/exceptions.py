9d45d8d02c8be4e67e86b7d8f16157577742db2e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:25 +0000

    Add package arclet-letoderea/0.2.1/arclet-letoderea-0.2.1.tar.gz

---
+++ packages/arclet-letoderea/arclet-letoderea-0.2.1.tar.gz/arclet-letoderea-0.2.1/arclet/letoderea/exceptions.py
@@ -0,0 +1,18 @@
+class UnexpectedArgument(Exception):
+    pass
+
+
+class UndefinedRequirement(Exception):
+    pass
+
+
+class MultipleInserter(Exception):
+    pass
+
+
+class RepeatedInserter(Exception):
+    pass
+
+
+class PropagationCancelled(Exception):
+    pass
