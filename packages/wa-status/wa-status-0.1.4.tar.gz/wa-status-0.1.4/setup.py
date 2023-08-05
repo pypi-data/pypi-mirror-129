7a293d38a3e1cad17445112d2e4ec7321602313c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:12:38 +0000

    Add package wa-status/0.1.4/wa-status-0.1.4.tar.gz

---
+++ packages/wa-status/wa-status-0.1.4.tar.gz/wa-status-0.1.4/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['wa_status']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'wa-status',
+    'version': '0.1.4',
+    'description': '',
+    'long_description': None,
+    'author': 'ggzor',
+    'author_email': '30713864+ggzor@users.noreply.github.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.9,<3.11',
+}
+
+
+setup(**setup_kwargs)
