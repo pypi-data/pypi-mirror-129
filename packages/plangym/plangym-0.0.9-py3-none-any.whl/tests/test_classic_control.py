f4de57e5d8fe8cfcc8d07ab185c873bc170665fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:15:16 +0000

    Add package plangym/0.0.9/plangym-0.0.9-py3-none-any.whl

---
+++ packages/plangym/plangym-0.0.9-py3-none-any.whl/tests/test_classic_control.py
@@ -0,0 +1,20 @@
+import sys
+
+import pytest
+
+from plangym.classic_control import ClassicControl
+from tests.test_core import TestGymEnvironment
+
+
+environments = [
+    "MountainCar-v0",
+    "Acrobot-v1",
+    "MountainCarContinuous-v0",
+    f"Pendulum-v0",
+    "CartPole-v0",
+]
+
+
+@pytest.fixture(params=environments, scope="class")
+def env(request) -> ClassicControl:
+    return ClassicControl(name=request.param)
