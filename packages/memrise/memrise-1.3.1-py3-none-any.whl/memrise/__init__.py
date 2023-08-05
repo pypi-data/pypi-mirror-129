51084397130371598cc62f3ac32833e810039ea7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:12:50 +0000

    Add package memrise/1.3.1/memrise-1.3.1-py3-none-any.whl

---
+++ packages/memrise/memrise-1.3.1-py3-none-any.whl/memrise/__init__.py
@@ -0,0 +1,9 @@
+"""Library Memrise Scraping"""
+from .memrise import Data
+from .extract import Level, Course
+from .data import TypeError
+from .translator import transUntilDone
+
+
+__all__ = ["Level", "Course", "Data", "TypeError", "transUntilDone"]
+__version__ = "1.3.1"
