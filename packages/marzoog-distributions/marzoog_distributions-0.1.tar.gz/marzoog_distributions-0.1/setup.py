789faed7300c86b2de6edf47f81563835e97aee1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:09:11 +0000

    Add package marzoog-distributions/0.1/marzoog_distributions-0.1.tar.gz

---
+++ packages/marzoog-distributions/marzoog_distributions-0.1.tar.gz/marzoog_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='marzoog_distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['marzoog_distributions'],
+      author='Marzoog AlGhazwi',
+      author_email='marzoog.tech@gmail.com',
+      zip_safe=False)
