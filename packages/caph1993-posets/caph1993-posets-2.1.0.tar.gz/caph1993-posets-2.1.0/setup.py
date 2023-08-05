d4499189074107549880b12ca1a85f69a2e47a47
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:09:51 +0000

    Add package caph1993-posets/2.1.0/caph1993-posets-2.1.0.tar.gz

---
+++ packages/caph1993-posets/caph1993-posets-2.1.0.tar.gz/caph1993-posets-2.1.0/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup
+'''
+To upload:
+python3 setup.py check
+python3 setup.py sdist
+twine upload dist/*
+'''
+
+setup(
+    name='caph1993-posets',
+    version='2.1.0',
+    description='Toolbox for finite posets and lattices',
+    url='https://github.com/caph1993/caph1993-posets',
+    author='Carlos Pinzón',
+    author_email='caph1993@gmail.com',
+    license='MIT',
+    packages=[
+        'cp93posets',
+    ],
+    install_requires=[
+        'numpy',
+        'caph1993-pytools>=0.2.37',
+        'pyhash',
+        'pydotplus',
+    ],
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+    ],
+)
