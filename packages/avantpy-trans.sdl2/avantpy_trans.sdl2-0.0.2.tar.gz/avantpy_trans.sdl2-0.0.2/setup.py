c2fb90387d8dee051aaf8ef8539f2b0aaaa9402b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:55 +0000

    Add package avantpy-trans.sdl2/0.0.2/avantpy_trans.sdl2-0.0.2.tar.gz

---
+++ packages/avantpy-trans.sdl2/avantpy_trans.sdl2-0.0.2.tar.gz/avantpy_trans.sdl2-0.0.2/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['avantpy_trans', 'avantpy_trans.sdl2']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['PyYAML>=6.0,<7.0']
+
+setup_kwargs = {
+    'name': 'avantpy-trans.sdl2',
+    'version': '0.0.2',
+    'description': 'avantpy translations',
+    'long_description': None,
+    'author': 'Phil Weir',
+    'author_email': 'phil.t.weir@googlemail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.6.5,<4.0.0',
+}
+
+
+setup(**setup_kwargs)
