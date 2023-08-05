8557ab1bacdb117b45ea4baf3268ecfb14ec475f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:32 +0000

    Add package proxy.py/2.4.0a2/proxy.py-2.4.0a2-py3-none-any.whl

---
+++ packages/proxy.py/proxy.py-2.4.0a2-py3-none-any.whl/proxy/core/event/__init__.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+    proxy.py
+    ~~~~~~~~
+    ⚡⚡⚡ Fast, Lightweight, Pluggable, TLS interception capable proxy server focused on
+    Network monitoring, controls & Application development, testing, debugging.
+
+    :copyright: (c) 2013-present by Abhinav Singh and contributors.
+    :license: BSD, see LICENSE for more details.
+
+    .. spelling::
+
+       eventing
+       iterable
+       Submodules
+"""
+from .queue import EventQueue
+from .names import EventNames, eventNames
+from .dispatcher import EventDispatcher
+from .subscriber import EventSubscriber
+from .manager import EventManager
+
+__all__ = [
+    'eventNames',
+    'EventNames',
+    'EventQueue',
+    'EventDispatcher',
+    'EventSubscriber',
+    'EventManager',
+]
