03f47642bde71a198e4f8fff6e923135150da264
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:10:30 +0000

    Add package cdk-ecrpublic-gc/0.1.168/cdk_ecrpublic_gc-0.1.168-py3-none-any.whl

---
+++ packages/cdk-ecrpublic-gc/cdk_ecrpublic_gc-0.1.168-py3-none-any.whl/cdk_ecrpublic_gc/_jsii/__init__.py
@@ -0,0 +1,29 @@
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import publication
+import typing_extensions
+
+import aws_cdk.aws_events._jsii
+import aws_cdk.aws_events_targets._jsii
+import aws_cdk.aws_iam._jsii
+import aws_cdk.aws_lambda._jsii
+import aws_cdk.aws_lambda_nodejs._jsii
+import aws_cdk.aws_stepfunctions._jsii
+import aws_cdk.aws_stepfunctions_tasks._jsii
+import aws_cdk.core._jsii
+import constructs._jsii
+
+__jsii_assembly__ = jsii.JSIIAssembly.load(
+    "cdk-ecrpublic-gc", "0.1.168", __name__[0:-6], "cdk-ecrpublic-gc@0.1.168.jsii.tgz"
+)
+
+__all__ = [
+    "__jsii_assembly__",
+]
+
+publication.publish()
