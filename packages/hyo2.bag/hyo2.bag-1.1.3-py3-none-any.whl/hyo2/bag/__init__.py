6e7bb1c46b429e20be841a50dc3ef1e6f20771b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:09:50 +0000

    Add package hyo2.bag/1.1.3/hyo2.bag-1.1.3-py3-none-any.whl

---
+++ packages/hyo2.bag/hyo2.bag-1.1.3-py3-none-any.whl/hyo2/bag/__init__.py
@@ -0,0 +1,10 @@
+"""
+Hydro-Package
+BAG
+"""
+
+name = 'BAG'
+__version__ = '1.1.3'
+__author__ = 'gmasetti@ccom.unh.edu'
+__license__ = 'LGPLv3 license'
+__copyright__ = 'Copyright (c) 2021, University of New Hampshire, Center for Coastal and Ocean Mapping'
