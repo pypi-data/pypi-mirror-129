33b46f5ce1c55d91b7342947f87f2e215917d3bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:10:33 +0000

    Add package maitri/0.1.0/maitri-0.1.0.tar.gz

---
+++ packages/maitri/maitri-0.1.0.tar.gz/maitri-0.1.0/setup.py
@@ -0,0 +1,7 @@
+from setuptools import find_packages, setup
+setup(name="maitri",
+	packages=find_packages(include=['wizard.py']),
+	version='0.1.0',
+	author='Prateek Yadav',
+    license='AIMTECH',
+	description="change background"),
