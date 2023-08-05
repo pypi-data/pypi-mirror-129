db1203fbe4cc2e0bebb2b1528980218c016de0a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:09:20 +0000

    Add package pypdc/0.0.5/pypdc-0.0.5.tar.gz

---
+++ packages/pypdc/pypdc-0.0.5.tar.gz/pypdc-0.0.5/pypdc/__init__.py
@@ -0,0 +1,16 @@
+"""
+PyPDC
+=========
+
+PyPDC is a Python package for calculating asymptotic Partial Directed Coherence and Directed Coherence estimation for brain connectivity analysis.
+
+"""
+
+name = "pypdc"
+
+#Current version:
+__version__ = "0.0.5"
+
+
+
+
