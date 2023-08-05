e6aba0672acfb891c2af0fe8522a606d6e0f8834
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:09:12 +0000

    Add package eclipse/0.3.0/eclipse-0.3.0.tar.gz

---
+++ packages/eclipse/eclipse-0.3.0.tar.gz/eclipse-0.3.0/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['eclipse']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['ftfy>=6.0.3,<7.0.0',
+ 'regex>=2021.11.10,<2022.0.0',
+ 'torch>=1.7.1,<2.0.0',
+ 'torchvision>=0.11.1,<0.12.0',
+ 'tqdm>=4.62.3,<5.0.0']
+
+setup_kwargs = {
+    'name': 'eclipse',
+    'version': '0.3.0',
+    'description': "A simplified and extended version of OpenAI's Contrastive Language-Image Pretraining",
+    'long_description': None,
+    'author': 'CWDT',
+    'author_email': 'null@cwdt.us',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.6,<4.0',
+}
+
+
+setup(**setup_kwargs)
