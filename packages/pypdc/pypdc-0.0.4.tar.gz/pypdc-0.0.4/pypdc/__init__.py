076e636e0ca5dc9bf30efe1c71420db7752452d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:14:01 +0000

    Add package pypdc/0.0.4/pypdc-0.0.4.tar.gz

---
+++ packages/pypdc/pypdc-0.0.4.tar.gz/pypdc-0.0.4/pypdc/__init__.py
@@ -0,0 +1,35 @@
+"""
+PyPDC
+=========
+
+"""
+
+name = "pypdc"
+
+__version__ = "0.0.4"
+
+import pypdc.adaptative_new
+from pygebraca.adaptative_new import *
+
+import pypdc.analysis_new
+from pypdc.analysis_new import *
+
+import pypdc.ar_data_new
+from pypdc.ar_data_new import *
+
+import pypdc.ar_fit_new
+from pypdc.ar_fit_new import *
+
+import pypdc.asymp_new
+from pypdc.asymp_new import *
+
+import pypdc.bootstrap_new
+from pypdc.bootstrap_new import *
+
+import pypdc.plotting_new
+from pypdc.plotting_new import *
+
+import pypdc.utils_new
+from pypdc.utils_new import *
+
+
