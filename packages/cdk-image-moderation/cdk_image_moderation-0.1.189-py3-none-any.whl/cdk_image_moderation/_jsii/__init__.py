6d5a1990cee47ee0fc140ef82ff0da74075c261d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:10:28 +0000

    Add package cdk-image-moderation/0.1.189/cdk_image_moderation-0.1.189-py3-none-any.whl

---
+++ packages/cdk-image-moderation/cdk_image_moderation-0.1.189-py3-none-any.whl/cdk_image_moderation/_jsii/__init__.py
@@ -0,0 +1,32 @@
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import publication
+import typing_extensions
+
+import aws_cdk.aws_iam._jsii
+import aws_cdk.aws_lambda._jsii
+import aws_cdk.aws_lambda_nodejs._jsii
+import aws_cdk.aws_s3._jsii
+import aws_cdk.aws_s3_notifications._jsii
+import aws_cdk.aws_sns._jsii
+import aws_cdk.aws_sns_subscriptions._jsii
+import aws_cdk.core._jsii
+import constructs._jsii
+
+__jsii_assembly__ = jsii.JSIIAssembly.load(
+    "cdk-image-moderation",
+    "0.1.189",
+    __name__[0:-6],
+    "cdk-image-moderation@0.1.189.jsii.tgz",
+)
+
+__all__ = [
+    "__jsii_assembly__",
+]
+
+publication.publish()
