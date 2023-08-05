477f8025bc415e3079e809c3e7ae5deadf8ad22a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:09:44 +0000

    Add package hyo2.abc/0.3.4/hyo2.abc-0.3.4.tar.gz

---
+++ packages/hyo2.abc/hyo2.abc-0.3.4.tar.gz/hyo2.abc-0.3.4/hyo2/abc/__init__.py
@@ -0,0 +1,14 @@
+"""
+Hyo2-Package
+ABC
+"""
+
+import logging
+logger = logging.getLogger(__name__)
+logger.addHandler(logging.NullHandler())
+
+name = "ABC"
+__version__ = "0.3.4"
+__author__ = "gmasetti@ccom.unh.edu"
+__license__ = "LGPLv3 license"
+__copyright__ = "Copyright 2021 University of New Hampshire, Center for Coastal and Ocean Mapping"
