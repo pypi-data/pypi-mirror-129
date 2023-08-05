cdbcc0613a80e3863adb00e00e0b697b51f1a87c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:09:52 +0000

    Add package dataclass-wizard/0.20.1/dataclass-wizard-0.20.1.tar.gz

---
+++ packages/dataclass-wizard/dataclass-wizard-0.20.1.tar.gz/dataclass-wizard-0.20.1/docs/index.rst
@@ -0,0 +1,26 @@
+Welcome to Dataclass Wizard's documentation!
+============================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+   readme
+   overview
+   installation
+   quickstart
+   examples
+   wiz_cli
+   using_field_properties
+   python_compatibility
+   common_use_cases/index
+   advanced_usage/index
+   modules
+   contributing
+   history
+
+Indices and tables
+==================
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
---
+++ packages/dataclass-wizard/dataclass-wizard-0.20.1.tar.gz/dataclass-wizard-0.20.1/tests/unit/conftest.py
@@ -0,0 +1,26 @@
+"""
+Common test fixtures and utilities.
+"""
+
+from pathlib import Path
+from uuid import UUID
+
+from ..conftest import PY36
+
+
+if PY36:
+    # Ignore these files, because Python 3.6 doesn't define the `__future__`
+    # import used, so we'll run into an error if we import these modules.
+    #
+    # Ref: https://docs.pytest.org/en/6.2.x/example/pythoncollection.html#customizing-test-collection
+    collect_ignore = ['test_load_with_future_import.py',
+                      'test_property_wizard_with_future_import.py']
+
+
+class MyUUIDSubclass(UUID):
+    """
+    Simple UUID subclass that calls :meth:`hex` when ``str()`` is invoked.
+    """
+
+    def __str__(self):
+        return self.hex
