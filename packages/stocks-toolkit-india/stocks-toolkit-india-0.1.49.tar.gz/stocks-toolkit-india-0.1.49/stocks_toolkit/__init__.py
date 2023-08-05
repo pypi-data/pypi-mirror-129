7ac4d6d8249fe90ea40d3af8ab75fdf88f468bff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:57 +0000

    Add package stocks-toolkit-india/0.1.49/stocks-toolkit-india-0.1.49.tar.gz

---
+++ packages/stocks-toolkit-india/stocks-toolkit-india-0.1.49.tar.gz/stocks-toolkit-india-0.1.49/stocks_toolkit/__init__.py
@@ -0,0 +1,3 @@
+from .stocks_toolkit import stocks_toolkit
+
+__version__ = "0.1.49"
---
+++ packages/stocks-toolkit-india/stocks-toolkit-india-0.1.49.tar.gz/stocks-toolkit-india-0.1.49/stocks_toolkit/candle_analysis.py
@@ -0,0 +1,39 @@
+import talib
+from .retrieval_layer import Retrieve
+import pandas as pd
+
+
+class Candle_analysis:
+    def __init__(self) -> None:
+        self.retrieve_stocks = Retrieve()
+
+    def candle_patterns(self) -> list:
+        return talib.get_function_groups()["Pattern Recognition"]
+
+    def filter_technicals(self, df) -> pd.DataFrame:
+        return pd.DataFrame(
+            {
+                "open": list(df["Open Price"]),
+                "close": list(df["Close Price"]),
+                "high": list(df["High Price"]),
+                "low": list(df["Low Price"]),
+                "date": list(df["Date"]),
+            }
+        )
+
+    def find_candles(self, symbol, from_date, to_date) -> pd.DataFrame:
+        data = self.filter_technicals(
+            self.retrieve_stocks.stock_data(symbol, from_date, to_date)
+        )
+        candles_found = [[] for _ in range(len(data))]
+        for candle in self.candle_patterns():
+            candle_weights = getattr(talib, candle)(
+                data["open"], data["high"], data["low"], data["close"]
+            )
+            for i in range(len(candle_weights)):
+                if candle_weights[i] == 100:
+                    candles_found[i].append(candle + ":Bullish")
+                elif candle_weights[i] == -100:
+                    candles_found[i].append(candle + ":Bearish")
+        data["candles_found"] = candles_found
+        return data
