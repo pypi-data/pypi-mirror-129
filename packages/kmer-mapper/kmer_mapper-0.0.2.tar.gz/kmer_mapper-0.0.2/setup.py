bb99eb1cc1cc9aa60dca0f80414dd77ac403e703
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:27 +0000

    Add package kmer-mapper/0.0.2/kmer_mapper-0.0.2.tar.gz

---
+++ packages/kmer-mapper/kmer_mapper-0.0.2.tar.gz/kmer_mapper-0.0.2/setup.py
@@ -0,0 +1,45 @@
+from setuptools import setup
+from distutils.core import setup, Extension
+from Cython.Build import cythonize
+from distutils.extension import Extension
+from Cython.Distutils import build_ext
+import numpy as np
+
+ext_modules=[
+    Extension("kmer_mapper.mapper",
+              ["kmer_mapper/mapper.pyx"],
+              libraries=["m"],
+              extra_compile_args = ["-O3", "-march=native"],
+              )
+      ]
+
+setup(name='kmer_mapper',
+      version='0.0.2',
+      description='Kmer Mapper',
+      url='http://github.com/ivargr/kmer_mapper',
+      author='Ivar Grytten',
+      author_email='',
+      license='MIT',
+      packages=["kmer_mapper"],
+      zip_safe=False,
+      install_requires=['numpy==1.20.3', 'cython', 'graph_kmer_index'],
+      classifiers=[
+            'Programming Language :: Python :: 3'
+      ],
+      entry_points={
+            'console_scripts': ['kmer_mapper=kmer_mapper.command_line_interface:main']
+      },
+      cmdclass = {"build_ext": build_ext},
+      #ext_modules = ext_modules
+      include_dirs=np.get_include(),
+      ext_modules=cythonize(["kmer_mapper/mapper.pyx"]),
+)
+
+"""
+rm -rf dist
+python3 setup.py sdist bdist_wheel
+auditwheel repair --plat manylinux_2_17_x86_64 dist/kmer_mapper-*-cp38-cp38-linux_x86_64.whl
+mv wheelhouse/* dist
+rm dist/kmer_mapper-0.0.1-cp38-cp38-linux_x86_64.whl
+python3 -m twine upload --repository pypi dist/*
+"""
