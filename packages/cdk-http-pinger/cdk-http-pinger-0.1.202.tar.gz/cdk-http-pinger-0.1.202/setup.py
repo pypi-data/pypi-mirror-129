5510ad57ae0b53b938f3e6ba1b5d8f8e0b45ad25
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:10:30 +0000

    Add package cdk-http-pinger/0.1.202/cdk-http-pinger-0.1.202.tar.gz

---
+++ packages/cdk-http-pinger/cdk-http-pinger-0.1.202.tar.gz/cdk-http-pinger-0.1.202/setup.py
@@ -0,0 +1,67 @@
+import json
+import setuptools
+
+kwargs = json.loads(
+    """
+{
+    "name": "cdk-http-pinger",
+    "version": "0.1.202",
+    "description": "HTTP Pinger for AWS CDK",
+    "license": "Apache-2.0",
+    "url": "https://github.com/pahud/cdk-http-pinger.git",
+    "long_description_content_type": "text/markdown",
+    "author": "Pahud Hsieh<pahudnet@gmail.com>",
+    "bdist_wheel": {
+        "universal": true
+    },
+    "project_urls": {
+        "Source": "https://github.com/pahud/cdk-http-pinger.git"
+    },
+    "package_dir": {
+        "": "src"
+    },
+    "packages": [
+        "cdk_http_pinger",
+        "cdk_http_pinger._jsii"
+    ],
+    "package_data": {
+        "cdk_http_pinger._jsii": [
+            "cdk-http-pinger@0.1.202.jsii.tgz"
+        ],
+        "cdk_http_pinger": [
+            "py.typed"
+        ]
+    },
+    "python_requires": ">=3.6",
+    "install_requires": [
+        "aws-cdk.aws-lambda>=1.73.0, <2.0.0",
+        "aws-cdk.aws-logs>=1.73.0, <2.0.0",
+        "aws-cdk.core>=1.73.0, <2.0.0",
+        "aws-cdk.custom-resources>=1.73.0, <2.0.0",
+        "constructs>=3.2.27, <4.0.0",
+        "jsii>=1.46.0, <2.0.0",
+        "publication>=0.0.3"
+    ],
+    "classifiers": [
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Programming Language :: JavaScript",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Typing :: Typed",
+        "Development Status :: 5 - Production/Stable",
+        "License :: OSI Approved"
+    ],
+    "scripts": []
+}
+"""
+)
+
+with open("README.md", encoding="utf8") as fp:
+    kwargs["long_description"] = fp.read()
+
+
+setuptools.setup(**kwargs)
