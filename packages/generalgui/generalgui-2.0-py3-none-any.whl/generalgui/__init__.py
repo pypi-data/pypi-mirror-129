5f0609529784afae34ae595ba702b0c8e67c63a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:09:41 +0000

    Add package generalgui/2.0/generalgui-2.0-py3-none-any.whl

---
+++ packages/generalgui/generalgui-2.0-py3-none-any.whl/generalgui/__init__.py
@@ -0,0 +1,10 @@
+from generalgui.elements.page import Page
+from generalgui.elements.label import Label
+from generalgui.elements.button import Button
+from generalgui.elements.checkbutton import Checkbutton
+from generalgui.elements.entry import Entry
+
+from generalgui.pages.plot import Plot
+
+class MethodGrouper(Plot, Page, Label, Button, Checkbutton):
+    pass
