cb57f0813b0b8636e5f7b8caba37552eba84bf0c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:18 +0000

    Add package pykorbit/0.2.2/pykorbit-0.2.2.tar.gz

---
+++ packages/pykorbit/pykorbit-0.2.2.tar.gz/pykorbit-0.2.2/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name            = 'pykorbit',
+    version         = '0.2.2',
+    description     = 'python wrapper for Korbit API',
+    url             = 'https://github.com/sharebook-kr/pykorbit',
+    author          = 'brayden.jo, jonghun.yoo',
+    author_email    = 'brayden.jo@outlook.com, jonghun.yoo@outlook.com',
+    install_requires= ['requests', 'pandas', 'datetime', 'numpy', 'xlrd', 'PyJWT', 'websockets'],
+    license         = 'MIT',
+    packages        = ['pykorbit'],
+    zip_safe        = False
+)
