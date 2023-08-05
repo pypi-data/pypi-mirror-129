341fceb15aea0607989dac0f809d91b89dce9f8d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:14:04 +0000

    Add package functionali/0.3.0/functionali-0.3.0-py3-none-any.whl

---
+++ packages/functionali/functionali-0.3.0-py3-none-any.whl/functionali/predicates.py
@@ -0,0 +1,471 @@
+"""A file containing useful predicates."""
+
+from typing import List, Iterable, Callable, Any, Sequence, Union, Tuple, Iterator
+
+from .higher_order_functions import partial, flip, reduce
+from operator import lt, le, ge, gt
+
+
+def identity(x):
+    """Returns its argument as it is."""
+    return x
+
+
+def equals(a, b=None, *args):
+    """
+    if only ``a`` is passed, a function is returned that returns True when the ``arg`` passed to it is equal
+    to ``a``; else returns True when ``a``,``b`` and ``*args`` are equal.
+
+    with one argument
+
+    >>> equals_one = equals(1)
+    >>> equals_one(1)
+    True
+    >>> equals_one(2)
+    False
+
+    with two or more arguments
+
+    >>> equals(1,1,1)
+    True
+    >>> equals(1,1,2)
+    False
+
+    Added in version: 0.1.0
+    """
+    if not b:
+        return lambda x: a == x
+    elif not args:
+        return a == b
+    else:
+        # TODO ugly. refactor
+        if a == b:
+            for arg in args:
+                if a != arg:
+                    return False
+            else:
+                return True
+        else:
+            return False
+
+
+def is_(a, b=None, *args):
+    """
+    if only ``a`` is passed, a function is returned that returns True when the ``arg`` passed is the same object
+    as ``a`` ; else returns True when ``a``,``b`` and ``*args`` are.
+
+    with one argument
+
+    >>> d1 = {1,2,3}
+    >>> d2 = {1,2,3}
+    >>> is_d1 = is_(d1)
+    >>> is_d1(d2)
+    >>> False
+    >>> d1 == d2
+    >>> True
+
+    with two or more arguments
+
+    >>> is_(d1,d1)
+    >>> True
+    >>> is_(d1,d1,d2)
+    >>> False
+
+
+    Added in version: 0.1.0
+    """
+    if not b:
+        return lambda x: a is x
+    elif not args:
+        return a is b
+    else:
+        # TODO ugly. refactor
+        if a is b:
+            for arg in args:
+                if not a is arg:
+                    return False
+            else:
+                return True
+        else:
+            return False
+
+
+def less_than(a, b=None, *args):
+    """
+    if only ``a`` is passed, a function is returned that returns True when the ``arg`` passed to is less than ``a``;
+    else returns True when ``a`` is less than``b`` and ``*args``.
+
+    with one argument
+
+    >>> less_than_one = less_than(1)
+    >>> less_than_one(2)
+    False
+    >>> less_than_one(0)
+    True
+
+
+    with two or more arguments
+
+    >>> less_than(1,2)
+    >>> True
+    >>> less_than(1,2,3)
+    True
+    >>> less_than(1,2,3,1)
+    False
+
+    Useful to use with filter
+
+    >>> list(filter(less_than(5),range(10)))
+    [0,1,2,3,4]
+
+    Added in version: 0.1.0
+    """
+    if not b:
+        return lambda x: x < a
+    elif not args:
+        return a < b
+    else:
+
+        def f(boolean, el, args):
+            if not boolean:
+                return False
+            elif not args:
+                return boolean
+            else:
+                return f(lt(el, args[0]), args[0], args[1:])
+
+        return f(lt(a, b), b, args)
+
+
+def less_than_eq(a, b=None, *args):
+    """
+    if only ``a`` is passed, a function is returned that returns True when the ``arg`` less than or equal to ``a``;
+    else returns True when ``a`` is less than or equal to ``b`` and ``*args``.
+
+    with one argument
+
+    >>> less_than_or_eq_to_one = less_than_eq(1)
+    >>> less_than_or_eq_to_one(2)
+    False
+    >>> less_than_or_eq_to_one(1)
+    True
+
+    with two or more arguments
+
+    >>> less_than_eq(1,2)
+    >>> True
+    >>> less_than_eq(1,2,3)
+    True
+    >>> less_than_eq(1,2,3,1)
+    True
+
+    Useful to use with filter
+
+    >>> list(filter(less_than_eq(5),range(10)))
+    [0,1,2,3,4,5]
+
+    Added in version: 0.1.0
+    """
+    if not b:
+        return lambda x: x <= a
+    elif not args:
+        return a <= b
+    else:
+        # TODO ugly. refactor
+        def f(boolean, el, args):
+            if not boolean:
+                return False
+            elif not args:
+                return boolean
+            else:
+                return f(le(el, args[0]), args[0], args[1:])
+
+        return f(le(a, b), b, args)
+
+
+def greater_than(a, b=None, *args):
+    """
+    if only ``a`` is passed, a function is returned that returns True when the ``arg`` passed to is greater than ``a``;
+    else returns True when ``a`` is greater than``b`` and ``*args``.
+
+    with one argument
+
+    >>> greater_than_one = greater_than(1)
+    >>> greater_than_one(2)
+    True
+    >>> greater_than_one(0)
+    False
+
+    with two or more arguments
+
+    >>> greater_than(2,1)
+    >>> True
+    >>> greater_than(3,2,1)
+    True
+    >>> greater_than(3,2,1,3)
+    False
+
+    Useful to use with filter
+
+    >>> list(filter(greater_than(5),range(10)))
+    [6,7,8,9]
+
+    Added in version: 0.1.0
+    """
+    if not b:
+        return lambda x: x > a
+    elif not args:
+        return a > b
+    else:
+        # TODO ugly. refactor
+        def f(boolean, el, args):
+            if not boolean:
+                return False
+            elif not args:
+                return boolean
+            else:
+                return f(gt(el, args[0]), args[0], args[1:])
+
+        return f(gt(a, b), b, args)
+
+
+def greater_than_eq(a, b=None, *args):
+    """
+    if only ``a`` is passed, a function is returned that returns True when the ``arg`` greater than or equal to ``a``;
+    else returns True when ``a`` is greater than or equal to ``b`` and ``*args``.
+
+    with one argument
+
+    >>> greater_than_eq_one = greater_than_eq(1)
+    >>> greater_than_eq_one(2)
+    True
+    >>> greater_than_eq_one(1)
+    True
+
+    with two or more arguments
+
+    >>> greater_than_eq(2,1)
+    >>> True
+    >>> greater_than_eq(3,2,1)
+    True
+    >>> greater_than_eq(3,2,1,3)
+    True
+
+    Useful to use with filter
+
+    >>> list(filter(greater_than_eq(5),range(10)))
+    [5,6,7,8,9]
+
+    Added in version: 0.1.0
+    """
+    if not b:
+        return lambda x: x >= a
+    elif not args:
+        return a >= b
+    else:
+        # TODO ugly. refactor
+        def f(boolean, el, args):
+            if not boolean:
+                return False
+            elif not args:
+                return boolean
+            else:
+                return f(ge(el, args[0]), args[0], args[1:])
+
+        return f(ge(a, b), b, args)
+
+
+def complement(
+    expr: Union[bool, Callable[[Any], bool]]
+) -> Union[bool, Callable[[Any], bool]]:
+    """Takes in a predicate or a Boolean expression and
+    returns a negated version of the predicate or expression.
+
+    >>> complement(True)
+    >>> False
+
+    >>> def fn(el): # returns the Boolean of el
+        return bool(el)
+    >>> negated_fn = complement(fn)
+    >>> fn(1)
+    >>> True
+    >>> negated_fn(1)
+    >>> False
+
+    Added in version: 0.1.0
+    """
+
+    if not isinstance(expr, Callable):
+        # Wrapped around bool to handle cases like not_(1).
+        # where 1 returns a boolean value of true.
+        return not bool(expr)
+
+    else:
+
+        def negated(*args, **kwargs):
+            return not expr(*args, **kwargs)
+
+        return negated
+
+
+def is_even(num: int) -> bool:
+    """Returns true when num is even.
+
+    Added in version: 0.1.0
+    """
+    return num % 2 == 0
+
+
+def is_odd(num: int) -> bool:
+    """Returns true when num is odd
+
+    Added in version: 0.1.0
+    """
+    return num % 2 != 0
+
+
+def is_prime(num: int) -> bool:
+    """Returns true when num is prime
+
+    Added in version: 0.1.0
+    """
+    if is_even(num) and num != 2:
+        # You don't need to compute the whole Sieve if num is even.
+        return False
+
+    primes = [True for i in range(num + 1)]
+    p = 2
+    primes[0] = False
+    primes[1] = False
+
+    while p * p <= num:
+        if primes[p] == True:
+            # Update all multiples of p
+            for i in range(p * 2, num + 1, p):
+                primes[i] = False
+
+        p += 1
+
+    return primes[num]
+
+
+def is_divisible(divident: Union[int, float], divisor: Union[int, float]) -> bool:
+    """Returns true if dividend is divisible by divisor.
+
+    Added in version: 0.1.0
+    """
+    return divident % divisor == 0
+
+
+def is_divisible_by(divisor: Union[int, float]) -> Callable[[Union[int, float]], bool]:
+    """Takes a ``divisor`` And returns a function (closure) That expects a dividend.
+    returns true if it passes the divisibility test.
+    for e.g.
+
+    >>> f = is_divisible_by(5)
+    >>> f(10)
+    True
+    >>> f(7)
+    False
+
+    This is particularly useful to use with a filter.
+
+    >>> list(filter(is_divisible_by(5), [1,2,3,4,5,6,7,8,9,10]))
+    [5, 10]
+
+    Suppose you want to filter out numbers that are divisible by 2 or 3
+
+    >>> list(filter(some_predicates([is_divisible_by(2), is_divisible_by(3)]), range(1, 10)))
+    [2, 3, 4, 6, 8, 9, 10]
+
+    Added in version: 0.1.0
+    """
+    return lambda dividend: dividend % divisor == 0
+
+
+def is_numeric(entity: Any) -> bool:
+    """Return True if ``entity`` Is an ``int``,  ``float``, or a ``complex``.
+
+    Added in version: 0.1.0
+    """
+    return any(map(isinstance, [entity, entity, entity], [int, float, complex]))
+
+
+def is_atom(entity: Any) -> bool:
+    """Everything that is NOT an iterable(except strings) are considered atoms.
+
+    >>> is_atom("plain string")
+        True
+    >>> is_atom(1)
+        True
+    >>> is_atom([1, 2])
+        False
+
+    Added in version: 0.1.0
+    """
+    if isinstance(entity, str):
+        return True
+    else:
+        return not isinstance(entity, Iterable)
+
+
+def contains(entity: Any, collection: Iterable) -> bool:
+    """Checks whether collection contains the given entity.
+    Note, won't automatically convert a tuple of keys and values to a ``dict``.
+
+    Added in version: 0.1.0
+    """
+    return entity in collection
+
+
+def is_empty(collection: Iterable) -> bool:
+    """Returns true if the collection is empty.
+
+    Added in version: 0.1.0
+    """
+    return not bool(collection)
+
+
+def is_nested(collection: Iterable) -> bool:
+    """returns true if a collection is nested.
+    Added in version: 0.1.0
+    """
+    if isinstance(collection, dict):
+        return any(map(lambda x: isinstance(x, dict), collection.values()))
+    else:
+        return any(map(complement(is_atom), collection))
+
+
+def all_predicates(*predicates: Callable[[Any], bool]) -> Callable[[Any], bool]:
+    """Takes a set of predicates and returns a function that takes an entity
+    and checks if it satisfies all the predicates.
+
+    >>> even_and_prime = all_predicates(is_even, is_prime)
+    >>> even_and_prime(2)
+    True
+    >>> even_and_prime(4)
+    False
+    >>> even_and_prime(3)
+    False
+
+    Added in version: 0.1.0
+    """
+    return lambda entity: all((p(entity) for p in predicates))
+
+
+def some_predicates(*predicates: Callable[[Any], bool]) -> Callable[[Any], bool]:
+    """Takes a set of predicates and returns a function that takes an entity
+    and checks if it satisfies some of the predicates.
+
+    >>> even_or_prime = some_predicates(is_even, is_prime)
+    >>> even_or_prime(2)
+    True
+    >>> even_and_prime(4)
+    True
+    >>> even_and_prime(3)
+    True
+
+    Added in version: 0.1.0
+    """
+
+    return lambda entity: any((p(entity) for p in predicates))
---
+++ packages/functionali/functionali-0.3.0-py3-none-any.whl/functionali/seq_transform.py
@@ -0,0 +1,308 @@
+"""Functions that transform sequences"""
+from collections import deque
+from .predicates import is_atom, is_nested, complement
+from .higher_order_functions import reduce
+
+from .seq_traverse import iter_
+
+from typing import (
+    Callable,
+    Iterable,
+    Generator,
+    Any,
+    Tuple,
+    Union,
+    Dict,
+    Tuple,
+    List,
+)
+
+
+def cons(arg: Any, iterable: Iterable) -> deque:
+    """Returns a deque with arg as the first element.
+
+    Adds to the left of a deque.
+
+    >>> cons(5, [1,2,3,4])
+    deque([5, 1, 2, 3, 4])
+
+    >>> cons(3, deque([1,2]))
+    deque([3, 1, 2])
+
+    >>> cons((3, "c"), {1:"a", 2: "b"})
+    deque([(3, "c"), (1, "a"), (2, "b")])
+
+    Added in version: 0.1.0
+    """
+
+    if isinstance(iterable, dict):
+        dq = deque(iterable.items())
+    else:
+        dq = deque(iterable)
+
+    dq.appendleft(arg)
+
+    return dq
+
+
+def conj(iterable: Iterable, *args: Any) -> Iterable:
+    """Short for conjoin, adds element to the iterable, at the appropriate end.
+    Adds to the left of a deque.
+
+    >>> conj([1,2,3,4],5)
+    [1, 2, 3, 4, 5]
+
+    >>> conj(deque([1,2]), 3,4)
+    deque([4, 3, 1, 2])
+
+    >>> conj([1,2,3,4],5,6,7,8)
+    [1, 2, 3, 4, 5, 6, 7, 8]
+
+    >>> conj([1,2,3,4],[5,6,7])
+    [1, 2, 3, 4, [5, 6, 7]]
+
+    >>> conj((1,2,3,4),5,6,7)
+    (1, 2, 3, 4, 5, 6, 7)
+
+    >>> conj(range(10), 11)
+    (0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 11)
+
+    >>> conj({1:"a", 2:"b"}, {3:"c"})
+    {1: 'a', 2: 'b', 3: 'c'}
+
+    Added in version: 0.1.0
+    """
+    if isinstance(iterable, list):
+
+        return iterable + list(args)  # since args is a tuple
+
+    elif isinstance(iterable, tuple):
+        return iterable + args
+
+    elif isinstance(iterable, dict):
+        # Make a copy of iterable, instead of mutating it.
+        conjed_dict = {**iterable}
+
+        for d in args:
+            conjed_dict.update(d)
+
+        return conjed_dict
+
+    elif isinstance(iterable, set):
+        return {*iterable, *args}
+
+    elif isinstance(iterable, deque):
+        dq = deque(iterable)
+        for element in args:
+            dq.appendleft(element)
+        return dq
+
+    else:
+        return tuple(iterable) + args
+
+
+def concat(iterable, *args):
+    """Add items to the end of the iterable.
+
+    >>> concat([1,2,3,4],5)
+    [1, 2, 3, 4, 5]
+    >>> concat(deque([1,2]), 3,4)
+    deque([1, 2, 3, 4])
+
+    Added in version: 0.1.0
+    """
+
+    if isinstance(iterable, deque):
+        dq = deque(iterable)
+
+        for element in args:
+            dq.append(element)
+
+        return dq
+
+    else:
+        # Since conj Behavior for data Structures other than
+        # deque are similar to concat behaviour.
+        # call conj
+        return conj(iterable, *args)
+
+
+def argmap(functions: Iterable[Callable], args: Iterable) -> Generator:
+    """Maps the same argument(s) to multiple functions.
+
+    >>> inc = lambda x:x+1
+    >>> dec = lambda x:x-1
+    >>> list(argmap([inc, dec],[1]))
+        [2,0]
+
+    you can even map multiple arguments
+
+    >>> add = lambda a,b: a+b
+    >>> sub = lambda a,b:  a-b
+    >>> list(argmap([add, sub], [2, 1])) # two arguments
+        [3, 1]
+
+    Added in version: 0.1.0
+    """
+    return (f(*args) for f in functions)
+
+
+def argzip(sequence: Iterable[Callable], *args: Any) -> Generator:
+    """
+    Similar to zip, but instead of zipping iterables,
+    It zips an argument(s) with all the values of the iterable.
+    for example.
+
+    >>> list(argzip([1,2,3,4], "number"))
+    [(1, 'number'), (2, 'number'), (3, 'number'), (4, 'number')]
+    >>> list(argzip([1,2,3,4], "number", "int"))
+    [(1, 'number', 'int'), (2, 'number', 'int'), (3, 'number', 'int'), (4, 'number', 'int')]
+
+    Added in version: 0.1.0
+    """
+    return ((a, *args) for a in sequence)
+
+
+def unzip(sequence: Iterable) -> Tuple[Any]:
+    """Opposite of zip. Unzip is shallow.
+
+    >>> unzip([[1,'a'], [2,'b'], [3,'c']])
+    ((1, 2, 3), ('a', 'b', 'c'))
+    >>> unzip([ [1,'a','A'], [2, 'b','B'], [3,'c','C'] ])
+    ((1, 2, 3), ('a', 'b', 'c'), ('A', 'B', 'C'))
+
+    shallow nature of unzip.
+
+    >>> unzip([ [[1,'num'],['a','str']], [[2,'num'],['b','str']] ])
+    (([1, 'num'], [2, 'num']), (['a', 'str'], ['b', 'str']))
+
+    Added in version: 0.1.0
+    """
+
+    # TODO find better name for split?
+    def split(constructed, inner_lis):
+        # constructed is a nested list like [[1,2,3], ['a','b','c']]
+        return tuple(map(conj, constructed, inner_lis))
+
+    def create_nested_list(sequence):
+        # to be passed as an initial value to reduce
+        # the number of 2nd level lists corresponds
+        # to the number of elements in the inner list
+        # of sequence. for e.g
+        # [ [1,'a'], [2,'b], [3,'c'] ] -> ( (), () )
+
+        return (() for i in range(len(sequence[0])))
+
+    return reduce(split, sequence, create_nested_list(sequence))
+
+
+def interleave(*seqs: Iterable) -> Tuple:
+    """Similar to clojure's interleave. returns a flat sequence with the contents of iterables interleaved.
+
+    >>> interleave([1,2,3],["a","b","c"])
+    (1, 'a', 2, 'b', 3, 'c')
+    >>> interleave([1,2,3],["int","int","int"], ["a","b","c"],["str","str","str" ])
+    (1, 'int', 'a', 'str', 2, 'int', 'b', 'str', 3, 'int', 'c', 'str')
+
+    Added in version: 0.1.0
+    """
+    return flatten(zip(*seqs))
+
+
+def flatten(sequence: Iterable) -> Tuple:
+    """Returns the contents of a nested sequence as a flat sequence.
+    Flatten is recursive.
+
+    >>> flatten([1,2,[3,[4],5],6,7])
+    (1, 2, 3, 4, 5, 6, 7)
+
+    Added in version: 0.1.0
+    """
+
+    def fn(initial_val, elem):
+        if is_atom(elem):
+            return conj(initial_val, elem)
+        elif is_nested(elem):
+            # recursively call flatten if elem is a nested sequence
+            return conj(initial_val, *flatten(elem))
+        else:
+            return conj(initial_val, *elem)
+
+    return reduce(fn, sequence, ())
+
+
+def insert(element: Any, iterable: Iterable, *, key: Callable = lambda x: x) -> Tuple:
+    """Inserts ``element`` right before the first element
+    in the iterable that is greater than ``element``
+
+    >>> insert(3, [1,2,4,2])
+    (1,2,3,4,2)
+
+    >>> insert((2, "b"), {1:"a", 3:"c"})
+    ((1, "a"), (2, "b"), (3, "c"))
+
+    Using the key Parameter
+
+    >>> Person = namedtuple("Person", ("name", "age"))
+    >>> person1 = Person("John", 18)
+    >>> person2 = Person("Abe", 50)
+    >>> person3 = Person("Cassy", 25)
+    >>> insert(person3, (person1, person2), key=lambda p:p.age)
+        (person1, person3, person2)
+    >>> insert(person3, (person1, person2), key=lambda p:p.name)
+        (person3, person1, person2)
+
+    Added in version: 0.1.0
+
+    """
+    # TODO refactor.
+    if not iterable:
+        return (element,)
+
+    it = iter_(iterable)
+    accumulator = []
+    elem = next(it)
+
+    while key(elem) <= key(element):
+        accumulator.append(elem)
+        try:
+            elem = next(it)
+        except StopIteration:
+            return tuple(accumulator) + (element,)
+
+    return tuple(accumulator) + (element, elem) + tuple(it)
+
+
+def remove(predicate: Callable, iterable: Iterable) -> Tuple:
+    """
+    Opposite of filter; Constructs an iterable of elements that falsify the predicate.
+
+    >>> remove(lambda x: x==1, [1,1,9,1,1]
+    [9]
+    >>> remove(lambda x: x%2==0, range(10))
+    [1,3,5,7,9] # filter would return [2,4,6,8]
+
+    Added in version: 0.1.0
+    """
+    return tuple(filter(complement(predicate), iterable))
+
+
+def tuplize(iterable: Iterable) -> Tuple:
+    """
+    Recursively converts ``iterable`` to tuple.
+
+    >>> tuplize([1,2,[3,4],5])
+    (1, 2, (3, 4), 5)
+    """
+
+    def fn(el):
+        if is_atom(el):
+            return el
+
+        elif is_nested(el):
+            return tuplize(el)
+
+        else:  # el is not a nested iterable
+            return tuple(el)
+
+    return tuple([fn(el) for el in iterable])
