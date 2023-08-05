7d666c42081eb01b516fee3f39b91f556b9e53ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:09:31 +0000

    Add package hello-world2021taishin/0.0.1/hello-world2021taishin-0.0.1.tar.gz

---
+++ packages/hello-world2021taishin/hello-world2021taishin-0.0.1.tar.gz/hello-world2021taishin-0.0.1/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='hello-world2021taishin',
+    version='0.0.1',
+    author="simple_logger",
+    author_email="samole@gmai.com",
+    url="https://taitasu.com",
+    description="A hello-world example package",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
