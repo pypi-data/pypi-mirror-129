617ceebeb4d4e64637d542d875da6441b1fafaf5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:43 +0000

    Add package tronblock/0.0.1/tronblock-0.0.1.tar.gz

---
+++ packages/tronblock/tronblock-0.0.1.tar.gz/tronblock-0.0.1/setup.py
@@ -0,0 +1,21 @@
+# -*- coding:utf-8 -*-
+# Time:     2021/11/29 0:00
+# Author:   Yasen Wang
+# Email:    my@wangyusheng.cn
+# File:     setup.py
+import setuptools
+
+setuptools.setup(
+    name='tronblock',
+    version='0.0.1',
+    author='Yasen Wang',
+    author_email='yasen@yasenstudio.com',
+    description='Tron Block',
+    url='https://github.com/YasenWang/tronblock',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
---
+++ packages/tronblock/tronblock-0.0.1.tar.gz/tronblock-0.0.1/tronblock/version.py
@@ -0,0 +1,9 @@
+# -*- coding:utf-8 -*-
+# Time:     2021/11/28 23:57
+# Author:   Yasen Wang
+# Email:    my@wangyusheng.cn
+# File:     version.py
+
+
+def version():
+    print('version:{}'.format('0.0.1'))
