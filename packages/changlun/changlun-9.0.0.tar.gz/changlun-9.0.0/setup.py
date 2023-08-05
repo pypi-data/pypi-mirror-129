f191495c61205614edbbc01243d536469e3cc76b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:09:29 +0000

    Add package changlun/9.0.0/changlun-9.0.0.tar.gz

---
+++ packages/changlun/changlun-9.0.0.tar.gz/changlun-9.0.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages,setup
+
+setup(
+    name = 'changlun',
+    version = '9.0.0',
+    packages = find_packages(),
+    author = 'leeshen',
+    author_email = '35981030@qq.com'
+)
