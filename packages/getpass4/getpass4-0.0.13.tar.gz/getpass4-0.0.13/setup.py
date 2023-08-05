35588b02d70b3512dd7d0a9e67fb2275e078bdb8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:18 +0000

    Add package getpass4/0.0.13/getpass4-0.0.13.tar.gz

---
+++ packages/getpass4/getpass4-0.0.13.tar.gz/getpass4-0.0.13/setup.py
@@ -0,0 +1,44 @@
+from setuptools import setup, find_packages
+import configparser
+
+class Config(configparser.ConfigParser):
+	def as_dict(self):
+		d = dict(self._sections)
+		for k in d:
+			d[k] = dict(self._defaults, **d[k])
+			d[k].pop('__name__', None)
+		return d
+
+config = Config()
+
+config.read('setup.cfg')
+
+dict = config.as_dict()
+
+metadata = dict['metadata']
+options = dict['options']
+
+package_dir = {metadata['name']: metadata['name']}
+
+LICENSE = ''
+
+with open('LICENSE', 'r') as license_file:
+	LICENSE = license_file.read()
+
+setup(
+	name=metadata['name'],
+	python_requires=options['python_requires'],
+	version=metadata['version'],
+	author=metadata['author'],
+	author_email=metadata['author_email'],
+	description=metadata['description'],
+	long_description="""# How to use getpass4
+from getpass4 import getpass
+
+
+password = getpass('Password: ')
+
+print(password)""",
+	packages=find_packages(),
+	install_requires=['caugetch', 'clipboard', 'colorama', 'pyperclip'],
+)
