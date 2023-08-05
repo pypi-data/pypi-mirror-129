ad5798c4b40dc5f0054059f16aa05679f044a645
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:13:53 +0000

    Add package pypdc/0.0.2/pypdc-0.0.2.tar.gz

---
+++ packages/pypdc/pypdc-0.0.2.tar.gz/pypdc-0.0.2/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+classifiers = [
+   'Development Status :: 5 - Production/Stable',
+   'Intended Audience :: Developers',
+   'Intended Audience :: Science/Research',
+   'License :: OSI Approved :: MIT License',
+   'Programming Language :: Python :: 3'
+   'Topic :: Software Development :: Libraries :: Python Modules',
+   'Topic :: Scientific/Engineering :: Bio-Informatics'
+]
+
+setup(
+  name='pypdc',
+  version='0.0.2',
+  description='Python asymptotic Partial Directed Coherence and Directed Coherence estimation package for brain connectivity analysis.'
+  #License='MIT',
+  #classifiers=classifiers,
+  #platforms = ["Linux", "Mac OSX", "Windows", "Unix"],
+  #keywords = ["Brain Connectivity", "PDC"],
+  #install_requires=['']
+)
