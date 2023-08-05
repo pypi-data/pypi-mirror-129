8533254891f7a4076cd51c55c9bb52b858cd9e4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:30 +0000

    Add package django-routine/0.0.4.0/django-routine-0.0.4.0.tar.gz

---
+++ packages/django-routine/django-routine-0.0.4.0.tar.gz/django-routine-0.0.4.0/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+version = '0.0.4.0'
+
+setup(
+    name='django-routine',
+    version=version,
+    description='A Django app for managing routines.',
+    author='Daniel Cuznetov',
+    author_email='danielcuznetov04@gmail.com',
+    packages=['django_routine'],
+    entry_points={
+        "console_scripts": [
+            "startapp = django_routine.start:start",
+
+        ]
+    },
+    install_requires=['djangorestframework', 'django'],
+)
