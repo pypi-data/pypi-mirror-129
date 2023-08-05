2d964d7a1a29e5fbd7a021fb14ba40e41892e60e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:22 +0000

    Add package caph1993-pytools/0.3.1/caph1993-pytools-0.3.1.tar.gz

---
+++ packages/caph1993-pytools/caph1993-pytools-0.3.1.tar.gz/caph1993-pytools-0.3.1/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+'''
+For submitting to PyPi: python3 sumbit.py
+'''
+
+setup(
+    name='caph1993-pytools',
+    version='0.3.1',
+    description='Python toolbox of Carlos Pinzón',
+    url='https://github.com/caph1993/caph1993-pytools',
+    author='Carlos Pinzón',
+    author_email='caph1993@gmail.com',
+    license='MIT',
+    packages=[
+        'cp93pytools',
+    ],
+    install_requires=[
+        'typing-extensions',
+    ],
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+    ],
+)
