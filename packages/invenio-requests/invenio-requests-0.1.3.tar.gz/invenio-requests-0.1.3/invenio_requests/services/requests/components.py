01c2bec7655b8e21eb48a57cf3795d4e239bf836
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:10:43 +0000

    Add package invenio-requests/0.1.3/invenio-requests-0.1.3.tar.gz

---
+++ packages/invenio-requests/invenio-requests-0.1.3.tar.gz/invenio-requests-0.1.3/invenio_requests/services/requests/components.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2021 CERN.
+# Copyright (C) 2021 TU Wien.
+#
+# Invenio-Requests is free software; you can redistribute it and/or
+# modify it under the terms of the MIT License; see LICENSE file for more
+# details.
+
+"""Component for creating request numbers."""
+
+from invenio_records_resources.services.records.components import ServiceComponent
+
+
+class RequestNumberComponent(ServiceComponent):
+    """Component for assigning request numbers to new requests."""
+
+    def create(self, identity, data=None, record=None, **kwargs):
+        """Create identifier when record is created."""
+        type(record).number.assign(record)
+
+
+class EntityReferencesComponent(ServiceComponent):
+    """Component for initializing a request's entity references."""
+
+    def create(self, identity, data=None, record=None, **kwargs):
+        """Initialize the entity reference fields of a request."""
+        for field in ("created_by", "receiver", "topic"):
+            if field in kwargs:
+                setattr(record, field, kwargs[field])
