05bb579582c3a2d3b38fac2b21106b6232df2013
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:13:22 +0000

    Add package fse/0.1.17/fse-0.1.17.tar.gz

---
+++ packages/fse/fse-0.1.17.tar.gz/fse-0.1.17/fse/__init__.py
@@ -0,0 +1,24 @@
+import logging
+
+from fse import models
+
+from .inputs import (
+    BaseIndexedList,
+    CIndexedList,
+    CSplitCIndexedList,
+    CSplitIndexedList,
+    IndexedLineDocument,
+    IndexedList,
+    SplitCIndexedList,
+    SplitIndexedList,
+)
+
+
+class NullHandler(logging.Handler):
+    def emit(self, record):
+        pass
+
+
+logger = logging.getLogger("fse")
+if len(logger.handlers) == 0:  # To ensure reload() doesn't add another one
+    logger.addHandler(NullHandler())
