0f086194281231a965fb3f07f08efc7867b2a8ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:22 +0000

    Add package jiosaavn/0.0.4/JioSaavn-0.0.4.tar.gz

---
+++ packages/jiosaavn/JioSaavn-0.0.4.tar.gz/JioSaavn-0.0.4/JioSaavn/__init__.py
@@ -0,0 +1,14 @@
+from JioSaavn._fetch import searchSong,searchAlbum,song,lyrics,album,playlist
+
+__title__ = "JioSaavn"
+__description__ = "JioSaavn Api for Python 3."
+
+
+__all__ =[
+    'searchSong',
+    'searchAlbum',
+    'song',
+    'album',
+    'lyrics',
+    'playlist',
+]
