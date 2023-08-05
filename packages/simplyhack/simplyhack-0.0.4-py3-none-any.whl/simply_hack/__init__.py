52255f735094e206fd1f2283018b05fd18464956
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:12:39 +0000

    Add package simplyhack/0.0.4/simplyhack-0.0.4-py3-none-any.whl

---
+++ packages/simplyhack/simplyhack-0.0.4-py3-none-any.whl/simply_hack/__init__.py
@@ -0,0 +1,25 @@
+#Version of simplyhack
+
+from simplyhack import protocols_portNumbers
+from simplyhack import help_funct
+from simplyhack import get_ownMAC
+from simplyhack import get_ownIPv4
+from simplyhack import get_TargetMAC_Address
+from simplyhack import GET_HTTP_RESPONSE
+from simplyhack import subdomain_scanner
+from simplyhack import directory_scanner
+from simplyhack import dns_lookup
+from simplyhack import reverse_dns_lookup
+from simplyhack import web_vuln_scan
+from simplyhack import local_scan
+from simplyhack import scanConnection
+from simplyhack import bannerGrab
+from simplyhack import port_vulnscan
+from simplyhack import detect_net_attack
+from simplyhack import detect_netattack_back
+from simplyhack import arp_spoof_back
+from simplyhack import arp_spoof_back_reverse
+from simplyhack import arp_spoof
+from simplyhack import urlProtocolNameExtractor
+from simplyhack import sniff_packets
+from simplyhack import processedNetworkPackets
