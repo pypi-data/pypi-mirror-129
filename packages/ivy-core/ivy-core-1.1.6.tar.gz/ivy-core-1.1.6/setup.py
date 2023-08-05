a98d7a443cc735d52b975b40dec9313b37b02a6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:10:14 +0000

    Add package ivy-core/1.1.6/ivy-core-1.1.6.tar.gz

---
+++ packages/ivy-core/ivy-core-1.1.6.tar.gz/ivy-core-1.1.6/setup.py
@@ -0,0 +1,43 @@
+# lint as: python3
+# Copyright 2021 The Ivy Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License..
+# ==============================================================================
+from distutils.core import setup
+import setuptools
+
+
+def _strip(line):
+    return line.split(' ')[0].split('#')[0].split(',')[0]
+
+
+setup(name='ivy-core',
+      version='1.1.6',
+      author='Ivy Team',
+      author_email='ivydl.team@gmail.com',
+      description='The templated deep learning framework, enabling framework-agnostic functions, layers and libraries.',
+      long_description="""# What is Ivy?\n\nIvy is a templated deep learning framework which maximizes the portability
+      of deep learning codebases. Ivy wraps the functional APIs of existing frameworks. Framework-agnostic functions,
+      libraries and layers can then be written using Ivy, with simultaneous support for all frameworks.
+      Ivy currently supports Jax, TensorFlow, PyTorch, MXNet and Numpy. Check out the [docs](https://ivy-dl.org/ivy) for more info!""",
+      long_description_content_type='text/markdown',
+      url='https://ivy-dl.org/ivy',
+      project_urls={
+            'Docs': 'https://ivy-dl.org/ivy/',
+            'Source': 'https://github.com/ivy-dl/ivy',
+      },
+      packages=setuptools.find_packages(),
+      install_requires=[_strip(line) for line in open('requirements.txt', 'r')],
+      classifiers=['License :: OSI Approved :: Apache Software License'],
+      license='Apache 2.0'
+      )
