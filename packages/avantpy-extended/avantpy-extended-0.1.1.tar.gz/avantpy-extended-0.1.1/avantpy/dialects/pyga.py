a14bcf8075f490d81ba947043fcb4b87dbd85994
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:56 +0000

    Add package avantpy-extended/0.1.1/avantpy-extended-0.1.1.tar.gz

---
+++ packages/avantpy-extended/avantpy-extended-0.1.1.tar.gz/avantpy-extended-0.1.1/avantpy/dialects/pyga.py
@@ -0,0 +1,75 @@
+"""Irish dialect"""
+
+pyga = {
+    "False": "Bréagach", # tearma
+    "None": "Neodar",
+    "True": "Fíor", # tearma
+    "and": "agus",
+    "as": "mar",
+    "assert": "cinntíodh", # 3rd imperative "Let [it] assert ...that..."
+    "async": "aisionc",  # tearma, aisioncronach
+    "await": "fantar",  # "Let X be awaited" fanadh
+    "break": "briseadh", # "Let [it] break"
+    "class": "aicme", # tearma
+    "continue": "leanadh", # tearma "Let [it] continue"
+    "def": "sain", # ["sainmínigh", "sain"], # tearma sainmínigh
+    "del": "scriostar", # ["scrios", "scriostar"], # tearma scrios
+    "elif": "eimá", # ["eimá", "eimátá", "eimásé", "eimásí"], # nó eile más
+    "else": ["eile", "eilenb"],
+    "except": "eisceadh", # tearma "Let [it] except [thing]"
+    "finally": "faoidheireadh",
+    "for": "do",
+    "from": "ó",
+    "global": "uilíoch", # tearma
+    "if": "má", # ["má", "másé", "másí", "mátá"],
+    "import": "iompórt", # ["iompórt", "iompórtáiltear"], # tearma
+    "in": "i_", # ["i_", "in"],
+    "is": "~", # ["~", "cosúille"],
+    "lambda": "lambda", # tearma
+    "nonlocal": "neamhlogánta", # tearma
+    "not": "ní",
+    "or": "nó",
+    "pass": "léimeadh", # "Let [it] pass"
+    "raise": "tógtar", # "Let X be raised"
+    "return": "aisch", # ["aisch", "aischuirtear"], # "Let [it] return"/"Let X be returned" tearma
+    "try": "tástaladh",
+    "while": "chomhfada", # ["chomhfada", "fada"],
+    "with": "le",
+    "yield": "tugtar", # ["tugtar", "tugadh"],
+    # a few builtins useful for beginners
+    "input": "iarradh", # "Let [user] input"
+    "print": "priont", # ["priont", "priontáiltear"], # tearma
+    "range": "raon", # tearma
+    "exit": "scoireadh", # ["scoir", "scoireadh"],  # useful for console
+    "__name__": "__ainm__",
+    "'__main__'": "'__príomhúil__'", # tearma
+    '"__main__"': '"__príomhúil__"', # tearma
+    "__init__": "__tús__", #, tearma
+    "self": "féin",
+    "dict": "focl", # ["focl", "foclóir"], # tearma
+    "list": "líosta",
+    "map": "mapáil", # tearma
+    "set": "tacar", # tearma
+    "tuple": "codach", # tearma
+    "bool": "boole", # tearma
+    "open": "oscail", # ["oscail", "osclaítear"], # tearma "Let X be opened"
+    "len": "fad",
+    "int": "slnu", # ["slnu", "slánuimhir"], # tearma
+    "float": "snph", # ["snph", "snámhphointe"], # tearma
+    "type": "cineál", # tearma
+    "super": "foraicme", # tearma
+    "round": "slánaítear", # tearma
+    "repeat": "atugadh",
+    "until": "godtí",
+    "forever": "choíche",
+    "nobreak": "nobreak",
+    "str": "tea", # ["tea", "teaghrán"], #string
+    "__str__": "__tea__", # ["tea", "teaghrán"], #string
+    "__repr__": "__léir__", # léiriú tearma
+    "NotImplemented": "NíChuirtearIBhfeidhm",
+    "Exception": "Eisceacht",
+    "filter": "scagtar",
+    "enumerate": "áirigh", # tearma
+    "abs": "dea", # dearbh tearma
+    "isinstance": "isásc",
+}
---
+++ packages/avantpy-extended/avantpy-extended-0.1.1.tar.gz/avantpy-extended-0.1.1/setup.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['avantpy', 'avantpy.dialects', 'avantpy.wrappers']
+
+package_data = \
+{'': ['*'],
+ 'avantpy': ['locales/*',
+             'locales/fr/LC_MESSAGES/*',
+             'locales/ga/LC_MESSAGES/*']}
+
+install_requires = \
+['PyYAML>=6.0,<7.0',
+ 'Unidecode>=1.3.2,<2.0.0',
+ 'friendly-traceback==0.0.10',
+ 'requests>=2.26.0,<3.0.0',
+ 'unicode>=2.8,<3.0',
+ 'xdg>=5.1.1,<6.0.0']
+
+setup_kwargs = {
+    'name': 'avantpy-extended',
+    'version': '0.1.1',
+    'description': 'AvantPy with Extensions',
+    'long_description': None,
+    'author': 'André Roberge',
+    'author_email': 'andre.roberge@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
