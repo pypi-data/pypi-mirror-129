77342fa586be8faf3dd41a8444e76e053548c4d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:56 +0000

    Add package easy-auth/1.0.11/easy_auth-1.0.11-py3-none-any.whl

---
+++ packages/easy-auth/easy_auth-1.0.11-py3-none-any.whl/easyauth/exceptions.py
@@ -0,0 +1,34 @@
+from fastapi import HTTPException
+
+class DuplicateUserError(HTTPException):
+    def __init__(self, username: str):
+        super().__init__(
+            status_code = 422,
+            detail = f"A user with name {username} already exists"
+        )
+
+class InvalidActivationCode(HTTPException):
+    def __init__(self, username: str):
+        super().__init__(
+            status_code = 404,
+            detail = f"Invalid activation code provided"
+        )
+class InvalidUsernameOrPassword(HTTPException):
+    def __init__(self):
+        super().__init__(
+            status_code = 401,
+            detail = f"Invalid Username or Password provided"
+        )
+
+class GoogleOauthNotEnabledOrConfigured(HTTPException):
+    def __init__(self):
+        super().__init__(
+            status_code = 503,
+            detail = f"Google authentication is not enabled or configured"
+        )
+class GoogleOauthHeaderMalformed(HTTPException):
+    def __init__(self):
+        super().__init__(
+            status_code = 503,
+            detail = f"Expected 'X-Google-OAuth2-Type' in header"
+        )
