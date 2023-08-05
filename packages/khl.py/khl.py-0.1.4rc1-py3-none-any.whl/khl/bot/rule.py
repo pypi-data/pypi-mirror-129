fec896f9af3c7fe18235ee71dd84f19eea1622e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:57 +0000

    Add package khl.py/0.1.4rc1/khl.py-0.1.4rc1-py3-none-any.whl

---
+++ packages/khl.py/khl.py-0.1.4rc1-py3-none-any.whl/khl/bot/rule.py
@@ -0,0 +1,23 @@
+from typing import Union, Callable, Coroutine, Any
+
+from .. import Message, User
+
+TypeRule = Callable[[Message], Union[bool, Coroutine[Any, Any, bool]]]
+
+
+def is_bot_mentioned(bot) -> TypeRule:
+    async def rule(msg: Message) -> bool:
+        return (await bot.fetch_me()).id in msg.extra.get('mention')
+
+    return rule
+
+
+def is_user_mentioned(user: User) -> TypeRule:
+    def rule(msg: Message) -> bool:
+        return user.id in msg.extra.get('mention')
+
+    return rule
+
+
+def is_mention_all(msg: Message) -> bool:
+    return msg.extra.get('mention_all', None) is not None
