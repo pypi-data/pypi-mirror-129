0bd45bcbff9244b6e03e41a928852913ccf9ec0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:26 +0000

    Add package graph-kmer-index/0.0.2/graph_kmer_index-0.0.2.tar.gz

---
+++ packages/graph-kmer-index/graph_kmer_index-0.0.2.tar.gz/graph_kmer_index-0.0.2/setup.py
@@ -0,0 +1,28 @@
+from distutils.core import setup, Extension
+
+
+
+setup(name='graph_kmer_index',
+      version='0.0.2',
+      description='Graph Kmer Index',
+      url='http://github.com/ivargr/graph_kmer_index',
+      author='Ivar Grytten',
+      author_email='',
+      license='MIT',
+      packages=["graph_kmer_index"],
+      zip_safe=False,
+      install_requires=['numpy', 'sortedcontainers', 'tqdm', 'biopython', 'numba', 'cython', 'SharedArray', 'pyfaidx'],
+      classifiers=[
+            'Programming Language :: Python :: 3'
+      ],
+      entry_points={
+            'console_scripts': ['graph_kmer_index=graph_kmer_index.command_line_interface:main']
+      },
+)
+
+
+""""
+python3 setup.py sdist
+twine upload --skip-existing dist/*
+
+"""
