bef73f00729efd043b512f6d58fb3b5e023485b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:15:09 +0000

    Add package vanoma-api-utils/0.8.0/vanoma-api-utils-0.8.0.tar.gz

---
+++ packages/vanoma-api-utils/vanoma-api-utils-0.8.0.tar.gz/vanoma-api-utils-0.8.0/setup.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['vanoma_api_utils',
+ 'vanoma_api_utils.django',
+ 'vanoma_api_utils.rest_framework']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['Django>=3.2.9,<4.0.0',
+ 'djangorestframework-camel-case>=1.2.0,<2.0.0',
+ 'djangorestframework>=3.12.4,<4.0.0',
+ 'phonenumbers>=8.12.37,<9.0.0']
+
+setup_kwargs = {
+    'name': 'vanoma-api-utils',
+    'version': '0.8.0',
+    'description': 'Python utils for vanoma APIs',
+    'long_description': None,
+    'author': 'Vanoma',
+    'author_email': 'contact@vanoma.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
