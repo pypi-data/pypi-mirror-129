3b4756c0a629c42b2575c82a789b1dd8a3762c19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:09:25 +0000

    Add package zabbix-enums/0.1.0/zabbix-enums-0.1.0.tar.gz

---
+++ packages/zabbix-enums/zabbix-enums-0.1.0.tar.gz/zabbix-enums-0.1.0/src/zabbix_enums/common/trigger.py
@@ -0,0 +1,39 @@
+from enum import IntEnum
+from . import _DiscoveryFlag, _EntityStatus, _Priority
+
+
+TriggerFlag = _DiscoveryFlag
+TriggerStatus = _EntityStatus
+TriggerPriority = _Priority
+
+
+class TriggerState(IntEnum):
+    NORMAL = 0
+    UNKNOWN = 1
+
+
+class TriggerType(IntEnum):
+    GENERATE_SINGLE_EVENT = 0
+    NOT_GENERATE_MULTIPLE_EVENTS = 0
+    GENERATE_MULTIPLE_EVENTS = 1
+
+
+class TriggerValue(IntEnum):
+    OK = 0
+    PROBLEM = 1
+
+
+class TriggerRecoveryMode(IntEnum):
+    EXPRESSION = 0
+    RECOVERY_EXPRESSION = 1
+    NONE = 2
+
+
+class TriggerCorrelationMode(IntEnum):
+    ALL_PROBLEMS = 0
+    TAG_VALUES_MATCH = 1
+
+
+class TriggerManualClose(IntEnum):
+    NO = 0
+    YES = 1
