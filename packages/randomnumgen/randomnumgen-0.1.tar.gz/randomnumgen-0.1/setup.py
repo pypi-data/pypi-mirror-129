576690d40ea77eddc3479dde6abef3d7f947c766
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:13:18 +0000

    Add package randomnumgen/0.1/randomnumgen-0.1.tar.gz

---
+++ packages/randomnumgen/randomnumgen-0.1.tar.gz/randomnumgen-0.1/setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+setup(
+  name = 'randomnumgen',         
+  packages = ['randomnumgen'],   
+  version = '0.1',      
+  license='MIT',        
+  description = 'A Python3 library that generates random numbers.',   
+  author = 'SriMethan',                   
+  url = 'https://github.com/SriMethan/randomnumgen',  
+  download_url = 'https://github.com/SriMethan/randomnumgen/archive/refs/heads/main.zip',    
+  keywords = ['SOME', 'MEANINGFULL', 'KEYWORDS'],   
+  classifiers=[
+    'Development Status :: 3 - Alpha',      
+    'Intended Audience :: Developers',      
+    'License :: OSI Approved :: MIT License',   
+    'Programming Language :: Python :: 3.7',
+  ],
+)
