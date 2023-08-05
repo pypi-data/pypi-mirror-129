8ddb01482d48833a1d63e2245ce32de5c3c61f2f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:09:12 +0000

    Add package syntactic-metaphor/0.3.4/syntactic-metaphor-0.3.4.tar.gz

---
+++ packages/syntactic-metaphor/syntactic-metaphor-0.3.4.tar.gz/syntactic-metaphor-0.3.4/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='syntactic-metaphor',
+      version='0.3.4',
+      description='Educational phrase-structure metaphor for structured control flow: supplementary materials',
+      url='https://katego.lt/metaphor',
+      author='Ignas Rudaitis',
+      author_email='ignas.rudaitis@gmail.com',
+      license='MIT',
+      packages=['syntactic_metaphor'],
+      zip_safe=False)
