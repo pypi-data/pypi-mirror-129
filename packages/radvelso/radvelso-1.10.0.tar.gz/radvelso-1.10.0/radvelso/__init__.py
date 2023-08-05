f8f2e3911371847cac286fc01f1de07ad3a4fbd2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:09:53 +0000

    Add package radvelso/1.10.0/radvelso-1.10.0.tar.gz

---
+++ packages/radvelso/radvelso-1.10.0.tar.gz/radvelso-1.10.0/radvelso/__init__.py
@@ -0,0 +1,4 @@
+from .superobs_sqlite import compute_average
+from .fill_ray        import fill_ray_dict
+from .tests.unittest_superobs import TestMethods
+from .graphics.plot_ppi_test import plot_ppi_test
