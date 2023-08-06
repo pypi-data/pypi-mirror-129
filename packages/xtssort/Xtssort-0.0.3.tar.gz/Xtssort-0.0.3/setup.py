604c4f7fe4b63ffcde3707253fd7e12e962e0bb7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:42:15 +0000

    Add package xtssort/0.0.3/Xtssort-0.0.3.tar.gz

---
+++ packages/xtssort/Xtssort-0.0.3.tar.gz/Xtssort-0.0.3/setup.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+from setuptools import setup, find_packages
+
+setup(
+    name='Xtssort',
+    version='0.0.3',
+    description=(
+        'sort'
+    ),
+    long_description=open('README.rst').read(),
+    author='xiongtianshuo',
+    author_email='seoul1k@163.com',
+    maintainer='xiontianshuo',
+    maintainer_email='seoul1k@163.com',
+    license='BSD License',
+    packages=find_packages(),
+    platforms=["all"],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Operating System :: OS Independent',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: Implementation',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Topic :: Software Development :: Libraries'
+    ],
+)
