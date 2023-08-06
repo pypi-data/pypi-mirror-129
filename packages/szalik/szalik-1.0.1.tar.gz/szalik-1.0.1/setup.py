2a7622098ce949c1907d2eb805d2bd8bb608a911
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:27:09 +0000

    Add package szalik/1.0.1/szalik-1.0.1.tar.gz

---
+++ packages/szalik/szalik-1.0.1.tar.gz/szalik-1.0.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+
+    name="szalik",
+    version="1.0.1",
+    description="Pakiet testowy",
+    url="http://tekst.pl",
+    install_requires = [],
+    packages=["pakiet"],
+
+#python .\setup.py sdist
+
+)
