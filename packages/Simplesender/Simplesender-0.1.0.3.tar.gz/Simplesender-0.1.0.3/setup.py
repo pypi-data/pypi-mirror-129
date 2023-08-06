7e7b0834571021450dc7e4aff6f0ca1ad2815c9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:41:44 +0000

    Add package Simplesender/0.1.0.3/Simplesender-0.1.0.3.tar.gz

---
+++ packages/Simplesender/Simplesender-0.1.0.3.tar.gz/Simplesender-0.1.0.3/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+with open('C:\\Users\\James\\Desktop\\Simplesender\\Simplesender\\README.md', encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name = 'Simplesender',
+    version = '0.1.0.3',
+    description = 'Webhook 을 이용해 채널에 메시지를 보냅니다.',
+    author = '9Hundred100Five1',
+    author_email = 'ohot2124@naver.com',
+    url = 'https://github.com/9Hundred100Five1/simple-discordWebhook-sender',
+    py_modules=  ['asyncio', 'requests'],
+    keywords = ['discord', 'discord.py', 'sWs', 'SimpleSender'],
+    license='GPL-3.0',
+    long_description = long_description,
+    long_description_content_type='text/markdown',
+    include_package_data=True,
+    python_requires='>=3.7',
+    classifiers=[
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9'
+    ]
+)
