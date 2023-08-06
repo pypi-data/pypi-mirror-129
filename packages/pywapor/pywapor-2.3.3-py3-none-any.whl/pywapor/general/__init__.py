da2e3b8067c79d0c0ebafb5edf7c4cc32c90d19b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:06 +0000

    Add package pywapor/2.3.3/pywapor-2.3.3-py3-none-any.whl

---
+++ packages/pywapor/pywapor-2.3.3-py3-none-any.whl/pywapor/general/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Authors: Tim Hessels
+Module: WAPOR/Functions
+"""
+
+from pywapor.general import landcover_converter, outputs, variables, tests, waitbar_console, processing_functions, compositer, lapse_rate
+
+__all__ = ['landcover_converter', 'outputs', 'variables', 'tests', 'waitbar_console', 'processing_functions', 'compositer', 'lapse_rate']
+
+__version__ = '0.1'
