e69d913dc1dc1a878c64b8759a2255d4e61d9ef5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:30:45 +0000

    Add package ipypopout/0.0.2/ipypopout-0.0.2-py2.py3-none-any.whl

---
+++ packages/ipypopout/ipypopout-0.0.2-py2.py3-none-any.whl/ipypopout/popout_button.vue
@@ -0,0 +1,58 @@
+<template>
+  <span>
+    <v-btn
+        v-if="!isInPopupMode() && kernel_id && base_url_available"
+        @click="openWindow"
+        @contextmenu.prevent="openTab"
+        icon
+    >
+      <v-icon>mdi-application-export</v-icon>
+    </v-btn>
+  </span>
+</template>
+<script>
+module.exports = {
+  created() {
+    this.base_url_available = !!this.getBaseUrl();
+    if (!this.base_url_available) {
+      console.info('BaseUrl not found, hiding popout button.');
+    }
+  },
+  methods: {
+    getBaseUrl() {
+      const labConfigData = document.getElementById('jupyter-config-data');
+      if (labConfigData) {
+        /* lab */
+        return JSON.parse(labConfigData.textContent).baseUrl;
+      }
+      const bodyBaseUrl = document.body.dataset.baseUrl
+      if (!bodyBaseUrl) {
+        return;
+      }
+      return bodyBaseUrl.endsWith('/voila/') ? bodyBaseUrl.slice(0, - 'voila/'.length) : bodyBaseUrl;
+    },
+    getUrl() {
+      const baseUrl = this.getBaseUrl();
+      const host = document.body.dataset.voilaHost || `${location.protocol}//${location.host}`
+      const result = `${host}${baseUrl}${this.popoutPageUrl}` +
+          `?kernelid=${this.kernel_id}&modelid=${this.target_model_id}&baseurl=${baseUrl}`
+
+      return result;
+    },
+    openWindow() {
+      window.open(this.getUrl(), this.target_model_id, 'resizable=yes');
+    },
+    openTab() {
+      window.open(this.getUrl(), '_blank');
+    },
+    isInPopupMode() {
+      return window.location.pathname.includes(this.popoutPageUrl);
+    }
+  },
+  computed: {
+    popoutPageUrl() {
+      return 'voila/templates/ipypopout/static/popout.html'
+    }
+  }
+}
+</script>
