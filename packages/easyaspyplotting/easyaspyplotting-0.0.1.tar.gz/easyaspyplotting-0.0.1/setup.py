019767fd7773bf8289bcbd08443a4d39ef3490e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:49:29 +0000

    Add package easyaspyplotting/0.0.1/easyaspyplotting-0.0.1.tar.gz

---
+++ packages/easyaspyplotting/easyaspyplotting-0.0.1.tar.gz/easyaspyplotting-0.0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(name='easyaspyplotting',
+      version='0.0.1',
+      description='Assorted helper functions to improve common plots',
+      url='https://github.com/selkamand/easyaspyplotting',
+      author='Selkamand',
+      author_email='selkamand@gmail.com',
+      license='MIT',
+      packages=find_packages(),
+      install_requires=['']
+      )
