97610c968673ce15a1ff4647edb7375643cafa7f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:20:37 +0000

    Add package ndefcdf/0.0.2/ndefcdf-0.0.2.tar.gz

---
+++ packages/ndefcdf/ndefcdf-0.0.2.tar.gz/ndefcdf-0.0.2/ndefcdf/__init__.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2021 Petr Kracik
+
+__version__ = "0.0.2"
+__license__ = "Apache Software License"
+__copyright__ = "Copyright (c) 2021 Petr Kracik"
+
+from . import baserecord
+from . import lnpay
+from . import lnwithdrawal
