f8411909b176f730a5006f15fbbf43cdf19be984
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:27:10 +0000

    Add package wilkor/1.0.1/wilkor-1.0.1.tar.gz

---
+++ packages/wilkor/wilkor-1.0.1.tar.gz/wilkor-1.0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="wilkor",
+    version="1.0.1",
+    descroption="Pakiet testowy",
+    url="http://test.pl",
+    install_requires = [],
+    packages = ["pakiet"],
+)
