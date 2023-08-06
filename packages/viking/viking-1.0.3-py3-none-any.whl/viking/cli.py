d93a97bce862994c7edeb91dc58d06cd65d0b99f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:21:20 +0000

    Add package viking/1.0.3/viking-1.0.3-py3-none-any.whl

---
+++ packages/viking/viking-1.0.3-py3-none-any.whl/viking/cli.py
@@ -0,0 +1,36 @@
+from viking.simulate import main as simulate
+from viking.translate import main as translate
+
+# CLI for Viking
+# Basicly Imports __init__ and controls it all
+# Made by Owen Shaule
+
+def front():
+    front = """
+    {}██╗░░░██╗{}██╗██╗░░██╗██╗███╗░░██╗░██████╗░
+    {}██║░░░██║{}██║██║░██╔╝██║████╗░██║██╔════╝░
+    {}╚██╗░██╔╝{}██║█████═╝░██║██╔██╗██║██║░░██╗░
+    {}░╚████╔╝{}░██║██╔═██╗░██║██║╚████║██║░░╚██╗
+    {}░░╚██╔╝{}░░██║██║░╚██╗██║██║░╚███║╚██████╔╝
+    {}░░░╚═╝{}░░░╚═╝╚═╝░░╚═╝╚═╝╚═╝░░╚══╝░╚═════╝░
+    {}{}Instruction System translator for 2D CSV
+    {}{}{}Experimental | Do not currently use with decimals 
+
+    """
+    for i in front.splitlines():
+        print(i.format("\u001b[36m", "\u001b[34m", "\u001b[1m\u001b[31m"))
+
+def main():
+    print("{}(1) Translate   CSV --> Viking Instruction".format("\u001b[34m", "\u001b[0m"), end="\n")
+    print("{}(2) Simulate Viking Instruction".format("\u001b[34m", "\u001b[0m"), end="\n")
+
+
+    inp = input("{}{}>>> ".format("\u001b[34m", "\u001b[0m"))
+
+    if inp.startswith("1"): 
+        translate()
+    elif inp.startswith("2"): 
+        simulate()
+
+if __name__ == "__main__":
+    main()
