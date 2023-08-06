39c1133ea150d0dd573e868994e973211d9e3213
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:20:56 +0000

    Add package botlib/145/botlib-145-py3-none-any.whl

---
+++ packages/botlib/botlib-145-py3-none-any.whl/bot/dpt.py
@@ -0,0 +1,20 @@
+# This file is placed in the Public Domain.
+
+
+from .obj import Object
+
+
+class Dispatcher(Object):
+
+    def __init__(self):
+        super().__init__()
+        self.cbs = Object()
+
+    def dispatch(self, event):
+        if event and event.type in self.cbs:
+            self.cbs[event.type](self, event)
+        else:
+            event.ready()
+
+    def register(self, k, v):
+        self.cbs[str(k)] = v
