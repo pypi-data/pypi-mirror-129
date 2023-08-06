7ba2f9be577559a5e20e453531c16b40dba8548b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:33 +0000

    Add package nanamilang/0.3.36/nanamilang-0.3.36-py3-none-any.whl

---
+++ packages/nanamilang/nanamilang-0.3.36-py3-none-any.whl/nanamilang/datatypes/vector.py
@@ -0,0 +1,49 @@
+"""NanamiLang Vector Data Type"""
+
+# This file is a part of NanamiLang Project
+# This project licensed under GNU GPL version 2
+# Initially made by @jedi2light (aka Stoian Minaiev)
+
+import nanamilang.shortcuts as shortcuts
+from .base import Base
+from .nil import Nil
+from .integernumber import IntegerNumber
+
+
+class Vector(Base):
+    """NanamiLang Vector Data Type Class"""
+
+    name: str = 'Vector'
+    _expected_type = list
+    _python_reference: list
+
+    def get(self, index: IntegerNumber) -> Base:
+        """NanamiLang Vector, get() implementation"""
+
+        shortcuts.ASSERT_IS_INSTANCE_OF(
+            index,
+            IntegerNumber,
+            message='Vector.get() index must be an IntegerNumber'
+        )
+
+        # In Python
+        # We could get an IndexError
+        # But in other languages, we could get just NULL value
+        return shortcuts.get(self.reference(), index.reference(), Nil('nil'))
+
+    def __init__(self, reference: list) -> None:
+        """NanamiLang Vector, initialize new instance"""
+
+        self.init_assert_only_base(reference)
+
+        super(Vector, self).__init__(reference=reference)
+
+    def format(self) -> str:
+        """NanamiLang Vector, format() method implementation"""
+
+        return '[' + f'{" ".join([i.format() for i in self.reference()])}' + ']'
+
+    def reference_as_list(self) -> list:
+        """NanamiLang Base Data Type, reference_as_list() implementation"""
+
+        return list(self.reference())
