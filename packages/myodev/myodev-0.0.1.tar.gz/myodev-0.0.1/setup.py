e34e429704553022912222ee52bc55da60d74abc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:11:44 +0000

    Add package myodev/0.0.1/myodev-0.0.1.tar.gz

---
+++ packages/myodev/myodev-0.0.1.tar.gz/myodev-0.0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="myodev",
+    version="0.0.1",
+    description="A set of tools to reproduce MyoDev signal processing and ML.",
+    author="Itokiana Rafidinarivo",
+    author_email="rafidinarivo.study@gmail.com",
+    packages=["processing", "ml"]
+)
