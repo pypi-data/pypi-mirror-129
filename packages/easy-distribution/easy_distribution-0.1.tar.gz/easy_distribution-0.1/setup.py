c2008b431000b8e3e48d20b604638c9a50a63b0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:27:31 +0000

    Add package easy-distribution/0.1/easy_distribution-0.1.tar.gz

---
+++ packages/easy-distribution/easy_distribution-0.1.tar.gz/easy_distribution-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="easy_distribution",
+    version="0.1",
+    description="A module for creating, modifying, and visualizing Gaussian and Binomial distributions.",
+    packages=["easy_distribution"],
+    author="Indra Tanaya",
+    author_email="aryaindratanaya@gmail.com",
+    zip_safe=False,
+)
