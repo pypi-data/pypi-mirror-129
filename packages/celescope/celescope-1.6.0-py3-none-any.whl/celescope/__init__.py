d5dd585ac8687c8a41b380debc16d9d656ea50f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:11 +0000

    Add package celescope/1.6.0/celescope-1.6.0-py3-none-any.whl

---
+++ packages/celescope/celescope-1.6.0-py3-none-any.whl/celescope/__init__.py
@@ -0,0 +1,36 @@
+import os
+
+__VERSION__ = "1.6.0"
+__version__ = __VERSION__
+
+ASSAY_DICT = {
+    "rna": "Single-cell rna",
+    "rna_virus": "Single Cell RNA-Seq Virus",
+    'capture_virus': "Single Cell Capture Virus",
+    'fusion': "Single Cell Fusion Gene",
+    'vdj': "Single-cell vdj",
+    'mut': 'Single Cell Insertion and Deletion',
+    'hla': 'Single Cell HLA',
+    'capture_rna': 'Single Cell Capture RNA',
+    'snp': 'Single-cell variant',
+    'tag': 'Single-cell tag',
+    'citeseq': 'Single Cell CITE-Seq',
+    'tcr_fl': 'Single Cell full length TCR',
+    'dynaseq': 'Single-cell dynaseq'
+}
+
+ROOT_PATH = os.path.dirname(__file__)
+
+RELEASED_ASSAYS = ['rna', 'vdj', 'tag', 'dynaseq', 'snp']
+
+HELP_DICT = {
+    'match_dir': 'Match celescope scRNA-Seq directory.',
+    'gene_list': 'Required. Gene list file, one gene symbol per line. Only results of these genes are reported.',
+    'genomeDir': 'Required. Genome directory after running `mkref`.',
+    'thread': 'Thread to use.',
+    'debug': 'If this argument is used, celescope may output addtional file for debugging.',
+    'fasta': 'Required. Genome fasta file. Use relative path to `genomeDir`.',
+    'outdir': 'Output directory.',
+    'matrix_dir': 'Match celescope scRNA-Seq matrix directory.',
+    'panel': 'The prefix of bed file in `celescope/data/snp/panel/`, such as `lung_1`.',
+}
