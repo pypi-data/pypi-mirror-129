0df34f1fa8ad91f877e4c105b3cb2fe8443a91f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:27:21 +0000

    Add package szakal-kot/1.0.1/szakal-kot-1.0.1.tar.gz

---
+++ packages/szakal-kot/szakal-kot-1.0.1.tar.gz/szakal-kot-1.0.1/pakiet/__init__.py
@@ -0,0 +1,1 @@
+print("Info z pakietu")
---
+++ packages/szakal-kot/szakal-kot-1.0.1.tar.gz/szakal-kot-1.0.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+
+    name="szakal-kot",
+    version="1.0.1",
+    description="Pakiet testowy",
+    url="http://test.pl",
+    install_requires = [],
+    packages=["pakiet"],
+
+)
+# python3 setup.py sdist
+
