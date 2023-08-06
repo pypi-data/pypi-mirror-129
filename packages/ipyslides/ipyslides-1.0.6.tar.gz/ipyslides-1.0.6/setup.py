97bc1a6a9cdf2b8dec26fc6614fbbaf2f48c23f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:45 +0000

    Add package ipyslides/1.0.6/ipyslides-1.0.6.tar.gz

---
+++ packages/ipyslides/ipyslides-1.0.6.tar.gz/ipyslides-1.0.6/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="ipyslides",
+    version="1.0.6",
+    author="Abdul Saboor",
+    author_email="mass_qau@outlook.com",
+    description="Live rich content slides in jupyter notebook",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/massgh/ipyslides",
+    project_urls={
+        "Bug Tracker": "https://github.com/massgh/ipyslides/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    requirements = ['markdown'],
+    packages = setuptools.find_packages(),
+    keywords        = ['Jupyter', 'Widgets', 'IPython'],
+    include_package_data = True,
+    python_requires=">=3.6",
+)
