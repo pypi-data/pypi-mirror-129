257a353c844b49fcf4049a707c88910699094a96
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:16:47 +0000

    Add package dicomweb-client/0.54.0/dicomweb_client-0.54.0-py3-none-any.whl

---
+++ packages/dicomweb-client/dicomweb_client-0.54.0-py3-none-any.whl/dicomweb_client/__init__.py
@@ -0,0 +1,4 @@
+__version__ = '0.54.0'
+
+from dicomweb_client.api import DICOMwebClient, DICOMfileClient  # noqa
+from dicomweb_client.uri import URI, URISuffix, URIType  # noqa
---
+++ packages/dicomweb-client/dicomweb_client-0.54.0-py3-none-any.whl/dicomweb_client/api.py
@@ -0,0 +1,3 @@
+"""Application Programming Interface (API)"""
+from dicomweb_client.web import DICOMwebClient  # noqa
+from dicomweb_client.file import DICOMfileClient  # noqa
