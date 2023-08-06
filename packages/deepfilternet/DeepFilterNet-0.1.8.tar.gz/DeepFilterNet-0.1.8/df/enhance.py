f1c512f7d7418a6a2a1342fc40f6e31a9381af38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:41:33 +0000

    Add package deepfilternet/0.1.8/DeepFilterNet-0.1.8.tar.gz

---
+++ packages/deepfilternet/DeepFilterNet-0.1.8.tar.gz/DeepFilterNet-0.1.8/df/enhance.py
@@ -0,0 +1,170 @@
+import argparse
+import os
+import time
+from typing import Optional, Tuple, Union
+
+import torch
+import torchaudio
+from loguru import logger
+from numpy import ndarray
+from torch import Tensor, nn
+from torch.nn import functional as F
+
+import df
+from df import config
+from df.checkpoint import load_model
+from df.logger import init_logger
+from df.model import ModelParams
+from df.modules import get_device
+from df.utils import as_complex, as_real, get_norm_alpha
+from libdf import DF, erb, erb_norm, unit_norm
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--model-base-dir",
+        "-m",
+        type=str,
+        default=None,
+        help="Model directory containing checkpoints and config.",
+    )
+    parser.add_argument(
+        "noisy_audio_files",
+        type=str,
+        nargs="+",
+        help="List of noise files to mix with the clean speech file.",
+    )
+    parser.add_argument(
+        "--pf",
+        help="Postfilter that slightly overattenuates very noisy sections.",
+        action="store_true",
+    )
+    parser.add_argument("--output-dir", "-o", type=str, default=None)
+    parser.add_argument(
+        "--compensate-delay",
+        "-d",
+        action="store_true",
+        help="Add some paddig to compensate the delay introduced by the real-time STFT/ISTFT implementation.",
+    )
+    args = parser.parse_args()
+    if args.model_base_dir is None:
+        args.model_base_dir = os.path.join(
+            os.path.dirname(df.__file__), os.pardir, "pretrained_models", "DeepFilterNet"
+        )
+        print(f"Using default model at {args.model_base_dir}")
+    if not os.path.isdir(args.model_base_dir):
+        NotADirectoryError("Base directory not found at {}".format(args.model_base_dir))
+    init_logger(file=os.path.join(args.model_base_dir, "enhance.log"))
+    config.load(
+        os.path.join(args.model_base_dir, "config.ini"),
+        config_must_exist=True,
+        allow_defaults=False,
+    )
+    if args.pf:
+        config.set(ModelParams().section, "mask_pf", True, bool)
+    p = ModelParams()
+    df_state = DF(
+        sr=p.sr,
+        fft_size=p.fft_size,
+        hop_size=p.hop_size,
+        nb_bands=p.nb_erb,
+        min_nb_erb_freqs=p.min_nb_freqs,
+    )
+    checkpoint_dir = os.path.join(args.model_base_dir, "checkpoints")
+    model, _ = load_model(checkpoint_dir, df_state)
+    model = model.to(get_device())
+    logger.info("Model loaded")
+    if args.output_dir is None:
+        args.output_dir = "."
+    elif not os.path.isdir(args.output_dir):
+        os.mkdir(args.output_dir)
+    # Set suffix to model name
+    suffix = os.path.basename(os.path.abspath(args.model_base_dir))
+    if args.pf:
+        suffix += "_pf"
+    for file in args.noisy_audio_files:
+        audio = enhance(model, df_state, file, log=True, pad=args.compensate_delay)
+        save_audio(file, audio, p.sr, args.output_dir, log=True, suffix=suffix)
+
+
+def df_features(audio: Tensor, df: DF, device=None) -> Tuple[Tensor, Tensor, Tensor]:
+    p = ModelParams()
+    spec = df.analysis(audio.numpy())  # [C, Tf] -> [C, Tf, F]
+    a = get_norm_alpha(False)
+    erb_fb = df.erb_widths()
+    erb_feat = torch.as_tensor(erb_norm(erb(spec, erb_fb), a)).unsqueeze(1)
+    spec_feat = as_real(torch.as_tensor(unit_norm(spec[..., : p.nb_df], a)).unsqueeze(1))
+    spec = as_real(torch.as_tensor(spec).unsqueeze(1))
+    if device is not None:
+        spec = spec.to(device)
+        erb_feat = erb_feat.to(device)
+        spec_feat = spec_feat.to(device)
+    return spec, erb_feat, spec_feat
+
+
+def save_audio(
+    file: str,
+    audio: Union[Tensor, ndarray],
+    sr: int,
+    output_dir: Optional[str] = None,
+    suffix: str = None,
+    log: bool = False,
+):
+    outpath = file
+    if suffix is not None:
+        file, ext = os.path.splitext(file)
+        outpath = file + f"_{suffix}" + ext
+    if output_dir is not None:
+        outpath = os.path.join(output_dir, os.path.basename(outpath))
+    if log:
+        logger.info(f"Saving audio file '{outpath}'")
+    audio = torch.as_tensor(audio)
+    if audio.ndim == 1:
+        audio.unsqueeze_(0)
+    if audio.dtype != torch.int16:
+        audio = (audio * (1 << 15)).to(torch.int16)
+
+    torchaudio.save(outpath, audio, sr)
+
+
+@torch.no_grad()
+def enhance(model: nn.Module, df_state: DF, file: str, log: bool = False, pad=False):
+    p = ModelParams()
+    model.eval()
+    audio, sr = torchaudio.load(file)
+    bs = audio.shape[0]
+    if hasattr(model, "reset_h0"):
+        model.reset_h0(batch_size=bs, device=get_device())
+    t_audio = audio.shape[-1] / sr
+    if sr != p.sr:
+        logger.warning(
+            f"Audio sampling rate does not match model sampling rate ({sr}, {p.sr}). Resampling..."
+        )
+        audio = torchaudio.functional.resample(audio, sr, p.sr)
+    orig_len = audio.shape[-1]
+    if pad:
+        # Pad audio to compensate for the delay due to the real-time STFT implementation
+        audio = F.pad(audio, (0, p.fft_size))
+    t0 = time.time()
+    spec, erb_feat, spec_feat = df_features(audio, df_state, device=get_device())
+    spec = model(spec, erb_feat, spec_feat)[0].cpu()
+    t1 = time.time()
+    audio = df_state.synthesis(as_complex(spec.squeeze(1)).numpy())
+    t = t1 - t0
+    rtf = t_audio / t
+    if log:
+        logger.info(
+            "Enhanced noisy audio file '{}' in {:.1f}s (RT factor: {})".format(file, t, rtf)
+        )
+    if pad:
+        # Overall, the STFT/ISTFT loop introduces a delay of p.fft_size. Since this python script
+        # operates on the full signal and not on a per-frame-basis, the frame size (i.e. p.hop_size)
+        # can be neglected.
+        d = p.fft_size - p.hop_size
+        audio = audio[:, d : orig_len + d]
+    return audio
+
+
+if __name__ == "__main__":
+    main()
---
+++ packages/deepfilternet/DeepFilterNet-0.1.8.tar.gz/DeepFilterNet-0.1.8/df/scripts/test_voicebank_demand.py
@@ -0,0 +1,221 @@
+import argparse
+import glob
+import os
+import tempfile
+import sys
+
+import numpy as np
+import pystoi
+import semetrics
+import torch
+import torchaudio
+from loguru import logger
+from pystoi.utils import resample_oct
+
+from df import config
+from df.enhance import df_features, save_audio
+from df.logger import init_logger
+from df.model import ModelParams
+from df.modules import get_device
+from df.train import load_model
+from df.utils import as_complex, resample
+from libdf import DF
+
+try:
+    from tqdm import tqdm
+except ImportError:
+
+    def tqdm(iterable, desc="Progress", total=None, fallback_estimate=1000):
+        # tqdm not available using fallback
+        try:
+            L = iterable.__len__()
+        except AttributeError:
+            e = ""
+            L = total or None
+
+        print("{}:  {: >2d}".format(desc, 0), end="")
+        for k, i in enumerate(iterable):
+            yield i
+            if L is not None:
+                p = (k + 1) / L
+                e = "" if k < (L - 1) else "\n"
+            else:
+                # Use an exponentially decaying function
+                p = 1 - np.exp(-k / fallback_estimate)
+            print("\b\b\b\b {: >2d}%".format(int(100 * p)), end=e)
+            sys.stdout.flush()
+        if L is None:
+            print()
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("model_base_dir", type=str, help="Directory e.g. for checkpoint loading.")
+    parser.add_argument(
+        "dataset_dir",
+        type=str,
+        help="Voicebank Demand Test set directory. Must contain 'noisy_testset_wav' and 'clean_testset_wav'",
+    )
+    parser.add_argument("--disable-df", action="store_true")
+    parser.add_argument("--pf", action="store_true")
+    parser.add_argument("--verbose", action="store_true")
+    parser.add_argument("--output-dir", "-o", type=str, default=None)
+    parser.add_argument("--disable-output", action="store_true", default=None)
+    args = parser.parse_args()
+    if not os.path.isdir(args.model_base_dir):
+        NotADirectoryError("Base directory not found at {}".format(args.model_base_dir))
+    init_logger(file=os.path.join(args.model_base_dir, "test_voicebank_demand.log"))
+    config.load(
+        os.path.join(args.model_base_dir, "config.ini"),
+        config_must_exist=True,
+        allow_defaults=False,
+    )
+    if args.pf:
+        config.set(ModelParams().section, "mask_pf", True, bool)
+    p = ModelParams()
+    df_state = DF(
+        sr=p.sr,
+        fft_size=p.fft_size,
+        hop_size=p.hop_size,
+        nb_bands=p.nb_erb,
+        min_nb_erb_freqs=p.min_nb_freqs,
+    )
+    checkpoint_dir = os.path.join(args.model_base_dir, "checkpoints")
+    model_n = os.path.basename(os.path.abspath(args.model_base_dir))
+    model, _ = load_model(checkpoint_dir, df_state, mask_only=args.disable_df)
+    logger.info("Model loaded")
+    assert os.path.isdir(args.dataset_dir)
+    noisy_dir = os.path.join(args.dataset_dir, "noisy_testset_wav")
+    clean_dir = os.path.join(args.dataset_dir, "clean_testset_wav")
+    if args.disable_output:
+        enh_dir = None
+    else:
+        if args.output_dir is not None:
+            enh_dir = args.output_dir
+        else:
+            enh_dir = os.path.join(args.dataset_dir, "enhanced")
+        os.makedirs(enh_dir, exist_ok=True)
+    assert os.path.isdir(noisy_dir) and os.path.isdir(clean_dir)
+    enh_stoi = []
+    noisy_stoi = []
+    enh_sisdr = []
+    noisy_sisdr = []
+    enh_comp = []
+    noisy_comp = []
+    noisy_files = glob.glob(noisy_dir + "/*wav")
+    clean_files = glob.glob(clean_dir + "/*wav")
+    for noisyfn, cleanfn in tqdm(zip(noisy_files, clean_files), total=len(noisy_files)):
+        noisy, sr = torchaudio.load(noisyfn)
+        clean, sr = torchaudio.load(cleanfn)
+        if sr != p.sr:
+            noisy = resample(noisy, sr, p.sr)
+            clean = resample(clean, sr, p.sr)
+            sr = p.sr
+        enh = enhance(model, df_state, noisy)[0]
+        clean = df_state.synthesis(df_state.analysis(clean.numpy()))[0]
+        noisy = df_state.synthesis(df_state.analysis(noisy.numpy()))[0]
+        enh_stoi.append(stoi(clean, enh, sr))
+        noisy_stoi.append(stoi(clean, noisy, sr))
+        enh_sisdr.append(si_sdr_speechmetrics(clean, enh))
+        noisy_sisdr.append(si_sdr_speechmetrics(clean, noisy))
+        noisy_comp.append(composite(clean, noisy, sr))
+        enh_comp.append(composite(clean, enh, sr))
+        if args.verbose:
+            print(cleanfn, enh_stoi[-1], enh_comp[-1], enh_sisdr[-1])
+        enh = torch.as_tensor(enh).to(torch.float32).view(1, -1)
+        if enh_dir is not None:
+            save_audio(
+                os.path.basename(cleanfn),
+                enh,
+                p.sr,
+                output_dir=enh_dir,
+                suffix=f"{model_n}_{enh_comp[-1][0]:.3f}",
+                log=args.verbose,
+            )
+    logger.info(f"noisy stoi: {np.mean(noisy_stoi)}")
+    logger.info(f"enhanced stoi: {np.mean(enh_stoi)}")
+    noisy_comp = np.stack(noisy_comp)
+    enh_comp = np.stack(enh_comp)
+    noisy_pesq = np.mean(noisy_comp[:, 0])
+    noisy_csig = np.mean(noisy_comp[:, 1])
+    noisy_cbak = np.mean(noisy_comp[:, 2])
+    noisy_covl = np.mean(noisy_comp[:, 3])
+    noisy_ssnr = np.mean(noisy_comp[:, 4])
+    enh_pesq = np.mean(enh_comp[:, 0])
+    enh_csig = np.mean(enh_comp[:, 1])
+    enh_cbak = np.mean(enh_comp[:, 2])
+    enh_covl = np.mean(enh_comp[:, 3])
+    enh_ssnr = np.mean(enh_comp[:, 4])
+    logger.info(f"noisy pesq: {np.mean(noisy_pesq)}")
+    logger.info(f"enhanced pesq: {np.mean(enh_pesq)}")
+    logger.info(f"noisy csig: {np.mean(noisy_csig)}")
+    logger.info(f"enhanced csig: {np.mean(enh_csig)}")
+    logger.info(f"noisy cbak: {np.mean(noisy_cbak)}")
+    logger.info(f"enhanced cbak: {np.mean(enh_cbak)}")
+    logger.info(f"noisy covl: {np.mean(noisy_covl)}")
+    logger.info(f"enhanced covl: {np.mean(enh_covl)}")
+    logger.info(f"noisy ssnr: {np.mean(noisy_ssnr)}")
+    logger.info(f"enhanced ssnr: {np.mean(enh_ssnr)}")
+    logger.info(f"noisy sisdr: {np.mean(noisy_sisdr)}")
+    logger.info(f"enhanced sisdr: {np.mean(enh_sisdr)}")
+
+
+def stoi(clean, degraded, sr, extended=False):
+    if sr != 10000:
+        clean = resample_oct(clean, 10000, sr)
+        degraded = resample_oct(degraded, 10000, sr)
+        sr = 10000
+    return pystoi.stoi(clean, degraded, sr, extended=extended)
+
+
+def composite(clean: np.ndarray, degraded: np.ndarray, sr: int) -> np.ndarray:
+    """Compute pesq, csig, cbak, covl, ssnr"""
+    if sr != 16000:
+        clean = resample_oct(clean, 16000, sr)
+        degraded = resample_oct(degraded, 16000, sr)
+        sr = 16000
+    cf = tempfile.NamedTemporaryFile(suffix=".wav")
+    save_audio(cf.name, clean, sr)
+    nf = tempfile.NamedTemporaryFile(suffix=".wav")
+    save_audio(nf.name, degraded, sr)
+    c = semetrics.composite(cf.name, nf.name)
+    cf.close()
+    nf.close()
+    return np.asarray(c)
+
+
+@torch.no_grad()
+def enhance(model, df_state, audio):
+    model.eval()
+    if hasattr(model, "reset_h0"):
+        model.reset_h0(batch_size=1, device=get_device())
+    spec, erb_feat, spec_feat = df_features(audio, df_state, get_device())
+    spec = model(spec, erb_feat, spec_feat)[0]
+    return df_state.synthesis(as_complex(spec.squeeze(0)).cpu().numpy())
+
+
+def si_sdr_speechmetrics(reference: np.ndarray, estimate: np.ndarray):
+    """This implementation is adopted from https://github.com/aliutkus/speechmetrics/blob/dde303e/speechmetrics/relative/sisdr.py"""
+    # as provided by @Jonathan-LeRoux and slightly adapted for the case of just one reference
+    # and one estimate.
+    # see original code here: https://github.com/sigsep/bsseval/issues/3#issuecomment-494995846
+    reference = reference.reshape(-1, 1)
+    estimate = estimate.reshape(-1, 1)
+    eps = np.finfo(reference.dtype).eps
+    Rss = np.dot(reference.T, reference)
+
+    # get the scaling factor for clean sources
+    a = (eps + np.dot(reference.T, estimate)) / (Rss + eps)
+
+    e_true = a * reference
+    e_res = estimate - e_true
+
+    Sss = (e_true ** 2).sum()
+    Snn = (e_res ** 2).sum()
+
+    sisdr = 10 * np.log10((eps + Sss) / (eps + Snn))
+    return sisdr
+
+
+if __name__ == "__main__":
+    main()
