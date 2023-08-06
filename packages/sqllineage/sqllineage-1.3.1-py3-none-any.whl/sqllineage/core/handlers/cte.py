c1660504d63c13e20d15035c2dd3d3b11a7f32cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:22:07 +0000

    Add package sqllineage/1.3.1/sqllineage-1.3.1-py3-none-any.whl

---
+++ packages/sqllineage/sqllineage-1.3.1-py3-none-any.whl/sqllineage/core/handlers/cte.py
@@ -0,0 +1,31 @@
+from sqlparse.sql import Identifier, IdentifierList, Token
+
+from sqllineage.core.handlers.base import NextTokenBaseHandler
+from sqllineage.core.holders import SubQueryLineageHolder
+from sqllineage.core.models import SubQuery
+from sqllineage.exceptions import SQLLineageException
+
+
+class CTEHandler(NextTokenBaseHandler):
+    """Common Table Expression (With Queries) Handler."""
+
+    CTE_TOKENS = ("WITH",)
+
+    def _indicate(self, token: Token) -> bool:
+        return token.normalized in self.CTE_TOKENS
+
+    def _handle(self, token: Token, holder: SubQueryLineageHolder) -> None:
+        if isinstance(token, Identifier):
+            cte = [token]
+        elif isinstance(token, IdentifierList):
+            cte = [token for token in token.tokens if isinstance(token, Identifier)]
+        else:
+            raise SQLLineageException(
+                "An Identifier or IdentifierList is expected, got %s[value: %s] instead."
+                % (type(token).__name__, token)
+            )
+        for token in cte:
+            sublist = list(token.get_sublists())
+            if sublist:
+                # CTE: tbl AS (SELECT 1), tbl is alias and (SELECT 1) is subquery Parenthesis
+                holder.add_cte(SubQuery.of(sublist[0], token.get_real_name()))
