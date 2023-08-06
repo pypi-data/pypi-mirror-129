fe446ca0f0ba6bd0ee608b17d2aa8176aab3cecd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:03 +0000

    Add package bdbizvizkafka/0.1/bdbizvizkafka-0.1.tar.gz

---
+++ packages/bdbizvizkafka/bdbizvizkafka-0.1.tar.gz/bdbizvizkafka-0.1/setup.py
@@ -0,0 +1,27 @@
+from distutils.core import setup
+
+setup(name='bdbizvizkafka',
+      version='0.1',
+      description='kafka connection',
+      url='https://github.com/anjana-bijilesh/bdb__',
+      download_url='https://github.com/anjana-bijilesh/bdb__/archive/refs/tags/v1.0.0.tar.gz',
+      author=' ',
+      author_email=' ',
+      keywords=['kafka'],
+      license='MIT',
+      install_requires=['requests', 'confluent_kafka', 'logger', ],
+
+      classifiers=[
+          'Development Status :: 3 - Alpha',
+          'Intended Audience :: Developers',
+          'Topic :: Software Development :: Build Tools',
+          'License :: OSI Approved :: MIT License',
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.4',
+          'Programming Language :: Python :: 3.6',
+          'Programming Language :: Python :: 3.7',
+          'Programming Language :: Python :: 3.8',
+          'Programming Language :: Python :: 3.9',
+          'Programming Language :: Python :: 3.10'
+      ],
+      )
