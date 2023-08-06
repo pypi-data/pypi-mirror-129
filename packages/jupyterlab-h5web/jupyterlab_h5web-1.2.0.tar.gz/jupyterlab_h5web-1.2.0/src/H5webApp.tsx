a044a04aa1766e094321d2cf181ec92ad8e5b091
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:13:23 +0000

    Add package jupyterlab-h5web/1.2.0/jupyterlab_h5web-1.2.0.tar.gz

---
+++ packages/jupyterlab-h5web/jupyterlab_h5web-1.2.0.tar.gz/jupyterlab_h5web-1.2.0/src/H5webApp.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { App, H5GroveProvider } from '@h5web/app';
+import { ServerConnection } from '@jupyterlab/services';
+
+function H5webApp(props: { filePath: string }) {
+  const { filePath } = props;
+  const { baseUrl } = ServerConnection.makeSettings();
+
+  return (
+    <div className="h5web-root">
+      <H5GroveProvider
+        url={`${baseUrl.endsWith('/') ? baseUrl.slice(0, -1) : baseUrl}/h5web`}
+        filepath={filePath}
+        axiosParams={{ file: filePath }}
+      >
+        <App />
+      </H5GroveProvider>
+    </div>
+  );
+}
+
+export default H5webApp;
