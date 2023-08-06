7f88ba4949dc6027c177202913af04e028bcf087
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:31 +0000

    Add package aa-esi-status/1.2.0/aa_esi_status-1.2.0-py3-none-any.whl

---
+++ packages/aa-esi-status/aa_esi_status-1.2.0-py3-none-any.whl/esistatus/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+
+class EsiStatus(models.Model):
+    """Meta model for app permissions"""
+
+    class Meta:
+        managed = False
+        default_permissions = ()
+        permissions = (("basic_access", "Can access this app"),)
---
+++ packages/aa-esi-status/aa_esi_status-1.2.0-py3-none-any.whl/esistatus/urls.py
@@ -0,0 +1,13 @@
+"""
+pages url config
+"""
+
+from django.urls import path
+
+from esistatus import views
+
+app_name: str = "esistatus"
+
+urlpatterns = [
+    path("", views.index, name="index"),
+]
