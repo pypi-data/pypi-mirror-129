8e80d6418c56bbe8749423fd9cf5b1ce9814bf21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:30 +0000

    Add package rumi-i18n/0.0.1a1.post7/rumi-i18n-0.0.1a1.post7.tar.gz

---
+++ packages/rumi-i18n/rumi-i18n-0.0.1a1.post7.tar.gz/rumi-i18n-0.0.1a1.post7/rumi/file_rumi/__init__.py
@@ -0,0 +1,16 @@
+# rumi.file_rumi
+# File-based translation monitoring
+#
+# Author: Tianshu Li
+# Created: Oct 28, 2021
+
+"""
+File-based translation monitoring, especially for hugo static sites.
+"""
+
+##########################################################################
+# Imports
+##########################################################################
+
+from .reader import *
+from .reporter import *
