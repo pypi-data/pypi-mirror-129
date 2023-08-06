b52e5d2206ce0619249b3efe29205eb04481ba36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:29:24 +0000

    Add package rlxutils/0.0.0/rlxutils-0.0.0.tar.gz

---
+++ packages/rlxutils/rlxutils-0.0.0.tar.gz/rlxutils-0.0.0/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+import setuptools
+
+setuptools.setup(version_config=True, setup_requires=["setuptools-git-versioning"])
+
+
+setup(name='rlxutils',
+      install_requires=['matplotlib','numpy', 'pandas','joblib',
+                        'progressbar2', 'psutil'
+                       ],
+      scripts=[],
+      packages=['rlxutils'],
+      include_package_data=True,
+      zip_safe=False)
