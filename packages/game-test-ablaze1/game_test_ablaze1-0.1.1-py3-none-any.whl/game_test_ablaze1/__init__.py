36530bf05ad5f64d9a7b3fa6384cac9f4c7a0aeb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:27:11 +0000

    Add package game-test-ablaze1/0.1.1/game_test_ablaze1-0.1.1-py3-none-any.whl

---
+++ packages/game-test-ablaze1/game_test_ablaze1-0.1.1-py3-none-any.whl/game_test_ablaze1/__init__.py
@@ -0,0 +1,42 @@
+__version__ = '0.1.1'
+
+
+import functoins
+
+commands = functoins.commands
+
+def get_command():
+	found = False
+	comand = input(">> ")
+	for com in commands:
+		if comand == com:
+			run = "functoins."+str(com)+"()"
+			exec(run)
+			found = True
+			get_command()
+	if found == False:
+		get_command()
+
+def start():
+	again = True
+	while again == True:
+		x = input("Type 1 to sign in and 2 to create an acount or 3 to sign in as a guest- ")
+		if x == "1":
+			us = functoins.sign_in()
+			again = False
+			return us
+		elif x == "2":
+			ig = functoins.sign_up()
+			again = False
+			return ig
+		elif x =="3":
+			ii = "guest"
+			print("signed in as a gest (some features might not work)")
+			return ii
+		else:
+			print("please type either 1 or 2")
+			
+
+user = start()
+# functoins.give_rank("user", "rank")
+get_command(user)
