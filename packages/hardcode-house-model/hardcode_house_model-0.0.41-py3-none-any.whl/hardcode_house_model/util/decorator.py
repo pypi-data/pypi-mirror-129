483c7e87780805724b369b2f0352f5f1344b66c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:47:45 +0000

    Add package hardcode-house-model/0.0.41/hardcode_house_model-0.0.41-py3-none-any.whl

---
+++ packages/hardcode-house-model/hardcode_house_model-0.0.41-py3-none-any.whl/hardcode_house_model/util/decorator.py
@@ -0,0 +1,40 @@
+
+from functools import wraps
+from typing import Tuple
+
+class ExceptionLogging(object):
+    def __init__(self, logger, swallow_exceptions=None):
+        self.logger = logger
+        if swallow_exceptions is not None and (not isinstance(swallow_exceptions, Tuple)):
+            raise ValueError("exceptions should be a tuple.")
+        self.swallow_exceptions = swallow_exceptions or (Exception,)
+
+    def __call__(self, func):
+        @wraps(func)
+        def wrapped(*args, **kwargs):
+            try:
+                return func(*args, **kwargs)
+            except Exception as ex:
+                self.logger.error({
+                    "func": func.__qualname__,
+                    "exception_type": type(ex),
+                    "exception": ex,
+                    "args": str(args),
+                    "kwargs": str(kwargs)
+                })
+                if not isinstance(ex, self.swallow_exceptions):
+                    raise
+
+        return wrapped
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_value, exc_traceback):
+        if exc_value is not None:
+            self.logger.error({
+                "exception_type": exc_type,
+                "exception": exc_value
+            })
+
+        return isinstance(exc_value, self.swallow_exceptions)
