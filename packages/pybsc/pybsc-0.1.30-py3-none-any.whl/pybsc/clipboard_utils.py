d5d228e0fd58ca444b42626013dd94eb7b657ecd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:11:45 +0000

    Add package pybsc/0.1.30/pybsc-0.1.30-py3-none-any.whl

---
+++ packages/pybsc/pybsc-0.1.30-py3-none-any.whl/pybsc/clipboard_utils.py
@@ -0,0 +1,31 @@
+import os
+import sys
+
+import pyperclip
+
+
+class ClipboardOutput(object):
+
+    def __init__(self):
+        self.buf = ''
+
+    def write(self, arg):
+        self.buf += arg
+
+
+class stdout_to_clipboard(object):
+
+    def __init__(self):
+        sys.stdout.flush()
+        self._org_stdout = sys.stdout
+        self._old_stdout_fno = os.dup(sys.stdout.fileno())
+        self._clip = ClipboardOutput()
+
+    def __enter__(self):
+        sys.stdout = self._clip
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        sys.stdout = self._org_stdout
+        sys.stdout.flush()
+        os.dup2(self._old_stdout_fno, 1)
+        pyperclip.copy(self._clip.buf)
