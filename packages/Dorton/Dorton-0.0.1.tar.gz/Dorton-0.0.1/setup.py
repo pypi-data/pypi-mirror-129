7a842082b3507279d64e867edb01091b4ef7fdfc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:41:48 +0000

    Add package Dorton/0.0.1/Dorton-0.0.1.tar.gz

---
+++ packages/Dorton/Dorton-0.0.1.tar.gz/Dorton-0.0.1/setup.py
@@ -0,0 +1,20 @@
+from setuptools import find_packages, setup
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+
+setup(
+    name="Dorton",
+    version="0.0.1",
+    description="Python Lightweight Framework",
+    author="wariored",
+    author_email="cheikh@cheikhkonteye.com",
+    packages=find_packages(),
+    python_requires=">=3.9",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
