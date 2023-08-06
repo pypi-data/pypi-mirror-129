429380152a416a57a5a1286260cba75f05f654cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:00:16 +0000

    Add package django-plans-paypal/0.4.4/django-plans-paypal-0.4.4.tar.gz

---
+++ packages/django-plans-paypal/django-plans-paypal-0.4.4.tar.gz/django-plans-paypal-0.4.4/plans_paypal/admin.py
@@ -0,0 +1,27 @@
+from django.contrib import admin
+
+from .models import PayPalPayment
+
+
+@admin.register(PayPalPayment)
+class PayPalPaymentAdmin(admin.ModelAdmin):
+    list_display = (
+        'order',
+        'user_plan',
+        'paypal_ipn',
+        'created',
+    )
+    autocomplete_fields = (
+        'order',
+        'user_plan',
+        'paypal_ipn',
+    )
+    search_fields = (
+        'user_plan__user__email',
+    )
+    readonly_fields = (
+        'author',
+        'created',
+        'modified',
+        'updated_by',
+    )
