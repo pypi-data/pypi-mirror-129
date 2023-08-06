16681adcdd25cac4b0601b1b86d0d5ad83e8f1fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:13:49 +0000

    Add package RobotFramework-Examples/0.0.5/RobotFramework-Examples-0.0.5.tar.gz

---
+++ packages/RobotFramework-Examples/RobotFramework-Examples-0.0.5.tar.gz/RobotFramework-Examples-0.0.5/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+with open("README.rst", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="RobotFramework-Examples",
+    version="0.0.5",
+    author="Vernon Crabtree",
+    author_email="vernon.b.crabtree@gmail.com",
+    description="Examples in Robot Framework. Expands example data to individual test cases",
+    long_description=long_description,
+    packages=['.'],
+    python_requires='>=3.8',
+    project_urls={
+        "Examples": "https://github.com/worldline/RobotFramework-Examples",
+    },    
+    install_requires=['robotframework', 'pandas', 'sqlalchemy']
+)
