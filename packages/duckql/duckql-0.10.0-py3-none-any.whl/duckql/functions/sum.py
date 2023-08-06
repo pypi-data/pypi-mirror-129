dbf4d67582b230492ebab50038c1e076297486a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:01 +0000

    Add package duckql/0.10.0/duckql-0.10.0-py3-none-any.whl

---
+++ packages/duckql/duckql-0.10.0-py3-none-any.whl/duckql/functions/sum.py
@@ -0,0 +1,25 @@
+from typing import Union
+
+try:
+    from typing import Literal
+except ImportError:
+    from typing_extensions import Literal
+
+from ..functions.base import BaseFunction
+from ..properties.property import Property
+from ..structures.cast_operator import CastOperator
+from ..structures.case import Case
+
+
+class Sum(BaseFunction):
+    obj: Literal['functions.Sum'] = 'functions.Sum'
+    property: Union[Property, BaseFunction, CastOperator, Case]
+    alias: str = None
+
+    def to_sql(self) -> str:
+        sql = f"SUM({self.property})"
+
+        if self.alias is not None:
+            sql = f"{sql} AS {self.alias}"
+
+        return sql
