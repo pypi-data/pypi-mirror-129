0f6138581a1f19c9e8d7b438d02ba0df61351a47
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:23:14 +0000

    Add package domain-trails/1.0.0/domain-trails-1.0.0.tar.gz

---
+++ packages/domain-trails/domain-trails-1.0.0.tar.gz/domain-trails-1.0.0/domaintrails/core/recon/dns_history.py
@@ -0,0 +1,59 @@
+import urllib
+from urllib.request import Request, urlopen
+import urllib.parse
+
+from domaintrails.core.resources import UserAgent, UserDomain
+
+from domaintrails.core.parser.html_parser import DnsHistoryParser as HistoryWebParser
+from domaintrails.core.parser import domain_for_history as UrlParser
+
+
+class DnsHistory:
+    '''
+    This class is used to collect information from server related to dns history, we are using
+    http://www.hosterstats.com service for grabbing records.
+    '''
+    __END_POINT__ = 'http://www.hosterstats.com/historicaldns.php'
+
+    def __init__(self, domain: str = None):
+        '''
+        init function
+        :param domain: domain name as input
+        :type domain: str
+        '''
+        if domain:
+            self.__domain = domain
+        else:
+            self.__domain = UserDomain.domain
+
+    def domain_history(self) -> list:
+        '''
+        This function download history from server for given domain name
+        :return: function returns list of records as part of its output
+        :rtype: list
+        '''
+        domain = UrlParser(self.__domain)
+        params = {
+            "domain": domain
+        }
+        query_string = urllib.parse.urlencode(params)
+        endpoint_url = self.__END_POINT__ + "?" + query_string
+        request = Request(endpoint_url)
+        user_agent = UserAgent()
+        request.add_header('Connection', 'keep-alive')
+        request.add_header('Cache-Control', 'max-age=0')
+        request.add_header('DNT', '1')
+        request.add_header('Upgrade-Insecure-Requests', '1')
+        request.add_header('Accept-Language', 'en-US,en;q=0.9')
+        request.add_header('Accept', 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,'
+                                     'image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9')
+        request.add_header('User-Agent', str(user_agent.user_agent()))
+
+        try:
+            content = urlopen(request).read()
+            history = HistoryWebParser(content)
+            report = history.parse()
+
+        except Exception:
+            return []
+        return report
---
+++ packages/domain-trails/domain-trails-1.0.0.tar.gz/domain-trails-1.0.0/domaintrails/core/recon/domain_available.py
@@ -0,0 +1,65 @@
+import json
+from urllib.error import HTTPError
+
+from domaintrails.core.resources import UserDomain, UserAgent
+from domaintrails.core.parser import domain_for_md as UrlParser
+from urllib.request import urlopen, Request
+from urllib.parse import urlencode
+
+
+class DomainAvailable:
+    '''
+    This class check if a domain is available or not. We are using mdchecker.com for collecting information
+    from server.
+    '''
+    __ENDPOINT__ = "https://madchecker.com/api/domain/get-information"
+
+    def __init__(self, domain: str = None):
+        '''
+        Init function for data initialization
+        :param domain: domain name required for analysis
+        :type domain: str
+        '''
+        if domain:
+            self.__domain = domain
+        else:
+            self.__domain = UserDomain.domain
+
+        self.__is_available = False
+
+    @property
+    def domain_available(self) -> bool:
+        '''
+        This function check domain availability from server, and return result.
+        :return: True/ False / raise exception in case of failure
+        :rtype: bool
+        '''
+        domain = UrlParser(self.__domain)
+        params = {
+            "domain": domain
+        }
+        query_string = urlencode(params)
+        endpoint_url = self.__ENDPOINT__ + "?" + query_string
+        try:
+            user_agent = UserAgent()
+            endpoint_request = Request(
+                endpoint_url,
+                None,
+                headers={
+                    'User-Agent': str(user_agent.user_agent()),
+                }
+            )
+            with urlopen(endpoint_request) as response:
+                response_text = response.read()
+                content = json.loads(response_text)
+                if "domain" in content:
+                    domain_details = content["domain"]
+                    available = domain_details["available"]
+                    if available:
+                        return True
+                    else:
+                        return False
+                # @TODO: Resolve this exception with tuple and let the main function decide
+                raise KeyError("We are failed to find domain status from server. Please try again in few minutes.")
+        except HTTPError as e:
+            raise e
