3b2f6c980fc72d4f1a0f641f61b06b678a8af3d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:42:55 +0000

    Add package metakernel/0.28.2/metakernel-0.28.2.tar.gz

---
+++ packages/metakernel/metakernel-0.28.2.tar.gz/metakernel-0.28.2/generate_help.py
@@ -0,0 +1,33 @@
+from __future__ import print_function
+import sys
+
+from metakernel_python import MetaKernelPython
+
+kernel = MetaKernelPython()
+
+path = "metakernel/magics/README.md"
+
+with open(path) as fid:
+    prev = fid.read()
+
+print("Generating README.md...")
+prefix = kernel.magic_prefixes['magic']
+text = "# Line Magics\n\n"
+for magic in sorted(kernel.line_magics.keys()):
+    text += "## `" + prefix + magic + "`\n\n"
+    text += kernel.get_help_on(prefix + magic) + "\n\n"
+
+text += "# Cell Magics\n\n"
+for magic in sorted(kernel.cell_magics.keys()):
+    text += "## `" + prefix + prefix + magic + "`\n\n"
+    text += kernel.get_help_on(prefix + prefix + magic) + "\n\n"
+
+with open(path, 'w') as fid:
+    fid.write(text)
+
+print("done!")
+
+if text != prev:
+    print('Readme changed, please commit the changes')
+    print('If this is on CI, run `make help` locally to regenerate')
+    sys.exit(1)
