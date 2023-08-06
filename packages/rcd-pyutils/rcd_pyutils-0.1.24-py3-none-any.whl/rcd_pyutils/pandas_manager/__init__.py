d9872759cd785f6bf7af93cada2df3f46fbe9034
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:12:58 +0000

    Add package rcd-pyutils/0.1.24/rcd_pyutils-0.1.24-py3-none-any.whl

---
+++ packages/rcd-pyutils/rcd_pyutils-0.1.24-py3-none-any.whl/rcd_pyutils/pandas_manager/__init__.py
@@ -0,0 +1,5 @@
+from .io import *
+from .manipulator import strip_all_text_column
+from .checker import check_na, check_duplication
+from .normalizer import normalize_date_column, normalize_df
+from .detector import detect_aws_type
