ebcf0b2d4b3a4a512de067a22891136e8a932a23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:48:25 +0000

    Add package immunopipe/0.0.2/immunopipe-0.0.2-py3-none-any.whl

---
+++ packages/immunopipe/immunopipe-0.0.2-py3-none-any.whl/immunopipe/pipeline.py
@@ -0,0 +1,18 @@
+"""Assemable the pipeline"""
+from pipen import Pipen
+from .version import __version__
+# Import your processeses
+from .processes import starts
+
+# Set the name and description of your pipeline here
+pipeline = Pipen(
+    name="immunopipe",
+    desc=(
+        "A pipeline for integrative analysis for scTCR- and scRNA-seq data\n"
+        f"version: {__version__}"
+    )
+)
+
+def main():
+    """Run the pipeline"""
+    pipeline.set_starts(starts).run()
