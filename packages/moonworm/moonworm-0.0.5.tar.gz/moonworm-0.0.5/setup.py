32137500908291fc37b27e9ab8f4416770da7b8e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:43:28 +0000

    Add package moonworm/0.0.5/moonworm-0.0.5.tar.gz

---
+++ packages/moonworm/moonworm-0.0.5.tar.gz/moonworm-0.0.5/setup.py
@@ -0,0 +1,39 @@
+from setuptools import find_packages, setup
+
+from moonworm.version import MOONWORM_VERSION
+
+long_description = ""
+with open("README.md") as ifp:
+    long_description = ifp.read()
+
+setup(
+    name="moonworm",
+    version=MOONWORM_VERSION,
+    packages=find_packages(),
+    package_data={"moonworm": ["py.typed"]},
+    install_requires=["web3[tester]", "tqdm", "libcst", "pysha3<2.0.0,>=1.0.0", "moonstreamdb", "typing-extensions<4,>=3.7.4"],
+    extras_require={
+        "dev": [
+            "black",
+            "mypy",
+            "isort",
+            "wheel",
+        ],
+        "distribute": ["setuptools", "twine", "wheel"],
+    },
+    description="moonworm: Generate a command line interface to any Ethereum smart contract",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Moonstream",
+    author_email="engineering@moonstream.to",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Programming Language :: Python",
+        "License :: OSI Approved :: Apache Software License",
+        "Topic :: Software Development :: Libraries",
+    ],
+    python_requires=">=3.6",
+    url="https://github.com/bugout-dev/moonworm/",
+    entry_points={"console_scripts": ["moonworm=moonworm.cli:main"]},
+    include_package_data=True,
+)
