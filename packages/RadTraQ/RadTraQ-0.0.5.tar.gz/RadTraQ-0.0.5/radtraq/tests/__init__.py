e196b0ca58d8c2d39f57583fcea314d237d75e58
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:22 +0000

    Add package RadTraQ/0.0.5/RadTraQ-0.0.5.tar.gz

---
+++ packages/RadTraQ/RadTraQ-0.0.5.tar.gz/RadTraQ-0.0.5/radtraq/tests/__init__.py
@@ -0,0 +1,17 @@
+"""
+=============
+radtraq.tests
+=============
+
+.. currentmodule:: radtraq.tests
+
+This module contains sample files used for testing RadTraQ
+
+.. autosummary::
+    :toctree: generated/
+
+    EXAMPLE_KAZR
+    EXAMPLE_RASTER
+"""
+
+from .sample_files import (EXAMPLE_KAZR, EXAMPLE_RASTER, EXAMPLE_PPI)
