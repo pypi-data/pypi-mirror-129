76e211e9977cd0e7f74aa3f5ffb0ef38c12dea38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:19 +0000

    Add package bfmplot/0.0.11/bfmplot-0.0.11.tar.gz

---
+++ packages/bfmplot/bfmplot-0.0.11.tar.gz/bfmplot-0.0.11/bfmplot/__init__.py
@@ -0,0 +1,39 @@
+import matplotlib as mpl
+import matplotlib.pyplot as pl
+
+__version__ = "0.0.11"
+
+__author__ = "Benjamin F. Maier"
+__copyright__ = "Copyright 2020-2021, " + __author__
+__credits__ = [__author__]
+__license__ = "MIT"
+__maintainer__ = __author__
+__email__ = "contact@benmaier.org"
+__status__ = "Development"
+
+
+#from .cividis import *
+from .sequences import *
+from .tools import *
+from .colortools import *
+
+figwidth = 5
+
+#mpl.rcParams['font.size'] = 12
+#mpl.rcParams['legend.fontsize'] = 'medium'
+#mpl.rcParams['figure.titlesize'] = 'medium'
+#mpl.rcParams['axes.titlesize'] = 'medium'
+mpl.rcParams['figure.figsize'] = golden_ratio(figwidth)
+##mpl.rcParams['xtick.labelsize'] = 'small'
+##mpl.rcParams['ytick.labelsize'] = 'small'
+#mpl.rcParams['xtick.labelsize'] = 'medium'
+#mpl.rcParams['ytick.labelsize'] = 'medium'
+#mpl.rcParams['lines.markersize'] = 4
+#mpl.rcParams['lines.linewidth'] = 1.0
+
+
+
+# colors
+#mpl.rcParams['axes.prop_cycle'] = cycler(color=brewer_qualitative)
+mpl.rcParams['axes.prop_cycle'] = cycler(color=epipack)
+mpl.rcParams['image.cmap'] = 'cividis'
