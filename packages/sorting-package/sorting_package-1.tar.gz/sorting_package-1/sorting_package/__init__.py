6da2c0e6d8281ae16006b6193eaf99429f90a17f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:28:33 +0000

    Add package sorting-package/1/sorting_package-1.tar.gz

---
+++ packages/sorting-package/sorting_package-1.tar.gz/sorting_package-1/sorting_package/__init__.py
@@ -0,0 +1,8 @@
+"""Пакет для быстрой сортировки списков методом Хоара"""
+__all__ = ['input_proc', 'sort_func']
+from . import *
+__title__ = 'sorting_package'
+__version__ = 1
+__author__ = 'Anastasia Fongrad'
+__email__ = 'fongradnastya@gmail.com'
+__copyright__ = f'Copyright (c) 2021, {__author__}'
