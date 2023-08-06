43d85172b941f695b1e59084831d87a77fe74be3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:20:50 +0000

    Add package AGONS/1.0.6/AGONS-1.0.6.tar.gz

---
+++ packages/AGONS/AGONS-1.0.6.tar.gz/AGONS-1.0.6/AGONS/IndTransformerModule.py
@@ -0,0 +1,24 @@
+# %%
+from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.preprocessing import StandardScaler, Normalizer
+
+
+class IndTransformer(BaseEstimator, TransformerMixin):
+    """Transformer class for sample indepdenent transformations.
+    Parameters
+    ----------
+    method : str, 'scaler' is for StandardScaler while 'norm' is for normalizer from sklearn.preprocessing."""
+    def __init__(self, method = 'scaler'): 
+        self.method = method
+    
+    def fit(self, X, y=None):
+        self.X = X
+        return self
+    
+    def transform(self, X, y=None):
+        if self.method == 'scaler':
+            X_t = StandardScaler().fit_transform(self.X.T).T
+            return X_t
+        if self.method == 'norm':
+            X_t = Normalizer.fit_transform(self.X)
+            return X_t
---
+++ packages/AGONS/AGONS-1.0.6.tar.gz/AGONS-1.0.6/AGONS/__init__.py
@@ -0,0 +1,3 @@
+# %%
+from AGONS.IndTransformerModule import IndTransformer
+from AGONS.AGONSModule import AGONS
