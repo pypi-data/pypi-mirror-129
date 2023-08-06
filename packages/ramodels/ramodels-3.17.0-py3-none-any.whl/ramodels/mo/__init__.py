1beae67f0ce631b98d6a501c7d27080cbbea252d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:11:24 +0000

    Add package ramodels/3.17.0/ramodels-3.17.0-py3-none-any.whl

---
+++ packages/ramodels/ramodels-3.17.0-py3-none-any.whl/ramodels/mo/__init__.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+# --------------------------------------------------------------------------------------
+# SPDX-FileCopyrightText: 2021 Magenta ApS <https://magenta.dk>
+# SPDX-License-Identifier: MPL-2.0
+# --------------------------------------------------------------------------------------
+# --------------------------------------------------------------------------------------
+# Imports
+# --------------------------------------------------------------------------------------
+from ._shared import MOBase
+from .employee import Employee
+from .employee import EmployeeRead
+from .employee import EmployeeWrite
+from .facet import FacetClass
+from .organisation import OrganisationRead
+from .organisation_unit import OrganisationUnit
+from .organisation_unit import OrganisationUnitRead
+from .organisation_unit import OrganisationUnitWrite
+
+# --------------------------------------------------------------------------------------
+# All
+# --------------------------------------------------------------------------------------
+
+__all__ = [
+    "MOBase",
+    "Employee",
+    "EmployeeRead",
+    "EmployeeWrite",
+    "FacetClass",
+    "OrganisationRead",
+    "OrganisationUnit",
+    "OrganisationUnitRead",
+    "OrganisationUnitWrite",
+]
