e506004100ab3f14692b8d7c57f7ad01c05330bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:08 +0000

    Add package tgext.rq/0.0.4/tgext.rq-0.0.4.tar.gz

---
+++ packages/tgext.rq/tgext.rq-0.0.4.tar.gz/tgext.rq-0.0.4/setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup, find_packages
+import sys, os
+
+here = os.path.abspath(os.path.dirname(__file__))
+try:
+    README = open(os.path.join(here, 'README.md')).read()
+except IOError:
+    README = ''
+
+version = "0.0.4"
+
+setup(
+    name='tgext.rq',
+    version=version,
+    description="",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    classifiers=['Development Status :: 5 - Production/Stable', 'Framework :: TurboGears'],  # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    keywords='turbogears2.extension',
+    author='Leonardo Baptista',
+    author_email='leonardoobaptistaa@gmail.com',
+    url='https://github.com/eureciclo/tgext.rq',
+    license='MIT',
+    packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+    namespace_packages = ['tgext'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        "TurboGears2 >= 2.0",
+        "rq >= 1.0",
+        "rq-gevent-worker @ git+https://github.com/mikeabrahamsen/rq-gevent-worker@master#egg=rq-gevent-worker"
+    ],
+    entry_points={
+        'gearbox.commands': [
+            'rq = tgext.rq.commands:RQCommand'
+        ],
+    },
+)
