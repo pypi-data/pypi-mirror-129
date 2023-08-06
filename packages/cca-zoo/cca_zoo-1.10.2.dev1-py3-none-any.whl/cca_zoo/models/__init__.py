0c3ffc75f5e55422f301957b294f6c8b1e9aeabf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:16:08 +0000

    Add package cca-zoo/1.10.2.dev1/cca_zoo-1.10.2.dev1-py3-none-any.whl

---
+++ packages/cca-zoo/cca_zoo-1.10.2.dev1-py3-none-any.whl/cca_zoo/models/__init__.py
@@ -0,0 +1,18 @@
+from .cca_base import _CCA_Base
+from .rcca import rCCA, CCA, PLS
+from .gcca import GCCA, KGCCA
+from .iterative import (
+    _Iterative,
+    PLS_ALS,
+    PMD,
+    CCA_ALS,
+    ElasticCCA,
+    ParkhomenkoCCA,
+    SCCA,
+    SCCA_ADMM,
+    SpanCCA,
+    SWCCA,
+)
+from .mcca import MCCA, KCCA
+from .ncca import NCCA
+from .tcca import TCCA, KTCCA
