9cf695e6d7fc2ae28208493adc44c65dd5906911
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:19:32 +0000

    Add package Small-fish/0.1.1/Small_fish-0.1.1.tar.gz

---
+++ packages/Small-fish/Small_fish-0.1.1.tar.gz/Small_fish-0.1.1/Small_fish/__main__.py
@@ -0,0 +1,53 @@
+import requests
+import getopt
+from colorama import init, Fore
+
+import datetime
+import os
+import sys
+
+def os_path():
+    _path=os.path.dirname(__file__)
+    _main_path=f"{_path}/Small_fish/"
+    sys.path.insert(1, _main_path)
+
+def get_url_html(_url, _headers=False):
+    r=requests.get(url=_url, headers=False)
+    return r.text
+
+def get_url_json(_url, _headers=False):
+    j=requests.get(url=_url, headers=_headers)
+    return j.json()
+
+class Smallfish:
+    init(autoreset = True)
+    def __init__(self):
+        self.opts,args=getopt.getopt(sys.argv[1:], '-v-h', ['version', 'HtmlText=', 'HtmlJson=', 'help'])
+        self.dtime=datetime.datetime.now()
+        print(Fore.CYAN+f"Small fish SystemTime:{self.dtime.strftime('%Y-%m-%d %H:%M:%S')} -help查看命令")
+    def main(self):
+        for opt,opt_v in self.opts:
+            if opt in ("-v", "--version"):
+                print(Fore.BLUE+"Version:0.1")
+            if opt in ("--HtmlText"):
+                print(f"{opt_v}")
+                print(Fore.LIGHTMAGENTA_EX+f"{get_url_html(opt_v)}")
+            if opt in ("--HtmlJson"):
+                print(f"{opt_v}")
+                print(Fore.LIGHTRED_EX+f"{get_url_json(opt_v)}")
+            if opt in ("-h", "--help"):
+                print(self.opts)
+                print(Fore.YELLOW+"""
+-------------------------------------------------------------------
+Small fish Help:
+    Command List:
+
+
+
+    
+                -v    Version 版本查询
+""")
+if __name__ in "__main__":
+    os_path()
+    s=Smallfish()
+    s.main()
