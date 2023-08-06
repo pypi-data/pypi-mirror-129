a919dd730e6f0bcc689387b757f1e78a52a0faa3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:39:26 +0000

    Add package MLatom/2.1.0/MLatom-2.1.0.tar.gz

---
+++ packages/MLatom/MLatom-2.1.0.tar.gz/MLatom-2.1.0/src/MLatom/MLatomF_src/constants.f90
@@ -0,0 +1,16 @@
+
+  !---------------------------------------------------------------------------! 
+  ! constants: physical constants and conversion factors                      ! 
+  ! Implementations by: Pavlo O. Dral                                         !
+  !---------------------------------------------------------------------------! 
+
+module constants
+  use precision, only : rprec
+  implicit none
+
+  real(kind=rprec), parameter :: Bohr2Angstrom = 0.52917721092_rprec             ! Peter J. Mohr, Barry N. Taylor, David B. Newell,
+                                                                                 ! CODATA Recommended Values of the
+                                                                                 ! Fundamental Physical Constants: 2010, NIST, 2012.
+  real(kind=rprec), parameter :: Angstrom2Bohr = 1.0_rprec / Bohr2Angstrom
+
+end module constants
