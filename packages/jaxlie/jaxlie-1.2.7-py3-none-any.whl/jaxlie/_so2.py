d69319ee480bc746b994f5e49f15e21061ddfffe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:14 +0000

    Add package jaxlie/1.2.7/jaxlie-1.2.7-py3-none-any.whl

---
+++ packages/jaxlie/jaxlie-1.2.7-py3-none-any.whl/jaxlie/_so2.py
@@ -0,0 +1,117 @@
+import jax
+import jax_dataclasses
+import numpy as onp
+from jax import numpy as jnp
+from overrides import overrides
+
+from . import _base, hints
+from .utils import register_lie_group
+
+
+@register_lie_group(
+    matrix_dim=2,
+    parameters_dim=2,
+    tangent_dim=1,
+    space_dim=2,
+)
+@jax_dataclasses.pytree_dataclass
+class SO2(_base.SOBase):
+    """Special orthogonal group for 2D rotations.
+
+    Internal parameterization is `(cos, sin)`. Tangent parameterization is `(omega,)`.
+    """
+
+    # SO2-specific
+
+    unit_complex: hints.Vector
+    """Internal parameters. `(cos, sin)`."""
+
+    @overrides
+    def __repr__(self) -> str:
+        unit_complex = jnp.round(self.unit_complex, 5)
+        return f"{self.__class__.__name__}(unit_complex={unit_complex})"
+
+    @staticmethod
+    def from_radians(theta: hints.Scalar) -> "SO2":
+        """Construct a rotation object from a scalar angle."""
+        cos = jnp.cos(theta)
+        sin = jnp.sin(theta)
+        return SO2(unit_complex=jnp.array([cos, sin]))
+
+    def as_radians(self) -> hints.ScalarJax:
+        """Compute a scalar angle from a rotation object."""
+        radians = self.log()[..., 0]
+        return radians
+
+    # Factory
+
+    @staticmethod
+    @overrides
+    def identity() -> "SO2":
+        return SO2(unit_complex=onp.array([1.0, 0.0]))
+
+    @staticmethod
+    @overrides
+    def from_matrix(matrix: hints.Matrix) -> "SO2":
+        assert matrix.shape == (2, 2)
+        return SO2(unit_complex=matrix[:, 0])
+
+    # Accessors
+
+    @overrides
+    def as_matrix(self) -> hints.MatrixJax:
+        cos, sin = self.unit_complex
+        return jnp.array(
+            [
+                [cos, -sin],
+                [sin, cos],
+            ]
+        )
+
+    @overrides
+    def parameters(self) -> hints.Vector:
+        return self.unit_complex
+
+    # Operations
+
+    @overrides
+    def apply(self: "SO2", target: hints.Vector) -> hints.VectorJax:
+        assert target.shape == (2,)
+        return self.as_matrix() @ target  # type: ignore
+
+    @overrides
+    def multiply(self: "SO2", other: "SO2") -> "SO2":
+        return SO2(unit_complex=self.as_matrix() @ other.unit_complex)
+
+    @staticmethod
+    @overrides
+    def exp(tangent: hints.TangentVector) -> "SO2":
+        (theta,) = tangent
+        cos = jnp.cos(theta)
+        sin = jnp.sin(theta)
+        return SO2(unit_complex=jnp.array([cos, sin]))
+
+    @overrides
+    def log(self: "SO2") -> hints.TangentVectorJax:
+        return jnp.arctan2(
+            self.unit_complex[..., 1, None], self.unit_complex[..., 0, None]
+        )
+
+    @overrides
+    def adjoint(self: "SO2") -> hints.MatrixJax:
+        return jnp.eye(1)
+
+    @overrides
+    def inverse(self: "SO2") -> "SO2":
+        return SO2(unit_complex=self.unit_complex * jnp.array([1, -1]))
+
+    @overrides
+    def normalize(self: "SO2") -> "SO2":
+        return SO2(unit_complex=self.unit_complex / jnp.linalg.norm(self.unit_complex))
+
+    @staticmethod
+    @overrides
+    def sample_uniform(key: jax.random.KeyArray) -> "SO2":
+        return SO2.from_radians(
+            jax.random.uniform(key=key, minval=0.0, maxval=2.0 * jnp.pi)
+        )
---
+++ packages/jaxlie/jaxlie-1.2.7-py3-none-any.whl/jaxlie/hints/__init__.py
@@ -0,0 +1,68 @@
+from typing import NamedTuple, Union
+
+import numpy as onp
+from jax import numpy as jnp
+
+# Type aliases for JAX/Numpy arrays; primarily for function inputs
+
+Array = Union[onp.ndarray, jnp.ndarray]
+"""Type alias for `Union[jnp.ndarray, onp.ndarray]`."""
+
+Scalar = Union[float, Array]
+"""Type alias for `Union[float, Array]`."""
+
+Matrix = Array
+"""Type alias for `Array`. Should not be instantiated.
+
+Refers to a square matrix, typically with shape `(Group.matrix_dim, Group.matrix_dim)`.
+For adjoints, shape should be `(Group.tangent_dim, Group.tangent_dim)`."""
+
+Vector = Array
+"""Type alias for `Array`. Should not be instantiated.
+
+Refers to a general 1D array."""
+
+TangentVector = Array
+"""Type alias for `Array`. Should not be instantiated.
+
+Refers to a 1D array with shape `(Group.tangent_dim,)`."""
+
+# Type aliases for JAX arrays; primarily for function outputs
+
+ArrayJax = jnp.ndarray
+"""Type alias for jnp.ndarray."""
+
+ScalarJax = ArrayJax
+"""Type alias for jnp.ndarray."""
+
+MatrixJax = ArrayJax
+"""Type alias for jnp.ndarray."""
+
+VectorJax = ArrayJax
+"""Type alias for jnp.ndarray."""
+
+TangentVectorJax = ArrayJax
+"""Type alias for jnp.ndarray."""
+
+
+class RollPitchYaw(NamedTuple):
+    """Tuple containing roll, pitch, and yaw Euler angles."""
+
+    roll: Scalar
+    pitch: Scalar
+    yaw: Scalar
+
+
+__all__ = [
+    "Array",
+    "Scalar",
+    "Matrix",
+    "Vector",
+    "TangentVector",
+    "ArrayJax",
+    "ScalarJax",
+    "MatrixJax",
+    "VectorJax",
+    "TangentVectorJax",
+    "RollPitchYaw",
+]
