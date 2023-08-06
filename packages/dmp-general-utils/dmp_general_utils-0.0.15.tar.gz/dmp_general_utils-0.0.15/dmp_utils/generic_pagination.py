1e408c017e1be51dbb787e327e9e4fa45e1a6302
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:11:56 +0000

    Add package dmp-general-utils/0.0.15/dmp_general_utils-0.0.15.tar.gz

---
+++ packages/dmp-general-utils/dmp_general_utils-0.0.15.tar.gz/dmp_general_utils-0.0.15/dmp_utils/generic_pagination.py
@@ -0,0 +1,17 @@
+from flask_marshmallow import Marshmallow
+
+
+class PaginationSchema:
+    # global schema_object
+    schema_object = None
+    ma = Marshmallow()
+
+    def __init__(self, ma, app, schema_object):
+        self.schema_object = schema_object
+        self.ma = ma
+        self.ma.init_app(app)
+
+
+    class Meta:
+        fields = ('has_prev', 'has_next', 'next_num', 'page', 'pages', 'per_page', 'prev_num', 'total', 'items')
+    items: ma.List(ma.Nested(schema_object))
