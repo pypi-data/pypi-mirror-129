2f72068f638711f1a47496cd814c9ed20d62a68d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:25:47 +0000

    Add package electronvolt/1.5.11/electronvolt-1.5.11.tar.gz

---
+++ packages/electronvolt/electronvolt-1.5.11.tar.gz/electronvolt-1.5.11/setup.py
@@ -0,0 +1,23 @@
+# python -iB tests.py
+# README.md
+# version number at setuptools.setup
+# commit, push, merge, pull, delete branch
+# pip install electronvolt -U
+# test from home directory or on mybinder.org
+
+import setuptools
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name="electronvolt",
+    version="1.5.11",
+    author_email="lw7jz@virginia.edu",
+    description="A physics quantity calculator with units.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/dw61/electronvolt",
+    py_modules=["electronvolt"],
+    python_requires=">=3.6",
+)
