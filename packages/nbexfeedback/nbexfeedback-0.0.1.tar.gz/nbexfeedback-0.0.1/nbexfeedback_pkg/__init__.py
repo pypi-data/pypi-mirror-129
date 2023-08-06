1bc4ecc8953aba9daa790f3840202746e1643485
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:19:54 +0000

    Add package nbexfeedback/0.0.1/nbexfeedback-0.0.1.tar.gz

---
+++ packages/nbexfeedback/nbexfeedback-0.0.1.tar.gz/nbexfeedback-0.0.1/nbexfeedback_pkg/__init__.py
@@ -0,0 +1,10 @@
+import sys
+print(sys.path)
+# from example import add_one
+#sys.path.append('/exception-feedback-pkg/base')
+# sys.path.append(src)
+print(sys.path)
+
+import example
+
+print(example.add_one(5))
