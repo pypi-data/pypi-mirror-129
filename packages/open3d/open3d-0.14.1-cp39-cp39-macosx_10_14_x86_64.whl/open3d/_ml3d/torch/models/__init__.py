963782b2aeee1de1d845773a523f765f36569c89
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:44:04 +0000

    Add package open3d/0.14.1/open3d-0.14.1-cp39-cp39-macosx_10_14_x86_64.whl

---
+++ packages/open3d/open3d-0.14.1-cp39-cp39-macosx_10_14_x86_64.whl/open3d/_ml3d/torch/models/__init__.py
@@ -0,0 +1,14 @@
+"""Networks for torch."""
+
+from .randlanet import RandLANet
+from .kpconv import KPFCNN
+from .point_pillars import PointPillars
+from .sparseconvnet import SparseConvUnet
+from .point_rcnn import PointRCNN
+from .point_transformer import PointTransformer
+from .pvcnn import PVCNN
+
+__all__ = [
+    'RandLANet', 'KPFCNN', 'PointPillars', 'PointRCNN', 'SparseConvUnet',
+    'PointTransformer', 'PVCNN'
+]
