2f51fc25883adf60f93760f2ac16bc002095aba1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:30 +0000

    Add package aa-bulletin-board/1.1.0/aa_bulletin_board-1.1.0-py3-none-any.whl

---
+++ packages/aa-bulletin-board/aa_bulletin_board-1.1.0-py3-none-any.whl/aa_bulletin_board/apps.py
@@ -0,0 +1,17 @@
+"""
+App config
+"""
+
+from django.apps import AppConfig
+
+from aa_bulletin_board import __version__
+
+
+class AaBulletinBoardConfig(AppConfig):
+    """
+    Application config
+    """
+
+    name = "aa_bulletin_board"
+    label = "aa_bulletin_board"
+    verbose_name = f"Bulletin Board v{__version__}"
