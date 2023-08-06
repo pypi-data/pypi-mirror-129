37518e6737dd2771f25c9b582cddee2e204c0540
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:28 +0000

    Add package dcoraid/0.8.4/dcoraid-0.8.4.tar.gz

---
+++ packages/dcoraid/dcoraid-0.8.4.tar.gz/dcoraid-0.8.4/dcoraid/api/errors.py
@@ -0,0 +1,26 @@
+class APIError(BaseException):
+    pass
+
+
+class APIAuthorizationError(APIError):
+    pass
+
+
+class APIBadGatewayError(APIError):
+    pass
+
+
+class APIConflictError(APIError):
+    pass
+
+
+class APIGatewayTimeoutError(APIError):
+    pass
+
+
+class APINotFoundError(APIError):
+    pass
+
+
+class NoAPIKeyError(APIError):
+    pass
