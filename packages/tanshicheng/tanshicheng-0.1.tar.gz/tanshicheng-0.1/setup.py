37c397e941c081162859e44fbde04b6816849c04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:13:22 +0000

    Add package tanshicheng/0.1/tanshicheng-0.1.tar.gz

---
+++ packages/tanshicheng/tanshicheng-0.1.tar.gz/tanshicheng-0.1/setup.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+LONGDOC = """
+Auto selector for NVIDIA GPUs and CUDA
+
+Support detection for tensorflow or torch
+"""
+
+setup(
+    name='tanshicheng',
+    version='0.1',
+    description="tanshicheng's tools",
+    long_description=LONGDOC,
+    author='tanshicheng',
+    license='GPLv3',
+    url='https://github.com/aitsc',
+    keywords='tools',
+    packages=find_packages(),
+
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Programming Language :: Python :: 3 :: Only',
+        'Topic :: Software Development :: Libraries',
+    ],
+    entry_points={
+        'console_scripts': ['tt=tsc_auto.main:main'],
+    },
+    python_requires='>=3.6',
+)
