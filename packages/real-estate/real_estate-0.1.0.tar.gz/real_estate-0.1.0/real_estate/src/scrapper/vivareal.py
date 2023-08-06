ad12c96b6ea18526612c64aac5438b168cce594f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:38:00 +0000

    Add package real-estate/0.1.0/real_estate-0.1.0.tar.gz

---
+++ packages/real-estate/real_estate-0.1.0.tar.gz/real_estate-0.1.0/real_estate/src/scrapper/vivareal.py
@@ -0,0 +1,153 @@
+from selenium.webdriver.support import expected_conditions as ec
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.chrome.options import Options
+from real_estate.src.utils.utils import init_driver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver import Chrome
+import re
+
+
+MAX_DELAY = 10
+# driver = init_driver()
+SITE = "https://www.vivareal.com.br/"
+address = "R. Monte Alegre"
+# driver.get(site)
+
+
+def accept_cookies(driver: Chrome):
+    try:
+        element = WebDriverWait(driver, 5).until(
+            ec.presence_of_element_located((By.XPATH, """//*[@id="cookie-notifier-cta"]"""))
+        )
+    except:
+        pass
+    else:
+        element.click()
+
+
+def select_rent_option(driver: Chrome, renting=True):
+    search_type = driver.find_element_by_xpath(
+        """//*[@id="js-site-main"]/section[1]/div/div/form[1]/div[1]/div/div/div[1]/select""")
+    search_type.click()
+    if renting:
+        driver.find_element_by_xpath("""//select[@class="js-select-business"]/option[@value="rent"]""").click()
+    else:
+        driver.find_element_by_xpath("""//select[@class="js-select-business"]/option[@value="sale"]""").click()
+
+
+def wait_for_addresses(driver: Chrome, address_label):
+    try:
+        WebDriverWait(driver, 5).until(
+            ec.presence_of_element_located(
+                (By.XPATH, """//*[@id="js-site-main"]/section[1]/div/div/form[1]/div[2]/div/div/div/div/div/*[1]"""))
+        )
+    except NoSuchElementException:
+        raise ReferenceError("Could found any real address with this address")
+    else:
+        address_label.send_keys(Keys.RETURN)
+
+
+def send_address(driver: Chrome, address: str):
+    address_label = driver.find_element_by_xpath("""//*[@id="filter-location-search-input"]""")
+    address_label.clear()
+    address_label.send_keys(address)
+    wait_for_addresses(driver, address_label)
+
+
+def collect_real_state_raw_data(driver: Chrome) -> list:
+    """
+    Find each announcement element inside the page by it's xpath
+    :param driver: Selenium driver
+    :return: list of WebDriverElements
+    """
+    cards_xpath = "//*[@data-type='property']"
+    element_present = ec.presence_of_element_located((By.XPATH, cards_xpath))
+    WebDriverWait(driver, MAX_DELAY).until(element_present)
+
+    return driver.find_elements_by_xpath(cards_xpath)
+
+
+def collect_elements_data(elements: list, driver) -> list:
+    data = list()
+
+    for element in elements:
+        driver.execute_script("arguments[0].scrollIntoView()", element)  # Scroll to element
+        id = element.get_attribute("id")
+        element_data = dict()
+        raw_renting = element.find_element_by_xpath(""".//section[@class="property-card__values  "]/div/p""").text
+        element_data["preço"] = int("".join(re.findall(r"\d+", raw_renting)))
+        try:
+            raw_condo_fee = element.find_element_by_xpath(""".//section[@class="property-card__values  "]/footer""").text
+        except NoSuchElementException:
+            element_data["valor_de_condominio"] = 0
+        else:
+            element_data["valor_de_condominio"] = int("".join(re.findall(r"\d+", raw_condo_fee)))
+        element_data["área"] = int(element.find_element_by_xpath(""".//li[@class="property-card__detail-item property-card__detail-area"]/span[1]""").text)
+        try:
+            element_data["vagas"] = int(element.find_element_by_xpath(""".//li[@class="property-card__detail-item property-card__detail-garage js-property-detail-garages"]/span[1]""").text)
+        except ValueError:
+            element_data["vagas"] = 0
+        element_data["quartos"] = int(element.find_element_by_xpath(""".//li[@class="property-card__detail-item property-card__detail-room js-property-detail-rooms"]/span[1]""").text)
+        element_data["banheiros"] = int(element.find_element_by_xpath(""".//li[@class="property-card__detail-item property-card__detail-bathroom js-property-detail-bathroom"]/span[1]""").text)
+        element_data["endereço"] = element.find_element_by_xpath(""".//*[@class="property-card__address"]""").text
+        element_data["texto"] = element.find_element_by_xpath(""".//*[@class="property-card__header"]""").text
+        element_data["link"] = f"https://www.vivareal.com.br/imovel/{id}"
+        element_data["img1"] = element.find_element_by_class_name("carousel__image").get_attribute("src")
+        element_data["site"] = "vivareal"
+
+        data.append(element_data)
+    return data
+
+
+def get_vivareal_data(driver_path: str, address: str, driver_options: Options = None) -> list:
+    """
+    Scrapes vivareal site and build a array of maps in the following format:
+
+    [
+        {
+            "preço": int,
+            "valor_de_condominio": int,
+            "banheiros": int,
+            "quartos": int,
+            "área": int,
+            "vagas": int,
+            "endereço": str
+            "texto": str
+        },
+        ...
+    ]
+
+
+    :param address: Address to search for
+    :param driver_options: driver options
+    :return: json like string
+    """
+    # Initialize browser
+    chrome = init_driver(driver_path, driver_options)
+    chrome.get(SITE)
+
+    # Collect  data
+    try:
+        accept_cookies(chrome)
+        select_rent_option(chrome)
+        send_address(chrome, address)
+        real_state_elements = collect_real_state_raw_data(chrome)
+        real_state_parsed_data = collect_elements_data(real_state_elements, chrome)
+
+    except Exception as e:
+        print(e)
+        real_state_parsed_data = None
+
+    finally:
+        chrome.close()
+
+    return real_state_parsed_data
+
+if __name__ == "__main__":
+    import time
+    start_time = time.time()
+    data = get_vivareal_data("Perdizes, SP")
+    print(f"Running time: {(time.time()-start_time):.2f} seconds.")
+    print(json.dumps(data, indent=4))
---
+++ packages/real-estate/real_estate-0.1.0.tar.gz/real_estate-0.1.0/real_estate/src/scrapper/zapimoveis.py
@@ -0,0 +1,138 @@
+from real_estate.src.utils.utils import get_regex_group_from_pattern, init_driver
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+import warnings
+import json
+import re
+
+
+SITE = "https://www.zapimoveis.com.br/"
+
+
+def rent_or_buy(driver, rent=True):
+    if rent:
+        rent_button = driver.find_element_by_xpath(
+            """/html/body/main/section/section[2]/div/section/form/div/div[1]/div[1]/div/button[2]""")
+        rent_button.click()
+
+
+def send_address(driver, address):
+    type_in = driver.find_element_by_xpath(
+        """/html/body/main/section/section[2]/div/section/form/div/div[2]/div/div/div/input""")
+    type_in.click()
+    type_in.send_keys(address)
+    try:
+        WebDriverWait(driver, 5).until(
+            EC.presence_of_element_located(
+                (By.XPATH, """/html/body/main/section/section[2]/div/section/form/div/div[2]/div/div/ul"""))
+        )
+        type_in.send_keys(Keys.ENTER)
+    except:
+        print("There's something wrong fam. Try again")
+
+
+def click_search(driver):
+    search_btn = driver.find_element_by_xpath(
+        """/html/body/main/section/section[2]/div/section/form/div/div[2]/button""")
+    search_btn.click()
+
+def get_condo(element):
+    try:
+        condo_xpath = ".//*[@class='card-price__item condominium text-regular']/*[@class='card-price__value']"
+        condo_text = element.find_element_by_xpath(condo_xpath).text
+    except NoSuchElementException:
+        return 0
+    else:
+        return int("".join(re.findall(r"\d", condo_text)))
+
+
+def get_elements(driver):
+    elements = None
+    try:
+        WebDriverWait(driver, 5).until(
+            EC.presence_of_element_located(
+                (By.XPATH, """/html/body/main/section/div[2]/div[3]/section/div/div[1]/div[1]/div[2]"""))
+        )
+
+        elements = driver.find_elements_by_xpath(r"""//div[@class="card-container js-listing-card"]""")
+
+    except:
+        warnings.warn("Where the hell is it fam?")
+    # page_numbers = driver.find_elements_by_class_name("pagination__item pagination__page link link-primary
+    # link--regular") page_one = driver.find_elements_by_class_name("pagination__item pagination__page link
+    # link-primary link--regular pagination__active js-active-page")
+
+    return elements
+
+
+def announcement_parser(method, arg, pattern=None):
+    try:
+        announcement_text = method(arg).text
+        if pattern is not None:
+            announcement_text = get_regex_group_from_pattern(announcement_text, pattern)
+    except NoSuchElementException:
+        announcement_text = 0
+
+    return announcement_text
+
+
+def get_announcement_data(elements: list, driver) -> list:
+    data = list()
+    price_text_pattern = r"^R\$\s([\d{1,3}\.?]+)"
+    area_text_pattern = r"(\d+) m²"
+    number_pattern = r"(\d+)"
+    for element in elements:
+        driver.execute_script("arguments[0].scrollIntoView()", element)  # Scroll to element
+        id = element.get_attribute("data-id")
+        preco_class = "simple-card__price"
+        preco_text = element.find_element_by_class_name(preco_class).text
+        card_info = {
+            "preço": int("".join(re.findall(r"\d", preco_text))),
+            "valor_de_condominio": get_condo(element),
+            "vagas": announcement_parser(element.find_element_by_class_name, "js-parking-spaces", number_pattern),
+            "banheiros": announcement_parser(element.find_element_by_class_name, "js-bathrooms", number_pattern),
+            "quartos": announcement_parser(element.find_element_by_class_name, "js-bedrooms", number_pattern),
+            "área": announcement_parser(element.find_element_by_class_name, "js-areas", area_text_pattern),
+            "endereço": element.find_element_by_class_name("simple-card__address ").text,
+            "texto": element.text,
+            "img1": element.find_elements_by_xpath(".//img")[0].get_attribute("src"),
+            "site": "zapimoveis",
+            "link": f"https://www.zapimoveis.com.br/imovel/{id}"
+        }
+
+        data.append(card_info)
+    return data
+
+
+def get_zapimoveis_data(driver_path: str, address: str, driver_options: Options = None) -> list:
+
+    chrome = init_driver(driver_path, driver_options)
+    # Set window size to collect all dynamic data
+    chrome.set_window_size(2000, 1000)
+    chrome.get(SITE)
+    try:
+        rent_or_buy(chrome)
+        send_address(chrome, address)
+        click_search(chrome)
+        real_state_elements = get_elements(chrome)
+        real_state_parsed_data = get_announcement_data(real_state_elements, chrome)
+    except Exception as e:
+        warnings.warn(str(e))
+        real_state_parsed_data = None
+
+    finally:
+        chrome.quit()
+
+    return real_state_parsed_data
+
+
+if __name__ == "__main__":
+    import time
+    start_time = time.time()
+    data = get_zapimoveis_data("Perdizes, São Paulo - São Paulo")
+    print(f"Execution time: {(time.time()-start_time):.2f} seconds.")
+    print(json.dumps(data, indent=4))
