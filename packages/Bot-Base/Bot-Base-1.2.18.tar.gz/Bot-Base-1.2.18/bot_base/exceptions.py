ecc12269f13a22089ea54f56e32857558f39e158
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:28 +0000

    Add package Bot-Base/1.2.18/Bot-Base-1.2.18.tar.gz

---
+++ packages/Bot-Base/Bot-Base-1.2.18.tar.gz/Bot-Base-1.2.18/bot_base/exceptions.py
@@ -0,0 +1,16 @@
+try:
+    from nextcord import DiscordException
+except ModuleNotFoundError:
+    from discord import DiscordException
+
+
+class PrefixNotFound(DiscordException):
+    """A prefix for this guild was not found."""
+
+
+class ExistingEntry(DiscordException):
+    """An entry was already found in the cache with this key."""
+
+
+class NonExistentEntry(DiscordException):
+    """No entry found in the cache with this key."""
---
+++ packages/Bot-Base/Bot-Base-1.2.18.tar.gz/Bot-Base-1.2.18/bot_base/wraps/__init__.py
@@ -0,0 +1,14 @@
+from .channel import WrappedChannel, WrappedChannelConvertor
+from .meta import Meta
+from .member import WrappedMember, WrappedMemberConvertor
+from .user import WrappedUser, WrappedUserConvertor
+
+__all__ = (
+    "WrappedChannel",
+    "WrappedChannelConvertor",
+    "Meta",
+    "WrappedMember",
+    "WrappedMemberConvertor",
+    "WrappedUser",
+    "WrappedUserConvertor",
+)
