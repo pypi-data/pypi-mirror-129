76bf8b866a352828ec7ea49fb10eef4b3974864b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:12 +0000

    Add package dpctl/0.11.2/dpctl-0.11.2-0-cp39-cp39-manylinux2014_x86_64.whl

---
+++ packages/dpctl/dpctl-0.11.2-0-cp39-cp39-manylinux2014_x86_64.whl/dpctl/_sycl_device_factory.pyx
@@ -0,0 +1,400 @@
+#                      Data Parallel Control (dpctl)
+#
+# Copyright 2020-2021 Intel Corporation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# distutils: language = c++
+# cython: language_level=3
+# cython: linetrace=True
+
+""" This module implements several device creation helper functions:
+
+  - wrapper functions to create a SyclDevice from the standard SYCL
+    device selector classes.
+  - functions to return a list of devices based on a specified device_type or
+    backend_type combination.
+"""
+
+from ._backend cimport (  # noqa: E211
+    DPCTLAcceleratorSelector_Create,
+    DPCTLCPUSelector_Create,
+    DPCTLDefaultSelector_Create,
+    DPCTLDevice_CreateFromSelector,
+    DPCTLDeviceMgr_GetDevices,
+    DPCTLDeviceMgr_GetNumDevices,
+    DPCTLDeviceSelector_Delete,
+    DPCTLDeviceVector_Delete,
+    DPCTLDeviceVector_GetAt,
+    DPCTLDeviceVector_Size,
+    DPCTLDeviceVectorRef,
+    DPCTLGPUSelector_Create,
+    DPCTLHostSelector_Create,
+    DPCTLSyclDeviceRef,
+    DPCTLSyclDeviceSelectorRef,
+    _backend_type,
+    _device_type,
+)
+
+from .enum_types import backend_type
+from .enum_types import device_type as device_type_t
+
+__all__ = [
+    "get_devices",
+    "select_accelerator_device",
+    "select_cpu_device",
+    "select_default_device",
+    "select_gpu_device",
+    "select_host_device",
+    "get_num_devices",
+    "has_cpu_devices",
+    "has_gpu_devices",
+    "has_accelerator_devices",
+    "has_host_device",
+]
+
+
+cdef _backend_type _string_to_dpctl_sycl_backend_ty(str backend_str):
+    backend_str = backend_str.strip().lower()
+    if backend_str == "all":
+        return _backend_type._ALL_BACKENDS
+    elif backend_str == "cuda":
+        return _backend_type._CUDA
+    elif backend_str == "host":
+        return _backend_type._HOST
+    elif backend_str == "level_zero":
+        return _backend_type._LEVEL_ZERO
+    elif backend_str == "opencl":
+        return _backend_type._OPENCL
+    else:
+        return _backend_type._UNKNOWN_BACKEND
+
+
+cdef _device_type _string_to_dpctl_sycl_device_ty(str dty_str):
+    dty_str = dty_str.strip().lower()
+    if dty_str == "accelerator":
+        return _device_type._ACCELERATOR
+    elif dty_str == "all":
+        return _device_type._ALL_DEVICES
+    elif dty_str == "automatic":
+        return _device_type._AUTOMATIC
+    elif dty_str == "cpu":
+        return _device_type._CPU
+    elif dty_str == "custom":
+        return _device_type._CUSTOM
+    elif dty_str == "gpu":
+        return _device_type._GPU
+    elif dty_str == "host":
+        return _device_type._HOST_DEVICE
+    else:
+        return _device_type._UNKNOWN_DEVICE
+
+
+cdef _backend_type _enum_to_dpctl_sycl_backend_ty(BTy):
+    if BTy == backend_type.all:
+        return _backend_type._ALL_BACKENDS
+    elif BTy == backend_type.cuda:
+        return _backend_type._CUDA
+    elif BTy == backend_type.host:
+        return _backend_type._HOST
+    elif BTy == backend_type.level_zero:
+        return _backend_type._LEVEL_ZERO
+    elif BTy == backend_type.opencl:
+        return _backend_type._OPENCL
+    else:
+        return _backend_type._UNKNOWN_BACKEND
+
+
+cdef _device_type _enum_to_dpctl_sycl_device_ty(DTy):
+    if DTy == device_type_t.all:
+        return _device_type._ALL_DEVICES
+    elif DTy == device_type_t.accelerator:
+        return _device_type._ACCELERATOR
+    elif DTy == device_type_t.automatic:
+        return _device_type._AUTOMATIC
+    elif DTy == device_type_t.cpu:
+        return _device_type._CPU
+    elif DTy == device_type_t.custom:
+        return _device_type._CUSTOM
+    elif DTy == device_type_t.gpu:
+        return _device_type._GPU
+    elif DTy == device_type_t.host:
+        return _device_type._HOST_DEVICE
+    else:
+        return _device_type._UNKNOWN_DEVICE
+
+
+cdef list _get_devices(DPCTLDeviceVectorRef DVRef):
+    cdef list devices = []
+    cdef size_t nelems = 0
+    if DVRef:
+        nelems = DPCTLDeviceVector_Size(DVRef)
+        for i in range(0, nelems):
+            DRef = DPCTLDeviceVector_GetAt(DVRef, i)
+            D = SyclDevice._create(DRef)
+            devices.append(D)
+
+    return devices
+
+
+cpdef list get_devices(backend=backend_type.all, device_type=device_type_t.all):
+    """ get_devices(backend=backend_type.all, device_type=device_type_t.all)
+    Returns a list of :class:`dpctl.SyclDevice` instances selected based on
+    the given :class:`dpctl.device_type` and :class:`dpctl.backend_type` values.
+
+    The function is analogous to ``sycl::devices::get_devices()``, but with an
+    additional functionality that allows filtering SYCL devices based on
+    ``backend`` in addition to only ``device_type``.
+
+    Args:
+        backend (optional): Defaults to ``dpctl.backend_type.all``.
+            A :class:`dpctl.backend_type` enum value or a string that
+            specifies a SYCL backend. Currently, accepted values are: "cuda",
+            "opencl", "level_zero", or "all".
+        device_type (optional): Defaults to ``dpctl.device_type.all``.
+            A :class:`dpctl.device_type` enum value or a string that
+            specifies a SYCL device type. Currently, accepted values are:
+            "gpu", "cpu", "accelerator", "host", or "all".
+    Returns:
+        list: A list of available :class:`dpctl.SyclDevice` instances that
+        satisfy the provided :class:`dpctl.backend_type` and
+        :class:`dpctl.device_type` values.
+    """
+    cdef _backend_type BTy = _backend_type._ALL_BACKENDS
+    cdef _device_type DTy = _device_type._ALL_DEVICES
+    cdef DPCTLDeviceVectorRef DVRef = NULL
+    cdef list devices
+
+    if isinstance(backend, str):
+        BTy = _string_to_dpctl_sycl_backend_ty(backend)
+    elif isinstance(backend, backend_type):
+        BTy = _enum_to_dpctl_sycl_backend_ty(backend)
+    else:
+        raise TypeError(
+            "backend should be specified as a str or an "
+            "``enum_types.backend_type``."
+        )
+
+    if isinstance(device_type, str):
+        DTy = _string_to_dpctl_sycl_device_ty(device_type)
+    elif isinstance(device_type, device_type_t):
+        DTy = _enum_to_dpctl_sycl_device_ty(device_type)
+    else:
+        raise TypeError(
+            "device type should be specified as a str or an "
+            "``enum_types.device_type``."
+        )
+
+    DVRef = DPCTLDeviceMgr_GetDevices(BTy | DTy)
+    devices = _get_devices(DVRef)
+    DPCTLDeviceVector_Delete(DVRef)
+
+    return devices
+
+
+cpdef int get_num_devices(
+    backend=backend_type.all, device_type=device_type_t.all
+):
+    """ get_num_devices(backend=backend_type.all, device_type=device_type_t.all)
+    A helper function to return the number of SYCL devices of a given
+    :class:`dpctl.device_type` and :class:`dpctl.backend_type`.
+
+    Args:
+        backend (optional): Defaults to ``dpctl.backend_type.all``.
+            A :class:`dpctl.backend_type` enum value or a string that
+            specifies a SYCL backend. Currently, accepted values are: "cuda",
+            "opencl", "level_zero", or "all".
+        device_type (optional): Defaults to ``dpctl.device_type.all``.
+            A :class:`dpctl.device_type` enum value or a string that
+            specifies a SYCL device type. Currently, accepted values are:
+            "gpu", "cpu", "accelerator", "host", or "all".
+    Returns:
+        int: The number of available SYCL devices that satisfy the provided
+        :class:`dpctl.backend_type` and :class:`dpctl.device_type` values.
+    """
+    cdef _backend_type BTy = _backend_type._ALL_BACKENDS
+    cdef _device_type DTy = _device_type._ALL_DEVICES
+    cdef int num_devices = 0
+
+    if isinstance(backend, str):
+        BTy = _string_to_dpctl_sycl_backend_ty(backend)
+    elif isinstance(backend, backend_type):
+        BTy = _enum_to_dpctl_sycl_backend_ty(backend)
+    else:
+        raise TypeError(
+            "backend should be specified as a ``str`` or an "
+            "``enum_types.backend_type``"
+        )
+
+    if isinstance(device_type, str):
+        DTy = _string_to_dpctl_sycl_device_ty(device_type)
+    elif isinstance(device_type, device_type_t):
+        DTy = _enum_to_dpctl_sycl_device_ty(device_type)
+    else:
+        raise TypeError(
+            "device type should be specified as a ``str`` or an "
+            "``enum_types.device_type``"
+        )
+
+    num_devices = DPCTLDeviceMgr_GetNumDevices(BTy | DTy)
+
+    return num_devices
+
+
+cpdef cpp_bool has_cpu_devices():
+    """ A helper function to check if there are any SYCL CPU devices available.
+
+    Returns:
+        bool: ``True`` if ``sycl::device_type::cpu`` devices are present,
+        ``False`` otherwise.
+    """
+    cdef int num_cpu_dev = DPCTLDeviceMgr_GetNumDevices(_device_type._CPU)
+    return <cpp_bool>num_cpu_dev
+
+
+cpdef cpp_bool has_gpu_devices():
+    """ A helper function to check if there are any SYCL GPU devices available.
+
+    Returns:
+        bool: ``True`` if ``sycl::device_type::gpu`` devices are present,
+        ``False`` otherwise.
+    """
+    cdef int num_gpu_dev = DPCTLDeviceMgr_GetNumDevices(_device_type._GPU)
+    return <cpp_bool>num_gpu_dev
+
+
+cpdef cpp_bool has_accelerator_devices():
+    """ A helper function to check if there are any SYCL Accelerator devices
+    available.
+
+    Returns:
+        bool: ``True`` if ``sycl::device_type::accelerator`` devices are
+        present, ``False`` otherwise.
+    """
+    cdef int num_accelerator_dev = DPCTLDeviceMgr_GetNumDevices(
+        _device_type._ACCELERATOR
+    )
+    return <cpp_bool>num_accelerator_dev
+
+
+cpdef cpp_bool has_host_device():
+    """ A helper function to check if there are any SYCL Host devices available.
+
+    Returns:
+        bool: ``True`` if ``sycl::device_type::host`` devices are present,
+        ``False`` otherwise.
+    """
+    cdef int num_host_dev = DPCTLDeviceMgr_GetNumDevices(
+        _device_type._HOST_DEVICE
+    )
+    return <cpp_bool>num_host_dev
+
+
+cpdef SyclDevice select_accelerator_device():
+    """ A wrapper for SYCL's ``accelerator_selector`` class.
+
+    Returns:
+        dpctl.SyclDevice: A Python object wrapping the SYCL ``device``
+        returned by the SYCL ``accelerator_selector``.
+    Raises:
+        ValueError: If the SYCL ``accelerator_selector`` is unable to select a
+                    ``device``.
+    """
+    cdef DPCTLSyclDeviceSelectorRef DSRef = DPCTLAcceleratorSelector_Create()
+    cdef DPCTLSyclDeviceRef DRef = DPCTLDevice_CreateFromSelector(DSRef)
+    # Free up the device selector
+    DPCTLDeviceSelector_Delete(DSRef)
+    if DRef is NULL:
+        raise ValueError("Device unavailable.")
+    Device = SyclDevice._create(DRef)
+    return Device
+
+
+cpdef SyclDevice select_cpu_device():
+    """ A wrapper for SYCL's ``cpu_selector`` class.
+
+    Returns:
+        dpctl.SyclDevice: A Python object wrapping the SYCL ``device``
+        returned by the SYCL ``cpu_selector``.
+    Raises:
+        ValueError: If the SYCL ``cpu_selector`` is unable to select a
+                    ``device``.
+    """
+    cdef DPCTLSyclDeviceSelectorRef DSRef = DPCTLCPUSelector_Create()
+    cdef DPCTLSyclDeviceRef DRef = DPCTLDevice_CreateFromSelector(DSRef)
+    # Free up the device selector
+    DPCTLDeviceSelector_Delete(DSRef)
+    if DRef is NULL:
+        raise ValueError("Device unavailable.")
+    Device = SyclDevice._create(DRef)
+    return Device
+
+
+cpdef SyclDevice select_default_device():
+    """ A wrapper for SYCL's ``default_selector`` class.
+
+    Returns:
+        dpctl.SyclDevice: A Python object wrapping the SYCL ``device``
+        returned by the SYCL ``default_selector``.
+    Raises:
+        ValueError: If the SYCL ``default_selector`` is unable to select a
+            ``device``.
+    """
+    cdef DPCTLSyclDeviceSelectorRef DSRef = DPCTLDefaultSelector_Create()
+    cdef DPCTLSyclDeviceRef DRef = DPCTLDevice_CreateFromSelector(DSRef)
+    # Free up the device selector
+    DPCTLDeviceSelector_Delete(DSRef)
+    if DRef is NULL:
+        raise ValueError("Device unavailable.")
+    Device = SyclDevice._create(DRef)
+    return Device
+
+
+cpdef SyclDevice select_gpu_device():
+    """ A wrapper for SYCL's ``gpu_selector`` class.
+
+    Returns:
+        dpctl.SyclDevice: A Python object wrapping the SYCL ``device``
+        returned by the SYCL ``gpu_selector``.
+    Raises:
+        ValueError: If the SYCL ``gpu_selector`` is unable to select a
+                    ``device``.
+    """
+    cdef DPCTLSyclDeviceSelectorRef DSRef = DPCTLGPUSelector_Create()
+    cdef DPCTLSyclDeviceRef DRef = DPCTLDevice_CreateFromSelector(DSRef)
+    # Free up the device selector
+    DPCTLDeviceSelector_Delete(DSRef)
+    if DRef is NULL:
+        raise ValueError("Device unavailable.")
+    Device = SyclDevice._create(DRef)
+    return Device
+
+
+cpdef SyclDevice select_host_device():
+    """ A wrapper for SYCL's ``host_selector`` class.
+
+    Returns:
+        dpctl.SyclDevice: A Python object wrapping the SYCL ``device``
+        returned by the SYCL ``host_selector``.
+    Raises:
+        ValueError: If the SYCL ``host_selector`` is unable to select a
+            ``device``.
+    """
+    cdef DPCTLSyclDeviceSelectorRef DSRef = DPCTLHostSelector_Create()
+    cdef DPCTLSyclDeviceRef DRef = DPCTLDevice_CreateFromSelector(DSRef)
+    # Free up the device selector
+    DPCTLDeviceSelector_Delete(DSRef)
+    if DRef is NULL:
+        raise ValueError("Device unavailable.")
+    Device = SyclDevice._create(DRef)
+    return Device
---
+++ packages/dpctl/dpctl-0.11.2-0-cp39-cp39-manylinux2014_x86_64.whl/dpctl/_sycl_platform.pyx
@@ -0,0 +1,375 @@
+#                      Data Parallel Control (dpctl)
+#
+# Copyright 2020 Intel Corporation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# distutils: language = c++
+# cython: language_level=3
+# cython: linetrace=True
+
+""" Implements SyclPlatform Cython extension type.
+"""
+
+from ._backend cimport (  # noqa: E211
+    DPCTLCString_Delete,
+    DPCTLDeviceSelector_Delete,
+    DPCTLFilterSelector_Create,
+    DPCTLPlatform_Copy,
+    DPCTLPlatform_Create,
+    DPCTLPlatform_CreateFromSelector,
+    DPCTLPlatform_Delete,
+    DPCTLPlatform_GetBackend,
+    DPCTLPlatform_GetName,
+    DPCTLPlatform_GetPlatforms,
+    DPCTLPlatform_GetVendor,
+    DPCTLPlatform_GetVersion,
+    DPCTLPlatformMgr_PrintInfo,
+    DPCTLPlatformVector_Delete,
+    DPCTLPlatformVector_GetAt,
+    DPCTLPlatformVector_Size,
+    DPCTLPlatformVectorRef,
+    DPCTLSyclDeviceSelectorRef,
+    DPCTLSyclPlatformRef,
+    _backend_type,
+)
+
+import warnings
+
+from .enum_types import backend_type
+
+__all__ = [
+    "get_platforms",
+    "lsplatform",
+    "SyclPlatform",
+]
+
+cdef class _SyclPlatform:
+    """ Data owner for SyclPlatform
+    """
+
+    def __dealloc__(self):
+        DPCTLPlatform_Delete(self._platform_ref)
+        DPCTLCString_Delete(self._name)
+        DPCTLCString_Delete(self._vendor)
+        DPCTLCString_Delete(self._version)
+
+
+cdef void _init_helper(_SyclPlatform platform, DPCTLSyclPlatformRef PRef):
+    "Populate attributes of class from opaque reference PRef"
+    platform._platform_ref = PRef
+    platform._name = DPCTLPlatform_GetName(PRef)
+    platform._version = DPCTLPlatform_GetVersion(PRef)
+    platform._vendor = DPCTLPlatform_GetVendor(PRef)
+
+
+cdef class SyclPlatform(_SyclPlatform):
+    """ SyclPlatform(self, arg=None)
+        Python class representing ``cl::sycl::platform`` class.
+
+        SyclPlatform() - create platform selected by sycl::default_selector
+        SyclPlatform(filter_selector) - create platform selected by filter
+        selector
+    """
+    @staticmethod
+    cdef SyclPlatform _create(DPCTLSyclPlatformRef pref):
+        """
+        This function calls ``DPCTLPlatform_Delete(pref)``.
+
+        The user of this function must pass a copy to keep the
+        pref argument alive.
+        """
+        cdef _SyclPlatform p = _SyclPlatform.__new__(_SyclPlatform)
+        # Initialize the attributes of the SyclPlatform object
+        _init_helper(<_SyclPlatform>p, pref)
+        return SyclPlatform(p)
+
+    cdef int _init_from__SyclPlatform(self, _SyclPlatform other):
+        self._platform_ref = DPCTLPlatform_Copy(other._platform_ref)
+        if (self._platform_ref is NULL):
+            return -1
+        self._name = DPCTLPlatform_GetName(self._platform_ref)
+        self._version = DPCTLPlatform_GetVersion(self._platform_ref)
+        self._vendor = DPCTLPlatform_GetVendor(self._platform_ref)
+
+    cdef int _init_from_cstring(self, const char *string):
+        cdef DPCTLSyclDeviceSelectorRef DSRef = NULL
+        DSRef = DPCTLFilterSelector_Create(string)
+        ret = self._init_from_selector(DSRef)
+        return ret
+
+    cdef int _init_from_selector(self, DPCTLSyclDeviceSelectorRef DSRef):
+        # Initialize the SyclPlatform from a DPCTLSyclDeviceSelectorRef
+        cdef DPCTLSyclPlatformRef PRef = DPCTLPlatform_CreateFromSelector(DSRef)
+        DPCTLDeviceSelector_Delete(DSRef)
+        if PRef is NULL:
+            return -1
+        else:
+            _init_helper(self, PRef)
+            return 0
+
+    cdef DPCTLSyclPlatformRef get_platform_ref(self):
+        """ Returns the ``DPCTLSyclPlatformRef`` pointer for this class.
+        """
+        return self._platform_ref
+
+    @property
+    def __name__(self):
+        return "SyclPlatform"
+
+    def __repr__(self):
+        return (
+            "<dpctl."
+            + self.__name__
+            + " ["
+            + self.name
+            + ", "
+            + self.vendor
+            + ", "
+            + " "
+            + self.version + "] at {}>".format(hex(id(self)))
+        )
+
+    def __cinit__(self, arg=None):
+        if type(arg) is unicode:
+            string = bytes(<unicode>arg, "utf-8")
+            filter_c_str = string
+            ret = self._init_from_cstring(filter_c_str)
+            if ret == -1:
+                raise ValueError(
+                    "Could not create a SyclPlatform with the selector string"
+                )
+        elif isinstance(arg, unicode):
+            string = bytes(<unicode>unicode(arg), "utf-8")
+            filter_c_str = string
+            ret = self._init_from_cstring(filter_c_str)
+            if ret == -1:
+                raise ValueError(
+                    "Could not create a SyclPlatform with the selector string"
+                )
+        elif isinstance(arg, _SyclPlatform):
+            ret = self._init_from__SyclPlatform(arg)
+            if ret == -1:
+                raise ValueError(
+                    "Could not create SyclPlatform from _SyclPlatform instance"
+                )
+        elif arg is None:
+            PRef = DPCTLPlatform_Create()
+            if PRef is NULL:
+                raise ValueError(
+                    "Could not create a SyclPlatform from default selector"
+                )
+            else:
+                _init_helper(self, PRef)
+        else:
+            raise ValueError(
+                "Invalid argument. Argument should be a str object specifying "
+                "a SYCL filter selector string."
+            )
+
+    def print_platform_info(self, verbosity=0):
+        """ Print information about the SYCL platform.
+
+        The level of information printed out by the function can be controlled
+        by the optional ``vebosity`` setting.
+
+            - **Verbosity level 0**: Prints out the list of platforms and their
+              names.
+            - **Verbosity level 1**: Prints out the name, version, vendor,
+              backend, number of devices for each platform.
+            - **Verbosity level 2**: At the highest level of verbosity
+              everything in the previous levels along with the name, version,
+              and filter string for each device is printed.
+
+        Args:
+            verbosity (optional): Defaults to 0.
+                The verbosity controls how much information is printed by the
+                function. 0 is the lowest level set by default and 2 is the
+                highest level to print the most verbose output.
+
+        """
+        cdef size_t v = 0
+
+        if not isinstance(verbosity, int):
+            warnings.warn(
+                "Illegal verbosity level. Accepted values are 0, 1, or 2. "
+                "Using the default verbosity level of 0."
+            )
+        else:
+            v = <size_t>(verbosity)
+            if v > 2:
+                warnings.warn(
+                    "Illegal verbosity level. Accepted values are 0, 1, or 2. "
+                    "Using the default verbosity level of 0."
+                )
+                v = 0
+        DPCTLPlatformMgr_PrintInfo(self._platform_ref, v)
+
+    @property
+    def vendor(self):
+        """ Returns the platform vendor name as a string.
+        """
+        return self._vendor.decode()
+
+    @property
+    def version(self):
+        """ Returns a backend-defined driver version as a string.
+        """
+        return self._version.decode()
+
+    @property
+    def name(self):
+        """ Returns the name of the platform as a string
+        """
+        return self._name.decode()
+
+    @property
+    def backend(self):
+        """Returns the backend_type enum value for this device
+
+        Returns:
+            backend_type: The backend for the device.
+        """
+        cdef _backend_type BTy = (
+            DPCTLPlatform_GetBackend(self._platform_ref)
+        )
+        if BTy == _backend_type._CUDA:
+            return backend_type.cuda
+        elif BTy == _backend_type._HOST:
+            return backend_type.host
+        elif BTy == _backend_type._LEVEL_ZERO:
+            return backend_type.level_zero
+        elif BTy == _backend_type._OPENCL:
+            return backend_type.opencl
+        else:
+            raise ValueError("Unknown backend type.")
+
+
+def lsplatform(verbosity=0):
+    """
+    Prints out the list of available SYCL platforms, and optionally extra
+    metadata about each platform.
+
+    The level of information printed out by the function can be controlled by
+    the optional ``vebosity`` setting.
+
+    - **Verbosity level 0**: Prints out the list of platforms and their names.
+    - **Verbosity level 1**: Prints out the name, version, vendor, backend,
+      number of devices for each platform.
+    - **Verbosity level 2**: At the highest level of verbosity everything in the
+      previous levels along with the name, version, and filter string for each
+      device is printed.
+
+    At verbosity level 2 (highest level) the following output is generated.
+
+    :Example:
+        On a system with an OpenCL CPU driver, OpenCL GPU driver,
+        Level Zero GPU driver, running the command. ::
+
+        $python -c "import dpctl; dpctl.lsplatform()"
+
+        returns ::
+
+            Platform 0::
+                Name        Intel(R) OpenCL
+                Version     OpenCL 2.1 LINUX
+                Vendor      Intel(R) Corporation
+                Profile     FULL_PROFILE
+                Backend     opencl
+                Devices     1
+                    Device 0::
+                    Name            Intel(R) Core(TM) i7-9700 CPU @ 3.00GHz
+                    Driver version  2020.11.11.0.13_160000
+                    Device type     cpu
+            Platform 1::
+                Name        Intel(R) OpenCL HD Graphics
+                Version     OpenCL 3.0
+                Vendor      Intel(R) Corporation
+                Profile     FULL_PROFILE
+                Backend     opencl
+                Devices     1
+                    Device 0::
+                    Name            Intel(R) Graphics Gen9 [0x3e98]
+                    Driver version  20.47.18513
+                    Device type     gpu
+            Platform 2::
+                Name        Intel(R) Level-Zero
+                Version     1.0
+                Vendor      Intel(R) Corporation
+                Profile     FULL_PROFILE
+                Backend     level_zero
+                Devices     1
+                    Device 0::
+                    Name            Intel(R) Graphics Gen9 [0x3e98]
+                    Driver version  1.0.18513
+                    Device type     gpu
+
+    Args:
+        verbosity (optional): Defaults to 0.
+            The verbosity controls how much information is printed by the
+            function. 0 is the lowest level set by default and 2 is the highest
+            level to print the most verbose output.
+    """
+    cdef DPCTLPlatformVectorRef PVRef = NULL
+    cdef size_t v = 0
+    cdef size_t size = 0
+    cdef DPCTLSyclPlatformRef PRef = NULL
+
+    if not isinstance(verbosity, int):
+        print(
+            "Illegal verbosity level. Accepted values are 0, 1, or 2. "
+            "Using the default verbosity level of 0."
+        )
+    else:
+        v = <size_t>(verbosity)
+        if v > 2:
+            warnings.warn(
+                "Illegal verbosity level. Accepted values are 0, 1, or 2. "
+                "Using the default verbosity level of 0."
+            )
+            v = 0
+
+    PVRef = DPCTLPlatform_GetPlatforms()
+
+    if PVRef is not NULL:
+        size = DPCTLPlatformVector_Size(PVRef)
+        for i in range(size):
+            if v != 0:
+                print("Platform ", i, "::")
+            PRef = DPCTLPlatformVector_GetAt(PVRef, i)
+            DPCTLPlatformMgr_PrintInfo(PRef, v)
+            DPCTLPlatform_Delete(PRef)
+    DPCTLPlatformVector_Delete(PVRef)
+
+
+cpdef list get_platforms():
+    """
+    Returns a list of all available SYCL platforms on the system.
+
+    Returns:
+        list: A list of SYCL platforms on the system.
+    """
+    cdef list platforms = []
+    cdef DPCTLPlatformVectorRef PVRef = NULL
+    cdef size_t size = 0
+
+    PVRef = DPCTLPlatform_GetPlatforms()
+    if PVRef is not NULL:
+        size = DPCTLPlatformVector_Size(PVRef)
+        for i in range(size):
+            PRef = DPCTLPlatformVector_GetAt(PVRef, i)
+            P = SyclPlatform._create(PRef)
+            platforms.append(P)
+
+    DPCTLPlatformVector_Delete(PVRef)
+    return platforms
