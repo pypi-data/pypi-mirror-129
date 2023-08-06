0be65f796a545ca3fbec0328b7e0e4688ff09940
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:32:00 +0000

    Add package lumibot/1.4.1/lumibot-1.4.1-py3-none-any.whl

---
+++ packages/lumibot/lumibot-1.4.1-py3-none-any.whl/my_dev/credentials.py
@@ -0,0 +1,25 @@
+class AlpacaConfig:
+    # Put your own Alpaca key here:
+    API_KEY = "PKN2NQZY2PPIV3TC835A"
+    # Put your own Alpaca secret here:
+    API_SECRET = "fP9LxZKGl7TXCNxnexSG3wPLBBJQ5wjYLzhKKdHG"
+    # If you want to go live, you must change this
+    ENDPOINT = "https://paper-api.alpaca.markets"
+
+
+class AlphaVantageConfig:
+    # Put your own Alpha Vantage key here:
+    API_KEY = "30WM6G3P2TVGCIWL"
+
+
+class InteractiveBrokersConfig:
+    SOCKET_PORT = 7497
+    CLIENT_ID = 888
+    IP = "127.0.0.1"
+
+
+class TwilioConfig:
+    ACCOUNT_SID = "ACbeae0962b9ba5ee37e15f241d73a4f67"
+    TOKEN = "ea788a70708806194e89b090e1d7cc33"
+    SENDING_NUMBER = "+12035775681"
+    RECEIVING_NUMBERS = ["+13476353416"]
