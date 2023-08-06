964f6c1b447f2f24df636762272b67a66fbb2be6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:26:53 +0000

    Add package poetspy/0.2.0/poetspy-0.2.0-py3-none-any.whl

---
+++ packages/poetspy/poetspy-0.2.0-py3-none-any.whl/poetspy/generate.py
@@ -0,0 +1,63 @@
+import json
+import os
+import sys
+from pathlib import Path
+
+import click
+
+
+@click.command(short_help="Change title")
+@click.argument("title", nargs=-1, required=True)
+def title(title):
+    """Modify the title of the current structure in .poets.json"""
+    t = " ".join(title)
+    p = Path("./.poets.json")
+    if p.exists():
+        if p.is_dir():
+            click.secho(".poets.json is a directory!", fg="red")
+            sys.exit(1)
+        k = json.load(p.open())
+        k["title"] = t
+    else:
+        k = {"title": t}
+    json.dump(k, p.open("w"))
+    click.echo(
+        click.style("title set to", fg="blue")
+        + " "
+        + click.style(t, fg="blue", underline=True)
+    )
+
+
+@click.command(short_help="Change description")
+@click.argument("des", nargs=-1, required=True)
+def des(des):
+    """Modify the description of the current structure in .poets.json"""
+    t = " ".join(des)
+    p = Path("./.poets.json")
+    if p.exists():
+        if p.is_dir():
+            click.secho(".poets.json is a directory!", fg="red")
+            sys.exit(1)
+        k = json.load(p.open())
+        k["subtitle"] = t
+    else:
+        k = {"subtitle": t}
+    json.dump(k, p.open("w"))
+    click.echo(
+        click.style("description set to", fg="blue")
+        + " "
+        + click.style(t, fg="blue", underline=True)
+    )
+
+
+@click.group()
+def main():
+    """Placeholder for subcommands"""
+    pass
+
+
+main.add_command(title)
+main.add_command(des)
+
+if __name__ == "__main__":
+    main()
