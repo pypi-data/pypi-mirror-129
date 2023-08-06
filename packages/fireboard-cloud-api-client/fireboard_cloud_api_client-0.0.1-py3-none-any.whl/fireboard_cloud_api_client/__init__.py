60b095876c4c8c602ef1711fb5c0eba9aa37c140
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:12:05 +0000

    Add package fireboard-cloud-api-client/0.0.1/fireboard_cloud_api_client-0.0.1-py3-none-any.whl

---
+++ packages/fireboard-cloud-api-client/fireboard_cloud_api_client-0.0.1-py3-none-any.whl/fireboard_cloud_api_client/__init__.py
@@ -0,0 +1,8 @@
+from fireboard_cloud_api_client.api import (
+    FireboardAPI,
+    FireboardApiError,
+    FireboardApiAuthError,
+    FireboardApiClientError,
+    FireboardApiServerError,
+    FireboardApiConnectionError,
+)
