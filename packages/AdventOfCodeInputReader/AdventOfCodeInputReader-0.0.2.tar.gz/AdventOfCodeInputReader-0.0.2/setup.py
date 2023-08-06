adadc07d3c657c2dc1c80784bb918d5a93e9a65c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:16:05 +0000

    Add package AdventOfCodeInputReader/0.0.2/AdventOfCodeInputReader-0.0.2.tar.gz

---
+++ packages/AdventOfCodeInputReader/AdventOfCodeInputReader-0.0.2.tar.gz/AdventOfCodeInputReader-0.0.2/setup.py
@@ -0,0 +1,27 @@
+from distutils.core import setup
+setup(
+  name = 'AdventOfCodeInputReader',         
+  packages = ['AdventOfCodeInputReader'],   
+  version = '0.0.2',      
+  license='MIT',        
+  description = 'Package to retrieve input for Advent Of Code',   
+  author = 'Zeph Ng',                   
+  author_email = 'zephngdev@gmail.com',      
+  url = 'https://github.com/zeph1997/AdventOfCodeInputReader',   
+  download_url = 'https://github.com/zeph1997/AdventOfCodeInputReader/archive/refs/tags/v0.0.2.tar.gz',    
+  keywords = ['reader', 'advent', 'of', 'code', 'api'],   
+  install_requires=['requests'],
+  classifiers=[
+    'Development Status :: 3 - Alpha',      
+    'Intended Audience :: Developers',      
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',   
+    'Programming Language :: Python :: 3',      
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: 3.9',
+  ],
+)
