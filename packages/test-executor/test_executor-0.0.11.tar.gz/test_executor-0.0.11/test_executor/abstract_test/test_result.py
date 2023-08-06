6ad93683d6c342f2d80bb107478ab19e01c29d78
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:00:45 +0000

    Add package test-executor/0.0.11/test_executor-0.0.11.tar.gz

---
+++ packages/test-executor/test_executor-0.0.11.tar.gz/test_executor-0.0.11/test_executor/abstract_test/test_result.py
@@ -0,0 +1,27 @@
+from enum import Enum
+
+from typing import List
+
+
+class TestVerdict(Enum):
+    """
+    Enum for test verdicts
+    """
+    PASSED = "Passed"
+    FAILED = "Failed"
+    ABORTED = "Aborted"
+
+
+class TestResult(object):
+    """
+    This class is a class data that contains data about a test
+    """
+    verdict: TestVerdict
+    failure_reasons: List[str]
+
+    def __init__(self, test_name: str, test_number: int):
+        self.test_name = test_name
+        self.verdict = TestVerdict.PASSED
+        self.failure_reasons = []
+        self.test_log = ""
+        self.test_number = test_number
