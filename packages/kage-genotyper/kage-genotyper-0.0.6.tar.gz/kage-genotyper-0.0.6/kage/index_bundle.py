fb4af7f79476698c07f9c8be708671c63f3c07ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:28:44 +0000

    Add package kage-genotyper/0.0.6/kage-genotyper-0.0.6.tar.gz

---
+++ packages/kage-genotyper/kage-genotyper-0.0.6.tar.gz/kage-genotyper-0.0.6/kage/index_bundle.py
@@ -0,0 +1,9 @@
+
+
+
+# Wrapper for all indexes required by kage genotyping
+class IndexBundle:
+    indexes = ["VariantToNodes", "NumpyVariants", "NodeCountModelAdvanced", "Help"]
+    def __init__(self, variant_to_nodes, variants, model, helper_variants, helper_variants_combo_matrix, tricky_variants):
+        pass
+
