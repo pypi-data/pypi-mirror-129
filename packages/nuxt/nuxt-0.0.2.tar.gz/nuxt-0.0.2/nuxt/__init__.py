2076af182593f76b1a0a83235fb5ad25ba53618d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:38:12 +0000

    Add package nuxt/0.0.2/nuxt-0.0.2.tar.gz

---
+++ packages/nuxt/nuxt-0.0.2.tar.gz/nuxt-0.0.2/nuxt/__init__.py
@@ -0,0 +1,6 @@
+from madara.blueprints import Blueprint
+from madara.wrappers import Request, Response
+from .wsgi_app import app as __app
+
+route = __app.route
+register_blueprint = __app.register_blueprint
