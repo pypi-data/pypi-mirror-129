ca415888bdc9e232d636c4de0969026267b60550
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:03 +0000

    Add package tic-tac-toe-alex-p1/0.1.1/tic_tac_toe_alex_p1-0.1.1-py3-none-any.whl

---
+++ packages/tic-tac-toe-alex-p1/tic_tac_toe_alex_p1-0.1.1-py3-none-any.whl/logic/main.py
@@ -0,0 +1,11 @@
+# from ttt_text_ui import TicTacToeTextUI
+from src.logic import TicTacToeTextUI
+
+
+def main():
+    game = TicTacToeTextUI()
+    game.start()
+
+
+if __name__ == "__main__":
+    main()
