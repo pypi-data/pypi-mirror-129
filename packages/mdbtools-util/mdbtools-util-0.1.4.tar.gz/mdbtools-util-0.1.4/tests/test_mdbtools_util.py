9d25a7c24e70bf805b33eada763150ab36fe826e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:24 +0000

    Add package mdbtools-util/0.1.4/mdbtools-util-0.1.4.tar.gz

---
+++ packages/mdbtools-util/mdbtools-util-0.1.4.tar.gz/mdbtools-util-0.1.4/tests/test_mdbtools_util.py
@@ -0,0 +1,22 @@
+from mdbtools_util import fix_column_name
+from mdbtools_util import extract_mdb_table
+from mdbtools_util import fix_mdb_column_definition
+from mdbtools_util import get_mdb_column_definition
+
+
+def test_fix_column_name():
+    pass
+
+
+def test_extract_mdb_table():
+    pass
+
+
+def test_fix_mdb_column_definition():
+    pass
+
+
+def test_get_mdb_column_definition():
+    pass
+
+
