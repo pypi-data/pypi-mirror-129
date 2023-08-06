88a73077e2f2454b2b44e4e4e293c262d158f380
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:22:20 +0000

    Add package urwid-satext/0.8.0/urwid_satext-0.8.0-py3-none-any.whl

---
+++ packages/urwid-satext/urwid_satext-0.8.0-py3-none-any.whl/urwid_satext/__init__.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+# Urwid SàT extensions
+# Copyright (C) 2009-2016 Jérôme Poisson (goffi@goffi.org)
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import gettext
+gettext.install('urwid_satext')
+
+
+__version__ = '0.8.0'
