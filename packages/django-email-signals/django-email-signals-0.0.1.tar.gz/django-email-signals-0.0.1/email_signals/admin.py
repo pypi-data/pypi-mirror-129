d591e3ab6943240be8217affaae1b926531b6770
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:25:58 +0000

    Add package django-email-signals/0.0.1/django-email-signals-0.0.1.tar.gz

---
+++ packages/django-email-signals/django-email-signals-0.0.1.tar.gz/django-email-signals-0.0.1/email_signals/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from . import models
+
+
+class SignalConstraintInline(admin.TabularInline):
+    model = models.SignalConstraint
+    extra = 0
+
+
+@admin.register(models.Signal)
+class SignalAdmin(admin.ModelAdmin):
+    list_display = ('name', 'content_type', 'signal_type', 'active')
+
+    inlines = [SignalConstraintInline]
