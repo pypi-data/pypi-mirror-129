bc310a7a89a6ad7600a5374730d84420b1541645
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:32:00 +0000

    Add package ast-magic/0.0.2/ast-magic-0.0.2.tar.gz

---
+++ packages/ast-magic/ast-magic-0.0.2.tar.gz/ast-magic-0.0.2/ast_magic/__init__.py
@@ -0,0 +1,17 @@
+from ast import dump as dump_ast, parse as ast_parse  # Avoid conflict w/ magic
+from typing import Optional
+
+from IPython.core.magic import register_line_cell_magic
+
+
+def print_ast(source: str) -> None:
+    print(dump_ast(ast_parse(source), indent=4))
+
+
+def load_ipython_extension(_ipython):
+    @register_line_cell_magic
+    def ast(line: Optional[str], cell: Optional[str] = None) -> None:
+        if cell is not None:
+            print_ast(cell)
+        else:
+            print_ast(line)
