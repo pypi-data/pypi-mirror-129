aebdb7333f683692119e93bf14145528b86b4660
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:46:39 +0000

    Add package rs4/0.3.2.3/rs4-0.3.2.3-py3-none-any.whl

---
+++ packages/rs4/rs4-0.3.2.3-py3-none-any.whl/rs4/apis/aws/s3.py
@@ -0,0 +1,31 @@
+import boto3
+from urllib import parse
+from rs4 import pathtool
+import os
+
+S3 = boto3.resource ('s3')
+def upload (source, target, acl = None):
+    parts = parse.urlparse (target) # 's3://roadkore/weights/adsa.h5'
+    assert parts.scheme == 's3'
+    bucket_name = parts.netloc
+    bucket = S3.Bucket(name = bucket_name)
+    extra_args = {}
+    if acl == 'public':
+        extra_args ['ACL'] = 'public-read'
+    bucket.upload_file (source, parts.path [1:], ExtraArgs = extra_args)
+
+def download (source, target):
+    parts = parse.urlparse (source) # 's3://roadkore/weights/adsa.h5'
+    assert parts.scheme == 's3'
+    bucket_name = parts.netloc
+    bucket = S3.Bucket(name = bucket_name)
+    pathtool.mkdir (os.path.dirname (target))
+    bucket.download_file (parts.path [1:], target)
+
+def remove (target):
+    parts = parse.urlparse (target) # 's3://roadkore/weights/adsa.h5'
+    assert parts.scheme == 's3'
+    bucket_name = parts.netloc
+    key = parts.path [1:]
+    S3.Object (bucket_name, key).delete ()
+delete = remove
