cff843cd81a37abbe715aa4e59b14e7c8f2342dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:29:51 +0000

    Add package axoproj/0.0.5/axoproj-0.0.5.tar.gz

---
+++ packages/axoproj/axoproj-0.0.5.tar.gz/axoproj-0.0.5/axoproj/__init__.py
@@ -0,0 +1,10 @@
+from axoproj.classes import BS_model
+from axoproj.classes import Disk_model
+from axoproj.classes import DW_model
+from axoproj.classes import Infall_model
+from axoproj.classes import WDS_model
+from axoproj.classes import Your_model
+from axoproj.funcs import create_datacube
+from axoproj.funcs import i_near
+
+__version__ = "0.0.5"
