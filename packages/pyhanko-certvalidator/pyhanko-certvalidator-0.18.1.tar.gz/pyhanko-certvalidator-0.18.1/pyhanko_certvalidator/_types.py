f5c3c909c931136cba53fef2399eac76aea1ac6a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:20:42 +0000

    Add package pyhanko-certvalidator/0.18.1/pyhanko-certvalidator-0.18.1.tar.gz

---
+++ packages/pyhanko-certvalidator/pyhanko-certvalidator-0.18.1.tar.gz/pyhanko-certvalidator-0.18.1/pyhanko_certvalidator/_types.py
@@ -0,0 +1,33 @@
+# coding: utf-8
+
+import inspect
+import sys
+
+
+if sys.version_info < (3,):
+    str_cls = unicode  # noqa
+    byte_cls = str
+
+else:
+    str_cls = str
+    byte_cls = bytes
+
+
+def type_name(value):
+    """
+    Returns a user-readable name for the type of an object
+
+    :param value:
+        A value to get the type name of
+
+    :return:
+        A unicode string of the object's type name
+    """
+
+    if inspect.isclass(value):
+        cls = value
+    else:
+        cls = value.__class__
+    if cls.__module__ in set(['builtins', '__builtin__']):
+        return cls.__name__
+    return '%s.%s' % (cls.__module__, cls.__name__)
