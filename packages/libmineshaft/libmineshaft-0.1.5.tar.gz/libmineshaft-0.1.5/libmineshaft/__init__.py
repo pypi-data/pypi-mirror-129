7a5c758a01a91e6d5cf84b11a51ef0497ad69804
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:31:40 +0000

    Add package libmineshaft/0.1.5/libmineshaft-0.1.5.tar.gz

---
+++ packages/libmineshaft/libmineshaft-0.1.5.tar.gz/libmineshaft-0.1.5/libmineshaft/__init__.py
@@ -0,0 +1,12 @@
+import os
+
+os.environ["PYGAME_HIDE_SUPPORT_PROMPT"] = "1"
+import pygame
+
+
+__version__ = "0.1.5"
+__author__ = "Double Fractal Game Studios"
+__credits__ = """Double Fractal Game Studios team:
+Mayu Sakurai
+Alexey Pavlov
+"""
