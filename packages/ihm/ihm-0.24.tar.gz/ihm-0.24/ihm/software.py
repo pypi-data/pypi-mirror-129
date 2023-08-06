862d2e68a86d2c4507e0c056f6def03b0d490027
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:16:47 +0000

    Add package ihm/0.24/ihm-0.24.tar.gz

---
+++ packages/ihm/ihm-0.24.tar.gz/ihm-0.24/ihm/software.py
@@ -0,0 +1,16 @@
+modeller
+cccp  https://arteni.cs.dartmouth.edu/cccp/index.gen.php
+IMP
+ATSAS https://www.embl-hamburg.de/biosaxs/software.html
+SAXSMoW https://saxs.ifsc.usp.br/
+HHpred https://toolkit.tuebingen.mpg.de/hhpred
+PSIPRED http://bioinf.cs.ucl.ac.uk/psipred/
+DISOPRED http://bioinf.cs.ucl.ac.uk/psipred/?disopred=1
+AllosMod https://salilab.org/allosmod
+DomPred http://bioinf.cs.ucl.ac.uk/dompred
+COILS/PCOILS https://toolkit.tuebingen.mpg.de/#/tools/pcoils
+EMAN2 http://blake.bcm.edu/emanwiki/EMAN2
+RELION https://www2.mrc-lmb.cam.ac.uk/relion/
+SGD https://www.yeastgenome.org/
+HeliQuest http://heliquest.ipmc.cnrs.fr/
+Multicoil2 http://groups.csail.mit.edu/cb/multicoil2/cgi-bin/multicoil2.cgi
