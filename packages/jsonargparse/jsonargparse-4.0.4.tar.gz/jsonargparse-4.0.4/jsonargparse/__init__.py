99ba4d749007224e3264f3ea81804fd3082fc904
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:41:22 +0000

    Add package jsonargparse/4.0.4/jsonargparse-4.0.4.tar.gz

---
+++ packages/jsonargparse/jsonargparse-4.0.4.tar.gz/jsonargparse-4.0.4/jsonargparse/__init__.py
@@ -0,0 +1,49 @@
+from argparse import (
+    OPTIONAL,
+    REMAINDER,
+    SUPPRESS,
+    PARSER,
+    ONE_OR_MORE,
+    ZERO_OR_MORE,
+)
+from .actions import *
+from .cli import *
+from .core import *
+from .deprecated import *
+from .formatters import *
+from .jsonnet import *
+from .jsonschema import *
+from .namespace import *
+from .optionals import *
+from .signatures import *
+from .typehints import *
+from .util import *
+
+
+_all_ = [
+    'OPTIONAL',
+    'REMAINDER',
+    'SUPPRESS',
+    'PARSER',
+    'ONE_OR_MORE',
+    'ZERO_OR_MORE',
+]
+
+for module in ['cli',
+               'core',
+               'signatures',
+               'typehints',
+               'jsonschema',
+               'jsonnet',
+               'actions',
+               'namespace',
+               'formatters',
+               'optionals',
+               'util',
+               'deprecated']:
+    _all_.extend(getattr(__import__('jsonargparse.'+module, fromlist=['__all__']), '__all__'))
+
+locals()['__all__'] = _all_   # Workaround because mypy does not handle dynamic __all__
+
+
+__version__ = '4.0.4'
