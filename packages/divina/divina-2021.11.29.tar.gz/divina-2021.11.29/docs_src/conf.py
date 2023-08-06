e4392c8b8b04870c6760d153045f9eefdd4a6c50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:29 +0000

    Add package divina/2021.11.29/divina-2021.11.29.tar.gz

---
+++ packages/divina/divina-2021.11.29.tar.gz/divina-2021.11.29/docs_src/conf.py
@@ -0,0 +1,20 @@
+import pathlib
+import sys
+
+sys.path.insert(0, pathlib.Path(__file__).parent.parent)
+
+extensions = ["sphinx_click", "sphinx.ext.autodoc", "sphinx-jsonschema"]
+
+project = "divina"
+
+autodoc_member_order = "bysource"
+
+html_static_path = ["_static"]
+
+html_css_files = [
+    "css/docs.css",
+]
+
+html_js_files = [
+    "js/docs.js",
+]
