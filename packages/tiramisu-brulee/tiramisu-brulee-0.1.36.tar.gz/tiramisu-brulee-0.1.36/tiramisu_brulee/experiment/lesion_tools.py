4f7dfe2e0509c5a19f72353e716b40e1a313f273
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:14:00 +0000

    Add package tiramisu-brulee/0.1.36/tiramisu-brulee-0.1.36.tar.gz

---
+++ packages/tiramisu-brulee/tiramisu-brulee-0.1.36.tar.gz/tiramisu-brulee-0.1.36/tiramisu_brulee/experiment/lesion_tools.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+tiramisu_brulee.experiment.lesion_tools
+
+functions specific to handling/processing lesion segmentations
+
+Author: Jacob Reinhold (jcreinhold)
+Created on: May 16, 2021
+"""
+
+__all__ = [
+    "almost_isbi15_score",
+    "clean_segmentation",
+]
+
+from typing import Tuple, Union
+
+import numpy as np
+from scipy.ndimage.morphology import binary_fill_holes, generate_binary_structure
+from skimage.morphology import remove_small_objects
+from torch import Tensor
+from torchmetrics.functional import dice_score, pearson_corrcoef, precision
+
+from tiramisu_brulee.experiment.util import image_one_hot
+
+
+def clean_segmentation(
+    label: np.ndarray,
+    *,
+    fill_holes: bool = True,
+    minimum_lesion_size: int = 3,
+) -> np.ndarray:
+    """clean binary array by removing small objs & filling holes"""
+    d = label.ndim
+    if fill_holes:
+        structure = generate_binary_structure(d, d)
+        label = binary_fill_holes(label, structure=structure)
+    if minimum_lesion_size > 0:
+        label = remove_small_objects(
+            label,
+            min_size=minimum_lesion_size,
+            connectivity=d,
+        )
+    return label
+
+
+def almost_isbi15_score(
+    pred: Tensor,
+    target: Tensor,
+    *,
+    return_dice_ppv: bool = False,
+) -> Union[Tensor, Tuple[Tensor, Tensor, Tensor]]:
+    """ISBI 15 MS challenge score excluding the LTPR & LFPR components"""
+    batch_size, num_classes = pred.shape[0:2]
+    multiclass = num_classes > 1
+    one_hot_classes = num_classes if multiclass else 2
+    pred_one_hot = (
+        pred if multiclass else image_one_hot(pred, num_classes=one_hot_classes)
+    )
+    dice = dice_score(pred_one_hot, target.int())
+    if multiclass and pred.shape != target.shape:
+        is_integer_label = pred.ndim != target.ndim
+        if is_integer_label:
+            target.unsqueeze_(1)
+            assert pred.ndim == target.ndim
+        target = image_one_hot(target.long(), num_classes=num_classes)
+    ppv = precision(
+        pred.int(),
+        target.int(),
+        num_classes=num_classes if multiclass else None,
+        mdmc_average="samplewise",
+        multiclass=multiclass or None,
+    )
+    isbi15_score = 0.5 * dice + 0.5 * ppv
+    if batch_size > 1 and not multiclass:
+        dims = list(range(1, pred.ndim))
+        corr = pearson_corrcoef(
+            pred.sum(dim=dims).float(),
+            target.sum(dim=dims).float(),
+        )
+        isbi15_score = 0.5 * isbi15_score + 0.5 * corr
+    if return_dice_ppv:
+        return isbi15_score, dice, ppv
+    else:
+        return isbi15_score
---
+++ packages/tiramisu-brulee/tiramisu-brulee-0.1.36.tar.gz/tiramisu-brulee-0.1.36/tiramisu_brulee/experiment/type.py
@@ -0,0 +1,199 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+tiramisu_brulee.experiment.type
+
+experiment-specific types
+
+Author: Jacob Reinhold (jcreinhold@gmail.com)
+Created on: May 28, 2021
+"""
+
+__all__ = [
+    "ArgParser",
+    "ArgType",
+    "Batch",
+    "BatchElement",
+    "file_path",
+    "Indices",
+    "ModelNum",
+    "Namespace",
+    "new_parse_type",
+    "nonnegative_float",
+    "nonnegative_int",
+    "nonnegative_int_or_none_or_all",
+    "PatchShapeOption",
+    "PatchShape",
+    "positive_float",
+    "positive_float_or_none",
+    "positive_int",
+    "positive_int_or_none",
+    "positive_odd_int_or_none",
+    "probability_float",
+    "probability_float_or_none",
+    "TiramisuBruleeInfo",
+]
+
+import argparse
+from collections import namedtuple
+from pathlib import Path
+from typing import Any, Callable, Dict, Iterable, List, Optional, Tuple, Union
+
+import jsonargparse
+from torch import Tensor
+
+BatchElement = Union[Tensor, Dict[str, Any], List[Any]]
+Batch = Dict[str, BatchElement]
+Indices = Tuple[int, int, int, int, int, int]
+ModelNum = namedtuple("ModelNum", ["num", "out_of"])
+Namespace = Union[argparse.Namespace, jsonargparse.Namespace]
+PatchShape2D = Tuple[int, int]
+PatchShape3D = Tuple[int, int, int]
+PatchShape = Union[PatchShape2D, PatchShape3D]
+PatchShape2DOption = Tuple[Optional[int], Optional[int]]
+PatchShape3DOption = Tuple[Optional[int], Optional[int], Optional[int]]
+PatchShapeOption = Union[PatchShape2DOption, PatchShape3DOption]
+ArgType = Optional[Union[Namespace, Iterable[str]]]
+ArgParser = Union[argparse.ArgumentParser, jsonargparse.ArgumentParser]
+TiramisuBruleeInfo = namedtuple("TiramisuBruleeInfo", ["version", "commit"])
+
+
+def return_none(func: Callable) -> Callable:
+    def new_func(self, string: Any) -> Any:  # type: ignore[no-untyped-def]
+        if string is None:
+            return None
+        elif isinstance(string, str):
+            if string.lower() in ("none", "null"):
+                return None
+        return func(self, string)
+
+    return new_func
+
+
+def return_str(match_string: str) -> Callable:
+    def decorator(func: Callable) -> Callable:
+        def new_func(self, string: Any) -> Any:  # type: ignore[no-untyped-def]
+            if isinstance(string, str):
+                if string.lower() == match_string:
+                    return match_string
+            return func(self, string)
+
+        return new_func
+
+    return decorator
+
+
+class _ParseType:
+    @property
+    def __name__(self) -> str:
+        name = self.__class__.__name__
+        assert isinstance(name, str)
+        return name
+
+    def __str__(self) -> str:
+        return self.__name__
+
+
+class file_path(_ParseType):
+    def __call__(self, string: str) -> str:
+        path = Path(string)
+        if not path.is_file():
+            msg = f"{string} is not a valid path."
+            raise argparse.ArgumentTypeError(msg)
+        return str(path.resolve())
+
+
+class positive_float(_ParseType):
+    def __call__(self, string: str) -> float:
+        num = float(string)
+        if num <= 0.0:
+            msg = f"{string} needs to be a positive float."
+            raise argparse.ArgumentTypeError(msg)
+        return num
+
+
+class positive_float_or_none(_ParseType):
+    @return_none
+    def __call__(self, string: str) -> Union[float, None]:
+        return positive_float()(string)
+
+
+class positive_int(_ParseType):
+    def __call__(self, string: str) -> int:
+        num = int(string)
+        if num <= 0:
+            msg = f"{string} needs to be a positive integer."
+            raise argparse.ArgumentTypeError(msg)
+        return num
+
+
+class positive_odd_int_or_none(_ParseType):
+    @return_none
+    def __call__(self, string: str) -> Union[int, None]:
+        num = int(string)
+        if num <= 0 or not (num % 2):
+            msg = f"{string} needs to be a positive odd integer."
+            raise argparse.ArgumentTypeError(msg)
+        return num
+
+
+class positive_int_or_none(_ParseType):
+    @return_none
+    def __call__(self, string: str) -> Union[int, None]:
+        return positive_int()(string)
+
+
+class nonnegative_int(_ParseType):
+    def __call__(self, string: str) -> int:
+        num = int(string)
+        if num < 0:
+            msg = f"{string} needs to be a nonnegative integer."
+            raise argparse.ArgumentTypeError(msg)
+        return num
+
+
+class nonnegative_int_or_none_or_all(_ParseType):
+    @return_none
+    @return_str("all")
+    def __call__(self, string: str) -> Union[int, None, str]:
+        return nonnegative_int()(string)
+
+
+class nonnegative_float(_ParseType):
+    def __call__(self, string: str) -> float:
+        num = float(string)
+        if num < 0.0:
+            msg = f"{string} needs to be a nonnegative float."
+            raise argparse.ArgumentTypeError(msg)
+        return num
+
+
+class probability_float(_ParseType):
+    def __call__(self, string: str) -> float:
+        num = float(string)
+        if num < 0.0 or num > 1.0:
+            msg = f"{string} needs to be between 0 and 1."
+            raise argparse.ArgumentTypeError(msg)
+        return num
+
+
+class probability_float_or_none(_ParseType):
+    @return_none
+    def __call__(self, string: str) -> Union[float, None]:
+        return probability_float()(string)
+
+
+class NewParseType:
+    def __init__(self, func: Callable, name: str):
+        self.name = name
+        self.func = func
+
+    def __str__(self) -> str:
+        return self.name
+
+    def __call__(self, val: Any) -> Any:
+        return self.func(val)
+
+
+def new_parse_type(func: Callable, name: str) -> NewParseType:
+    return NewParseType(func, name)
