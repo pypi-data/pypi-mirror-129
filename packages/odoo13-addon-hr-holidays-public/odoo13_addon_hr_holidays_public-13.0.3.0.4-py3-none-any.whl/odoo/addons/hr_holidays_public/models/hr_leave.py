4cde03ef64019f3034f877d97da9dc21aba30ab6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:47:32 +0000

    Add package odoo13-addon-hr-holidays-public/13.0.3.0.4/odoo13_addon_hr_holidays_public-13.0.3.0.4-py3-none-any.whl

---
+++ packages/odoo13-addon-hr-holidays-public/odoo13_addon_hr_holidays_public-13.0.3.0.4-py3-none-any.whl/odoo/addons/hr_holidays_public/models/hr_leave.py
@@ -0,0 +1,30 @@
+# Copyright 2017-2018 Tecnativa - Pedro M. Baeza
+# Copyright 2018 Brainbean Apps
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+from odoo import api, models
+
+
+class HrLeave(models.Model):
+    _inherit = "hr.leave"
+
+    def _get_number_of_days(self, date_from, date_to, employee_id):
+        if self.holiday_status_id.exclude_public_holidays or not self.holiday_status_id:
+            instance = self.with_context(
+                employee_id=employee_id, exclude_public_holidays=True
+            )
+        else:
+            instance = self
+        return super(HrLeave, instance)._get_number_of_days(
+            date_from, date_to, employee_id
+        )
+
+    @api.depends("number_of_days")
+    def _compute_number_of_hours_display(self):
+        if self.holiday_status_id.exclude_public_holidays or not self.holiday_status_id:
+            instance = self.with_context(
+                exclude_public_holidays=True, employee_id=self.employee_id.id
+            )
+        else:
+            instance = self
+        return super(HrLeave, instance)._compute_number_of_hours_display()
