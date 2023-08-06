4fcd8f149040fa4f8e9c95f3bd236b113c5a688e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:21:39 +0000

    Add package ledgereth/0.2.0/ledgereth-0.2.0-py3-none-any.whl

---
+++ packages/ledgereth/ledgereth-0.2.0-py3-none-any.whl/ledgereth/transactions.py
@@ -0,0 +1,121 @@
+import binascii
+from typing import Any, Union
+
+from eth_utils import decode_hex
+from rlp import Serializable, encode
+
+from ledgereth.comms import chunks, dongle_send_data, init_dongle
+from ledgereth.constants import DEFAULT_CHAIN_ID, DEFAULT_PATH_STRING
+from ledgereth.objects import SignedTransaction, Transaction
+from ledgereth.utils import is_bip32_path, is_hex_string, parse_bip32_path
+
+Text = Union[str, bytes]
+
+
+def sign_transaction(
+    tx: Serializable, sender_path: str = DEFAULT_PATH_STRING, dongle: Any = None
+) -> SignedTransaction:
+    """Sign a transaction object (rlp.Serializable)"""
+    given_dongle = dongle is not None
+    dongle = init_dongle(dongle)
+    retval = None
+
+    assert isinstance(
+        tx, Transaction
+    ), "Only Transaction objects are currently supported"
+    print("tx: ", tx)
+    encoded_tx = encode(tx, Transaction)
+
+    if not is_bip32_path(sender_path):
+        raise ValueError("Invalid sender BIP32 path given to sign_transaction")
+
+    path = parse_bip32_path(sender_path)
+    payload = (len(path) // 4).to_bytes(1, "big") + path + encoded_tx
+
+    chunk_count = 0
+    for chunk in chunks(payload):
+        chunk_size = len(chunk)
+        if chunk_count == 0:
+            retval = dongle_send_data(
+                dongle,
+                "SIGN_TX_FIRST_DATA",
+                chunk,
+                Lc=chunk_size.to_bytes(1, "big"),
+            )
+        else:
+            retval = dongle_send_data(
+                dongle,
+                "SIGN_TX_SECONDARY_DATA",
+                chunk,
+                Lc=chunk_size.to_bytes(1, "big"),
+            )
+        chunk_count += 1
+
+    if retval is None or len(retval) < 64:
+        raise Exception("Invalid response from Ledger")
+
+    chain_id = tx["chainid"] or DEFAULT_CHAIN_ID
+    if (chain_id * 2 + 35) + 1 > 255:
+        ecc_parity = retval[0] - ((chain_id * 2 + 35) % 256)
+        v = (chain_id * 2 + 35) + ecc_parity
+    else:
+        v = retval[0]
+
+    r = int(binascii.hexlify(retval[1:33]), 16)
+    s = int(binascii.hexlify(retval[33:65]), 16)
+
+    signed = SignedTransaction(
+        nonce=tx.nonce,
+        gasprice=tx.gasprice,
+        startgas=tx.startgas,
+        to=tx.to,
+        value=tx.value,
+        data=tx.data,
+        v=v,
+        r=r,
+        s=s,
+    )
+
+    # If this func inited the dongle, it close it, otherwise core dump
+    if not given_dongle:
+        dongle.close()
+
+    return signed
+
+
+def create_transaction(
+    to: bytes,
+    value: int,
+    gas: int,
+    gas_price: int,
+    nonce: int,
+    data: Text,
+    chain_id: int = DEFAULT_CHAIN_ID,
+    sender_path: str = DEFAULT_PATH_STRING,
+    dongle: Any = None,
+) -> SignedTransaction:
+    """Create and sign a transaction from indiv args"""
+    given_dongle = dongle is not None
+    dongle = init_dongle(dongle)
+
+    if not is_hex_string(data):
+        data = decode_hex(data) or b""
+
+    # Create a serializable tx object
+    tx = Transaction(
+        to=decode_hex(to),
+        value=value,
+        startgas=gas,
+        gasprice=gas_price,
+        data=data,
+        nonce=nonce,
+        chainid=chain_id,
+    )
+
+    signed = sign_transaction(tx, sender_path, dongle=dongle)
+
+    # If this func inited the dongle, it close it, otherwise core dump
+    if not given_dongle:
+        dongle.close()
+
+    return signed
