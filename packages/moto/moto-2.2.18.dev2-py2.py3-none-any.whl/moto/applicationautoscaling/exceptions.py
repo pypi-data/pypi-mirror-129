8a1d0981f94ad3f37b0149225654dcd44613ffc4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:11:23 +0000

    Add package moto/2.2.18.dev2/moto-2.2.18.dev2-py2.py3-none-any.whl

---
+++ packages/moto/moto-2.2.18.dev2-py2.py3-none-any.whl/moto/applicationautoscaling/exceptions.py
@@ -0,0 +1,8 @@
+from moto.core.exceptions import JsonRESTError
+
+
+class AWSValidationException(JsonRESTError):
+    def __init__(self, message, **kwargs):
+        super(AWSValidationException, self).__init__(
+            "ValidationException", message, **kwargs
+        )
