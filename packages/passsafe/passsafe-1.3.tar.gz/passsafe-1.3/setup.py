b5de4bd80af564f0e6a2ced7c5a0eddc819855fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:39:45 +0000

    Add package passsafe/1.3/passsafe-1.3.tar.gz

---
+++ packages/passsafe/passsafe-1.3.tar.gz/passsafe-1.3/setup.py
@@ -0,0 +1,12 @@
+import setuptools
+setuptools.setup(name='passsafe',
+version='1.3',
+author='Advay',
+author_email='catch149@gmail.com',
+description='For making passwords safe and memorable! The (0.4 version is actually 0.3 version so, do not get confused)!',
+packages=setuptools.find_packages(),
+classifiers=[
+    "Programming Language :: Python :: 3",
+    "License :: OSI Approved :: MIT License",
+    "Operating System :: OS Independent",
+])
