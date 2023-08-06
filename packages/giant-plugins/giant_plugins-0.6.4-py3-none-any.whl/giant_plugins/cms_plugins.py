c24cead31b37b1eab01ed97408599021654bd74d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:49:40 +0000

    Add package giant-plugins/0.6.4/giant_plugins-0.6.4-py3-none-any.whl

---
+++ packages/giant-plugins/giant_plugins-0.6.4-py3-none-any.whl/giant_plugins/cms_plugins.py
@@ -0,0 +1,21 @@
+from django.utils.module_loading import import_string
+from cms.exceptions import PluginAlreadyRegistered
+
+import_list = [
+    "content_width_video",
+    "content_width_image",
+    "page_card",
+    "pullquote",
+    "rich_text",
+    "hero_image",
+    "donate",
+    "featured_cta",
+    "logo_grid",
+    "share_this_page",
+]
+
+for plugin in import_list:
+    try:
+        import_string(f"giant_plugins.{plugin}.cms_plugins.__all__")
+    except PluginAlreadyRegistered:
+        pass
---
+++ packages/giant-plugins/giant_plugins-0.6.4-py3-none-any.whl/giant_plugins/models.py
@@ -0,0 +1,11 @@
+# flake8: noqa
+from .content_width_image.models import *
+from .content_width_video.models import *
+from .page_card.models import *
+from .pullquote.models import *
+from .rich_text.models import *
+from .hero_image.models import *
+from .donate.models import *
+from .logo_grid.models import *
+from .featured_cta.models import *
+from .share_this_page.models import *
