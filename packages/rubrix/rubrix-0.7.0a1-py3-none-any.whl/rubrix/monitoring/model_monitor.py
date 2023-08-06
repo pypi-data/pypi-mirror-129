e3731310c1b610f45bba68d9937af2283162907a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:12:13 +0000

    Add package rubrix/0.7.0a1/rubrix-0.7.0a1-py3-none-any.whl

---
+++ packages/rubrix/rubrix-0.7.0a1-py3-none-any.whl/rubrix/monitoring/model_monitor.py
@@ -0,0 +1,28 @@
+import logging
+import warnings
+from typing import Any
+
+from ._spacy import Language, ner_monitor
+from ._transformers import Pipeline, huggingface_monitor
+
+_LOGGER = logging.getLogger(__name__)
+
+
+def monitor(task_model: Any, dataset: str, sample_rate: float = 0.3):
+    model_monitor = None
+    if isinstance(task_model, Language):
+        model_monitor = ner_monitor(
+            task_model, dataset=dataset, sample_rate=sample_rate
+        )
+    elif isinstance(task_model, Pipeline):
+        model_monitor = huggingface_monitor(
+            task_model, dataset=dataset, sample_rate=sample_rate
+        )
+    if model_monitor:
+        return model_monitor
+
+    warnings.warn(
+        "The provided task model is not supported by monitoring module. "
+        "Predictions won't be logged into rubrix"
+    )
+    return task_model
