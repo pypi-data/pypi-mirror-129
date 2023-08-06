1a772087ab05e6cd3ad9236c35895371106ba07a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:27 +0000

    Add package linpg/3.1.4/linpg-3.1.4-py3-none-any.whl

---
+++ packages/linpg/linpg-3.1.4-py3-none-any.whl/linpg/__init__.py
@@ -0,0 +1,9 @@
+"""
+结构:
+exception -> config -> lang -> api -> core -> ui -> dialog -> battle -> interface
+"""
+from .interface import *
+from platform import python_version
+
+print("linpg {0} ({1}, Python {2})".format(Info.current_version, get_library_info(), python_version()))
+print("Hello from the linpg community. {}".format(Info.repository_url))
