204d12e504689bec3d2b5d28649f89e32a1f594e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:22:09 +0000

    Add package guide-bot/0.0.12/guide_bot-0.0.12.tar.gz

---
+++ packages/guide-bot/guide_bot-0.0.12.tar.gz/guide_bot-0.0.12/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+from _version import __version__
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+print(__version__)
+
+setup(
+    name='guide_bot',
+    version=__version__,
+    author="Mads Bertelsen",
+    author_email="Mads.Bertelsen@ess.eu",
+    description="Neutron guide optimization package",
+    include_package_data=True,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://git.esss.dk/highness/guide_bot",
+    install_requires=["pyswarm", "dill", "numpy", "matplotlib", "PyYAML", "mcstasscript", "ipywidgets", "ipympl", "scipy"],
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Topic :: Scientific/Engineering"
+
+    ])
