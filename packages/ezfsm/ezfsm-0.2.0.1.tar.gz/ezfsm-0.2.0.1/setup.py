e522e36e7ec979c9c2782c1f56d437a160360a0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:58:34 +0000

    Add package ezfsm/0.2.0.1/ezfsm-0.2.0.1.tar.gz

---
+++ packages/ezfsm/ezfsm-0.2.0.1.tar.gz/ezfsm-0.2.0.1/setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# coding:utf-8
+
+from setuptools import find_packages, setup
+
+setup(
+name='ezfsm',
+version='0.2.0.1',
+description='python/micropython easy fsm.',
+author="Eagle'sBaby",
+author_email='2229066748@qq.com',
+maintainer="Eagle'sBaby",
+maintainer_email='2229066748@qq.com',
+packages=find_packages(),
+platforms=["all"],
+url='https://gitee.com/eagle-s_baby/fsm/tree/master',
+license='Apache Licence 2.0',
+classifiers=[
+'Programming Language :: Python',
+'Programming Language :: Python :: 3',
+],
+install_requires = ["graphviz"],
+keywords = ['fsm', 'easy'],
+python_requires='>=3', 
+)
