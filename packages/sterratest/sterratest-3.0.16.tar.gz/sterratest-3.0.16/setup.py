4e4252e8369fbf438886f8ffc1daf46fb3df9261
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:28:14 +0000

    Add package sterratest/3.0.16/sterratest-3.0.16.tar.gz

---
+++ packages/sterratest/sterratest-3.0.16.tar.gz/sterratest-3.0.16/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'sterratest',
+    version = '3.0.16',
+    description = 'OSINT tool to work with follower/following list of instagram users.',
+    long_description = 'OSINT tool to work with follower/following list of instagram users, see README of https://github.com/novitae/sterraxcyl',
+    author = 'novitae',
+    url = 'https://github.com/novitae/sterraxcyl',
+    licence = 'GNU General Public License v3 (GPLv3)',
+    classifiers = [
+        'Programming Language :: Python :: 3.9',
+    ],
+    packages = find_packages(),
+    install_requires = ['aiohttp', 'argparse', 'datetime', 'openpyxl', 'requests', 'string-color', 'tqdm'],
+    entry_points = {'console_scripts': ['sterra = sterra.core:main', 'sterraxcyl = sterra.core:main']}
+)
