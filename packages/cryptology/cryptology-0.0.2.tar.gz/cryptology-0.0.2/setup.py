61115a8063dbccc54a3e35184cedc90af6e48a18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:35 +0000

    Add package cryptology/0.0.2/cryptology-0.0.2.tar.gz

---
+++ packages/cryptology/cryptology-0.0.2.tar.gz/cryptology-0.0.2/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+with open("README.md","r") as fh:
+      long_description=fh.read()
+
+setup(name='cryptology',
+      version='0.0.2',
+      description='Decrypt/Encrypt text using various cipher techniques',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      author='Antonio Felton',
+      author_email='',
+      py_modules=['crypto'],
+      package_dir={'':'cryptology'}
+)
