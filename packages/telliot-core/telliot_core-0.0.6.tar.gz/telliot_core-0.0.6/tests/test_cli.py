42e45b20026f815bfa2007c3af1361f13152f038
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:28:20 +0000

    Add package telliot-core/0.0.6/telliot_core-0.0.6.tar.gz

---
+++ packages/telliot-core/telliot_core-0.0.6.tar.gz/telliot_core-0.0.6/tests/test_cli.py
@@ -0,0 +1,14 @@
+"""
+Unit tests covering telliot_core CLI commands.
+"""
+from click.testing import CliRunner
+
+from telliot_core.cli import main
+
+
+def test_config_cmd():
+    """Test telliot_core CLI command: report."""
+    runner = CliRunner()
+    result = runner.invoke(main, ["config", "init"])
+
+    print(result)
