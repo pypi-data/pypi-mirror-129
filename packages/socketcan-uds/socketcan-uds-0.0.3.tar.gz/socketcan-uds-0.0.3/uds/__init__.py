15b0b6af04ef9ac7bbf73b47398ca24a3c9b6a36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:49:30 +0000

    Add package socketcan-uds/0.0.3/socketcan-uds-0.0.3.tar.gz

---
+++ packages/socketcan-uds/socketcan-uds-0.0.3.tar.gz/socketcan-uds-0.0.3/uds/__init__.py
@@ -0,0 +1,7 @@
+from uds.uds import Uds, ResponseCode, NegativeResponse, UdsProtocolException, ResponseTooLong, DiagnosticSession, \
+    ServiceId, ResponseCode, ServiceNotSupported, ServiceNotSupportedInActiveSession, SubfunctionNotSupported, \
+    SubFunctionNotSupportedInActiveSession, GeneralReject, IncorrectMessageLengthOrInvalidFormat, InvalidKey, \
+    GeneralProgrammingFailure, UdsTimeoutError, RequestSequenceError, BusyRepeatRequest, RequestOutOfRange, \
+    RequestCorrectlyReceivedButResponsePending, ConditionsNotCorrect, NoResponseFromSubnetComponent, \
+    UploadDownloadNotAccepted, RequiredTimeDelayNotExpired, ExceededNumberOfAttempts, \
+    FaultPreventsExecutionOfRequestedAction, SecurityAccessDenied, WrongBlockSequenceCounter, TransferDataSuspended
