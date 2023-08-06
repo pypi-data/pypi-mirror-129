2d719d58f458eb15f5739ad8b90b5263c964b8da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:31:46 +0000

    Add package py-configuration-Orinnass/2.3.11/py-configuration-Orinnass-2.3.11.tar.gz

---
+++ packages/py-configuration-Orinnass/py-configuration-Orinnass-2.3.11.tar.gz/py-configuration-Orinnass-2.3.11/config/__init__.py
@@ -0,0 +1,10 @@
+"""
+Модуль с описанием класса Configuration
+"""
+# from .handlers import DBHandler
+from .configuration import Configuration
+
+__version__ = '2.3.11'
+__name__ = 'py-configuration-Orinnass'
+
+# TODO: изменить минимальный процент покрытия кода тестами
