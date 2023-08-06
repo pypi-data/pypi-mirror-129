4c2d59f59aea104014186f3fca17ca2709bbb53c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:29:32 +0000

    Add package image-fabio/0.0.4/image_fabio-0.0.4.tar.gz

---
+++ packages/image-fabio/image_fabio-0.0.4.tar.gz/image_fabio-0.0.4/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as f:
+    page_description = f.read()
+
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name="image_fabio",
+    version="0.0.4",
+    author="Fabio_Tolentino",
+    author_email="fabio.tolentino@uol.com.br",
+    description="Pacote para tratar imagens",
+    long_description=page_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/FabioTolentino19/image_fabio",
+    packages=find_packages(),
+    install_requires=requirements,
+    python_requires='>=3.0',
+)
