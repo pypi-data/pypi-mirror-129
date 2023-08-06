1c3a77867e5e1c13769beef68e4bca6edbdc17a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:11:29 +0000

    Add package examplepkg-andihalim/0.0.1/examplepkg-andihalim-0.0.1.tar.gz

---
+++ packages/examplepkg-andihalim/examplepkg-andihalim-0.0.1.tar.gz/examplepkg-andihalim-0.0.1/README.md
@@ -0,0 +1,3 @@
+# exception_feedback_pkg
+
+This is the package that produces a feedback form upon error in Jupyter notebooks. 
---
+++ packages/examplepkg-andihalim/examplepkg-andihalim-0.0.1.tar.gz/examplepkg-andihalim-0.0.1/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+VERSION = '0.0.1'
+DESCRIPTION = 'Error feedback form for Jupyter notebooks'
+LONG_DESCRIPTION = 'A package that creates a feedback form when a Jupyter notebook error is produced.'
+
+# Setting up
+setup(
+    name="examplepkg-andihalim",
+    version=VERSION,
+    author="Blueprint (Andi Halim)",
+    author_email="<andihalim@berkeley.edu>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['python', 'form', 'feedback'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
