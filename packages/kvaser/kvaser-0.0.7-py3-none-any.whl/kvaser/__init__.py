0cb463bbf91352b61567c10664eb0444fdd7e7c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:27:06 +0000

    Add package kvaser/0.0.7/kvaser-0.0.7-py3-none-any.whl

---
+++ packages/kvaser/kvaser-0.0.7-py3-none-any.whl/kvaser/__init__.py
@@ -0,0 +1,4 @@
+# from .data import *  # noqa: F401,F403
+from .data import getdata  # noqa: F401
+from .__about__ import __version__  # noqa: F401
+from .blob_storage import blob_storage # noqa F401
