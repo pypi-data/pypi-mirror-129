2672167d1070038a5006fd0572ba66077bdcf6b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:59:31 +0000

    Add package PyCTPM/1.0.2/PyCTPM-1.0.2.tar.gz

---
+++ packages/PyCTPM/PyCTPM-1.0.2.tar.gz/PyCTPM-1.0.2/PyCTPM/docs/eosData.py
@@ -0,0 +1,42 @@
+# ACCESS COMPONENT
+# -----------------
+
+# import packages/modules
+import json
+# internals
+from PyCTPM.core.store import storeData
+
+#
+
+
+class dbClass(storeData):
+    #! init
+    def __init__(self):
+        #
+        self.data2 = dict()
+        super().__init__()
+
+    #! load json data
+    def loadData(self):
+        """
+        load data from data store 
+        """
+        return self.data
+
+    #! get component data
+
+    def loadItemData(self, componentName):
+        # print(type(componentName))
+        # print(componentName)
+        # var
+        res = []
+        for i in componentName:
+            resLoop = [item for item in self.data if item['symbol'] == i]
+            #! check
+            if len(resLoop) == 1:
+                res.append(resLoop[0])
+                print("resLoop {}".format(resLoop))
+            else:
+                print(f"component {i} not found")
+        # return
+        return res
---
+++ packages/PyCTPM/PyCTPM-1.0.2.tar.gz/PyCTPM-1.0.2/PyCTPM/docs/prop.py
@@ -0,0 +1,6 @@
+# PROPERTY SETTING
+# -----------------
+
+
+propList = ["MW", "Tc", "Pc", "w", "dHf25",
+            "dGf25", "MW-MIX", "Vi", "Vi-MIX", "ThCo", "ThCo-MIX", "DiCo-MIX", "Cpp", "Cpp-MEAN", "Cpp-MIX"]
