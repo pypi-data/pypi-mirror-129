5f6cb08ad56cb7f790495abd348303dfcba00d3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:44:29 +0000

    Add package mypackagejiogs/5.5.5.5/mypackagejiogs-5.5.5.5.tar.gz

---
+++ packages/mypackagejiogs/mypackagejiogs-5.5.5.5.tar.gz/mypackagejiogs-5.5.5.5/setup.py
@@ -0,0 +1,20 @@
+from distutils.core import setup
+setup(
+  name = 'mypackagejiogs',     
+  packages = ['mypackagejiogs'],
+  version = '5.5.5.5',
+  license='MIT',      
+  description = 'test',   
+  author = 'Anton',                 
+  author_email = 'ant4731@yandex.ru',
+  url = 'https://github.com/ivanovanton/',   
+  download_url = 'https://github.com/ivanovanton/mypackagejiogs/archive/refs/tags/v5.5.5.5.tar.gz',    
+  keywords = ['mypackagejiogs'],  
+  install_requires=[],
+  classifiers=[
+    'Intended Audience :: Developers',   
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',  
+    'Programming Language :: Python :: 3',    
+  ],
+)
