602fc46cbb0fe309ea62f22faffa281100afe833
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:48 +0000

    Add package thompson/1.0.0/thompson-1.0.0.tar.gz

---
+++ packages/thompson/thompson-1.0.0.tar.gz/thompson-1.0.0/thompson/__init__.py
@@ -0,0 +1,22 @@
+from thompson.thompson import (
+	import_example,
+    thompson,
+	UCB,
+	UCB_random,
+	plot,
+    wget,
+)
+
+__author__ = 'Erdogan Tasksen'
+__email__ = 'erdogant@gmail.com'
+__version__ = '1.0.0'
+
+# module level doc-string
+__doc__ = """
+thompson - The multi-armed bandit by Thompson Sampling, UCB-Upper confidence Bound, and randomized sampling.
+=====================================================================
+
+**thompson** 
+See README.md file for more information.
+
+"""
