0f207febbbb18053d895d0e6d88f082f9c6999a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:17:45 +0000

    Add package PyCTPM/1.0.3/PyCTPM-1.0.3-py3-none-any.whl

---
+++ packages/PyCTPM/PyCTPM-1.0.3-py3-none-any.whl/PyCTPM/database/dataInfo.py
@@ -0,0 +1,28 @@
+# DATABASE INFO
+# --------------
+
+# database folder
+DATABASE_FOLDER_NAME = "database"
+
+# database info
+DATABASE_INFO = [
+    {
+        "name": "GENERAL",
+        "file": "data_general.csv"
+    },
+    {
+        "name": "HEAT_CAPACITY_PRESSURE",
+        "file": "data_heat_capacity_constant_pressure.csv"
+    },
+    {
+        "name": "THERMAL_CONDUCTIVITY",
+        "file": "data_thermal_conductivity.csv"
+    },
+    {
+        "name": "VISCOSITY",
+        "file": "data_viscosity.csv"
+    },
+]
+
+# general database info
+DATABASE_GENERAL_ITEMS = ["MW", "Tc", "Pc", "w", "dHf25", "dGf25"]
