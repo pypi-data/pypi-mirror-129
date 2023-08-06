9f8811f38188f3f95ce035d5f0750ab3a233ee66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:39:47 +0000

    Add package simple-starlette/0.0.5/simple_starlette-0.0.5.tar.gz

---
+++ packages/simple-starlette/simple_starlette-0.0.5.tar.gz/simple_starlette-0.0.5/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+requires = [
+    "starlette >= 0.16.0",
+    "pydantic ~= 1.8.2",
+    "werkzeug >= 2.0.2",
+    "uvicorn[standard] == 0.15.0",
+    "python_version >= '3.8.0'",
+    "SQLAlchemy >= 1.4.27",
+    "aiomysql >= 0.0.21",
+    "jsonrpcclient == 4.0.1",
+    "jsonrpcserver == 5.0.4",
+    "requests ~= 2.25.1",
+]
+
+
+setup(
+    name="simple_starlette",
+    version="0.0.5",
+    author="mapyJJJ",
+    author_email="wszsdpyjjj@gmail.com",
+    description=u"a micro server",
+    install_requires=requires,
+    packages=find_packages(),
+)
