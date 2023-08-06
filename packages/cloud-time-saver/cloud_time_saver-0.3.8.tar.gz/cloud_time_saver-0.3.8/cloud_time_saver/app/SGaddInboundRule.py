80a7f4e52e8d3c5d612363012fc5e9f9e27ea8c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:28:22 +0000

    Add package cloud-time-saver/0.3.8/cloud_time_saver-0.3.8.tar.gz

---
+++ packages/cloud-time-saver/cloud_time_saver-0.3.8.tar.gz/cloud_time_saver-0.3.8/cloud_time_saver/app/SGaddInboundRule.py
@@ -0,0 +1,20 @@
+import boto3
+from botocore.exceptions import ClientError
+
+
+def run():
+    client = boto3.client('ec2')
+    security_group_id = input("Please enter security gtoup id: ")
+    port = input("Please enter port number: ")
+    try:
+        data = client.authorize_security_group_egress(
+                    GroupId=security_group_id,
+                    IpPermissions=[
+                        {'IpProtocol': 'tcp',
+                        'FromPort': int(port),
+                        'ToPort': int(port),
+                        'IpRanges': [{'CidrIp': '0.0.0.0/0'}]},
+                        ])
+        print('Rule Successfully Set %s' % data)
+    except ClientError as e:
+        print(e)
