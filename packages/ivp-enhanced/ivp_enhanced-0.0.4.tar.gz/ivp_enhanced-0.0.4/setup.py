ee2c24311126331723ed3a424149901079b0b484
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:41:50 +0000

    Add package ivp-enhanced/0.0.4/ivp_enhanced-0.0.4.tar.gz

---
+++ packages/ivp-enhanced/ivp_enhanced-0.0.4.tar.gz/ivp_enhanced-0.0.4/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+with open("README.md") as fh:
+    long_description = fh.read()
+setup(
+    name='ivp_enhanced',
+    version='0.0.4',
+    packages=find_packages(include=['ivp_enhanced', 'ivp_enhanced.*']),
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/zhaofeng-shu33/ivp-enhanced',
+    author='zhaofeng-shu33',
+    author_email='616545598@qq.com',
+    description='enhance some missing features of scipy.integrate.solve_ivp'
+)
