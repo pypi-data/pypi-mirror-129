38f68fed1d595014bd8792b4d28725693f82744e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:45:15 +0000

    Add package jklwcs/1.2/jklwcs-1.2.tar.gz

---
+++ packages/jklwcs/jklwcs-1.2.tar.gz/jklwcs-1.2/jklwcs/osspath.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+'''
+@File  : osspath.py
+@Author: Jike Data Analysis Modeling Group
+@Date  : 2021/12/4 12:13 上午
+@Desc  : oss数据存储路径
+'''
+
+M_CROWD_PACK_RESULT_PATH = 'oss://geek-results/jklwcs/results/m_crowd_pack/'
+M_CROWD_PACK_TEMP_PATH = 'oss://geek-results/jklwcs/temp/m_crowd_pack/'
+M_ARRIVED_DETAILS_RESULT_PATH = 'oss://geek-results/jklwcs/results/m_arrived_details/'
+M_ARRIVED_DETAILS_TEMP_PATH = 'oss://geek-results/jklwcs/temp/m_arrived_details/'
