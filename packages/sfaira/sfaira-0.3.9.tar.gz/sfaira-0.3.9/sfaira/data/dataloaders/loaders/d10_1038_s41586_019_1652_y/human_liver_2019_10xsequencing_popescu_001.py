16e84af5d6d6b0191e6f7b94f2b68e65c0d4b6de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:08:19 +0000

    Add package sfaira/0.3.9/sfaira-0.3.9.tar.gz

---
+++ packages/sfaira/sfaira-0.3.9.tar.gz/sfaira-0.3.9/sfaira/data/dataloaders/loaders/d10_1038_s41586_019_1652_y/human_liver_2019_10xsequencing_popescu_001.py
@@ -0,0 +1,36 @@
+import anndata
+import os
+
+from sfaira.data import DatasetBase
+
+
+class Dataset(DatasetBase):
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.download_url_data = "private,fetal_liver_alladata_.h5ad"
+        self.download_url_meta = None
+
+        self.assay_sc = "10x 3' v2"
+        self.author = "Popescu"
+        self.disease = "healthy"
+        self.doi_journal = "10.1038/s41586-019-1652-y"
+        self.normalization = "raw"
+        self.organ = "liver"
+        self.organism = "Homo sapiens"
+        self.primary_data = True
+        self.sample_source = "primary_tissue"
+        self.state_exact = "healthy"
+        self.year = 2019
+
+        self.gene_id_symbols_var_key = "index"
+        self.cell_type_obs_key = "cell.labels"
+
+        self.set_dataset_id(idx=1)
+
+
+def load(data_dir, **kwargs):
+    fn = os.path.join(data_dir, "fetal_liver_alladata_.h5ad")
+    adata = anndata.read(fn)
+
+    return adata
