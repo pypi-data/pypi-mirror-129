0c36396ffd39f13165d227460574982433442b36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:44 +0000

    Add package test-executor/0.0.1/test_executor-0.0.1.tar.gz

---
+++ packages/test-executor/test_executor-0.0.1.tar.gz/test_executor-0.0.1/setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+setup(
+    name='test_executor',
+    packages=['test_executor'],
+    version='0.0.1',
+    description='This modules allows you to execute ',
+    author='Idan Cohen',
+    author_email='idan57@gmail.com',
+    url='https://github.com/idan57/test_executor',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+    ],
+)
