e6ba6bc66de43f445685bd0c388da19dd5de726c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:17 +0000

    Add package yqdata/1.0.3/yqdata-1.0.3-py3-none-any.whl

---
+++ packages/yqdata/yqdata-1.0.3-py3-none-any.whl/yqdata/__init__.py
@@ -0,0 +1,22 @@
+# -*- coding: UTF-8 -*-
+
+import pkgutil
+
+__version__ = '1.0.0'
+__author__ = 'Hu Min'
+
+def get_version():
+    return __version__
+
+def init():
+    import importlib
+    for loader, module_name, is_pkg in pkgutil.walk_packages(path=__path__, prefix='yqdata.'):
+        print(module_name)
+        if module_name.startswith("yqdata.services") and not is_pkg:
+            try:
+                importlib.import_module(module_name)
+            except ImportError:
+                print("can not find mod [{}], ignored".format(module_name))
+                continue
+        
+init()
