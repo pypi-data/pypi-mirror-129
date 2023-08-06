1daa6bef1869391fc4c67ce7e1468422140a363a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:46:09 +0000

    Add package nuq-eui/0.0.1/nuq_eui-0.0.1.tar.gz

---
+++ packages/nuq-eui/nuq_eui-0.0.1.tar.gz/nuq_eui-0.0.1/setup.py
@@ -0,0 +1,31 @@
+from distutils.core import setup
+
+# setup(name="bao_test", #包名称
+# version="1.0", #版本号
+# description="包的发送与接收", #代码描述
+# long_description="包的发送与接收的测试", #代码详细描述
+# author="dudu", #作者
+# author_email="931932073@qq.com", #邮箱
+# py_modules=["bao_test.receiv_message",                   #要上传的包模块。注意模块后面是带.py
+# "bao_test.send_message"])
+
+
+
+setup(
+    name="nuq_eui",
+    version="0.0.1",
+    author="Example Author",
+    author_email="285151481@qq.com",
+    description="A small example package",
+    # long_description=long_description,
+    # long_description_content_type="text/markdown",
+    # url="https://github.com/pypa/sampleproject",
+    # packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
+
+
