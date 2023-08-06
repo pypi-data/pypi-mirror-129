9698163cc9d6da22d3a0a5bc9e20ddcd26faa9f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:26:18 +0000

    Add package django-entries/0.1.4/django_entries-0.1.4-py3-none-any.whl

---
+++ packages/django-entries/django_entries-0.1.4-py3-none-any.whl/entries/urls.py
@@ -0,0 +1,22 @@
+from django.urls import path
+
+from .views import (
+    add_entry,
+    delete_entry,
+    edit_entry,
+    list_entries,
+    view_about,
+    view_entry,
+    view_home,
+)
+
+app_name = "entries"
+urlpatterns = [
+    path("entries/list", list_entries, name="list_entries"),
+    path("create/add", add_entry, name="add_entry"),
+    path("edit/<slug:slug>", edit_entry, name="edit_entry"),
+    path("delete/<slug:slug>", delete_entry, name="delete_entry"),
+    path("view/<slug:slug>", view_entry, name="view_entry"),
+    path("about/", view_about, name="view_about"),
+    path("", view_home, name="view_home"),
+]
