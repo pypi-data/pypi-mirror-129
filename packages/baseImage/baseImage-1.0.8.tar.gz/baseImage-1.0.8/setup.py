a36db6aa1ced89c620a6cc6c04ffc8a085b5f70f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:19:48 +0000

    Add package baseImage/1.0.8/baseImage-1.0.8.tar.gz

---
+++ packages/baseImage/baseImage-1.0.8.tar.gz/baseImage-1.0.8/setup.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+setup(
+    name='baseImage',
+    version='1.0.8',
+    author='hakaboom',
+    author_email='1534225986@qq.com',
+    license='Apache License 2.0',
+    description='This is a secondary package of OpenCV,for manage image data',
+    url='https://github.com/hakaboom/base_image',
+    packages=['baseImage'],
+    install_requires=['colorama>=0.4.4',
+                      "loguru>=0.5.3",
+                      "pydantic",
+],
+)
