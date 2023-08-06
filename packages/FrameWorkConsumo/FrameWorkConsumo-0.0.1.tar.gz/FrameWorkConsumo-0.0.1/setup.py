c4a42e10ca14474908e6ce7d305c6230dc68322b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:38:48 +0000

    Add package FrameWorkConsumo/0.0.1/FrameWorkConsumo-0.0.1.tar.gz

---
+++ packages/FrameWorkConsumo/FrameWorkConsumo-0.0.1.tar.gz/FrameWorkConsumo-0.0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='FrameWorkConsumo',
+    version='0.0.1',
+
+    url='https://github.com/Dauerrr/FrameworkConsumo',
+    author='Pedro Dauer',
+    author_email='pedro.dauer2003@gmail.com',
+
+    py_modules=['descritor'],
+    install_requires=[
+    'pandas',
+    'requests',
+],
+)
