5e9544b88bdb06368b3c9d39e128a16841fec6d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:39:50 +0000

    Add package peekingduck/1.1.1/peekingduck-1.1.1-py3-none-any.whl

---
+++ packages/peekingduck/peekingduck-1.1.1-py3-none-any.whl/peekingduck/pipeline/nodes/output/screen.py
@@ -0,0 +1,49 @@
+# Copyright 2021 AI Singapore
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""
+Shows the outputs on your display.
+"""
+
+from typing import Any, Dict
+
+import cv2
+
+from peekingduck.pipeline.nodes.node import AbstractNode
+
+
+class Node(AbstractNode):
+    """Streams the output on your display.
+
+    Inputs:
+        |img|
+
+    Outputs:
+        |pipeline_end|
+
+    Configs:
+        None.
+    """
+
+    def __init__(self, config: Dict[str, Any] = None, **kwargs: Any) -> None:
+        super().__init__(config, node_path=__name__, **kwargs)
+
+    def run(self, inputs: Dict[str, Any]) -> Dict[str, Any]:
+        """Show the outputs on your display"""
+        cv2.imshow("PeekingDuck", inputs["img"])
+
+        if cv2.waitKey(1) & 0xFF == ord("q"):
+            return {"pipeline_end": True}
+
+        return {"pipeline_end": False}
