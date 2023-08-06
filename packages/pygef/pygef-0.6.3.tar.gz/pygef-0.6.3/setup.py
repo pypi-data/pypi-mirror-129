466c5d0aec01ce7fe95a1203c38204e8b6725bb4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:20 +0000

    Add package pygef/0.6.3/pygef-0.6.3.tar.gz

---
+++ packages/pygef/pygef-0.6.3.tar.gz/pygef-0.6.3/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+exec(open("pygef/_version.py").read())
+
+setup(
+    name="pygef",
+    version=__version__,
+    author="Ritchie Vink",
+    author_email="ritchie46@gmail.com",
+    url="https://www.ritchievink.com",
+    license="mit",
+    packages=["pygef", "pygef.been_jefferies", "pygef.robertson"],
+    install_requires=[
+        "polars>= 0.9.5",
+        "matplotlib>= 3.4.2",
+        "xmlschema==1.8.2",
+        "pandas>=1.1.5",
+    ],
+    python_requires=">=3.6",
+    include_package_data=True,
+)
