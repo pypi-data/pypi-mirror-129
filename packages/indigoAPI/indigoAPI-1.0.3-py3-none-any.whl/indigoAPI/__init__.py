64368cd2088c2d1e1dbf34d76b9ad4811dbe862c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:58:30 +0000

    Add package indigoAPI/1.0.3/indigoAPI-1.0.3-py3-none-any.whl

---
+++ packages/indigoAPI/indigoAPI-1.0.3-py3-none-any.whl/indigoAPI/__init__.py
@@ -0,0 +1,14 @@
+import os
+
+
+class social:
+  async def tiktok():
+    test = input("Please Enter A Valid API Key: ")
+    if test == "avdhr1awDB":
+      print()
+    else:
+      print("іnvalіd АРӀ ΚЕҮ Үоu ЅΚID")
+      os._exit(0)
+
+  
+
