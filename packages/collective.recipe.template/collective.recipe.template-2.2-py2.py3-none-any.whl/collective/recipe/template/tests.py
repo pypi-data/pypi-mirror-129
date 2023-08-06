f27d78fb9ee5d0ff5818ff35e07e8365f1e613dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:31:15 +0000

    Add package collective.recipe.template/2.2/collective.recipe.template-2.2-py2.py3-none-any.whl

---
+++ packages/collective.recipe.template/collective.recipe.template-2.2-py2.py3-none-any.whl/collective/recipe/template/tests.py
@@ -0,0 +1,42 @@
+"""Test setup for collective.recipe.template.
+"""
+
+import re
+
+import zc.buildout.testing
+
+import doctest
+import unittest
+from zope.testing import renormalizing
+
+
+def setUp(test):
+    zc.buildout.testing.buildoutSetUp(test)
+    zc.buildout.testing.install_develop('collective.recipe.template', test)
+    zc.buildout.testing.install('zope.testing', test)
+    zc.buildout.testing.install('six', test)
+    zc.buildout.testing.install('Genshi', test)
+
+
+checker = renormalizing.RENormalizing([
+    zc.buildout.testing.normalize_path,
+    (re.compile('#![^\n]+\n'), ''),
+    (re.compile(r'-\S+-py\d[.]\d(-\S+)?.egg'),
+     '-pyN.N.egg',
+     ),
+])
+
+
+def test_suite():
+    return unittest.TestSuite([
+        doctest.DocFileSuite(
+            'README.rst',
+            setUp=setUp, tearDown=zc.buildout.testing.buildoutTearDown,
+            optionflags=doctest.ELLIPSIS | doctest.NORMALIZE_WHITESPACE,
+            checker=checker),
+        doctest.DocFileSuite(
+            'genshitemplate.rst',
+            setUp=setUp, tearDown=zc.buildout.testing.buildoutTearDown,
+            optionflags=doctest.ELLIPSIS | doctest.NORMALIZE_WHITESPACE,
+            checker=checker),
+    ])
