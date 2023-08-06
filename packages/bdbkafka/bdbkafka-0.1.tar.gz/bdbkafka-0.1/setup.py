baf8654f97b95dd3553d00097de1db3398b06d8b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:08 +0000

    Add package bdbkafka/0.1/bdbkafka-0.1.tar.gz

---
+++ packages/bdbkafka/bdbkafka-0.1.tar.gz/bdbkafka-0.1/setup.py
@@ -0,0 +1,27 @@
+from distutils.core import setup
+
+setup(name='bdbkafka',
+      version='0.1',
+      description='kafka connection',
+      url=' ',
+      download_url=' ',
+      author=' ',
+      author_email=' ',
+      keywords=['kafka'],
+      license='MIT',
+      install_requires=['requests', 'confluent_kafka', 'logger', ],
+
+      classifiers=[
+          'Development Status :: 3 - Alpha',
+          'Intended Audience :: Developers',
+          'Topic :: Software Development :: Build Tools',
+          'License :: OSI Approved :: MIT License',
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.4',
+          'Programming Language :: Python :: 3.6',
+          'Programming Language :: Python :: 3.7',
+          'Programming Language :: Python :: 3.8',
+          'Programming Language :: Python :: 3.9',
+          'Programming Language :: Python :: 3.10'
+      ],
+      )
