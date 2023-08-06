07694b14b220874fe0dca5f86738fe5e2e2ec477
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:28:06 +0000

    Add package blint/1.0.14/blint-1.0.14.tar.gz

---
+++ packages/blint/blint-1.0.14.tar.gz/blint-1.0.14/blint/logger.py
@@ -0,0 +1,33 @@
+import logging
+import os
+
+from rich.console import Console
+from rich.logging import RichHandler
+from rich.theme import Theme
+
+custom_theme = Theme({"info": "cyan", "warning": "purple4", "danger": "bold red"})
+console = Console(
+    log_time=False,
+    log_path=False,
+    theme=custom_theme,
+    color_system="auto",
+    record=True,
+)
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(message)s",
+    datefmt="[%X]",
+    handlers=[
+        RichHandler(
+            console=console, markup=True, show_path=False, enable_link_path=False
+        )
+    ],
+)
+LOG = logging.getLogger(__name__)
+
+# Set logging level
+if os.getenv("SCAN_DEBUG_MODE") == "debug":
+    LOG.setLevel(logging.DEBUG)
+
+DEBUG = logging.DEBUG
