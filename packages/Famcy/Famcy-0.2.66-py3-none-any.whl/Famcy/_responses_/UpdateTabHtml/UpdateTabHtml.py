ba63e910bd444a3dc874fa9f50a771839189d641
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:49:34 +0000

    Add package Famcy/0.2.66/Famcy-0.2.66-py3-none-any.whl

---
+++ packages/Famcy/Famcy-0.2.66-py3-none-any.whl/Famcy/_responses_/UpdateTabHtml/UpdateTabHtml.py
@@ -0,0 +1,17 @@
+import Famcy
+
+class UpdateTabHtml(Famcy.FamcyResponse):
+	"""
+	This is the response to update the entire page (Tab)
+	Make sure self.target is a fpage
+	"""
+	def __init__(self, extra_script=None, target=None):
+		super(UpdateTabHtml, self).__init__(target=target)
+		self.extra_script = extra_script
+
+	def response(self, sijax_response):
+		if self.target:
+			sijax_response.html('#content_section', self.target.render_inner())
+			self.target.layout.setSijaxLayout(sijax_response)
+			sijax_response.script(self.extra_script)
+			sijax_response.script(self.finish_loading_script)
