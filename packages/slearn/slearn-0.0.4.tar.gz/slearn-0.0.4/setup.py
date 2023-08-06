3893696306aba72e70e78563896f260a82550e3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:07 +0000

    Add package slearn/0.0.4/slearn-0.0.4.tar.gz

---
+++ packages/slearn/slearn-0.0.4.tar.gz/slearn-0.0.4/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+import numpy
+
+with open("README.md", 'r') as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name="slearn",
+    packages=["slearn"],
+    version="0.0.4",
+    setup_requires=["numpy>=1.20.0"],
+    install_requires=["numpy>=1.20.0", "scikit-learn", "pandas", "fABBA", "lightgbm", "request"],
+    include_dirs=[numpy.get_include()],
+    long_description=long_description,
+    author="Stefan Güttel, Xinye Chen, Roberto Cahuantzi",
+    author_email="stefan.guettel@manchester.ac.uk, xinye.chen@manchester.ac.uk, Roberto.Cahuantzi@manchester.ac.uk",
+    maintainer="Xinye Chen",
+    maintainer_email="xinye.chen@manchester.ac.uk",
+    description="A package linking symbolic representation with sklearn for time series prediction",
+    long_description_content_type='text/markdown',
+    url="https://github.com/nla-group/slearn.git",
+    license='MIT License'
+)
