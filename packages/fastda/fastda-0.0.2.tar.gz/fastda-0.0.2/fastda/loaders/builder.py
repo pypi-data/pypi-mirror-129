5504fbd36f310641ca78695e408de9a8187ebeca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:24 +0000

    Add package fastda/0.0.2/fastda-0.0.2.tar.gz

---
+++ packages/fastda/fastda-0.0.2.tar.gz/fastda-0.0.2/fastda/loaders/builder.py
@@ -0,0 +1,9 @@
+# Author: Vincent Zhang
+# Mail: zhyx12@gmail.com
+# ----------------------------------------------
+from mmcv.utils import Registry
+
+DATASETS = Registry('datasets')
+
+if __name__ == "__main__":
+    pass
