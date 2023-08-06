13a01efe659cf0c1f36971824a2d10b4d4325a78
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:26:52 +0000

    Add package nnfpy/1.0.2/nnfpy-1.0.2.tar.gz

---
+++ packages/nnfpy/nnfpy-1.0.2.tar.gz/nnfpy-1.0.2/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+setup(name='nnfpy',
+    version='1.0.2',
+    description='Neural network on python',
+    long_description=readme(),
+    long_description_content_type="text/markdown",
+    url='https://github.com/sopho-s/nnfpy',
+    author='Nick Woods',
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.9",
+    ],
+    author_email='woodsnicholas01@gmail.com',
+    license='MIT',
+    keywords='',
+    packages=find_packages('src'),
+    package_dir={'': 'src'}
+)
