360f640d10173141871b05e1f679b4abbbacfd04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:08:53 +0000

    Add package fastmath/0.0.88/fastmath-0.0.88-py3-none-any.whl

---
+++ packages/fastmath/fastmath-0.0.88-py3-none-any.whl/fastmath/__init__.py
@@ -0,0 +1,7 @@
+from .lib import quadratic
+from .main import add, sub, mul, div, fac, sqrt, pi, circle_area
+from .stats import mean, stdev, calc_prob, calculate_class_probabilities
+from .trig import sin, cos, tan, cot, csc, sec
+from .cosine_sim import cosine_sim
+from .sigmoid import sigmoid
+from .gaussain import *
