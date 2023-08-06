20e024acf9325f604b6d7885b355279fd7896343
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:58:59 +0000

    Add package pytorch-adapt/0.0.37/pytorch_adapt-0.0.37-py3-none-any.whl

---
+++ packages/pytorch-adapt/pytorch_adapt-0.0.37-py3-none-any.whl/pytorch_adapt/adapters/adda.py
@@ -0,0 +1,41 @@
+import copy
+
+from ..containers import KeyEnforcer, MultipleContainers, Optimizers
+from ..hooks import ADDAHook
+from ..utils.common_functions import check_domain
+from .base_adapter import BaseAdapter
+from .utils import default_optimizer_tuple, with_opt
+
+
+class ADDA(BaseAdapter):
+    """
+    Wraps [ADDAHook][pytorch_adapt.hooks.adda].
+    """
+
+    hook_cls = ADDAHook
+
+    def inference_default(self, x, domain):
+        domain = check_domain(self, domain)
+        fe = "G" if domain == 0 else "T"
+        features = self.models[fe](x)
+        logits = self.models["C"](features)
+        return features, logits
+
+    def get_default_containers(self):
+        optimizers = Optimizers(default_optimizer_tuple(), keys=["T", "D"])
+        return MultipleContainers(optimizers=optimizers)
+
+    def get_key_enforcer(self):
+        return KeyEnforcer(
+            models=["G", "C", "D", "T"],
+            optimizers=["D", "T"],
+        )
+
+    def init_hook(self, hook_kwargs):
+        self.hook = self.hook_cls(
+            d_opts=with_opt(["D"]), g_opts=with_opt(["T"]), **hook_kwargs
+        )
+
+    def init_containers_and_check_keys(self):
+        self.containers["models"]["T"] = copy.deepcopy(self.containers["models"]["G"])
+        super().init_containers_and_check_keys()
