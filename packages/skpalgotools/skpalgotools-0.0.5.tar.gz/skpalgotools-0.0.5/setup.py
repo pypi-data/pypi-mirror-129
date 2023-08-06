6ed65a5b116c0014ccb5f0dc606b361d4b08bc28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:52 +0000

    Add package skpalgotools/0.0.5/skpalgotools-0.0.5.tar.gz

---
+++ packages/skpalgotools/skpalgotools-0.0.5.tar.gz/skpalgotools-0.0.5/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+from distutils.core import setup
+
+setup(
+    name="skpalgotools",
+    version="0.0.5",
+    description="A one stop solution for solving your required algorithms",
+    author_email="shiridikumarpeddinti836@gmail.com",
+    py_modules=["skpalgotools"],
+    package_dir={"":"src"},
+    install_requires=["matplotlib","pandas","numpy","seaborn"]
+
+)
