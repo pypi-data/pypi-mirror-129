4384bb946591ca032f1d9b8464303f3abce69010
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:27 +0000

    Add package odoo14-addon-hr-holidays-public/14.0.2.0.1/odoo14_addon_hr_holidays_public-14.0.2.0.1-py3-none-any.whl

---
+++ packages/odoo14-addon-hr-holidays-public/odoo14_addon_hr_holidays_public-14.0.2.0.1-py3-none-any.whl/odoo/addons/hr_holidays_public/models/hr_leave.py
@@ -0,0 +1,70 @@
+# Copyright 2017-2018 Tecnativa - Pedro M. Baeza
+# Copyright 2018 Brainbean Apps
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+from odoo import api, fields, models
+
+
+class HrLeave(models.Model):
+    _inherit = "hr.leave"
+
+    def _get_number_of_days(self, date_from, date_to, employee_id):
+        if self.holiday_status_id.exclude_public_holidays or not self.holiday_status_id:
+            instance = self.with_context(
+                employee_id=employee_id, exclude_public_holidays=True
+            )
+        else:
+            instance = self
+        return super(HrLeave, instance)._get_number_of_days(
+            date_from, date_to, employee_id
+        )
+
+    @api.depends("number_of_days")
+    def _compute_number_of_hours_display(self):
+        if self.holiday_status_id.exclude_public_holidays or not self.holiday_status_id:
+            instance = self.with_context(
+                exclude_public_holidays=True, employee_id=self.employee_id.id
+            )
+        else:
+            instance = self
+        return super(HrLeave, instance)._compute_number_of_hours_display()
+
+    @api.model
+    def get_unusual_days(self, date_from, date_to=None):
+        res = super().get_unusual_days(date_from, date_to=date_to)
+        domain = [("date", ">=", date_from)]
+        if date_to:
+            domain.append(
+                (
+                    "date",
+                    "<",
+                    date_to,
+                )
+            )
+        country_id = self.env.user.employee_id.address_id.country_id.id
+        if not country_id:
+            country_id = self.env.company.country_id.id or False
+        if country_id:
+            domain.extend(
+                [
+                    "|",
+                    ("year_id.country_id", "=", False),
+                    ("year_id.country_id", "=", country_id),
+                ]
+            )
+        state_id = self.env.user.employee_id.address_id.state_id.id
+        if not state_id:
+            state_id = self.env.company.state_id.id or False
+        if state_id:
+            domain.extend(
+                [
+                    "|",
+                    ("state_ids", "in", [state_id]),
+                    ("state_ids", "=", False),
+                ]
+            )
+
+        public_holidays = self.env["hr.holidays.public.line"].search(domain)
+        for public_holiday in public_holidays:
+            res[fields.Date.to_string(public_holiday.date)] = True
+        return res
