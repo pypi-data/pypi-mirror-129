2a5109d7a82be74c57730dfe83ddc9660c958a3e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:24:39 +0000

    Add package juliang/0.1.10/juliang-0.1.10.tar.gz

---
+++ packages/juliang/juliang-0.1.10.tar.gz/juliang-0.1.10/juliangip/common/UsersGetAllOrders.py
@@ -0,0 +1,11 @@
+class UsersGetAllOrders(object):
+    def __init__(self, key, user_id):
+        self.key = key
+        self.user_id = user_id
+
+    # 业务密钥
+    key = ""
+    # 用户id
+    user_id = ""
+    # 产品类型
+    product_type = ""
