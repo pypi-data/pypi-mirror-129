6c201aeceaa55749b85e1db5f0570a7e541d99ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:20:34 +0000

    Add package secondwwwr/1.2.3/secondwwwr-1.2.3.tar.gz

---
+++ packages/secondwwwr/secondwwwr-1.2.3.tar.gz/secondwwwr-1.2.3/setup.py
@@ -0,0 +1,20 @@
+from distutils.core import setup
+setup(
+  name = 'secondwwwr',     
+  packages = ['secondwwwr'],
+  version = '1.2.3',
+  license='MIT',      
+  description = 'test',   
+  author = 'Anton',                 
+  author_email = 'ant4731@yandex.ru',
+  url = 'https://github.com/ivanovanton/',   
+  download_url = 'https://github.com/ivanovanton/secondwwwr/archive/refs/tags/v7.7.7.tar.gz',    
+  keywords = ['secondwwwr'],  
+  install_requires=[],
+  classifiers=[
+    'Intended Audience :: Developers',   
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',  
+    'Programming Language :: Python :: 3',    
+  ],
+)
