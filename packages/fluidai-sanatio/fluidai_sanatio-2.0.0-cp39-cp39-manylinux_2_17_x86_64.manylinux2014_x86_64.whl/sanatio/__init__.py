24cc8d12badac9d3a0846a17a1a0af9173de85c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:00:06 +0000

    Add package fluidai-sanatio/2.0.0/fluidai_sanatio-2.0.0-cp39-cp39-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/fluidai-sanatio/fluidai_sanatio-2.0.0-cp39-cp39-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/sanatio/__init__.py
@@ -0,0 +1,5 @@
+from .validations import authenticate_session, set_key
+# try:
+#     from .validations import authenticate_session, set_key
+# except ModuleNotFoundError as e:
+#     print("Module import error")
