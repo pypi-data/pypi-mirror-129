85812e5bfd38c97a1d21b124ee08e9d40de37eae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:58 +0000

    Add package popoto/0.4.1/popoto-0.4.1.tar.gz

---
+++ packages/popoto/popoto-0.4.1.tar.gz/popoto-0.4.1/src/popoto/fields/key_field.py
@@ -0,0 +1,158 @@
+from .field import Field, logger
+import uuid
+
+from ..redis_db import POPOTO_REDIS_DB
+
+
+class KeyField(Field):
+    """
+    A KeyField is for unique identifiers and category searches
+    All KeyFields are used for indexing on the DB.
+    All keys together have unique_together enforced.
+
+    UniqueKeyField and AutoKeyField provide unique constraint and auto-id generation respectively.
+    All models must have one or more KeyFields.
+    However an AutoKeyField will be automatically added to models without any specified KeyFields.
+
+    todo: add support for https://github.com/ai/nanoid
+    """
+    unique: bool = False
+    indexed: bool = False
+    auto: bool = False
+    auto_uuid_length: int = 32
+    auto_id: str = ""
+    null: bool = False
+    max_length: int = 128
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        keyfield_defaults = {
+            'unique': False,
+            'indexed': False,
+            'auto': False,
+            'auto_uuid_length': 32,
+            'auto_id': "",
+            'null': False,
+            'max_length': 128,  # Redis limit is 512MB
+        }
+        self.field_defaults.update(keyfield_defaults)
+        # set keyfield_options, let kwargs override
+        for k, v in keyfield_defaults.items():
+            setattr(self, k, kwargs.get(k, v))
+
+    def get_new_auto_key_value(self):
+        return uuid.uuid4().hex[:self.auto_uuid_length]
+
+    def set_auto_key_value(self, force: bool = False):
+        if self.auto or force:
+            self.default = self.get_new_auto_key_value()
+
+    @classmethod
+    def is_valid(cls, field, value, null_check=True, **kwargs) -> bool:
+        if not super().is_valid(field, value, null_check):
+            return False
+        if field.auto and len(value) != field.auto_uuid_length:
+            logger.error(f"auto key value is length {len(value)}. It should be {field.auto_uuid_length}")
+            return False
+        return True
+
+    def get_filter_query_params(self, field_name: str) -> list:
+        return super().get_filter_query_params(field_name) + [
+            f'{field_name}',    # takes a str, exact match :x:
+            f'{field_name}__contains',  # takes a str, matches :*x*:
+            f'{field_name}__startswith',  # takes a str, matches :x*:
+            f'{field_name}__endswith',  # takes a str, matches :*x:
+            f'{field_name}__in',  # takes a list, returns any matches
+        ]
+
+    @classmethod
+    def filter_query(cls, model: 'Model', field_name: str, **query_params) -> set:
+        """
+        :param model: the popoto.Model to query from
+        :param field_name: the name of the field being filtered on
+        :param query_params: dict of filter args and values
+        :return: set{db_key, db_key, ..}
+        """
+
+        keys_lists_to_intersect = list()
+        db_key_length, field_key_position = model._meta.db_key_length, model._meta.get_db_key_index_position(field_name)
+        num_keys_before = field_key_position
+        num_keys_after = db_key_length - field_key_position - 1
+
+        pipeline = POPOTO_REDIS_DB.pipeline()
+
+        def get_key_pattern(query_value_pattern):
+            key_pattern = f"{model._meta.db_class_key}:"
+            key_pattern += ("*:" * (num_keys_before-1))
+            key_pattern += query_value_pattern
+            key_pattern += ":*" * num_keys_after
+            return key_pattern
+
+        for query_param, query_value in query_params.items():
+
+            if query_param.endswith('__in'):
+                pipeline_2 = POPOTO_REDIS_DB.pipeline()
+                for query_value_elem in query_value:
+                    key_pattern = get_key_pattern(f"{query_value_elem}")
+                    pipeline_2 = pipeline_2.keys(key_pattern)
+                keys_lists_to_union = pipeline_2.execute()
+                keys_lists_to_intersect.append(set.union(*[set(key_list) for key_list in keys_lists_to_union]))
+
+            else:
+                if query_param == f'{field_name}':
+                    key_pattern = get_key_pattern(f"{query_value}")
+                elif query_param.endswith('__startswith'):
+                    key_pattern = get_key_pattern(f"{query_value}*")
+                elif query_param.endswith('__endswith'):
+                    key_pattern = get_key_pattern(f"*{query_value}")
+
+                pipeline = pipeline.keys(key_pattern)
+            # todo: refactor to use HSCAN or sets, https://redis.io/commands/keys
+            # https://redis-py.readthedocs.io/en/stable/index.html#redis.Redis.hscan_iter
+
+        keys_lists_to_intersect += pipeline.execute()
+
+        logger.debug(keys_lists_to_intersect)
+        if len(keys_lists_to_intersect):
+            return set.intersection(*[set(key_list) for key_list in keys_lists_to_intersect])
+        return set()
+
+
+class UniqueKeyField(KeyField):
+    """
+    UniqueKeyField() is equivalent to KeyField(unique=True)
+    """
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        uniquekeyfield_defaults = {
+            'unique': True,
+        }
+        self.field_defaults.update(uniquekeyfield_defaults)
+        # set keyfield_options, let kwargs override
+        for k, v in uniquekeyfield_defaults.items():
+            setattr(self, k, kwargs.get(k, v))
+
+        # todo: move this to field init validation
+        if not kwargs.get('unique', True):
+            from ..models.base import ModelException
+            raise ModelException("UniqueKey field MUST be unique")
+
+
+class AutoKeyField(UniqueKeyField):
+    """
+    AutoKeyField() is equivalent to KeyField(unique-True, auto=True)
+    The AutoKeyField is an auto-generated, universally unique key
+    It will be automatically added to models with no specified KeyFields
+    Include this field in your model if you cannot otherwise enforce a unique-together constraint with other KeyFields.
+    They auto-generated key is random and newly generated for a model instance.
+    Model instances with otherwise identical properties are saved as separate instances with different auto-keys.
+    """
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        autokeyfield_defaults = {
+            'auto': True,
+        }
+        self.field_defaults.update(autokeyfield_defaults)
+        # set keyfield_options, let kwargs override
+        for k, v in autokeyfield_defaults.items():
+            setattr(self, k, kwargs.get(k, v))
---
+++ packages/popoto/popoto-0.4.1.tar.gz/popoto-0.4.1/src/popoto/models/base.py
@@ -0,0 +1,395 @@
+import logging
+
+import redis
+
+from .encoding import encode_popoto_model_obj
+from .query import Query
+from .. import GeoField
+from ..fields.field import Field
+from ..fields.key_field import KeyField, AutoKeyField
+from ..fields.sorted_field import SortedField
+from ..redis_db import POPOTO_REDIS_DB
+
+logger = logging.getLogger('POPOTO.model_base')
+
+
+class ModelException(Exception):
+    pass
+
+
+class ModelOptions:
+    def __init__(self, model_name):
+        self.model_name = model_name
+        self.hidden_fields = {}
+        self.explicit_fields = {}
+        self.key_field_names = []
+        self.list_field_names = []
+        # self.set_field_names = []
+        self.sorted_field_names = []
+        self.geo_field_names = []
+        self.indexed_field_names = []
+
+        # todo: should this be a dict of related objects or just a list of field names?
+        # self.related_fields = {}  # model becomes graph node
+
+        # todo: allow customizing this in model.Meta class
+        self.db_class_key = self.model_name
+
+        self.abstract = False
+        self.unique_together = []
+        self.index_together = []
+        self.parents = []
+        self.auto_created = False
+        self.base_meta = None
+
+    def add_field(self, field_name: str, field: Field):
+        if field_name.startswith("_") and field_name not in self.hidden_fields:
+            self.hidden_fields[field_name] = field
+        elif field_name not in self.explicit_fields:
+            self.explicit_fields[field_name] = field
+        else:
+            raise ModelException(f"{field_name} is already a Field on the model")
+
+        if field.indexed:
+            self.indexed_field_names.append(field_name)
+
+        if isinstance(field, KeyField):
+            self.key_field_names.append(field_name)
+        elif isinstance(field, SortedField):
+            self.sorted_field_names.append(field_name)
+        elif isinstance(field, GeoField):
+            self.geo_field_names.append(field_name)
+        # elif isinstance(field, ListField):
+        #     self.list_field_names.append(field_name)
+
+    @property
+    def fields(self) -> dict:
+        return {**self.explicit_fields, **self.hidden_fields}
+
+    @property
+    def field_names(self) -> list:
+        return list(self.fields.keys())
+
+    @property
+    def db_key_length(self):
+        return 1 + len(self.key_field_names)
+
+    def get_db_key_index_position(self, field_name):
+        return 1 + sorted(self.key_field_names).index(field_name)
+
+
+class ModelBase(type):
+    """Metaclass for all Popoto Models."""
+
+    def __new__(cls, name, bases, attrs, **kwargs):
+
+        # Initialization is only performed for a Model and its subclasses
+        parents = [b for b in bases if isinstance(b, ModelBase)]
+        if not parents:
+            return super().__new__(cls, name, bases, attrs, **kwargs)
+
+        # logger.debug({k: v for k, v in attrs.items() if not k.startswith('__')})
+        module = attrs.pop('__module__')
+        new_attrs = {'__module__': module}
+        attr_meta = attrs.pop('Meta', None)
+        options = ModelOptions(name)
+        options.parents = parents
+
+        for obj_name, obj in attrs.items():
+            if obj_name.startswith("__"):
+                # builtin or inherited private vars and methods
+                new_attrs[obj_name] = obj
+
+            elif isinstance(obj, Field):
+                # save field instance
+                # attr will be overwritten as a field.type
+                # model will handle this and set default values
+                options.add_field(obj_name, obj)
+
+            elif callable(obj) or hasattr(obj, '__func__') or hasattr(obj, '__set__'):
+                # a callable method or property
+                new_attrs[obj_name] = obj
+
+            elif obj_name.startswith("_"):
+                # a private static attr not to be saved in the db
+                new_attrs[obj_name] = obj
+
+            else:
+                raise ModelException(
+                    f"public model attributes must inherit from class Field. "
+                    f"Try using a private var (eg. _{obj_name})_"
+                )
+
+        # todo: handle multiple inheritance
+        # for base in parents:
+        #     for field_name, field in base.auto_fields.items():
+        #         options.add_field(field_name, field)
+
+        new_class = super().__new__(cls, name, bases, new_attrs)
+
+        options.abstract = getattr(attr_meta, 'abstract', False)
+        options.meta = attr_meta or getattr(new_class, 'Meta', None)
+        options.base_meta = getattr(new_class, '_meta', None)
+        new_class._meta = options
+        new_class.objects = new_class.query = Query(new_class)
+        return new_class
+
+
+class Model(metaclass=ModelBase):
+    query: Query
+
+    def __init__(self, **kwargs):
+        cls = self.__class__
+        # self._ttl = kwargs.get('ttl', None)
+        # self._expire_at = kwargs.get('expire_at', None)
+
+        # allow init kwargs to set any base parameters
+        self.__dict__.update(kwargs)
+
+        # add auto KeyField if needed
+        if not len(self._meta.key_field_names):
+            self._meta.add_field('_auto_key', AutoKeyField(key=cls.__name__))
+
+        # prep AutoKeys with new default ids
+        for field in self._meta.fields.values():
+            if isinstance(field, KeyField):
+                field.set_auto_key_value()
+
+        # set defaults
+        for field_name, field in self._meta.fields.items():
+            setattr(self, field_name, field.default)
+
+        # set field values from init kwargs
+        for field_name in self._meta.fields.keys() & kwargs.keys():
+            setattr(self, field_name, kwargs.get(field_name))
+
+        # todo: handle some key management?
+        # perhaps need to prepare to handle events such as key change, partial key search, ...
+
+        self._ttl = None  # todo: set default in child Meta class
+        self._expire_at = None  # todo: datetime? or timestamp?
+
+        # validate initial attributes
+        if not self.is_valid(null_check=False):  # exclude null, will validate null values on pre-save
+            raise ModelException(f"Could not instantiate class {self}")
+
+        self._db_key = None
+        # _db_key used by Redis cannot be known without performance cost
+        # _db_key is predicted until synced during save() call
+        if None not in [getattr(self, key_field_name) for key_field_name in self._meta.key_field_names]:
+            self._db_key = self.db_key
+        self.obsolete_key = None  # to be used when db_key changes between loading and saving the object
+        self._db_content = dict()  # empty until synced during save() call
+
+    @property
+    def db_key(self):
+        """
+        the db key must include the class name - equivalent to db table name
+        keys append alphabetically.
+        if another order is required, propose feature request in GitHub issue
+        possible solutions include param on each model's KeyField order=int
+        OR model Meta: key_order = [keyname, keyname, ]
+        OR both
+        """
+        return f"{self._meta.db_class_key}:" + ":".join([
+            getattr(self, key_field_name) or "" for key_field_name in sorted(self._meta.key_field_names)
+        ])
+
+    def __repr__(self):
+        return str({k: v for k, v in self.__dict__.items() if k in self._meta.fields})
+
+    def __str__(self):
+        return str(self.db_key)
+
+    def __eq__(self, other):
+        """
+        equality method
+        instances with the same key(s) and class are considered equal
+        except when any key(s) are None, they are not equal to anything except themselves.
+
+        for evaluating all instance values against each other, use something like this:
+        self_dict = self._meta.fields.update((k, self.__dict__[k]) for k in set(self.__dict__).intersection(self._meta.fields))
+        other_dict = other._meta.fields.update((k, other.__dict__[k]) for k in set(other.__dict__).intersection(other._meta.fields))
+        return repr(dict(sorted(self_dict))) == repr(dict(sorted(other_dict)))
+        """
+        try:
+            if isinstance(other, self.__class__):
+                # always False if if any KeyFields are None
+                if (None in [
+                    self._meta.fields.get(key_field_name) for key_field_name in self._meta.key_field_names
+                ]) or (None in [
+                    other._meta.fields.get(key_field_name) for key_field_name in other._meta.key_field_names
+                ]):
+                    return repr(self) == repr(other)
+                return self.db_key == other.db_key
+        except:
+            return False
+        else:
+            return False
+
+    # @property
+    # def field_names(self):
+    #     return [
+    #         k for k, v in self.__dict__.items()
+    #         if all([not k.startswith("_"), k + "_meta" in self.__dict__])
+    #     ]
+
+    def is_valid(self, null_check=True) -> bool:
+        """
+            todo: validate values
+            - field.type ✅
+            - field.null ✅
+            - field.max_length ✅
+            - ttl, expire_at - todo
+        """
+
+        for field_name in self._meta.field_names:
+            # type check the field values against their class specified type, unless null/None
+
+            if all([
+                getattr(self, field_name) is not None,
+                not isinstance(getattr(self, field_name), self._meta.fields[field_name].type)
+            ]):
+                try:
+                    if getattr(self, field_name) is not None:
+                        setattr(self, field_name, self._meta.fields[field_name].type(getattr(self, field_name)))
+                    if not isinstance(getattr(self, field_name), self._meta.fields[field_name].type):
+                        raise TypeError(f"{field_name} is not type {self._meta.fields[field_name].type}. ")
+                except TypeError as e:
+                    logger.error(
+                        f"{str(e)} \n Change the value or modify type on {self.__class__.__name__}.{field_name}"
+                    )
+                    return False
+
+            # check non-nullable fields
+            if null_check and \
+                    self._meta.fields[field_name].null is False and \
+                    getattr(self, field_name) is None:
+                error = f"{field_name} is None/null. " \
+                        f"Set a value or set null=True on {self.__class__.__name__}.{field_name}"
+                logger.error(error)
+                return False
+
+            # validate str max_length
+            if self._meta.fields[field_name].type == str and \
+                    getattr(self, field_name) and \
+                    len(getattr(self, field_name)) > self._meta.fields[field_name].max_length:
+                error = f"{field_name} is greater than max_length={self._meta.fields[field_name].max_length}"
+                logger.error(error)
+                return False
+
+            if self._ttl and self._expire_at:
+                raise ModelException("Can set either ttl and expire_at. Not both.")
+
+        for field_name, field_value in self.__dict__.items():
+            if field_name in self._meta.fields.keys():
+                field_class = self._meta.fields[field_name].__class__
+                if not field_class.is_valid(self._meta.fields[field_name], field_value, null_check=null_check):
+                    error = f"Validation on [{field_name}] Field failed"
+                    logger.error(error)
+                    return False
+
+        return True
+
+    def save(self, pipeline: redis.client.Pipeline = None, ttl=None, expire_at=None, ignore_errors: bool = False, *args,
+             **kwargs):
+        """
+            Default save method. Uses Redis HSET command with key, dict of values, ttl.
+        """
+        if not self.is_valid():
+            error_message = "Model instance parameters invalid. Failed to save."
+            if ignore_errors:
+                logger.error(error_message)
+            else:
+                raise ModelException(error_message)
+            return pipeline or 0
+
+        ttl, expire_at = (ttl or self._ttl), (expire_at or self._expire_at)
+
+        # run any necessary formatting on field data before saving
+        for field_name, field in self._meta.fields.items():
+            setattr(
+                self, field_name,
+                field.format_value_pre_save(getattr(self, field_name))
+            )
+
+        hset_mapping = encode_popoto_model_obj(self)
+        self._db_content = hset_mapping
+        if self._db_key and self._db_key != self.db_key:
+            self.obsolete_key = self._db_key
+
+        for field_name, field in self._meta.fields.items():
+            if pipeline:
+                pipeline = field.on_save(
+                    self, field_name=field_name,
+                    field_value=getattr(self, field_name),
+                    pipeline=pipeline
+                )
+            else:
+                db_response = field.on_save(
+                    self, field_name=field_name,
+                    field_value=getattr(self, field_name)
+                )
+
+        if isinstance(pipeline, redis.client.Pipeline):
+            pipeline = pipeline.hset(self.db_key, mapping=hset_mapping)
+            pipeline = pipeline if ttl is None else pipeline.expire(self.db_key, ttl)
+            pipeline = pipeline if expire_at is None else pipeline.expire_at(self.db_key, expire_at)
+            if self.obsolete_key: pipeline.delete(self.obsolete_key)
+            self.obsolete_key = None
+            self._db_key = self.db_key
+            return pipeline
+        else:
+            db_response = POPOTO_REDIS_DB.hset(self.db_key, mapping=hset_mapping)
+            if ttl is not None: POPOTO_REDIS_DB.expire(self.db_key, ttl)
+            if expire_at is not None: POPOTO_REDIS_DB.expireat(self.db_key, ttl)
+            if self.obsolete_key: POPOTO_REDIS_DB.delete(self.obsolete_key)
+            self.obsolete_key = None
+            self._db_key = self.db_key
+            return db_response
+
+    @classmethod
+    def create(cls, **kwargs):
+        instance = cls(**kwargs)
+        instance.save()
+        return instance
+
+    @classmethod
+    def get(cls, db_key: str = None, **kwargs):
+        return cls.query.get(db_key=db_key, **kwargs)
+
+    def delete(self, pipeline=None):
+
+        for field_name, field in self._meta.fields.items():
+            if pipeline:
+                pipeline = field.on_delete(
+                    model_instance=self, field_name=field_name,
+                    pipeline=pipeline
+                )
+            else:
+                db_response = field.on_delete(
+                    model_instance=self, field_name=field_name
+                )
+
+        self._db_content = dict()
+        if pipeline is not None:
+            pipeline = pipeline.delete(self.db_key)
+            return pipeline
+        else:
+            db_response = POPOTO_REDIS_DB.delete(self.db_key)
+            if db_response >= 0:
+                return True
+
+
+    @classmethod
+    def get_info(cls):
+        from itertools import chain
+        query_filters = list(chain(*[
+            field.get_filter_query_params(field_name)
+            for field_name, field in cls._meta.fields.items()
+        ]))
+        return {
+            'name': cls.__name__,
+            'fields': cls._meta.field_names,
+            'query_filters': query_filters,
+        }
