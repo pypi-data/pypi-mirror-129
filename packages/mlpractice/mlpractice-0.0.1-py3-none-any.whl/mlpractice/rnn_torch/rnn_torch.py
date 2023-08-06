2a31c84191fe384b144c5568c6701f04de88f915
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:32 +0000

    Add package mlpractice/0.0.1/mlpractice-0.0.1-py3-none-any.whl

---
+++ packages/mlpractice/mlpractice-0.0.1-py3-none-any.whl/mlpractice/rnn_torch/rnn_torch.py
@@ -0,0 +1,18 @@
+def make_tokens(input_text):
+    r"""Computes all unique characters in the `input_text`.
+
+    Parameters
+    ----------
+    input_text : str
+        Input text for RNN training. Should be simple plain text file.
+
+    Returns
+    -------
+    tokens : list of str
+        List with all unique tokens.
+    """
+    # TODO: implement make_tokens
+    # Your final implementation shouldn't have any loops
+    raise NotImplementedError('Not implemented!')
+
+
