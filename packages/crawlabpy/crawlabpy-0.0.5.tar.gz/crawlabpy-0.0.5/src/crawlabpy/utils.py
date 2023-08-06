8db0a803cfb99eda031b9cdb015ec572b8fa206f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:40:55 +0000

    Add package crawlabpy/0.0.5/crawlabpy-0.0.5.tar.gz

---
+++ packages/crawlabpy/crawlabpy-0.0.5.tar.gz/crawlabpy-0.0.5/src/crawlabpy/utils.py
@@ -0,0 +1,40 @@
+import os
+import paramiko
+import requests
+from crawlabpy.datapool import get_target_config
+from crawlabpy.encoder import JSONEncoder
+
+
+def notify_target(item, files):
+    json_encoder = JSONEncoder()
+    target = get_target_config()
+    response = requests.post(target.Notify, json={
+        'files': files,
+        'record': json_encoder.encode(item)
+    })
+    if response and response['Status'] != 'ok':
+        print('notify target error:{}'.format(response['Message']))
+
+
+# 上传文件
+def save_file(name, data):
+    # 获取配置
+    target = get_target_config()
+    local = '/tmp/{}'.format(name)
+    remote = os.path.join(target.Path, name)
+    # 将图片写到本地
+    with open(local, 'wb+') as img:
+        img.write(data)
+    sf = paramiko.Transport((target.Host, int(target.Port)))
+    sf.connect(username=target.Username, password=target.Password)
+    sftp = paramiko.SFTPClient.from_transport(sf)
+    try:
+        if os.path.isdir(local):
+            for f in os.listdir(local):
+                sftp.put(os.path.join(local + f), os.path.join(remote + f))
+        else:
+            sftp.put(local, remote)
+    except Exception:
+        print('upload error:')
+    sf.close()
+    os.unlink(local)
