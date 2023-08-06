e44547fab62dd29b222e7606ae3ef8e6090e2607
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:30 +0000

    Add package jupyterlab-scenes/1.2.0/jupyterlab_scenes-1.2.0-py3-none-any.whl

---
+++ packages/jupyterlab-scenes/jupyterlab_scenes-1.2.0-py3-none-any.whl/jupyterlab_scenes-1.2.0.data/data/share/jupyter/labextensions/jupyterlab_scenes/static/style_index_js.0906ca7ddc5e94ee5d11.js
@@ -0,0 +1,73 @@
+"use strict";
+(self["webpackChunkjupyterlab_scenes"] = self["webpackChunkjupyterlab_scenes"] || []).push([["style_index_js"],{
+
+/***/ "./node_modules/css-loader/dist/cjs.js!./style/base.css":
+/*!**************************************************************!*\
+  !*** ./node_modules/css-loader/dist/cjs.js!./style/base.css ***!
+  \**************************************************************/
+/***/ ((module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony import */ var _node_modules_css_loader_dist_runtime_cssWithMappingToString_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../node_modules/css-loader/dist/runtime/cssWithMappingToString.js */ "./node_modules/css-loader/dist/runtime/cssWithMappingToString.js");
+/* harmony import */ var _node_modules_css_loader_dist_runtime_cssWithMappingToString_js__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_node_modules_css_loader_dist_runtime_cssWithMappingToString_js__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var _node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../node_modules/css-loader/dist/runtime/api.js */ "./node_modules/css-loader/dist/runtime/api.js");
+/* harmony import */ var _node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(_node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_1__);
+// Imports
+
+
+var ___CSS_LOADER_EXPORT___ = _node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_1___default()((_node_modules_css_loader_dist_runtime_cssWithMappingToString_js__WEBPACK_IMPORTED_MODULE_0___default()));
+// Module
+___CSS_LOADER_EXPORT___.push([module.id, ".scenes-ScenesSidebar {\n    flex: 1 1 auto;\n    margin: 0;\n    padding: 0;\n    list-style-type: none;\n    background: var(--jp-layout-color1);\n    color: var(--jp-ui-font-color1);\n    font-size: var(--jp-ui-font-size1);\n    height: 100%;\n  }\n\n.scenes-ScenesSidebar .scenes-Header {\n    flex: 0 0 auto;\n    margin: 0px;\n    padding: 8px 12px;\n    text-transform: uppercase;\n    font-size: var(--jp-ui-font-size0);\n    font-weight: 600;\n    letter-spacing: 1px;\n    border-bottom: var(--jp-border-width) solid var(--jp-border-color2);\n}\n\n/* *****************************************************************\n * Toolbar\n * *****************************************************************/\n\n .scenes-Toolbar {\n    flex: 0 0 auto;\n    display: flex;\n    align-items: center;\n    flex-direction: row;\n    padding: 4px;\n    margin: 0px;\n    border-bottom: var(--jp-border-width) solid var(--jp-border-color2);\n}\n\n.scenes-ToolbarButton {\n    height: 24px;\n    width: 24px;\n    background-color: inherit;\n\n    display: inline-block;\n    align-items: center;\n    vertical-align: middle;\n    border: none;\n    margin: 0px;\n    outline: none;\n    padding: 2px;\n}\n\n.scenes-ToolbarButton:hover {\n    background-color: var(--jp-layout-color2);\n    cursor: pointer;\n}\n\n\n/* *****************************************************************\n * Scene List\n * *****************************************************************/\n\n.scenes-ScenesSidebar .scenes-SceneItem {\n    padding: 7px;\n    margin: 5px;\n    height: 28px;\n    background: var(--jp-layout-color2);\n\n    flex: 0 0 auto;\n    display: flex;\n    align-items: center;\n    flex-direction: row;\n}\n/*\n.scenes-active {\nbackground: var(--jp-layout-color3);\nfont-weight: bold;\n}\n\n.scenes-SceneItemSpacer {\nflex-grow: 1;\nflex-shrink: 1;\n}\n\n.scenes-ItemText {\npadding-left: 6px;\n}\n\n/* just testing\n.scenes-IconButton .jp-icon3 {\n    fill:rgb(129, 68, 129) !important;\n}\n*/\n\n\n\n/* *****************************************************************\n * To be cleaned-up\n * *****************************************************************/\n\n .scenes-IconButton {\n    height: 16px;\n    width: 16px;\n    background-color: inherit;\n\n    display: inline-block;\n    align-items: center;\n    vertical-align: middle;\n    border: none;\n    margin: 0px;\n    outline: none;\n    padding: 2px;\n}\n\n.scenes-IconButton:hover {\n    background-color: var(--jp-layout-color2);\n    cursor: pointer;\n}\n\n.scenes-ScenesSidebar .scenes-SceneItem:hover {\n    background: var(--jp-layout-color4);\n    cursor: pointer;\n}\n\n\n\n.scenes-ScenesSidebar .scenes-SceneItemActive:hover {\n    background: var(--jp-layout-color4);\n    font-weight: bold;\n    cursor: pointer;\n}\n\n\n.scene-cell .CodeMirror:after {\n    height: 10px;\n    width: 10px;\n    display: block;\n    content: '';\n    position: absolute;\n    right: 10px;\n    top: 10px;\n    border-radius: 50%;\n    background-color: rgb(129, 68, 129);\n}\n\n.scenes-InitSceneButton {\n    border-radius: 8px;\n    color: var(--jp-ui-font-color3);\n    border-style: solid;\n    border-color: var(--jp-ui-font-color3);\n    float: right;\n    background-color: inherit;\n    border-width: 2px;\n\n}\n\n.scenes-InitSceneButton:hover {\n    color: var(--jp-ui-font-color2);\n    border-color: var(--jp-ui-font-color2);\n    border-width: 4px;\n}\n\n.scenes-InitSceneButtonActive {\n    border-radius: 8px;\n    color: rgb(129, 68, 129);\n    border-color: rgb(129, 68, 129);\n    float: right;\n    border-style: solid;\n    background-color: inherit;\n    border-width: 2px;\n}\n\n.scenes-InitSceneButtonActive:hover {\n    border-width: 4px;\n}\n\n/* Scene Item */\n\n", "",{"version":3,"sources":["webpack://./style/base.css"],"names":[],"mappings":"AAAA;IACI,cAAc;IACd,SAAS;IACT,UAAU;IACV,qBAAqB;IACrB,mCAAmC;IACnC,+BAA+B;IAC/B,kCAAkC;IAClC,YAAY;EACd;;AAEF;IACI,cAAc;IACd,WAAW;IACX,iBAAiB;IACjB,yBAAyB;IACzB,kCAAkC;IAClC,gBAAgB;IAChB,mBAAmB;IACnB,mEAAmE;AACvE;;AAEA;;oEAEoE;;CAEnE;IACG,cAAc;IACd,aAAa;IACb,mBAAmB;IACnB,mBAAmB;IACnB,YAAY;IACZ,WAAW;IACX,mEAAmE;AACvE;;AAEA;IACI,YAAY;IACZ,WAAW;IACX,yBAAyB;;IAEzB,qBAAqB;IACrB,mBAAmB;IACnB,sBAAsB;IACtB,YAAY;IACZ,WAAW;IACX,aAAa;IACb,YAAY;AAChB;;AAEA;IACI,yCAAyC;IACzC,eAAe;AACnB;;;AAGA;;oEAEoE;;AAEpE;IACI,YAAY;IACZ,WAAW;IACX,YAAY;IACZ,mCAAmC;;IAEnC,cAAc;IACd,aAAa;IACb,mBAAmB;IACnB,mBAAmB;AACvB;AACA;;;;;;;;;;;;;;;;;;;CAmBC;;;;AAID;;oEAEoE;;CAEnE;IACG,YAAY;IACZ,WAAW;IACX,yBAAyB;;IAEzB,qBAAqB;IACrB,mBAAmB;IACnB,sBAAsB;IACtB,YAAY;IACZ,WAAW;IACX,aAAa;IACb,YAAY;AAChB;;AAEA;IACI,yCAAyC;IACzC,eAAe;AACnB;;AAEA;IACI,mCAAmC;IACnC,eAAe;AACnB;;;;AAIA;IACI,mCAAmC;IACnC,iBAAiB;IACjB,eAAe;AACnB;;;AAGA;IACI,YAAY;IACZ,WAAW;IACX,cAAc;IACd,WAAW;IACX,kBAAkB;IAClB,WAAW;IACX,SAAS;IACT,kBAAkB;IAClB,mCAAmC;AACvC;;AAEA;IACI,kBAAkB;IAClB,+BAA+B;IAC/B,mBAAmB;IACnB,sCAAsC;IACtC,YAAY;IACZ,yBAAyB;IACzB,iBAAiB;;AAErB;;AAEA;IACI,+BAA+B;IAC/B,sCAAsC;IACtC,iBAAiB;AACrB;;AAEA;IACI,kBAAkB;IAClB,wBAAwB;IACxB,+BAA+B;IAC/B,YAAY;IACZ,mBAAmB;IACnB,yBAAyB;IACzB,iBAAiB;AACrB;;AAEA;IACI,iBAAiB;AACrB;;AAEA,eAAe","sourcesContent":[".scenes-ScenesSidebar {\n    flex: 1 1 auto;\n    margin: 0;\n    padding: 0;\n    list-style-type: none;\n    background: var(--jp-layout-color1);\n    color: var(--jp-ui-font-color1);\n    font-size: var(--jp-ui-font-size1);\n    height: 100%;\n  }\n\n.scenes-ScenesSidebar .scenes-Header {\n    flex: 0 0 auto;\n    margin: 0px;\n    padding: 8px 12px;\n    text-transform: uppercase;\n    font-size: var(--jp-ui-font-size0);\n    font-weight: 600;\n    letter-spacing: 1px;\n    border-bottom: var(--jp-border-width) solid var(--jp-border-color2);\n}\n\n/* *****************************************************************\n * Toolbar\n * *****************************************************************/\n\n .scenes-Toolbar {\n    flex: 0 0 auto;\n    display: flex;\n    align-items: center;\n    flex-direction: row;\n    padding: 4px;\n    margin: 0px;\n    border-bottom: var(--jp-border-width) solid var(--jp-border-color2);\n}\n\n.scenes-ToolbarButton {\n    height: 24px;\n    width: 24px;\n    background-color: inherit;\n\n    display: inline-block;\n    align-items: center;\n    vertical-align: middle;\n    border: none;\n    margin: 0px;\n    outline: none;\n    padding: 2px;\n}\n\n.scenes-ToolbarButton:hover {\n    background-color: var(--jp-layout-color2);\n    cursor: pointer;\n}\n\n\n/* *****************************************************************\n * Scene List\n * *****************************************************************/\n\n.scenes-ScenesSidebar .scenes-SceneItem {\n    padding: 7px;\n    margin: 5px;\n    height: 28px;\n    background: var(--jp-layout-color2);\n\n    flex: 0 0 auto;\n    display: flex;\n    align-items: center;\n    flex-direction: row;\n}\n/*\n.scenes-active {\nbackground: var(--jp-layout-color3);\nfont-weight: bold;\n}\n\n.scenes-SceneItemSpacer {\nflex-grow: 1;\nflex-shrink: 1;\n}\n\n.scenes-ItemText {\npadding-left: 6px;\n}\n\n/* just testing\n.scenes-IconButton .jp-icon3 {\n    fill:rgb(129, 68, 129) !important;\n}\n*/\n\n\n\n/* *****************************************************************\n * To be cleaned-up\n * *****************************************************************/\n\n .scenes-IconButton {\n    height: 16px;\n    width: 16px;\n    background-color: inherit;\n\n    display: inline-block;\n    align-items: center;\n    vertical-align: middle;\n    border: none;\n    margin: 0px;\n    outline: none;\n    padding: 2px;\n}\n\n.scenes-IconButton:hover {\n    background-color: var(--jp-layout-color2);\n    cursor: pointer;\n}\n\n.scenes-ScenesSidebar .scenes-SceneItem:hover {\n    background: var(--jp-layout-color4);\n    cursor: pointer;\n}\n\n\n\n.scenes-ScenesSidebar .scenes-SceneItemActive:hover {\n    background: var(--jp-layout-color4);\n    font-weight: bold;\n    cursor: pointer;\n}\n\n\n.scene-cell .CodeMirror:after {\n    height: 10px;\n    width: 10px;\n    display: block;\n    content: '';\n    position: absolute;\n    right: 10px;\n    top: 10px;\n    border-radius: 50%;\n    background-color: rgb(129, 68, 129);\n}\n\n.scenes-InitSceneButton {\n    border-radius: 8px;\n    color: var(--jp-ui-font-color3);\n    border-style: solid;\n    border-color: var(--jp-ui-font-color3);\n    float: right;\n    background-color: inherit;\n    border-width: 2px;\n\n}\n\n.scenes-InitSceneButton:hover {\n    color: var(--jp-ui-font-color2);\n    border-color: var(--jp-ui-font-color2);\n    border-width: 4px;\n}\n\n.scenes-InitSceneButtonActive {\n    border-radius: 8px;\n    color: rgb(129, 68, 129);\n    border-color: rgb(129, 68, 129);\n    float: right;\n    border-style: solid;\n    background-color: inherit;\n    border-width: 2px;\n}\n\n.scenes-InitSceneButtonActive:hover {\n    border-width: 4px;\n}\n\n/* Scene Item */\n\n"],"sourceRoot":""}]);
+// Exports
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (___CSS_LOADER_EXPORT___);
+
+
+/***/ }),
+
+/***/ "./style/base.css":
+/*!************************!*\
+  !*** ./style/base.css ***!
+  \************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony import */ var _node_modules_style_loader_dist_runtime_injectStylesIntoStyleTag_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! !../node_modules/style-loader/dist/runtime/injectStylesIntoStyleTag.js */ "./node_modules/style-loader/dist/runtime/injectStylesIntoStyleTag.js");
+/* harmony import */ var _node_modules_style_loader_dist_runtime_injectStylesIntoStyleTag_js__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_node_modules_style_loader_dist_runtime_injectStylesIntoStyleTag_js__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var _node_modules_css_loader_dist_cjs_js_base_css__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! !!../node_modules/css-loader/dist/cjs.js!./base.css */ "./node_modules/css-loader/dist/cjs.js!./style/base.css");
+
+            
+
+var options = {};
+
+options.insert = "head";
+options.singleton = false;
+
+var update = _node_modules_style_loader_dist_runtime_injectStylesIntoStyleTag_js__WEBPACK_IMPORTED_MODULE_0___default()(_node_modules_css_loader_dist_cjs_js_base_css__WEBPACK_IMPORTED_MODULE_1__["default"], options);
+
+
+
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (_node_modules_css_loader_dist_cjs_js_base_css__WEBPACK_IMPORTED_MODULE_1__["default"].locals || {});
+
+/***/ }),
+
+/***/ "./style/index.js":
+/*!************************!*\
+  !*** ./style/index.js ***!
+  \************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _base_css__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./base.css */ "./style/base.css");
+
+
+
+/***/ })
+
+}]);
+//# sourceMappingURL=style_index_js.0906ca7ddc5e94ee5d11.js.map
