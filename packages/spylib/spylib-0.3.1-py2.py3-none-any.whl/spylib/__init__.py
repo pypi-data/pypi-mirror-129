d7faa67b83ca695df3beb1b18f814d050c58b4e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:44:22 +0000

    Add package spylib/0.3.1/spylib-0.3.1-py2.py3-none-any.whl

---
+++ packages/spylib/spylib-0.3.1-py2.py3-none-any.whl/spylib/__init__.py
@@ -0,0 +1,8 @@
+"""A library to facilitate interfacing with Shopify's API"""
+
+__version__ = '0.3.1'
+
+
+from .token import OfflineTokenABC, OnlineTokenABC, Token
+
+__all__ = ['OfflineTokenABC', 'OnlineTokenABC', 'Token']
