787b67ea3f82b0651b87e8ce6087a5969a1c5c6a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:15:50 +0000

    Add package imageSizeReducer/0.1.7/imageSizeReducer-0.1.7.tar.gz

---
+++ packages/imageSizeReducer/imageSizeReducer-0.1.7.tar.gz/imageSizeReducer-0.1.7/setup.py
@@ -0,0 +1,29 @@
+import pathlib
+from setuptools import find_packages, setup
+
+Here = pathlib.Path(__file__).parent
+
+README = (Here / "README.md").read_text()
+
+setup(
+    name='imageSizeReducer',
+    packages=find_packages(include=['./imageResize/imageResize.py']),
+    version='0.1.7',
+    description='Reduce image size',
+    long_description=README,
+    long_description_content_type="text/markdown",
+    author='Bramhesh Kumar Srivastava',
+    author_email='brahmesh1996@gmail.com',
+    license='MIT',
+    install_requires=['pillow', 'python-math'],
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest==4.4.1'],
+    test_suite='tests',
+    classifiers = [
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+    ]
+)
