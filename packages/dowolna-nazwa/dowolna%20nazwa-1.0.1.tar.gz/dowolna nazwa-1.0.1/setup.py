8a80e14f17195605dfec6706ac82c729296dece0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:27:14 +0000

    Add package dowolna-nazwa/1.0.1/dowolna%20nazwa-1.0.1.tar.gz

---
+++ packages/dowolna-nazwa/dowolna%20nazwa-1.0.1.tar.gz/dowolna nazwa-1.0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name = "dowolna nazwa",
+    version = "1.0.1",
+    description = "Pakiet testowy",
+    url = "http://google.com",
+    install_requires = [],
+    packages = ["pakiet"],
+)
