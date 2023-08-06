94d160533f2effc2159d9b8608bf9d32703e687d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:16:07 +0000

    Add package django-dataclasses/0.1.4/django_dataclasses-0.1.4-py3-none-any.whl

---
+++ packages/django-dataclasses/django_dataclasses-0.1.4-py3-none-any.whl/django_dataclasses/__init__.py
@@ -0,0 +1,15 @@
+from .api import get, patch, post, put
+from .auth import login_required
+from .dataclasses import dataclass
+from .schema import ErrorResponse, enforce_schemas
+
+__all__ = [
+    "login_required",
+    "dataclass",
+    "ErrorResponse",
+    "enforce_schemas",
+    "get",
+    "post",
+    "put",
+    "patch",
+]
