816c0d533ab561149d66b9ac7d92155fd28cf82a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:38:10 +0000

    Add package eolian/0.1.2/eolian-0.1.2-py3-none-any.whl

---
+++ packages/eolian/eolian-0.1.2-py3-none-any.whl/eolian/__init__.py
@@ -0,0 +1,19 @@
+# type: ignore[attr-defined]
+"""Core package for the EOLIAN platform which includes database models and Azure wrappers for faster development."""
+
+import sys
+
+if sys.version_info >= (3, 8):
+    from importlib import metadata as importlib_metadata
+else:
+    import importlib_metadata
+
+
+def get_version() -> str:
+    try:
+        return importlib_metadata.version(__name__)
+    except importlib_metadata.PackageNotFoundError:  # pragma: no cover
+        return "unknown"
+
+
+version: str = get_version()
