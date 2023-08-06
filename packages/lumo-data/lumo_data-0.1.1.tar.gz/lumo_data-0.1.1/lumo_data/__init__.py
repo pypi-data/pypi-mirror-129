5dca78eecb5a3b8f75489b213ecb2af6207dae4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:00:45 +0000

    Add package lumo-data/0.1.1/lumo_data-0.1.1.tar.gz

---
+++ packages/lumo-data/lumo_data-0.1.1.tar.gz/lumo_data-0.1.1/lumo_data/__init__.py
@@ -0,0 +1,4 @@
+from .dataset import BatchDataset, DBDataset
+from .dataset import BatchDataset as Dataset
+from .dataloader import LokyDataLoader
+from .dataloader import LokyDataLoader as DataLoader
