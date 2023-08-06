15ecbc730886b70471d5931a81b1f149504e9ac4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:13:18 +0000

    Add package ghana/1.0.0/ghana-1.0.0-py3-none-any.whl

---
+++ packages/ghana/ghana-1.0.0-py3-none-any.whl/ghana.py
@@ -0,0 +1,66 @@
+# Regions in ghana and their capitals
+
+country_data = {
+    'AHAFO': 'GOASO',
+    'ASHANTI': 'KUMASI',
+    'BONO EAST': 'TECHIMAN',
+    'BRONG AHAFO': 'SUNYANI',
+    'CENTRAL': 'CAPE COAST',
+    'EASTERN': 'KOFORIDUA',
+    'GREATER ACCRA': 'ACCRA',
+    'NORTH EAST': 'NALERIGU',
+    'NORTHERN': 'TAMALE',
+    'OTI': 'DAMBAI',
+    'SAVANNAH': 'DAMONGO',
+    'UPPER EAST': 'BOLGATANGA',
+    'UPPER WEST': 'WA',
+    'WESTERN': 'SEKONDI-TAKORADI',
+    'WESTERN NORTH': 'SEFWI WIASO',
+    'VOLTA': 'HO',
+}
+
+regions = [
+    'AHAFO', 'ASHANTI', 'BONO EAST', 'BRONG AHAFO',
+    'CENTRAL', 'EASTERN', 'GREATER ACCRA', 'NORTH EAST',
+    'NORTHERN', 'OTI', 'SAVANNAH', 'UPPER EAST',
+    'UPPER WEST', 'WESTERN', 'WESTERN NORTH', 'VOLTA'
+]
+
+capitals = [
+    'GOASO', 'KUMASI', 'TECHIMAN', 'SUNYANI',
+    'CAPE COAST', 'KOFORIDUA', 'ACCRA', 'NALERIGU',
+    'TAMALE', 'DAMBAI', 'DAMONGO', 'BOLGATANGA',
+    'WA', 'SEKONDI-TAKORADI', 'SEFWI WIASO', 'HO'
+]
+
+
+def region_art():
+    print("""
+     ____                   _                       
+    |  _ \    ___    __ _  (_)   ___    _ __    ___ 
+    | |_) |  / _ \  / _` | | |  / _ \  | '_ \  / __|
+    |  _ <  |  __/ | (_| | | | | (_) | | | | | \____
+    |_| \_\  \___|  \__, | |_|  \___/  |_| |_| |___/
+                    |___/                           
+    """)
+
+
+def capital_art():
+    print(""" 
+      ____                    _   _             _       
+     / ___|    __ _   _ __   (_) | |_    __ _  | |  ___ 
+    | |       / _` | | '_ \  | | | __|  / _` | | | / __|
+    | |___   | (_| | | |_) | | | | |_  | (_| | | | \____
+     \____|   \__,_| | .__/  |_|  \__|  \__,_| |_| |___/
+                     |_| 
+    """)
+
+
+def ghana_art():
+    print(""" 
+      ____   _                             
+     / ___| | |__     __ _   _ __     __ _ 
+    | |  _  | '_ \   / _` | | '_ \   / _` |
+    | |_| | | | | | | (_| | | | | | | (_| |
+     \____| |_| |_|  \__,_| |_| |_|  \__,_|
+    """)
