5e5d48613684ba0c1a62f9d67da2cd5cd89c46ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:26:16 +0000

    Add package filedate/1.0/filedate-1.0-py3-none-any.whl

---
+++ packages/filedate/filedate-1.0-py3-none-any.whl/filedate/__init__.py
@@ -0,0 +1,11 @@
+"""Simple file-date changing utility"""
+import os
+from .__main__ import *
+from .Utils import *
+
+#---#
+
+__author__		= "kubinka0505"
+__credits__		= __author__
+__version__		= "1.0"
+__date__		= "04.12.2020"
