001974a666934bb5f0004608b2185388ee5540bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:21:20 +0000

    Add package easy-biologic/0.3.2/easy-biologic-0.3.2.tar.gz

---
+++ packages/easy-biologic/easy-biologic-0.3.2.tar.gz/easy-biologic-0.3.2/setup.py
@@ -0,0 +1,40 @@
+import setuptools
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+project_urls = {
+    'Source Code':      'https://github.com/bicarlsen/easy-biologic',
+    'Bug Tracker':      'https://github.com/bicarlsen/easy-biologic/issues'
+}
+
+
+setuptools.setup(
+    name = "easy-biologic",
+    version = "0.3.2",
+    author = "Brian Carlsen",
+    author_email = "carlsen.bri@gmail.com",
+    description = "Controller class for communicating with BioLogic devices.",
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
+    keywords = [ 'biologic' ],
+    url = "",
+    project_urls = project_urls,
+    packages = setuptools.find_packages(),
+    python_requires = '>=3.7',
+    classifiers = [
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: Microsoft :: Windows",
+        "Development Status :: 3 - Alpha"
+    ],
+    install_requires = [],
+    package_data = {
+        'easy_biologic': [
+            'technique_verison.json',
+            f'techniques-*/*'
+        ]
+    }
+)
