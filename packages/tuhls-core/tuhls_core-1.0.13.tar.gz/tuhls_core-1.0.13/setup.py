2dd62dca9fda49abe90274a42b708094fd4f039e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:36 +0000

    Add package tuhls-core/1.0.13/tuhls_core-1.0.13.tar.gz

---
+++ packages/tuhls-core/tuhls_core-1.0.13.tar.gz/tuhls_core-1.0.13/setup.py
@@ -0,0 +1,20 @@
+from setuptools import find_packages, setup
+
+import versioneer
+
+setup(
+    name="tuhls_core",
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+    packages=find_packages(),
+    package_data={
+        "tuhls_design": ("static/country_flags/svg/*.svg", "static/css/dist/style.css"),
+    },
+    install_requires=["django"],
+    license_files=("LICENSE",),
+    url="https://gitlab.com/tuhls/tuhls_core",
+    author="Herbert Rusznak (tlb)",
+    author_email="herbert.rusznak@gmail.com",
+    license="MIT",
+    python_requires=">=3.8",
+)
