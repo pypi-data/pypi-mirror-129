dee7a63346df09550fd56331f8b97648321741bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:51 +0000

    Add package aa-fleetpings/2.7.0/aa-fleetpings-2.7.0.tar.gz

---
+++ packages/aa-fleetpings/aa-fleetpings-2.7.0.tar.gz/aa-fleetpings-2.7.0/fleetpings/form.py
@@ -0,0 +1,24 @@
+"""
+form declarations
+"""
+
+from django.forms import ModelForm, TextInput
+
+from fleetpings.models import FleetType
+
+
+class FleetTypeAdminForm(ModelForm):
+    """
+    form definitions for the FleetType form in admin
+    """
+
+    class Meta:  # pylint: disable=too-few-public-methods
+        """
+        Meta
+        """
+
+        model = FleetType
+        fields = "__all__"
+        widgets = {
+            "embed_color": TextInput(attrs={"type": "color"}),
+        }
