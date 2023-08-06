e76471e77acc7eb9577c6b01de67ae0381e1cbbe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:45 +0000

    Add package places-geocode/0.2.3/places_geocode-0.2.3.tar.gz

---
+++ packages/places-geocode/places_geocode-0.2.3.tar.gz/places_geocode-0.2.3/setup.py
@@ -0,0 +1,44 @@
+"""setup file for places geocoding package"""
+from setuptools import setup, find_packages
+import setuptools
+from pathlib import Path
+this_directory = Path(__file__).parent
+
+VERSION = '0.2.3'
+DESCRIPTION = 'Package for Places Geocoding API service'
+
+# Setting up
+setup(
+    name="places_geocode",
+    version=VERSION,
+    author="Martin Mashalov",
+    author_email="hello@places.place",
+    description=DESCRIPTION,
+    long_description=open('/Users/martinmashalov/Documents/places_geocoding_package/README.md').read(),
+    long_description_content_type="text/markdown",
+    #packages=['places_geocode'],
+    packages=setuptools.find_packages(where="places_geocode"),
+    license="MIT",
+    package_dir={"": "places_geocode"},
+    install_requires=['pymongo', 'numpy', 'pandas', 'sklearn', 'dependency_injector', 'pydantic', 'fastapi', 'cython',
+                      'state_machine'],
+    keywords=['python',
+              'geocoding',
+              'forward geocoding',
+              'reverse geocoding',
+              'radius loading',
+              'loading radius',
+              'autocomplete',
+              'places of interest',
+              'POI'
+              ],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "License :: Free for non-commercial use",
+        "Intended Audience :: Information Technology",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
