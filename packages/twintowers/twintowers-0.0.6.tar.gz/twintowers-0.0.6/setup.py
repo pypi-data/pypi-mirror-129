f83991f22b860a0899bc3f1466ddfcef9c173f56
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:30:31 +0000

    Add package twintowers/0.0.6/twintowers-0.0.6.tar.gz

---
+++ packages/twintowers/twintowers-0.0.6.tar.gz/twintowers-0.0.6/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+setup(
+    name="twintowers",
+    version="0.0.6",
+    author="LTEnjoy",
+    author_email="sujinltenjoy@gmail.com",
+    description="双塔模型搜索蛋白质同源序列",
+    install_requires=['faiss'],
+    
+    # 你要安装的包，通过 setuptools.find_packages 找到当前目录下有哪些包
+    packages=find_packages(),
+    
+    package_data={
+        "software": ['*']
+    },
+    
+    entry_points={
+        'console_scripts': [
+            'twintowers = TwinTowers.command:tasks'
+        ]
+    }
+)
