d1f95ec83c2c576906e3925be3bc5a5dd7272c58
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:11:20 +0000

    Add package negosaki-lib/0.0.1/negosaki-lib-0.0.1.tar.gz

---
+++ packages/negosaki-lib/negosaki-lib-0.0.1.tar.gz/negosaki-lib-0.0.1/src/negosaki/example.py
@@ -0,0 +1,7 @@
+def add_one(number: int) -> int:
+    """
+    Adds one the given number and returns the result.
+    Arguments:
+    number: int - The number to add one to
+    """
+    return number + 1
