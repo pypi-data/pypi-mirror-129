f0d355d9a7f60531498ff51eef586738539f3c8b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:47:36 +0000

    Add package fft-gyujin/1.0.0/fft_gyujin-1.0.0.tar.gz

---
+++ packages/fft-gyujin/fft_gyujin-1.0.0.tar.gz/fft_gyujin-1.0.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+        name = 'fft_gyujin',
+        version = '1.0.0',
+	license = 'MIT',
+        author = 'gyujin',
+        author_email = 'limmok0731@naver.com',
+	description = 'fft',
+        url = None,
+        py_modules = ['fft_gyujin']
+)
