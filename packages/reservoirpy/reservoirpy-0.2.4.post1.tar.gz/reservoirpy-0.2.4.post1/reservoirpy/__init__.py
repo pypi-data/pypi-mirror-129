c4c4b1b85919f041e990d9ce10dfcfd2b524f5a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:10 +0000

    Add package reservoirpy/0.2.4.post1/reservoirpy-0.2.4.post1.tar.gz

---
+++ packages/reservoirpy/reservoirpy-0.2.4.post1.tar.gz/reservoirpy-0.2.4.post1/reservoirpy/__init__.py
@@ -0,0 +1,32 @@
+import os
+import tempfile
+
+from ._version import __version__
+
+from .utils.save import load
+
+from . import mat_gen
+from . import observables
+from . import regression_models
+from . import activationsfunc
+from . import hyper
+
+from ._esn import ESN
+from ._esn_online import ESNOnline
+
+_TEMPDIR = os.path.join(tempfile.gettempdir(), "reservoirpy-temp")
+if not os.path.exists(_TEMPDIR):
+    try:
+        os.mkdir(_TEMPDIR)
+    except OSError:
+        _TEMPDIR = tempfile.gettempdir()
+
+tempfile.tempdir = _TEMPDIR
+
+__all__ = [
+    "__version__",
+    "ESN", "ESNOnline",
+    "mat_gen", "activationsfunc",
+    "observables", "regression_models",
+    "hyper", "load"
+]
