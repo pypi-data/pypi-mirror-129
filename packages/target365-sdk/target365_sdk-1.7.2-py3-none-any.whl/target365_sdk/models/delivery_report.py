7c55e8cbf6a22c18841320751672fea6f54050dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:28 +0000

    Add package target365-sdk/1.7.2/target365_sdk-1.7.2-py3-none-any.whl

---
+++ packages/target365-sdk/target365_sdk-1.7.2-py3-none-any.whl/target365_sdk/models/delivery_report.py
@@ -0,0 +1,20 @@
+from .model import Model
+
+class DeliveryReport(Model):
+
+    def _accepted_params(self):
+        return [
+            'correlationId',
+            'sessionId',
+            'transactionId',
+            'price',
+            'sender',
+            'recipient',
+            'operatorId',
+            'statusCode',
+            'detailedStatusCode',
+            'delivered',
+            'billed',
+            'smscTransactionId',
+            'smscMessageParts',
+        ]
