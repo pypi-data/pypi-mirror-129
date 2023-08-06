b5150e781ab35277b2ea5aaf50fcefdcf9dc592a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:40:34 +0000

    Add package python-web-exceptions/1.0/python_web_exceptions-1.0-py3-none-any.whl

---
+++ packages/python-web-exceptions/python_web_exceptions-1.0-py3-none-any.whl/python_web_exceptions/base_http_exception.py
@@ -0,0 +1,20 @@
+from typing import Optional, Union
+
+
+class BaseHttpException(Exception):
+
+    status_code: int
+    message: str
+    header: Union[str, None]
+
+    def __init__(self, status_code: int, message: str, header: Optional[str] = None) -> None:
+        self.status_code = status_code
+        self.message = message
+        self.header = header
+    
+    def __repr__(self) -> str:
+        class_name = self.__class__.__name__
+        if (self.header is None):
+            return f"{class_name}(status_code={self.status_code}, message={self.message})"
+        else:
+            return f"{class_name}(status_code={self.status_code}, message={self.message}, header={self.header})"
