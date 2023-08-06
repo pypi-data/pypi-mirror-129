b314ce601f8582b70e002f7bf3f3ffc2aef1c58a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:10 +0000

    Add package keygen-licensing-tools/0.2.0/keygen_licensing_tools-0.2.0.tar.gz

---
+++ packages/keygen-licensing-tools/keygen_licensing_tools-0.2.0.tar.gz/keygen_licensing_tools-0.2.0/src/keygen_licensing_tools/__init__.py
@@ -0,0 +1,10 @@
+from ._exceptions import ValidationError
+from ._main import validate_license_key_cached, validate_license_key_online
+from ._offline_keys import validate_offline_key
+
+__all__ = [
+    "validate_license_key_online",
+    "validate_license_key_cached",
+    "validate_offline_key",
+    "ValidationError",
+]
