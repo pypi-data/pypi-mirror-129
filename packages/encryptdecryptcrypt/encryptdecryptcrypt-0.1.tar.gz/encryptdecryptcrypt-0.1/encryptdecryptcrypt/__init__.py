7ea9912a736c5b30c926c693b93ccca382140476
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:58 +0000

    Add package encryptdecryptcrypt/0.1/encryptdecryptcrypt-0.1.tar.gz

---
+++ packages/encryptdecryptcrypt/encryptdecryptcrypt-0.1.tar.gz/encryptdecryptcrypt-0.1/encryptdecryptcrypt/__init__.py
@@ -0,0 +1,4 @@
+from encryptdecryptcrypt import encrypt
+from encryptdecryptcrypt import decrypt
+from encryptdecryptcrypt import encryptfromfile
+from encryptdecryptcrypt import decryptfromfile
---
+++ packages/encryptdecryptcrypt/encryptdecryptcrypt-0.1.tar.gz/encryptdecryptcrypt-0.1/setup.py
@@ -0,0 +1,28 @@
+from distutils.core import setup
+setup(
+  name = 'encryptdecryptcrypt',
+  packages = ['encryptdecryptcrypt'],
+  version = '0.1',
+  license='MIT',
+  description = 'simple vignere encoder with numbers as keys',
+  author = 'Sreenath Sreekrishna',
+  author_email = 'sreenath.sreekrishna.21@gmail.com',
+  url = 'https://github.com/SreenathSreekrishna/encryptdecryptcrypt',
+  download_url = 'https://github.com/SreenathSreekrishna/encryptdecryptcrypt/archive/refs/tags/v_01.tar.gz',
+  keywords = ['python', 'cryptography', 'encoder', 'decoder'],
+  install_requires=[],
+  classifiers=[
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: 3.9',
+    'Programming Language :: Python :: 3.10',
+  ],
+)
