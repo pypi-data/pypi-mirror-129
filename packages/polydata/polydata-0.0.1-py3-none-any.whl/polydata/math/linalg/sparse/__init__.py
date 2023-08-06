be2c6de945199fbe26ecd40a161e30aebfb57ae5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:34 +0000

    Add package polydata/0.0.1/polydata-0.0.1-py3-none-any.whl

---
+++ packages/polydata/polydata-0.0.1-py3-none-any.whl/polydata/math/linalg/sparse/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul  2 18:31:06 2018
+
+@author: Bence
+"""
+
+from polydata.math.linalg.sparse.csr import csr_matrix
+
---
+++ packages/polydata/polydata-0.0.1-py3-none-any.whl/polydata/math/linalg/sparse/utils.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+import numpy as np
+from numpy import ndarray
+from numba import njit, prange
+__cache = True
+
+
+def lower_spdata(data: ndarray, rows: ndarray, cols: ndarray):
+    inds = np.where(rows >= cols)[0]
+    return data[inds], rows[inds], cols[inds]
+
+
+def upper_spdata(data: ndarray, rows: ndarray, cols: ndarray):
+    inds = np.where(cols >= rows)[0]
+    return data[inds], rows[inds], cols[inds]
+
+
+@njit(nogil=True, parallel=True, cache=__cache)
+def get_shape_sp(indptr: np.ndarray):
+    nE = len(indptr)-1
+    widths = np.zeros(nE, dtype=indptr.dtype)
+    for iE in prange(nE):
+        widths[iE] = indptr[iE+1] - indptr[iE]
+    return nE, widths.max()
