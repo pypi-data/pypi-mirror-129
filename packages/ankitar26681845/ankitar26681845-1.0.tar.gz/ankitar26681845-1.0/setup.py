20e1f0117280aaaecd422d19908680eb6b40f011
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:12:58 +0000

    Add package ankitar26681845/1.0/ankitar26681845-1.0.tar.gz

---
+++ packages/ankitar26681845/ankitar26681845-1.0.tar.gz/ankitar26681845-1.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='ankitar26681845',
+      version='1.0',
+      description='Calculate and visuallize, Gaussian, Binomial and Normal distributions',
+      packages=['ankitar26681845'],
+      author_email='ankitbhardvaj18@gmail.com',
+      zip_safe=False)
