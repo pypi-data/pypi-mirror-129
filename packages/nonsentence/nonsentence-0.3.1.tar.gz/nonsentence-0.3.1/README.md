f8cc36e23f70460513305481ad40a719468f1aef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:27 +0000

    Add package nonsentence/0.3.1/nonsentence-0.3.1.tar.gz

---
+++ packages/nonsentence/nonsentence-0.3.1.tar.gz/nonsentence-0.3.1/README.md
@@ -0,0 +1,15 @@
+# nonsentence
+A Python function / module that generates nonsense sentences under the scheme vowel-consonsant-vowel, with customisable sentence and word length.
+
+## Usage
+Install from PyPi:
+`pip install nonsentence`
+```py
+import nonsentence
+nonsentence.new_sentence(sentence_length, min_word_length, max_word_length)
+```
+### Example
+```py
+>>> nonsentence.new_sentence(10, 4, 12)
+'xeratihut lux qihaci def zevepehot piroqihuc tegot wejijiquh tet gaza piqitadu.'
+```
---
+++ packages/nonsentence/nonsentence-0.3.1.tar.gz/nonsentence-0.3.1/nonsentence/nonsentence.py
@@ -0,0 +1,28 @@
+import string
+import random
+
+def new_sentence(sentence_length, word_min_length, word_max_length):
+    letters = string.ascii_letters
+
+    # Create consonant and vowel list
+    vowels = [letter for letter in string.ascii_lowercase if letter in "aeiou"]
+    consonants = [letter for letter in string.ascii_lowercase if letter not in "aeiou"]
+
+    # Create a list of words
+    word = []
+    sentence = []
+    sentence_str = ""
+
+    for i in range(sentence_length):
+        for n in range(random.randint(word_min_length,word_max_length)):
+            if n % 2 == 0:
+                word.append(random.choice(consonants))
+            else:
+                word.append(random.choice(vowels))
+        
+        sentence.append("".join(word))
+        word = []
+        
+    sentence_str = ' '.join(sentence) + "."
+    sentence_str = sentence_str.capitalize()
+    return(sentence_str)
