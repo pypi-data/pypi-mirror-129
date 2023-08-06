adf858588b927be3c7d4040de1cd6bdbe2b8f15c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:49:37 +0000

    Add package paddlepaddle/2.2.1/paddlepaddle-2.2.1-cp36-cp36m-macosx_10_6_intel.whl

---
+++ packages/paddlepaddle/paddlepaddle-2.2.1-cp36-cp36m-macosx_10_6_intel.whl/paddle/version/__init__.py
@@ -0,0 +1,61 @@
+# THIS FILE IS GENERATED FROM PADDLEPADDLE SETUP.PY
+#
+full_version    = '2.2.1'
+major           = '2'
+minor           = '2'
+patch           = '1'
+rc              = '0'
+cuda_version    = 'False'
+cudnn_version   = 'False'
+istaged         = True
+commit          = 'a5cf2e305b744e3ebd2f2210341f88d349d4ec5e'
+with_mkl        = 'OFF'
+
+__all__ = ['cuda', 'cudnn']
+
+def show():
+    if istaged:
+        print('full_version:', full_version)
+        print('major:', major)
+        print('minor:', minor)
+        print('patch:', patch)
+        print('rc:', rc)
+    else:
+        print('commit:', commit)
+
+def mkl():
+    return with_mkl
+
+def cuda():
+    """Get cuda version of paddle package.
+
+    Returns:
+        string: Return the version information of cuda. If paddle package is CPU version, it will return False.
+    
+    Examples:
+        .. code-block:: python
+
+            import paddle
+
+            paddle.version.cuda()
+            # '10.2'
+
+    """
+    return cuda_version
+
+def cudnn():
+    """Get cudnn version of paddle package.
+
+    Returns:
+        string: Return the version information of cudnn. If paddle package is CPU version, it will return False.
+    
+    Examples:
+        .. code-block:: python
+
+            import paddle
+
+            paddle.version.cudnn()
+            # '7.6.5'
+
+    """
+    return cudnn_version
