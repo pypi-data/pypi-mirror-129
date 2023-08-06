003314bddc58f20084edd569185b49c392c3dca8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:28:36 +0000

    Add package pyrolite/0.3.1/pyrolite-0.3.1-py3-none-any.whl

---
+++ packages/pyrolite/pyrolite-0.3.1-py3-none-any.whl/pyrolite/util/plot/__init__.py
@@ -0,0 +1,33 @@
+"""
+Utility functions for working with matplotlib.
+
+Parameters
+----------
+DEFAULT_CONT_COLORMAP : :class:`matplotlib.colors.ScalarMappable`
+    Default continuous colormap.
+DEFAULT_DICS_COLORMAP : :class:`matplotlib.colors.ScalarMappable`
+    Default discrete colormap.
+USE_PCOLOR : :class:`bool`
+    Option to use the :func:`matplotlib.pyplot.pcolor` function in place
+    of :func:`matplotlib.pyplot.pcolormesh`.
+"""
+from sys import platform
+
+from ..log import Handle
+
+logger = Handle(__name__)
+
+if platform == "darwin":
+    logger.debug("Using TkAgg renderer for Mac.")
+    import matplotlib
+
+    matplotlib.use("TkAgg")
+
+from .density import USE_PCOLOR
+from .style import DEFAULT_CONT_COLORMAP, DEFAULT_DISC_COLORMAP
+
+FONTSIZE = 12
+
+from .export import save_axes, save_figure
+
+__all__ = ["save_figure", "save_axes", "DEFAULT_CONT_COLORMAP", "DEFAULT_DISC_COLORMAP"]
