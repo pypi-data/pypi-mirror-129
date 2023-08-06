16b6267ae8dd6b2a01fcabb3dba32895329a1f32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:04 +0000

    Add package pedurma/0.1.52/pedurma-0.1.52.tar.gz

---
+++ packages/pedurma/pedurma-0.1.52.tar.gz/pedurma-0.1.52/pedurma/config.py
@@ -0,0 +1,3 @@
+TEXT_LIST_URL = "https://raw.githubusercontent.com/OpenPecha-dev/editable-text/main/t_text_list.json"
+KANGYUR_ARCHIVE_ID = "870326402308432ba173ee3d9043224a"
+TENGYUR_ARCHIVE_ID = "187ed94f85154ea5b1ac374a651e1770"
