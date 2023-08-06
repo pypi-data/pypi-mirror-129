f27b4c1e5dec668ed00e8f53f3e46ea4ec51a2e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:54 +0000

    Add package firexapp/4.9.7/firexapp-4.9.7.tar.gz

---
+++ packages/firexapp/firexapp-4.9.7.tar.gz/firexapp-4.9.7/firexapp/submit/report_trigger.py
@@ -0,0 +1,24 @@
+from firexkit.task import flame_collapse
+from firexapp.engine.celery import app
+from firexapp.submit.reporting import ReportersRegistry
+from firexkit.task import flame_collapse
+
+
+__all__ = ["RunInitialReport"]
+
+
+def run_initial_reporting(kwargs):
+    # only run task if necessary; generators implement pre-load overloads
+    generators = ReportersRegistry.get_generators()
+    generators_with_pre_run = [g for g in generators if g.__class__.__dict__.get("pre_run_report")]
+    if not generators_with_pre_run:
+        return None
+
+    return RunInitialReport.s(**kwargs).enqueue(block=False)
+
+
+# noinspection PyPep8Naming
+@app.task
+@flame_collapse('self')
+def RunInitialReport(**kwargs):
+    ReportersRegistry.pre_run_report(kwargs)
