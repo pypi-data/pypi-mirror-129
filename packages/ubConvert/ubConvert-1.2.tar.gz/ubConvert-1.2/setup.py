732103515132eeb6d984a6334d940e5089ac8aa4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:29:03 +0000

    Add package ubConvert/1.2/ubConvert-1.2.tar.gz

---
+++ packages/ubConvert/ubConvert-1.2.tar.gz/ubConvert-1.2/setup.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+modules = \
+['ubconvert']
+setup_kwargs = {
+    'name': 'ubconvert',
+    'version': '1.2',
+    'description': 'Time, Temperature, Speed & Distance, Volume, Weight conversion module',
+    'long_description': None,
+    'author': 'ZennDogg, Utility_Belt Designs, Tacoma, WA',
+    'author_email': None,
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'py_modules': modules,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
