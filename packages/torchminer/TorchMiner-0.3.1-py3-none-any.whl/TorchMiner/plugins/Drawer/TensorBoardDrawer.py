e76115c3049199872e53a2ec654f93caf4bb495a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:39:40 +0000

    Add package torchminer/0.3.1/TorchMiner-0.3.1-py3-none-any.whl

---
+++ packages/torchminer/TorchMiner-0.3.1-py3-none-any.whl/TorchMiner/plugins/Drawer/TensorBoardDrawer.py
@@ -0,0 +1,33 @@
+# -*- coding:utf-8 -*-
+import os
+
+from tensorboardX import SummaryWriter
+
+from TorchMiner.plugins.Drawer import BasicDrawer
+
+
+class TensorboardDrawer(BasicDrawer):
+    """To visualize everything in training process using tensorboard"""
+
+    def __init__(self, miner, state=None):
+        super().__init__(miner, state)
+        self.writer = SummaryWriter(
+            log_dir=os.path.join(miner.alchemistic_directory, miner.experiment)
+        )
+
+    def scalars(self, x, value, graph):
+        """
+        Add a scalar on a graph
+        :param x:
+        :param value:
+        :param graph:
+        :return:
+        """
+        if graph not in self.state:
+            self.state[graph] = 0
+        key = "{}/{}".format(self.miner.experiment, graph)
+        if isinstance(value, dict):
+            self.writer.add_scalars(key, value, self.state[graph])
+        else:
+            self.writer.add_scalar(key, value, self.state[graph])
+        self.state[graph] += 1
