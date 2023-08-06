d4deecd4c3776d89919400b58972ab3c1a6b1ac4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:57:38 +0000

    Add package harrix-pyssg/0.5/harrix_pyssg-0.5-py3-none-any.whl

---
+++ packages/harrix-pyssg/harrix_pyssg-0.5-py3-none-any.whl/harrixpyssg/__init__.py
@@ -0,0 +1,4 @@
+from .markdown_to_html import *
+from .dir_md_to_dir_html import *
+from .article import *
+from .static_site_generator import *
---
+++ packages/harrix-pyssg/harrix_pyssg-0.5-py3-none-any.whl/harrixpyssg/article.py
@@ -0,0 +1,11 @@
+import harrixpylib as h
+
+
+class Article:
+    def __init__(self):
+        self.md = ""
+        self.meta = dict()
+        self.html = ""
+        self.path_html = ""
+        self.featured_image = False
+        self.featured_image_svg = False
