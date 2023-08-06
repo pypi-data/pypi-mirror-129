eacf50dd78756288ddfda64f4e801f3c6daedca6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:27:24 +0000

    Add package nnfpy/1.0.1/nnfpy-1.0.1-py3-none-any.whl

---
+++ packages/nnfpy/nnfpy-1.0.1-py3-none-any.whl/nnfpy/__init__.py
@@ -0,0 +1,37 @@
+import matplotlib.pyplot as plt
+from pickle import dump, load
+
+
+class StoreData:
+    def __init__(self):
+        self.costl = []
+        self.learningl = []
+        self.momentuml = []
+
+    def cost(self, cost):
+        self.costl.append(cost)
+
+    def learning(self, learning):
+        self.learningl.append(learning)
+
+    def returndata(self):
+        return self.learningl, self.costl, self.momentuml
+
+    def plotdata(self):
+        fig, axs = plt.subplots(nrows=2)
+        axs[0].plot([i for i in range(len(self.costl))], [sum(i) for i in self.costl], color="blue")
+        axs[1].plot(self.learningl, color="red")
+        axs[0].set_title("Cost")
+        axs[1].set_title("Learning")
+        plt.show()
+
+
+def storelist(chosenfile, wb):
+    with open(chosenfile + ".list", "wb") as fp:
+        dump(wb, fp)
+
+
+def loadlist(chosenfile):
+    with open(chosenfile + ".list", "rb") as fp:
+        wb = load(fp)
+    return wb
---
+++ packages/nnfpy/nnfpy-1.0.1-py3-none-any.whl/nnfpy/learningrate.py
@@ -0,0 +1,49 @@
+class linearloss:
+    def __init__(self, lossrate):
+        self.lossrate = lossrate
+        self.type = "loss"
+
+    def newrate(self, learningrate):
+        return learningrate - self.lossrate if learningrate - self.lossrate > 0 else learningrate
+
+
+class timesloss:
+    def __init__(self, lossrate):
+        self.lossrate = lossrate
+        self.type = "loss"
+
+    def newrate(self, learningrate):
+        return learningrate - learningrate * self.lossrate
+
+
+class learnboost:
+    def __init__(self, epochnum, boost):
+        self.epochnum = epochnum
+        self.boostam = boost
+        self.type = "boost"
+
+    def boost(self, epochs, learning):
+        try:
+            if epochs % self.epochnum == 0 and epochs != 0:
+                return learning + self.boostam
+            else:
+                return learning
+        except:
+            return learning
+
+
+class SmartBoost:
+    def __init__(self, epochnum, boost, boostloss=1):
+        self.epochnum = epochnum
+        self.boostam = boost
+        self.type = "SmartBoost"
+        self.boostloss = boostloss
+
+    def boost(self, epochs, learning, totalepochs):
+        try:
+            if epochs % self.epochnum == 0 and epochs != 0:
+                return learning + self.boostam * (1 - epochs / totalepochs / self.boostloss)
+            else:
+                return learning
+        except:
+            return learning
