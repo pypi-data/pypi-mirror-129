a86262e4b23ae5f3abc22d9ee1811d90b3002fa7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:30:01 +0000

    Add package pymobiledevice3/1.14.9/pymobiledevice3-1.14.9-py3-none-any.whl

---
+++ packages/pymobiledevice3/pymobiledevice3-1.14.9-py3-none-any.whl/pymobiledevice3/cli/list_devices.py
@@ -0,0 +1,24 @@
+import click
+
+from pymobiledevice3 import usbmux
+from pymobiledevice3.cli.cli_common import print_json
+from pymobiledevice3.lockdown import LockdownClient
+
+
+@click.group()
+def cli():
+    """ list-devices group """
+    pass
+
+
+@cli.command('list-devices')
+@click.option('--color/--no-color', default=True)
+def list_devices(color):
+    """ list connected devices """
+    connected_devices = []
+    for device in usbmux.list_devices():
+        udid = device.serial
+        lockdown = LockdownClient(udid)
+        connected_devices.append(lockdown.all_values)
+
+    print_json(connected_devices, colored=color)
