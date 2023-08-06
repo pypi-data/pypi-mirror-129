ffb2e9022c1a9788abc9f1ebc036d3988febf43a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:12:18 +0000

    Add package seti-python/0.1.3/seti-python-0.1.3.tar.gz

---
+++ packages/seti-python/seti-python-0.1.3.tar.gz/seti-python-0.1.3/seti_python/class_users.py
@@ -0,0 +1,43 @@
+
+from seti_python.class_tools import BasicResponse
+
+
+# Read total CashOUts
+class ValidateUsersExist:
+    exist = False
+
+    def __init__(self, data: dict):
+        self.exist = data['exist']
+
+
+class ResponseValidateUsersExist(BasicResponse):
+    data: ValidateUsersExist
+
+    def __init__(self, data: dict):
+        self.msg = data['msg']
+        self.res = data['res']
+        if data['res'] == 200:
+            self.data = ValidateUsersExist(data['data'])
+        else:
+            self.data = ValidateUsersExist({})
+
+# Create user Data
+
+
+class NewUsers:
+    secret: str
+
+    def __init__(self, data: dict):
+        self.secret = data['secret']
+
+
+class ResponseCreateUser(BasicResponse):
+    data: NewUsers
+
+    def __init__(self, data: dict):
+        self.msg = data['msg']
+        self.res = data['res']
+        if data['res'] == 200:
+            self.data = NewUsers(data['data'])
+        else:
+            self.data = NewUsers({})
