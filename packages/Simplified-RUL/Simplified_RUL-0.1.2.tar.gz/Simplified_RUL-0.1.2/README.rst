15e5f746ff01aaedea23c9e11a55e1d7173b0668
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:22:15 +0000

    Add package Simplified-RUL/0.1.2/Simplified_RUL-0.1.2.tar.gz

---
+++ packages/Simplified-RUL/Simplified_RUL-0.1.2.tar.gz/Simplified_RUL-0.1.2/README.rst
@@ -0,0 +1,25 @@
+Meta-Data
+
+Description rul calculation package
+
+name  Dheiver Santos
+
+Name of your package. RUL_SIMPLIFIED
+
+version  version='0.1.1'
+
+Current version of your pip package.
+
+scripts
+
+author and author_email Dheiver dheiver.santos@gmail.com
+
+description
+
+A short description of the package.
+
+Use for other package dependencies.
+
+classifiers
+
+Contains all the classifiers of your project.
---
+++ packages/Simplified-RUL/Simplified_RUL-0.1.2.tar.gz/Simplified_RUL-0.1.2/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Simplified_RUL',
+    version='0.1.2',    
+    description='Simplified_RUL_',
+    url='https://github.com/gavbdheiver/RUL_simplified-0.1.1.tar.gz',
+    author='Dheiver Santos',
+    long_description_content_type="text/markdown",
+    long_description='README.rst',
+    author_email ='dheiver.santos@gmail.com',
+    install_requires=['numpy']
+)
