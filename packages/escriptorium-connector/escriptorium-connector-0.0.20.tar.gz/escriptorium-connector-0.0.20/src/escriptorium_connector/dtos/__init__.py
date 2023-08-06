13d6e1a6b9b6d572ae93e183af9b0f310ae65b17
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:29:11 +0000

    Add package escriptorium-connector/0.0.20/escriptorium-connector-0.0.20.tar.gz

---
+++ packages/escriptorium-connector/escriptorium-connector-0.0.20.tar.gz/escriptorium-connector-0.0.20/src/escriptorium_connector/dtos/__init__.py
@@ -0,0 +1,24 @@
+from .document_dtos import (
+    PostDocument,
+    PutDocument,
+    GetDocument,
+    GetDocuments,
+    ReadDirection,
+    LineOffset,
+)
+
+from .line_dtos import GetLineType
+from .region_dtos import GetRegionType
+from .transcription_dtos import GetTranscription
+from .annotation_dtos import (
+    TextMarkerType,
+    GetAnnotationTaxonomy,
+    GetAnnotationTaxonomies,
+    GetTypology,
+    GetComponent,
+    PostAnnotationTaxonomy,
+    PostTypology,
+    PostComponent,
+)
+from .user_dtos import GetUser, GetOnboarding
+from .super_dtos import PagenatedResponse
