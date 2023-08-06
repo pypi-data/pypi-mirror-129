3d4eb02698e75af053965c4a88c6ef03d408ea55
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:38:18 +0000

    Add package genshin/0.2.0/genshin-0.2.0.tar.gz

---
+++ packages/genshin/genshin-0.2.0.tar.gz/genshin-0.2.0/genshin/models/__init__.py
@@ -0,0 +1,14 @@
+"""Pydantic models used by genshin.py"""
+from . import *
+from .abyss import *
+from .activities import *
+from .base import *
+from .character import *
+from .daily import *
+from .diary import *
+from .hoyolab import *
+from .intermap import *
+from .notes import *
+from .stats import *
+from .transaction import *
+from .wish import *
