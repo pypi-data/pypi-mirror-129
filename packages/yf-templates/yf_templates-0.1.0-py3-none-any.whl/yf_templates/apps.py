84e2305f02428a6e7bc97ec6d3c56f06fc7ca6ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:14 +0000

    Add package yf-templates/0.1.0/yf_templates-0.1.0-py3-none-any.whl

---
+++ packages/yf-templates/yf_templates-0.1.0-py3-none-any.whl/yf_templates/apps.py
@@ -0,0 +1,9 @@
+from django.apps import AppConfig
+
+from . import __version__
+
+
+class YfTemplatesConfig(AppConfig):
+    name = "yf_templates"
+    label = "yf_templates"
+    verbose_name = f"Yulai Federation Auth Templates v{__version__}"
