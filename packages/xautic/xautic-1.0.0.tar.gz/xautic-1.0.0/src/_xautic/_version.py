e93353d8d7b009d60f2a1cfe57657dcd3d97e4e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:20:46 +0000

    Add package xautic/1.0.0/xautic-1.0.0.tar.gz

---
+++ packages/xautic/xautic-1.0.0.tar.gz/xautic-1.0.0/src/_xautic/_version.py
@@ -0,0 +1,14 @@
+"""
+Changelog and version history for xautic.
+
+.. versionadded:: 1.0.0
+    Built and setup the structure for the application.
+
+.. versionadded:: 1.0.0
+    Support for `debug` decorator was added along with major code
+    refactoring.
+"""
+
+__all__ = ["__version__"]
+
+__version__ = "1.0.0"
