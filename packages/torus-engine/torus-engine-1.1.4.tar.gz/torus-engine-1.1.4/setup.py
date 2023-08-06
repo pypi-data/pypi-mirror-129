707e1c29b4c831481cd86ad9820ee92329b05b6a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:51 +0000

    Add package torus-engine/1.1.4/torus-engine-1.1.4.tar.gz

---
+++ packages/torus-engine/torus-engine-1.1.4.tar.gz/torus-engine-1.1.4/setup.py
@@ -0,0 +1,27 @@
+import os
+import setuptools
+
+rootdir = os.path.abspath(os.path.dirname(__file__))
+long_description = open(os.path.join(rootdir, 'README.md')).read()
+
+setuptools.setup(
+    name="torus-engine",
+    version="1.1.4",
+    author="Luca Albinati",
+    author_email="luca.albinati@gmail.com",
+    description="3D ASCII character rendering engine",
+    long_description = long_description,
+    long_description_content_type='text/markdown',
+    url="https://github.com/lucaalbinati/torus-engine",
+    project_urls={
+        "Bug Tracker": "https://github.com/lucaalbinati/torus-engine/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.7",
+)
