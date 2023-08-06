11c7ce5c6ed2340d9c7dedebc378d600dce6d1c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:01:50 +0000

    Add package jesse/0.29.3/jesse-0.29.3.tar.gz

---
+++ packages/jesse/jesse-0.29.3.tar.gz/jesse-0.29.3/jesse/services/db.py
@@ -0,0 +1,37 @@
+from playhouse.postgres_ext import PostgresqlExtDatabase
+from typing import Dict, List
+
+import jesse.helpers as jh
+
+if not jh.is_unit_testing():
+
+    keepalive_kwargs = {
+        "keepalives": 1,
+        "keepalives_idle": 60,
+        "keepalives_interval": 10,
+        "keepalives_count": 5
+    }
+
+    # connect to the database
+    db = PostgresqlExtDatabase(database=jh.get_config('env.databases.postgres_name'),
+                               user=jh.get_config('env.databases.postgres_username'),
+                               password=jh.get_config('env.databases.postgres_password'),
+                               host=str(jh.get_config('env.databases.postgres_host')),
+                               port=int(jh.get_config('env.databases.postgres_port')),
+                               **keepalive_kwargs)
+
+
+    def close_connection() -> None:
+        db.close()
+
+
+    # connect
+    db.connect()
+else:
+    db = None
+
+
+def store_candles(candles: List[Dict]) -> None:
+    from jesse.models import Candle
+
+    Candle.insert_many(candles).on_conflict_ignore().execute()
---
+++ packages/jesse/jesse-0.29.3.tar.gz/jesse-0.29.3/jesse/services/quantstats.py
@@ -0,0 +1,41 @@
+import os
+from datetime import datetime, timedelta
+
+import arrow
+import pandas as pd
+import quantstats as qs
+
+from jesse.config import config
+from jesse.store import store
+
+
+def quantstats_tearsheet(buy_and_hold_returns: pd.Series, study_name: str) -> None:
+    daily_returns = pd.Series(store.app.daily_balance).pct_change(1).values
+
+    start_date = datetime.fromtimestamp(store.app.starting_time / 1000)
+
+    date_index = pd.date_range(start=start_date, periods=len(store.app.daily_balance))
+
+    returns_time_series = pd.Series(daily_returns, index=date_index)
+
+    mode = config['app']['trading_mode']
+    modes = {
+        'backtest': ['BT', 'Backtest'],
+        'livetrade': ['LT', 'LiveTrade'],
+        'papertrade': ['PT', 'PaperTrade']
+    }
+
+    os.makedirs('./storage/full-reports', exist_ok=True)
+
+    file_path = f'storage/full-reports/{modes[mode][0]}-{str(arrow.utcnow())[0:19]}-{study_name}.html'.replace(":", "-")
+
+    title = f"{modes[mode][1]} → {arrow.utcnow().strftime('%d %b, %Y %H:%M:%S')} → {study_name}"
+
+    try:
+        qs.reports.html(returns=returns_time_series, periods_per_year=365, benchmark=buy_and_hold_returns, title=title, output=file_path)
+    except IndexError:
+        qs.reports.html(returns=returns_time_series, periods_per_year=365, title=title, output=file_path)
+    except:
+        raise
+
+    print(f'\nFull report output saved at:\n{file_path}')
