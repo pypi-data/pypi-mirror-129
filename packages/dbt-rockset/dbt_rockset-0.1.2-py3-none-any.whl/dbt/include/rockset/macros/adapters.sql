2b0d19f80aade003b2c4e1571ea437eb15b365af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:28:13 +0000

    Add package dbt-rockset/0.1.2/dbt_rockset-0.1.2-py3-none-any.whl

---
+++ packages/dbt-rockset/dbt_rockset-0.1.2-py3-none-any.whl/dbt/include/rockset/macros/adapters.sql
@@ -0,0 +1,11 @@
+-- Rockset does not have a notion of database, so do not include it when resolving refs or sources
+-- It resolves as schema.identifier (i.e. workspace.collection), instead of database.schema.identifier
+{% macro ref(modelname) %}
+  {{ log("in ref macro") }}
+  {{ builtins.ref(modelname).include(database=False).render() }}
+{% endmacro %}
+
+{% macro source(source_name, table_name) %}
+  {{ log("In source macro: " ~ source_name ~ ", " ~ table_name) }}
+  {{ builtins.source(source_name, table_name).include(database=False).render() }}
+{% endmacro %}
