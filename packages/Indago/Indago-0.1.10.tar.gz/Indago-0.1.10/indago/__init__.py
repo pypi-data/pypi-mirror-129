631a876cfea983481cd3139878ce58329c3935bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:47:24 +0000

    Add package Indago/0.1.10/Indago-0.1.10.tar.gz

---
+++ packages/Indago/Indago-0.1.10.tar.gz/Indago-0.1.10/indago/__init__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+#import indago.optimizer as optimizer
+#import indago.pso as pso
+#import indago.fwa as fwa
+#import indago.abca as abca
+
+#import optimizer
+#import pso
+#import fwa
+#import abca
+
+from indago.pso import PSO
+from indago.fwa import FWA
+from indago.de import DE
+from indago.mmo import MMO
+from indago.ssa import SSA
+from indago.ba import BA
+from indago.efo import EFO
+from indago.mrfo import MRFO
+from indago.abca import ABC
+from indago.direct_search import NelderMead
+
+
+__version__ = '0.1.10'
