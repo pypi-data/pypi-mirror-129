ff551d5034d5e616a0477ae38fcfd5d3107ef3b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:12:50 +0000

    Add package manifest-to-helm/0.1.2/manifest-to-helm-0.1.2.tar.gz

---
+++ packages/manifest-to-helm/manifest-to-helm-0.1.2.tar.gz/manifest-to-helm-0.1.2/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['manifest_to_helm']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['PyYAML>=6.0,<7.0']
+
+setup_kwargs = {
+    'name': 'manifest-to-helm',
+    'version': '0.1.2',
+    'description': 'A helper library to convert Kubernetes YAML manifests to Helm charts',
+    'long_description': None,
+    'author': 'John Carter',
+    'author_email': 'jfcarter2358@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
