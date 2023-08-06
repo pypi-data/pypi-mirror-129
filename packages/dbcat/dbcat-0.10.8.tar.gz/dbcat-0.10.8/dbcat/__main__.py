c56152a2e1851d03044e42c9d39aa299fa3030b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:11:49 +0000

    Add package dbcat/0.10.8/dbcat-0.10.8.tar.gz

---
+++ packages/dbcat/dbcat-0.10.8.tar.gz/dbcat-0.10.8/dbcat/__main__.py
@@ -0,0 +1,111 @@
+import logging
+import logging.config
+from pathlib import Path
+from typing import Optional
+
+import typer
+
+from dbcat import __version__
+from dbcat.api import OutputFormat
+from dbcat.app_state import app_state
+from dbcat.cli import app as catalog_app
+
+app = typer.Typer()
+
+
+class TyperLoggerHandler(logging.Handler):
+    def emit(self, record: logging.LogRecord) -> None:
+        fg = None
+        bg = None
+        if record.levelno == logging.DEBUG:
+            fg = typer.colors.YELLOW
+        elif record.levelno == logging.INFO:
+            fg = typer.colors.BRIGHT_BLUE
+        elif record.levelno == logging.WARNING:
+            fg = typer.colors.BRIGHT_MAGENTA
+        elif record.levelno == logging.CRITICAL:
+            fg = typer.colors.BRIGHT_RED
+        elif record.levelno == logging.ERROR:
+            fg = typer.colors.BRIGHT_WHITE
+            bg = typer.colors.RED
+        typer.secho(self.format(record), bg=bg, fg=fg)
+
+
+def log_config(log_level: str):
+    return {
+        "version": 1,
+        "disable_existing_loggers": False,
+        "formatters": {
+            "simple": {
+                "format": "{asctime}:{levelname}:{name} {message}",
+                "style": "{",
+            },
+        },
+        "handlers": {
+            "console": {"class": "logging.StreamHandler", "formatter": "simple"},
+            "typer": {
+                "class": "dbcat.__main__.TyperLoggerHandler",
+                "formatter": "simple",
+            },
+        },
+        "root": {"handlers": ["typer"], "level": log_level},
+        "dbcat": {"handlers": ["typer"], "level": log_level},
+        "sqlachemy": {"handlers": ["typer"], "level": "DEBUG"},
+        "alembic": {"handlers": ["typer"], "level": "DEBUG"},
+        "databuilder": {"handlers": ["typer"], "level": "INFO"},
+    }
+
+
+def version_callback(value: bool):
+    if value:
+        print("{}".format(__version__))
+        typer.Exit()
+
+
+@app.callback()
+def cli(
+    log_level: str = typer.Option("WARNING", help="Logging Level"),
+    output_format: OutputFormat = typer.Option(
+        OutputFormat.tabular, case_sensitive=False
+    ),
+    catalog_path: Path = typer.Option(
+        None, help="Path to store catalog state. Use if NOT using a database"
+    ),
+    catalog_host: str = typer.Option(
+        None, help="hostname of Postgres database. Use if catalog is a database."
+    ),
+    catalog_port: int = typer.Option(
+        None, help="port of Postgres database. Use if catalog is a database."
+    ),
+    catalog_user: str = typer.Option(
+        None, help="user of Postgres database. Use if catalog is a database."
+    ),
+    catalog_password: str = typer.Option(
+        None, help="password of Postgres database. Use if catalog is a database."
+    ),
+    catalog_database: str = typer.Option(
+        None, help="database of Postgres database. Use if catalog is a database."
+    ),
+    version: Optional[bool] = typer.Option(
+        None, "--version", callback=version_callback
+    ),
+):
+    logging.config.dictConfig(log_config(log_level=log_level.upper()))
+
+    app_dir = typer.get_app_dir("tokern")
+    app_dir_path = Path(app_dir)
+    app_dir_path.mkdir(parents=True, exist_ok=True)
+
+    app_state["catalog_connection"] = {
+        "path": catalog_path,
+        "user": catalog_user,
+        "password": catalog_password,
+        "host": catalog_host,
+        "port": catalog_port,
+        "database": catalog_database,
+        "app_dir": app_dir_path,
+    }
+    app_state["output_format"] = output_format
+
+
+app.add_typer(catalog_app, name="catalog")
