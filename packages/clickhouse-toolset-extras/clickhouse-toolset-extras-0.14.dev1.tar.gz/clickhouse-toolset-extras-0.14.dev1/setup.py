5c5df380e874c56a64f1b8e82ba8597a61293a77
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:06:49 +0000

    Add package clickhouse-toolset-extras/0.14.dev1/clickhouse-toolset-extras-0.14.dev1.tar.gz

---
+++ packages/clickhouse-toolset-extras/clickhouse-toolset-extras-0.14.dev1.tar.gz/clickhouse-toolset-extras-0.14.dev1/setup.py
@@ -0,0 +1,48 @@
+from setuptools import setup, Extension
+
+NAME = 'clickhouse-toolset'
+VERSION = '0.14.dev1'
+
+try:
+    from conf import *
+    chquery = Extension(
+        'chtoolset._query',
+        sources=['src/query.cpp'],
+        depends=['src/ClickHouseQuery.h'],
+        include_dirs=include_dirs,
+        extra_compile_args=['-std=gnu++20'],
+        library_dirs=library_dirs,
+        libraries=libraries
+    )
+    setup(
+        name=NAME,
+        version=VERSION,
+        url='https://gitlab.com/tinybird/clickhouse-toolset',
+        author='Tinybird.co',
+        author_email='support@tinybird.co',
+        packages=['chtoolset'],
+        package_dir={'': 'src'},
+        python_requires='>=3.6, <3.11',
+        install_requires=[],
+        extras_require={
+            'test': requirements_from_file('requirements-test.txt')
+        },
+        cmdclass={
+            'clickhouse': ClickHouseBuildExt,
+            'build_ext': CustomBuildWithClang,
+        },
+        ext_modules=[chquery]
+    )
+
+except ModuleNotFoundError:
+    setup(
+        name=NAME,
+        version=VERSION,
+        url='https://gitlab.com/tinybird/clickhouse-toolset',
+        author='Tinybird.co',
+        author_email='support@tinybird.co',
+        packages=['chtoolset'],
+        package_dir={'': 'src'},
+        python_requires='>=3.6, <3.11',
+        install_requires=[],
+    )
