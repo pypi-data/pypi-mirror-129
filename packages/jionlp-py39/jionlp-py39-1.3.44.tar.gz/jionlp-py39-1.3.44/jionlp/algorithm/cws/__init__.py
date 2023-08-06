bb23150742675c56edbe83ae008bbda503d419d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:25:31 +0000

    Add package jionlp-py39/1.3.44/jionlp-py39-1.3.44.tar.gz

---
+++ packages/jionlp-py39/jionlp-py39-1.3.44.tar.gz/jionlp-py39-1.3.44/jionlp/algorithm/cws/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding=utf-8 -*-
+# library: jionlp
+# author: dongrixinyu
+# license: Apache License 2.0
+# Email: dongrixinyu.89@163.com
+# github: https://github.com/dongrixinyu/JioNLP
+# description: Preprocessing tool for Chinese NLP
+
+
+from .cws_data_converter import tag2word, word2tag
+
