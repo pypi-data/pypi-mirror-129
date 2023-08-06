e3bdcf0d5336394c06009cbadf422bc76870c65b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:49:48 +0000

    Add package hahomematic/0.0.16/hahomematic-0.0.16.tar.gz

---
+++ packages/hahomematic/hahomematic-0.0.16.tar.gz/hahomematic-0.0.16/hahomematic/data.py
@@ -0,0 +1,15 @@
+"""
+Module to store data required for operation.
+"""
+from __future__ import annotations
+
+# {instance_name, central_unit}
+INSTANCES = {}
+
+
+def get_client_by_interface_id(interface_id):
+    """Return client by interface_id"""
+    for central in INSTANCES.values():
+        client = central.clients.get(interface_id)
+        if client:
+            return client
