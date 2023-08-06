4c8c8559901879aa99e2b66eb2f45b939ab90d25
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:29:11 +0000

    Add package metacity/0.1.10/metacity-0.1.10.tar.gz

---
+++ packages/metacity/metacity-0.1.10.tar.gz/metacity-0.1.10/README.md
@@ -0,0 +1,9 @@
+# Metacity
+
+[![Build Status](https://github.com/MetacitySuite/Metacity/workflows/Metacity%20CI/badge.svg)](https://github.com/MetacitySuite/Metacity/actions?query=workflow%3Apytesting)
+[![Coverage Status](https://coveralls.io/repos/github/MetacitySuite/Metacity/badge.svg?branch=main)](https://coveralls.io/github/MetacitySuite/Metacity?branch=main)
+
+Toolkit for Urban Data processing
+
+https://pypi.org/project/metacity/
+
---
+++ packages/metacity/metacity-0.1.10.tar.gz/metacity-0.1.10/metacity/geometrycpp/timepoints.hpp
@@ -0,0 +1,30 @@
+#pragma once
+#include "models.hpp"
+
+class Interval;
+
+class MultiTimePoint : public BaseModel {
+public: 
+    void set_points_from_b64(const string & data);
+    void set_start_time(const uint32_t & start_time);
+
+    const uint32_t get_start_time() const;
+    const uint32_t get_end_time() const;
+    const bool empty() const;
+    vector<shared_ptr<MultiTimePoint>> slice_to_timeline(const uint32_t interval_size) const;
+
+    virtual json serialize() const override;
+    virtual void deserialize(const json data) override;
+
+    virtual const char * type() const override;
+    virtual shared_ptr<Model> transform() const override;
+    
+protected:
+    uint32_t start;
+    vector<tvec3> points;
+
+    friend class Interval;
+};
+
+
+
