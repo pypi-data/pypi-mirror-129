1cd5f8f4492875e204d21ec3b4bc92a9126cd8a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:29:01 +0000

    Add package je-auto-control/0.0.75/je_auto_control-0.0.75.tar.gz

---
+++ packages/je-auto-control/je_auto_control-0.0.75.tar.gz/je_auto_control-0.0.75/je_auto_control/linux_with_x11/keyboard/x11_linux_keyboard_control.py
@@ -0,0 +1,27 @@
+import sys
+
+from je_auto_control.utils.je_auto_control_exception.exceptions import AutoControlException
+from je_auto_control.utils.je_auto_control_exception.exception_tag import linux_import_error
+
+if sys.platform not in ["linux", "linux2"]:
+    raise AutoControlException(linux_import_error)
+
+from je_auto_control.linux_with_x11.core.utils.x11_linux_display import display
+from Xlib.ext.xtest import fake_input
+from Xlib import X
+
+
+def press_key(keycode: int):
+    """
+    :param keycode which keycode we want to press
+    """
+    fake_input(display, X.KeyPress, keycode)
+    display.sync()
+
+
+def release_key(keycode: int):
+    """
+    :param keycode which keycode we want to release
+    """
+    fake_input(display, X.KeyRelease, keycode)
+    display.sync()
