11116c8998ae2a95e011f287cf3b705f180643ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:42:08 +0000

    Add package pwy/1.4.7/pwy-1.4.7-py3-none-any.whl

---
+++ packages/pwy/pwy-1.4.7-py3-none-any.whl/pwy/ascii.py
@@ -0,0 +1,74 @@
+from rich import color
+
+clear_sky = [
+    "[b yellow]    \\   /    [/]",
+    "[b yellow]     .-.     [/]",
+    "[b yellow]  ‒ (   ) ‒  [/]",
+    "[b yellow]     `-᾿     [/]",
+    "[b yellow]    /   \\    [/]",
+]
+
+
+few_clouds = [
+    "[b yellow]   \\  /       [/]",
+    '[b yellow] _ /""[/][b white].-.     [/]',
+    "[b yellow]   \\ [/][b white]_(  ).-. [/]",
+    "[b yellow]   /[/][b white](________)[/]",
+    "              ",
+]
+
+
+overcast_cloud = [
+    "[b grey]          [/]",
+    "[b grey]  .-.     [/]",
+    "[b grey] (   ).-. [/]",
+    "[b grey](________)[/]",
+    "[b grey]          [/]",
+]
+
+
+rain = [
+    "[b grey]    .-.     [/]",
+    "[b grey]   (   ).-. [/]",
+    "[b grey]  (________)[/]",
+    "[b blue]  ‚ʻ‚ʻ‚ʻ‚ʻ  [/]",
+    "[b blue]  ‚ʻ‚ʻ‚ʻ‚ʻ  [/]",
+    "            ",
+]
+
+
+thunderstorm = [
+    "[b grey]  .-.     [/]",
+    "[b grey] (   ).-. [/]",
+    "[b grey](________)[/]",
+    "[b blue]‚ʻ[/][b yellow]⚡[/][b blue]ʻ‚[/][b yellow]⚡[/][b blue]‚ʻ[/]",
+    "[b blue]‚ʻ‚ʻ[/][b yellow]⚡[/][b blue]ʻ‚ʻ [/]",
+    "         ",
+]
+
+
+snow = [
+    "[b grey]  .-.     [/]",
+    "[b grey] (   ).-. [/]",
+    "[b grey](________)[/]",
+    "[b white]  * * * * [/]",
+    "[b white] * * * *  [/]",
+]
+
+
+mist = [
+    "[b grey]             [/]",
+    "[b grey] _ - _ - _ - [/]",
+    "[b grey]  _ - _ - _  [/]",
+    "[b grey] _ - _ - _ - [/]",
+    "             ",
+]
+
+
+unknown = [
+    "[b white]    .-.      [/]",
+    "[b white]     __)     [/]",
+    "[b white]    (        [/]",
+    "[b white]     `-᾿     [/]",
+    "[b white]      •      [/]",
+]
