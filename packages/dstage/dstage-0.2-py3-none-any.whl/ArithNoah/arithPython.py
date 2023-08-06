2b422852c2e5b4710a30a827b5e38e24664c8519
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:21:05 +0000

    Add package dstage/0.2/dstage-0.2-py3-none-any.whl

---
+++ packages/dstage/dstage-0.2-py3-none-any.whl/ArithNoah/arithPython.py
@@ -0,0 +1,14 @@
+def add(x, y):
+    """
+     Adds values from two inputs and return the sum
+
+    :param x: first value
+    :param y: second value
+    :return: sum of two values
+
+    Example:
+
+    >>> add(10, 20)
+    30
+    """
+    return x + y
