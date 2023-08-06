f0d166b24333b39149c5f7c5c6cd6286e16a1301
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:27 +0000

    Add package gosu/1.0.43/gosu-1.0.43.tar.gz

---
+++ packages/gosu/gosu-1.0.43.tar.gz/gosu-1.0.43/setup.py
@@ -0,0 +1,41 @@
+from setuptools import find_packages, setup
+
+import versioneer
+
+setup(
+    name="gosu",
+    description="the extensible taskrunner",
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        "plumbum",
+        "typer",
+        "click<8",
+        "python-dotenv",
+        "semver",
+        "pytest",
+        "pyupgrade",
+        "isort",
+        "black",
+        "flake8",
+        "mccabe",
+        "flake8-comprehensions",
+        "flake8-print",
+        "flake8-cognitive-complexity",
+        "pre_commit",
+        "pytest-xdist",
+        "pytest-sugar",
+        "coverage",
+        "django_coverage_plugin",
+        "pyupgrade",
+        "twine",
+    ],
+    license_files=("LICENSE",),
+    url="https://gitlab.com/tuhls/gosu",
+    author="Herbert Rusznak (tlb)",
+    author_email="herbert.rusznak@gmail.com",
+    license="MIT",
+    entry_points={"console_scripts": ("gosu = gosu.main:app",)},
+)
