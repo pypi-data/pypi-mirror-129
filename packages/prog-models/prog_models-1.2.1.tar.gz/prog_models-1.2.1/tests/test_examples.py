5a70350591e8e53a60684f9aaec3f2ac54ff67c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:22:07 +0000

    Add package prog-models/1.2.1/prog_models-1.2.1.tar.gz

---
+++ packages/prog-models/prog_models-1.2.1.tar.gz/prog_models-1.2.1/tests/test_examples.py
@@ -0,0 +1,207 @@
+# Copyright © 2021 United States Government as represented by the Administrator of the National Aeronautics and Space Administration.  All Rights Reserved.
+# This ensures that the directory containing examples is in the python search directories 
+
+import sys
+from os.path import dirname, join
+sys.path.append(join(dirname(__file__), ".."))
+
+import unittest
+from io import StringIO 
+from examples import *
+from unittest.mock import patch
+
+
+class TestExamples(unittest.TestCase):
+    def test_sim_example(self):
+        # set stdout (so it wont print)
+        _stdout = sys.stdout
+        sys.stdout = StringIO()
+
+        # Run example
+        sim.run_example()
+
+        # Reset stdout 
+        sys.stdout = _stdout
+    
+    def test_deriv_paramexample(self):
+        # set stdout (so it wont print)
+        _stdout = sys.stdout
+        sys.stdout = StringIO()
+
+        # Run example
+        derived_params.run_example()
+
+        # Reset stdout 
+        sys.stdout = _stdout
+
+    def test_sim_valve_example(self):
+        # set stdout (so it wont print)
+        _stdout = sys.stdout
+        sys.stdout = StringIO()
+
+        # Run example
+        sim_valve.run_example()
+
+        # Reset stdout 
+        sys.stdout = _stdout
+
+    def test_sim_battery_eol_example(self):
+        # set stdout (so it wont print)
+        _stdout = sys.stdout
+        sys.stdout = StringIO()
+
+        # Run example
+        sim_battery_eol.run_example()
+
+        # Reset stdout 
+        sys.stdout = _stdout
+
+    def test_benchmark_example(self):
+        # set stdout (so it wont print)
+        _stdout = sys.stdout
+        sys.stdout = StringIO()
+
+        # Run example
+        benchmarking.run_example()
+
+        # Reset stdout 
+        sys.stdout = _stdout
+
+    def test_new_model_example(self):
+        # set stdout (so it wont print)
+        _stdout = sys.stdout
+        sys.stdout = StringIO()
+
+        # Run example
+        new_model.run_example()
+
+        # Reset stdout 
+        sys.stdout = _stdout
+
+    def test_sensitivity_example(self):
+        # set stdout (so it wont print)
+        _stdout = sys.stdout
+        sys.stdout = StringIO()
+
+        # Run example
+        sensitivity.run_example()
+
+        # Reset stdout 
+        sys.stdout = _stdout
+
+    def test_model_gen_example(self):
+        # set stdout (so it wont print)
+        _stdout = sys.stdout
+        sys.stdout = StringIO()
+
+        # Run example
+        model_gen.run_example()
+
+        # Reset stdout 
+        sys.stdout = _stdout
+    
+    def test_noise_example(self):
+        # set stdout (so it wont print)
+        _stdout = sys.stdout
+        sys.stdout = StringIO()
+
+        # Run example
+        noise.run_example()
+
+        # Reset stdout 
+        sys.stdout = _stdout
+
+    def test_dynamic_step(self):
+        # set stdout (so it wont print)
+        _stdout = sys.stdout
+        sys.stdout = StringIO()
+
+        # Run example
+        dynamic_step_size.run_example()
+
+        # Reset stdout 
+        sys.stdout = _stdout
+
+    def test_events(self):
+        # set stdout (so it wont print)
+        _stdout = sys.stdout
+        sys.stdout = StringIO()
+
+        # Run example
+        with patch('matplotlib.pyplot') as p:
+            events.run_example()
+
+        # Reset stdout 
+        sys.stdout = _stdout
+
+    def test_future_loading(self):
+        # set stdout (so it wont print)
+        _stdout = sys.stdout
+        sys.stdout = StringIO()
+
+        # Run example
+        with patch('matplotlib.pyplot') as p:
+            future_loading.run_example()
+
+        # Reset stdout 
+        sys.stdout = _stdout
+
+    def test_param_est(self):
+        # set stdout (so it wont print)
+        _stdout = sys.stdout
+        sys.stdout = StringIO()
+
+        # Run example
+        param_est.run_example()
+
+        # Reset stdout 
+        sys.stdout = _stdout
+
+    def test_state_bounds(self):
+        # set stdout (so it wont print)
+        _stdout = sys.stdout
+        sys.stdout = StringIO()
+
+        # Run example
+        state_limits.run_example()
+
+        # Reset stdout 
+        sys.stdout = _stdout
+
+    def test_vectorized(self):
+        # set stdout (so it wont print)
+        _stdout = sys.stdout
+        sys.stdout = StringIO()
+
+        # Run example
+        vectorized.run_example()
+
+        # Reset stdout 
+        sys.stdout = _stdout
+
+    def test_sim_pump_example(self):
+        # set stdout (so it wont print)
+        _stdout = sys.stdout
+        sys.stdout = StringIO()
+
+        # Run example
+        sim_pump.run_example()
+
+        # Reset stdout 
+        sys.stdout = _stdout
+
+# This allows the module to be executed directly
+def run_tests():
+    unittest.main()
+    
+def main():
+    l = unittest.TestLoader()
+    runner = unittest.TextTestRunner()
+    print("\n\nTesting Examples")
+    result = runner.run(l.loadTestsFromTestCase(TestExamples)).wasSuccessful()
+
+    if not result:
+        raise Exception("Failed test")
+
+if __name__ == '__main__':
+    main()
