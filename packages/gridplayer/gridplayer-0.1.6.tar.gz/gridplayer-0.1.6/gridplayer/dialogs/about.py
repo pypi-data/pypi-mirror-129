367fecfd132a33c76ad7212d843f0bb3658c1a2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:00:45 +0000

    Add package gridplayer/0.1.6/gridplayer-0.1.6.tar.gz

---
+++ packages/gridplayer/gridplayer-0.1.6.tar.gz/gridplayer-0.1.6/gridplayer/dialogs/about.py
@@ -0,0 +1,141 @@
+import sys
+from typing import NamedTuple
+
+from pydantic.version import VERSION as PYDANTIC_VERSION
+from PyQt5.Qt import PYQT_VERSION_STR
+from PyQt5.QtCore import QT_VERSION_STR
+from PyQt5.QtGui import QIcon
+from PyQt5.QtWidgets import QDialog
+
+from gridplayer import params_env
+from gridplayer.dialogs.about_dialog_ui import Ui_AboutDialog
+from gridplayer.version import (
+    __app_bugtracker_url__,
+    __app_license_url__,
+    __app_url__,
+    __display_name__,
+    __version__,
+)
+
+
+class Attribution(NamedTuple):
+    title: str
+    version: str
+    author: str
+    license: str
+    url: str
+
+
+def generate_attributions(attributions):
+    attributions_txt = []
+    for a in attributions:
+        version = f" {a.version}" if a.version else ""
+
+        attributions_txt.append(
+            f"<p><b>{a.title}{version}</b>"  # noqa: WPS221
+            f' by <a href="{a.url}">{a.author}</a><br>'
+            f"<i>Licensed under {a.license}</i></p>"
+        )
+
+    return "\n".join(attributions_txt)
+
+
+class AboutDialog(QDialog, Ui_AboutDialog):
+    def __init__(self, parent):
+        super().__init__(parent)
+
+        self.setupUi(self)
+
+        self.logo.setPixmap(QIcon(":/icons/main_ico_big.svg").pixmap(self.logo.size()))
+
+        self.name.setText(__display_name__)
+        self.version.setText(f"version {__version__}")
+
+        about_info = self.info.text()
+        about_info = about_info.replace("{APP_URL}", __app_url__)
+        about_info = about_info.replace("{APP_BUGTRACKER_URL}", __app_bugtracker_url__)
+        about_info = about_info.replace("{APP_LICENSE_URL}", __app_license_url__)
+        self.info.setText(about_info)
+
+        attributions_general = [
+            Attribution(
+                "Python",
+                sys.version.split(" ")[0],
+                "Python Software Foundation",
+                "Python Software Foundation License",
+                "https://www.python.org/",
+            ),
+            Attribution(
+                "Qt",
+                QT_VERSION_STR,
+                "Qt Project",
+                "GPL 2.0, GPL 3.0, and LGPL 3.0",
+                "https://www.qt.io/",
+            ),
+            Attribution(
+                "VLC",
+                None,
+                "VideoLAN",
+                "GPL 2.0 or later",
+                "https://www.videolan.org/",
+            ),
+        ]
+
+        attributions_python = [
+            Attribution(
+                "PyQt",
+                PYQT_VERSION_STR,
+                "Riverbank Computing",
+                "Riverbank Commercial License and GPL v3",
+                "https://riverbankcomputing.com/",
+            ),
+            Attribution(
+                "python-vlc",
+                params_env.VLC_PYTHON_VERSION,
+                "Olivier Aubert",
+                "GPL 2.0 and LGPL 2.1",
+                "https://github.com/oaubert/python-vlc",
+            ),
+            Attribution(
+                "pydantic",
+                PYDANTIC_VERSION,
+                "Samuel Colvin",
+                "MIT License",
+                "https://github.com/samuelcolvin/pydantic",
+            ),
+        ]
+
+        attributions_gui = [
+            Attribution(
+                "Hack Font",
+                "3.003",
+                "Source Foundry",
+                "MIT License",
+                "http://sourcefoundry.org/hack/",
+            ),
+            Attribution(
+                "Basic Icons",
+                None,
+                "Icongeek26",
+                "Flaticon License",
+                "https://www.flaticon.com/authors/icongeek26",
+            ),
+            Attribution(
+                "Suru Icons",
+                None,
+                "Sam Hewitt",
+                "Creative Commons Attribution-Share Alike 4.0",
+                "https://snwh.org/",
+            ),
+        ]
+
+        attributions_txt = [
+            "<style>p, h3 {text-align: center;}</style>",
+            generate_attributions(attributions_general),
+            "<h3>Python packages</h3>",
+            generate_attributions(attributions_python),
+            "<h3>Graphics</h3>",
+            generate_attributions(attributions_gui),
+        ]
+
+        self.attributionsBox.setText("\n".join(attributions_txt))
---
+++ packages/gridplayer/gridplayer-0.1.6.tar.gz/gridplayer-0.1.6/gridplayer/dialogs/settings.py
@@ -0,0 +1,249 @@
+import contextlib
+import logging
+import os
+import platform
+import subprocess
+
+from PyQt5.QtCore import QUrl
+from PyQt5.QtGui import QDesktopServices, QIcon
+from PyQt5.QtWidgets import QCheckBox, QComboBox, QDialog, QSpinBox
+
+from gridplayer import params_env, utils
+from gridplayer.dialogs.messagebox import QCustomMessageBox
+from gridplayer.dialogs.settings_dialog_ui import Ui_SettingsDialog
+from gridplayer.params_static import GridMode, VideoAspect, VideoDriver
+from gridplayer.settings import Settings
+from gridplayer.utils.app_dir import get_app_data_dir
+from gridplayer.utils.misc import qt_connect
+
+logger = logging.getLogger(__name__)
+
+VIDEO_DRIVERS_MULTIPROCESS = (
+    VideoDriver.VLC_SW,
+    VideoDriver.VLC_HW,
+)
+
+MAX_VLC_PROCESSES = 64
+
+
+def _fill_combo_box(combo_box, values_dict):
+    for i_id, i_name in values_dict.items():
+        combo_box.addItem(i_name, i_id)
+
+
+def _set_combo_box(combo_box, data_value):
+    idx = combo_box.findData(data_value)
+    combo_box.setCurrentIndex(idx)
+
+
+class SettingsDialog(QDialog, Ui_SettingsDialog):
+    def __init__(self, parent):
+        super().__init__(parent)
+
+        self.setupUi(self)
+        self.ui_customize()
+
+        self.settings_map = {
+            "player/video_driver": self.playerVideoDriver,
+            "player/video_driver_players": self.playerVideoDriverPlayers,
+            "player/pause_background_videos": self.playerPauseBackgroundVideos,
+            "player/pause_minimized": self.playerPauseWhenMinimized,
+            "player/inhibit_screensaver": self.playerInhibitScreensaver,
+            "player/one_instance": self.playerOneInstance,
+            "playlist/grid_mode": self.gridMode,
+            "playlist/grid_fit": self.gridFit,
+            "playlist/grid_size": self.gridSize,
+            "playlist/save_position": self.playlistSavePosition,
+            "playlist/save_state": self.playlistSaveState,
+            "playlist/save_window": self.playlistSaveWindow,
+            "playlist/seek_synced": self.playlistSeekSync,
+            "video_defaults/aspect": self.videoAspect,
+            "video_defaults/random_loop": self.videoRandomLoop,
+            "video_defaults/muted": self.videoMuted,
+            "video_defaults/paused": self.videoPaused,
+            "misc/overlay_timeout": self.timeoutOverlay,
+            "misc/mouse_hide": self.timeoutMouseHideFlag,
+            "misc/mouse_hide_timeout": self.timeoutMouseHide,
+            "logging/log_level": self.logLevel,
+            "logging/log_level_vlc": self.logLevelVLC,
+            "internal/opaque_hw_overlay": self.miscOpaqueHWOverlay,
+        }
+
+        self.ui_fill()
+
+        self.ui_connect()
+
+        self.load_settings()
+
+        self.ui_customize_dynamic()
+
+    def ui_customize(self):  # noqa: WPS213
+        for btn in self.buttonBox.buttons():
+            btn.setIcon(QIcon())
+
+        font = self.playerVideoDriverBox.font()
+        font.setBold(True)
+        self.playerVideoDriverBox.setFont(font)
+
+        # Restore the font of each children to regular.
+        font.setBold(False)
+        for child in self.playerVideoDriverBox.children():
+            with contextlib.suppress(AttributeError):
+                child.setFont(font)
+
+        if platform.system() == "Darwin":
+            self.lay_body.setContentsMargins(4, 0, 0, 0)
+            self.horizontalLayout.setContentsMargins(0, 0, 15, 0)  # noqa: WPS432
+            self.lay_playerVideoDriverPlayers.setContentsMargins(3, 0, 2, 0)
+            self.playerVideoDriverBox.setStyleSheet(
+                "QGroupBox:title{padding: 0 4px 0 3px;margin-left:-5px;}"
+            )
+
+        if platform.system() != "Linux":
+            self.section_misc.hide()
+            self.miscOpaqueHWOverlay.hide()
+
+    def ui_fill(self):
+        self.fill_playerVideoDriver()
+        self.fill_gridMode()
+        self.fill_videoAspect()
+        self.fill_logLevel()
+        self.fill_logLevelVLC()
+
+    def ui_customize_dynamic(self):
+        self.driver_selected(self.playerVideoDriver.currentIndex())
+        self.timeoutMouseHide.setEnabled(self.timeoutMouseHideFlag.isChecked())
+
+        self.playerVideoDriverPlayers.setRange(1, MAX_VLC_PROCESSES)
+        self.timeoutOverlay.setRange(1, 60)
+        self.timeoutMouseHide.setRange(1, 60)
+
+        self.gridSize.setRange(0, 1000)
+        self.gridSize.setSpecialValueText("Auto")
+
+    def ui_connect(self):
+        qt_connect(
+            (self.playerVideoDriver.currentIndexChanged, self.driver_selected),
+            (self.timeoutMouseHideFlag.stateChanged, self.timeoutMouseHide.setEnabled),
+            (self.logFileOpen.clicked, self.open_logfile),
+        )
+
+    def open_logfile(self):
+        log_path = os.path.join(get_app_data_dir(), "gridplayer.log")
+
+        logger.debug(f"Opening log file {log_path}")
+
+        if not os.path.isfile(log_path):
+            return QCustomMessageBox.critical(self, "Error", "Log file does not exist!")
+
+        if params_env.IS_SNAP:
+            # https://forum.snapcraft.io/t/xdg-open-or-gvfs-open-qdesktopservices-openurl-file-somelocation-file-txt-wont-open-the-file/16824
+            subprocess.call(["xdg-open", log_path])  # noqa: S603, S607
+        else:
+            QDesktopServices.openUrl(QUrl.fromLocalFile(log_path))
+
+    def fill_logLevelVLC(self):
+        log_levels = {
+            utils.log_config.DISABLED: "None",
+            logging.ERROR: "Error",
+            logging.WARNING: "Warning",
+            logging.INFO: "Info",
+            logging.DEBUG: "Debug",
+        }
+
+        _fill_combo_box(self.logLevelVLC, log_levels)
+
+    def fill_logLevel(self):
+        log_levels = {
+            utils.log_config.DISABLED: "None",
+            logging.CRITICAL: "Critical",
+            logging.ERROR: "Error",
+            logging.WARNING: "Warning",
+            logging.INFO: "Info",
+            logging.DEBUG: "Debug",
+        }
+
+        _fill_combo_box(self.logLevel, log_levels)
+
+    def fill_videoAspect(self):
+        aspect_ratios = {
+            VideoAspect.FIT: "Fit",
+            VideoAspect.STRETCH: "Stretch",
+            VideoAspect.NONE: "None",
+        }
+
+        _fill_combo_box(self.videoAspect, aspect_ratios)
+
+    def fill_gridMode(self):
+        grid_modes = {
+            GridMode.AUTO_ROWS: "Rows First",
+            GridMode.AUTO_COLS: "Columns First",
+        }
+
+        _fill_combo_box(self.gridMode, grid_modes)
+
+    def fill_playerVideoDriver(self):
+        if platform.system() == "Darwin":
+            video_drivers = {
+                VideoDriver.VLC_HW_SP: f"Hardware SP <VLC {params_env.VLC_VERSION}>",
+                VideoDriver.VLC_SW: f"Software <VLC {params_env.VLC_VERSION}>",
+                VideoDriver.DUMMY: "Dummy",
+            }
+        else:
+            video_drivers = {
+                VideoDriver.VLC_HW: f"Hardware <VLC {params_env.VLC_VERSION}>",
+                VideoDriver.VLC_SW: f"Software <VLC {params_env.VLC_VERSION}>",
+                VideoDriver.DUMMY: "Dummy",
+            }
+
+        _fill_combo_box(self.playerVideoDriver, video_drivers)
+
+    def driver_selected(self, idx):
+        driver_id = self.playerVideoDriver.itemData(idx)
+
+        if driver_id in VIDEO_DRIVERS_MULTIPROCESS:
+            self.playerVideoDriverPlayers.setDisabled(False)
+        else:
+            self.playerVideoDriverPlayers.setDisabled(True)
+
+    def load_settings(self):
+        elements_value_set_fun = {
+            QCheckBox: lambda e, v: e.setChecked(v),
+            QSpinBox: lambda e, v: e.setValue(v),
+            QComboBox: _set_combo_box,
+        }
+
+        for setting, element in self.settings_map.items():
+            setting_value = Settings().get(setting)
+
+            try:
+                set_function = elements_value_set_fun[type(element)]
+            except KeyError:
+                raise ValueError(f"No element decoder for {setting}")
+
+            set_function(element, setting_value)
+
+    def save_settings(self):
+        elements_value_read_attr = {
+            QCheckBox: "isChecked",
+            QSpinBox: "value",
+            QComboBox: "currentData",
+        }
+
+        for setting, element in self.settings_map.items():
+            if not element.isEnabled():
+                continue
+
+            try:
+                value_attr = elements_value_read_attr[type(element)]
+            except KeyError:
+                raise ValueError(f"No element decoder for {setting}")
+
+            new_value = getattr(element, value_attr)()
+
+            Settings().set(setting, new_value)
+
+    def accept(self):
+        self.save_settings()
+
+        super().accept()
