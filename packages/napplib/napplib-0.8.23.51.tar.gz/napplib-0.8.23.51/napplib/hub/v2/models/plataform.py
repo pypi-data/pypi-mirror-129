0247befc93a1735d7b0deaa2ee7d2fdfb2c60a91
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:10:03 +0000

    Add package napplib/0.8.23.51/napplib-0.8.23.51.tar.gz

---
+++ packages/napplib/napplib-0.8.23.51.tar.gz/napplib-0.8.23.51/napplib/hub/v2/models/plataform.py
@@ -0,0 +1,14 @@
+from enum import Enum
+
+class HubPlataform(Enum):
+	BLING	                = 'BLING'
+	NUVEMSHOP               = 'NUVEMSHOP'
+	SHOPIFY                 = 'SHOPIFY'
+	TINY                    = 'TINY'
+	WOOCOMMERCE             = 'WOOCOMMERCE'
+	VTEX                    = 'VTEX'
+	OMNIK_ON_STORES         = 'OMNIK_ON_STORES'
+	UNILEVER_SITE_MERCADO   = 'UNILEVER_SITE_MERCADO'
+	JCPM                    = 'JCPM'
+	YAMI                    = 'YAMI'
+	MPMS_JCC                = 'MPMS_JCC'
