b96abac0eb7f2441a84d51ffd44d45a988b899fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:47:09 +0000

    Add package itba-cde-tpf-python-applications-fvidal90/1.1.1/itba_cde_tpf_python_applications_fvidal90-1.1.1.tar.gz

---
+++ packages/itba-cde-tpf-python-applications-fvidal90/itba_cde_tpf_python_applications_fvidal90-1.1.1.tar.gz/itba_cde_tpf_python_applications_fvidal90-1.1.1/fvidal_packages/db_api_client/postgres_cli.py
@@ -0,0 +1,38 @@
+"""Implements a client class to interact with a Postgres DB."""
+from fvidal_packages.db_api_client.parent_cli import DBAPIClient
+
+
+class PostgresClient(DBAPIClient):
+    """Children class to connect to a Postgres DB using SQLAlchemy.
+
+    Attributes
+    ----------
+        host: str
+            dns name of the host of the postgres database.
+        port: int
+            port where the postgres database is listening.
+        user: str
+            username to connect to the postgres database.
+        password: str
+            password for this username.
+        database: str
+            database to connect
+    """
+    def __init__(self, user, password, host, port, database):
+        """Constructor method for this children class.
+
+        Parameters
+        ----------
+        host: str
+            dns name of the host of the postgres database.
+        port: int
+            port where the postgres database is listening.
+        user: str
+            username to connect to the postgres database.
+        password: str
+            password for this username.
+        database: str
+            database to connect
+        """
+        db = f'//{user}:{password}@{host}:{port}/{database}'
+        super().__init__('postgresql', db)
