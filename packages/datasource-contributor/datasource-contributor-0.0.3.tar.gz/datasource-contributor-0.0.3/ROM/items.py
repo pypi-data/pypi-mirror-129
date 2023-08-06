8e4f1d44b29b19780bc995df55e7d2eedeb221ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:11:51 +0000

    Add package datasource-contributor/0.0.3/datasource-contributor-0.0.3.tar.gz

---
+++ packages/datasource-contributor/datasource-contributor-0.0.3.tar.gz/datasource-contributor-0.0.3/ROM/items.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://docs.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+
+class RomItem(scrapy.Item):
+    # define the fields for your item here like:
+    # name = scrapy.Field()
+    name = scrapy.Field()
+    content = scrapy.Field()
+    url = scrapy.Field()
+    downloads = scrapy.Field()
+    tag = scrapy.Field()
