a47662a0f42329132e7571e1e5417f286ccd78c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:27:18 +0000

    Add package figaria/1.0.2/figaria-1.0.2.tar.gz

---
+++ packages/figaria/figaria-1.0.2.tar.gz/figaria-1.0.2/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name="figaria",
+    version="1.0.2",
+    description="Pakiet testowy",
+    url="http://tekst.pl",
+    install_requires=[],
+    packages=["pakiet"],
+
+#python ./setup.py sdist
+
+)
