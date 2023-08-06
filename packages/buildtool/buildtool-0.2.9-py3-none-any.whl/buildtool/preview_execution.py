1489ff8357d74bc8744170d1ff70a3c6100615f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:41:24 +0000

    Add package buildtool/0.2.9/buildtool-0.2.9-py3-none-any.whl

---
+++ packages/buildtool/buildtool-0.2.9-py3-none-any.whl/buildtool/preview_execution.py
@@ -0,0 +1,133 @@
+from typing import Callable, Dict
+
+from cache import make_cache_load
+from common.hash_utils import HashState
+from context import Env, MemorizeContext
+from fs_utils import hash_file
+from monitoring import log
+from state import BuildState, Rule
+from utils import CacheMiss, MissingDependency
+
+
+class PreviewContext(MemorizeContext):
+    def __init__(
+        self,
+        cwd: str,
+        macros: Dict[str, Callable],
+        hashstate: HashState,
+        dep_fetcher: Callable[[str], Rule],
+        cache_fetcher: Callable[[str, str], str],
+    ):
+        super().__init__(cwd, macros, hashstate, dep_fetcher)
+        self.cache_fetcher = cache_fetcher
+
+    def input(self, sh: str = None, *, env: Env = None):
+        state = self.hashstate.state()
+        super().input(sh, env=env)
+        return self.cache_fetcher(state, HashState().record(sh, env).state())
+
+
+def make_dep_fetcher(build_state: BuildState):
+    def dep_fetcher(input_path, type: str = "rule"):
+        try:
+            rule = None
+            if input_path not in build_state.source_files:
+                rule = build_state.target_rule_lookup.try_lookup(input_path)
+                # this input may be stale / unbuilt / no longer exists
+                # if so, do not read it, but instead throw MissingDependency
+                if rule is None or rule not in build_state.ready:
+                    raise MissingDependency(input_path)
+                # so it's already ready for use!
+
+            if type == "hash":
+                return hash_file(input_path)
+            elif type == "rule":
+                return rule
+            else:
+                raise Exception(f"Unknown dep type {type}")
+        except FileNotFoundError:
+            raise MissingDependency(input_path)
+
+    return dep_fetcher
+
+
+def get_deps(build_state: BuildState, rule: Rule, *, skip_cache_key: bool):
+    """
+    Use static dependencies and caches to try and identify as *many*
+    needed dependencies as possible, without *any* spurious dependencies.
+    """
+    hashstate = HashState()
+    cache_load_string, _ = make_cache_load(build_state.cache_directory)
+    dep_fetcher = make_dep_fetcher(build_state)
+
+    ctx = PreviewContext(
+        rule.location,
+        build_state.macros,
+        hashstate,
+        dep_fetcher,
+        cache_load_string,
+    )
+
+    ok = False
+    provided_value = None
+    try:
+        log(f"Running impl of {rule} to discover dependencies")
+        if not skip_cache_key:
+            for out in rule.outputs:
+                # needed so that if we ask for another output, we don't panic if it's not in the cache
+                hashstate.record(out)
+        provided_value = rule.impl(ctx)
+        log(f"Impl of {rule} completed with deps: {ctx.inputs}")
+        ok = True
+    except CacheMiss:
+        log(f"Cache miss while running impl of {rule}")
+        pass  # stops context execution
+    except MissingDependency as e:
+        log(f"Dependencies {e.paths} were unavailable while running impl of {rule}")
+        pass  # dep already added to ctx.inputs
+    except Exception as e:
+        print(
+            "Error occurred during PreviewExecution. This may be normal, if a cached file that has not "
+            "yet been reported / processed has been changed. However, it may also be an internal error, so "
+            "it is being logged here. If it is an internal error, please contact the maintainer."
+        )
+        print(repr(e))
+        if not ctx.uses_dynamic_inputs:
+            raise
+    # if `ok`, hash loaded dynamic dependencies
+    if ok:
+        log(f"Inputs and dependencies resolved for {rule}")
+        for input_path in ctx.inputs:
+            if input_path.startswith(":"):
+                input_dep = build_state.target_rule_lookup.try_lookup(input_path)
+                if input_dep is None or input_dep not in build_state.ready:
+                    ok = False
+                    log(
+                        f"Rule dependency {input_path} is not yet ready (or does not exist)"
+                    )
+                    break
+            else:
+                if not skip_cache_key:
+                    hashstate.update(input_path.encode("utf-8"))
+                try:
+                    data = dep_fetcher(input_path, "rule" if skip_cache_key else "hash")
+                except MissingDependency as e:
+                    # this dependency was not needed for deps calculation
+                    # but is not verified to be up-to-date
+                    ok = False
+                    log(
+                        f"Dependencies {e.paths} were not needed for the impl, but are not up to date"
+                    )
+                    break
+                else:
+                    if not skip_cache_key:
+                        hashstate.update(data)
+        hashstate.record("done")
+
+    return (
+        hashstate.state() if ok else None,
+        provided_value,
+        ctx.inputs,
+        ctx.deferred_inputs,
+        ctx.uses_dynamic_inputs,
+    )
---
+++ packages/buildtool/buildtool-0.2.9-py3-none-any.whl/buildtool/workspace_setup.py
@@ -0,0 +1,173 @@
+import os
+import traceback
+from typing import Callable, Dict, List, Optional, Sequence, Set
+
+from colorama import Style
+
+from cache import make_cache_load, make_cache_store
+from context import Env
+from execution import ExecutionContext
+from fs_utils import hash_file
+from monitoring import create_status_monitor, log
+from state import Rule, TargetLookup
+from utils import BuildException, CacheMiss, MissingDependency
+from common.hash_utils import HashState
+
+
+class WorkspaceExecutionContext(ExecutionContext):
+    never_defer = True
+
+    def __init__(
+        self,
+        hashstate: HashState,
+        dep_fetcher: Callable[[str], Rule],
+    ):
+        super().__init__(
+            os.curdir,
+            os.curdir,
+            {},
+            hashstate,
+            dep_fetcher,
+            lambda *args: None,
+        )
+
+    def normalize(self, env: Optional[Env]):
+        if env is None:
+            env = {}
+        return super().normalize({**env, **os.environ})
+
+
+def initialize_workspace(
+    setup_rule_lookup: TargetLookup,
+    setup_targets: List[str],
+    state_directory: str,
+    quiet: bool,
+):
+    # we don't need the indirect lookup as we only have rule and source deps
+    direct_lookup: Dict[str, Rule] = setup_rule_lookup.direct_lookup
+    work_queue = []
+    for setup_target in setup_targets:
+        if setup_target not in direct_lookup:
+            raise BuildException(f"Unknown or unspecified setup target {setup_target}")
+        work_queue.append(direct_lookup[setup_target])
+
+    rebuilt: Set[str] = set()
+    ready: Set[str] = set()
+
+    cache_load_string, _ = make_cache_load(state_directory)
+    cache_store_string, _ = make_cache_store(state_directory)
+
+    if work_queue:
+        status_monitor = create_status_monitor(1, quiet)
+        status_monitor.move(total=len(work_queue))
+
+    def dep_fetcher(dep):
+        if dep.startswith(":"):
+            if dep not in direct_lookup:
+                raise BuildException(f"Unable to find setup rule {dep}")
+            dep_rule = direct_lookup[dep]
+            log(f"Looking up setup rule {dep}")
+            if dep_rule not in ready:
+                raise MissingDependency(dep)
+            return dep_rule
+
+    while work_queue:
+        todo = work_queue.pop()
+        log(f"Popping setup rule {todo} off work queue")
+        try:
+            if todo.name is None:
+                raise BuildException(
+                    f"All setup rules must have names, but {todo} does not."
+                )
+
+            hashstate = HashState()
+            ctx = WorkspaceExecutionContext(hashstate, dep_fetcher)
+            unchecked_rules = []
+
+            try:
+                todo.set_provided_value(
+                    todo.impl(ctx),
+                    None,
+                    ctx.inputs,
+                    ctx.deferred_inputs,
+                    [],  # todo: implement output providers for setup rules
+                )
+                if ctx.out_of_date_deps:
+                    raise MissingDependency(*ctx.out_of_date_deps)
+            except MissingDependency as e:
+                unchecked_rules = [direct_lookup[x] for x in e.paths]
+
+            if unchecked_rules:
+                for dep in unchecked_rules:
+                    if dep not in work_queue:
+                        log(f"Setup rule {todo} is enqueuing {dep}")
+                        status_monitor.move(total=1)
+                        work_queue.append(dep)
+                    else:
+                        log(
+                            f"Setup rule {todo} is waiting on {dep}, which is already enqueued"
+                        )
+                    dep.runtime_dependents.add(todo)
+                    todo.pending_rule_dependencies.add(dep)
+            else:
+                log(
+                    f"Setup rule {todo} ran with inputs {ctx.inputs + ctx.deferred_inputs}"
+                )
+                for dep in ctx.inputs + ctx.deferred_inputs:
+                    if dep.startswith(":"):
+                        continue
+                    try:
+                        hashstate.record(dep)
+                        hashstate.update(hash_file(dep))
+                    except FileNotFoundError:
+                        raise BuildException(f"Source file {dep} not found.")
+
+                try:
+                    ok = cache_load_string("workspace", todo.name) == hashstate.state()
+                    if not ok:
+                        log(f"State mismatch for rule {todo}, need to rerun")
+                except CacheMiss:
+                    log(f"State not found for rule {todo}, need to run for first time")
+                    ok = False
+
+                for dep in ctx.inputs + ctx.deferred_inputs:
+                    if dep.startswith(":"):
+                        if direct_lookup[dep] in rebuilt:
+                            log(
+                                f"Dependency {dep} of setup rule {todo} was rebuilt, so we must rebuild {todo} as well"
+                            )
+                            ok = False
+
+                for out in todo.outputs:
+                    if not os.path.exists(out):
+                        log(
+                            f"Output {out} is missing for setup rule {todo}, forcing rerun"
+                        )
+                        ok = False
+                        break
+
+                if not ok:
+                    # we need to fully run
+                    log(f"Fully running setup rule {todo}")
+                    ctx.run_shell_queue()
+                    rebuilt.add(todo)
+                    cache_store_string("workspace", todo.name, hashstate.state())
+
+                # either way, now we can trigger our dependents
+                ready.add(todo)
+                for dep in todo.runtime_dependents:
+                    dep.pending_rule_dependencies.remove(todo)
+                    if not dep.pending_rule_dependencies:
+                        work_queue.append(dep)
+                        status_monitor.move(total=1)
+
+            status_monitor.move(curr=1)
+        except Exception as e:
+            if not isinstance(e, BuildException):
+                suffix = f"\n{Style.RESET_ALL}" + traceback.format_exc()
+            else:
+                suffix = ""
+            status_monitor.stop()
+            raise BuildException(
+                f"Error while executing rule {todo}: " + str(e) + suffix
+            )
