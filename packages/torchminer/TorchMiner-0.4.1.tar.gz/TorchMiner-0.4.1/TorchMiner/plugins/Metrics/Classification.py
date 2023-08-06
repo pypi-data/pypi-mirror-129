4e6cc73ca10faa3ae5594bd4e8271f27b566181a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:49:27 +0000

    Add package torchminer/0.4.1/TorchMiner-0.4.1.tar.gz

---
+++ packages/torchminer/TorchMiner-0.4.1.tar.gz/TorchMiner-0.4.1/TorchMiner/plugins/Metrics/Classification.py
@@ -0,0 +1,91 @@
+# -*- coding:utf-8 -*-
+import io
+
+import numpy as np
+import pandas as pd
+import seaborn as sn
+from matplotlib import pyplot as plt
+from sklearn.metrics import classification_report, confusion_matrix, cohen_kappa_score
+
+from TorchMiner import BasePlugin
+
+
+class MultiClassesClassification(BasePlugin):
+    """MultiClassesClassificationMetric
+    This can be used directly if your loss function is torch.nn.CrossEntropy
+    """
+    requirements = ["TensorboardDrawer"]
+
+    def __init__(
+            self,
+            accuracy=True,
+            confusion=True,
+            kappa_score=True,
+            report=True,
+            backend="TensorboardDrawer",
+            forward=None
+    ):
+        super().__init__()
+        self.accuracy = accuracy
+        self.confusion_matrix = confusion
+        self.kappa_score = kappa_score
+        self.classification_report = report
+        self.backend = backend  # TODO:Can be used for sheet recorder
+        self.forward = forward
+
+    # def before_init(self):
+    #     self.create_sheet_column("latest_confusion_matrix", "Latest Confusion Matrix")
+    #     self.create_sheet_column("kappa_score", "Kappa Score")
+    #     self.create_sheet_column("accuracy", "Accuracy")
+
+    def after_init(self, *args, **kwargs):
+        self.recorder = self.miner.plugins.get(self.backend)
+
+    def before_val_epoch_start(self, epoch, **ignore):
+        # TODO:Recommended to attach these attributes to Miner Obj, for combination with Other Plugins
+        self.predicts = []
+        self.label = []
+
+    def after_val_iteration_ended(self, predicts, data, **ignore):
+        raw_output = predicts.detach().cpu().numpy()
+        if self.forward:
+            predicts, label = self.forward(raw_output, data)
+        else:
+            predicts = np.argmax(raw_output, axis=1)  # Batch first
+            label = data[1].cpu().numpy()  # label
+        self.predicts.append(predicts)
+        self.label.append(label)
+
+    def after_val_epoch_end(self, val_loss, **ignore):
+        predicts = np.concatenate(self.predicts)
+        label = np.concatenate(self.label)
+        if self.accuracy:
+            accuracy = (predicts == label).sum() / len(predicts)
+            self.logger.info(f"Val Accuracy:{accuracy}")
+            self.recorder.scalar("Val/Accuracy", accuracy)
+
+        if self.confusion_matrix:
+            matrix = confusion_matrix(label, predicts)
+            df_cm = pd.DataFrame(matrix)
+            svm = sn.heatmap(df_cm, annot=True, cmap="OrRd", fmt=".3g")
+            figure = svm.get_figure()
+            if val_loss < self.miner.lowest_val_loss:
+                # Solution to store Matplotlib Figure in TensorBoardX
+                # https://stackoverflow.com/questions/7821518/matplotlib-save-plot-to-numpy-array
+                with io.BytesIO() as buff:
+                    figure.savefig(buff, format='raw')
+                    buff.seek(0)
+                    data = np.frombuffer(buff.getvalue(), dtype=np.uint8)
+                w, h = figure.canvas.get_width_height()
+                im = data.reshape((int(h), int(w), -1))
+                im = im.transpose((2, 0, 1))  # Change to CHW
+                self.recorder.figure("Val/ConfusionMatrix", im)
+            plt.close(figure)
+
+        if self.kappa_score:
+            kappa = cohen_kappa_score(label, predicts, weights="quadratic")
+            self.recorder.scalar("Val/KappaScore", kappa)
+
+        if self.classification_report:
+            # TODO:Design a better way to output or reord classification report
+            self.logger.info(classification_report(label, predicts))
---
+++ packages/torchminer/TorchMiner-0.4.1.tar.gz/TorchMiner-0.4.1/setup.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['torchminer',
+ 'torchminer.plugins',
+ 'torchminer.plugins.Drawer',
+ 'torchminer.plugins.Logger',
+ 'torchminer.plugins.Metrics',
+ 'torchminer.plugins.Sheet']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['Pillow>=8.4.0,<9.0.0',
+ 'google-api-python-client>=2.31.0,<3.0.0',
+ 'ipython>=7.18.0,<8.0.0',
+ 'matplotlib>=3.5.0,<4.0.0',
+ 'pandas>=1.3.4,<2.0.0',
+ 'pylint>=2.12.1,<3.0.0',
+ 'seaborn>=0.11.2,<0.12.0',
+ 'sklearn>=0.0,<0.1',
+ 'tensorboardX>=2.4.1,<3.0.0',
+ 'torch>=1.8.0,<2.0.0',
+ 'tqdm>=4.50.0,<5.0.0']
+
+setup_kwargs = {
+    'name': 'torchminer',
+    'version': '0.4.1',
+    'description': 'Run Torch With A Simple Miner',
+    'long_description': "This Project is Forked From [MineTorch](https://github.com/louis-she/minetorch).\n\nPublished on [pypi](https://pypi.org/project/torchminer/)\n\nPackaged Using [Poetry](https://python-poetry.org/)\n\n# Description\nTorchMiner is designed to automatic process the training ,evaluating and testing process for PyTorch DeepLearning,with a simple API.\n\nYou can access all Functions of MineTorch simply use `Miner`.\n\n## Project ToDo\n [!] compatible with paddlepaddle\n\n [!!!] Test Cases\n \n [!] Add A thread to accept CLI input when training\n \n [!] Abstract Miner process, for easier patches\n \n [] Abstract Plugin Manager\n\n [] Move ***Drawer*** Operations Outside of Miner as a Plugin\n \n [] A Plugin that can record every output of network for future analysis\n \n [] Add Plugin Able And Disable Stat\n \n [] Move Miner Options to yaml File, Add Config Class\n \n Now Plugins only supports output functions, they can't modify or change the data of the Miner class.Any Ideas? I am glad to know.\n \n [] Write about my design concept\n \n Critical \n \n [] Deal About the input size problem, such as Batch-first...",
+    'author': 'InEase',
+    'author_email': 'inease28@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.7.1,<4.0',
+}
+
+
+setup(**setup_kwargs)
