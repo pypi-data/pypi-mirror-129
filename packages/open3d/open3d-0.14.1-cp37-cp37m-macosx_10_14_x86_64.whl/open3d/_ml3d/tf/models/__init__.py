dcd502203fc2170ad280e84633667924e5c9b567
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:43:46 +0000

    Add package open3d/0.14.1/open3d-0.14.1-cp37-cp37m-macosx_10_14_x86_64.whl

---
+++ packages/open3d/open3d-0.14.1-cp37-cp37m-macosx_10_14_x86_64.whl/open3d/_ml3d/tf/models/__init__.py
@@ -0,0 +1,14 @@
+"""Tensorflow network models."""
+
+from .randlanet import RandLANet
+from .kpconv import KPFCNN
+from .point_pillars import PointPillars
+from .sparseconvnet import SparseConvUnet
+from .point_rcnn import PointRCNN
+from .point_transformer import PointTransformer
+from .pvcnn import PVCNN
+
+__all__ = [
+    'RandLANet', 'KPFCNN', 'PointPillars', 'SparseConvUnet', 'PointRCNN',
+    'PointTransformer', 'PVCNN'
+]
