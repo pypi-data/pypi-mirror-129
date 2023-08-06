fa28864c0ba8093f1c74bd02e3a91a9af59bce07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:27:05 +0000

    Add package itba-cde-tpf-python-applications-fvidal90/1.3.0/itba_cde_tpf_python_applications_fvidal90-1.3.0-py3-none-any.whl

---
+++ packages/itba-cde-tpf-python-applications-fvidal90/itba_cde_tpf_python_applications_fvidal90-1.3.0-py3-none-any.whl/fvidal_packages/db_api_client/sqlite_cli.py
@@ -0,0 +1,22 @@
+"""Implements a client class to interact with a SqLite DB."""
+from fvidal_packages.db_api_client.parent_cli import DBAPIClient
+
+
+class SqLiteClient(DBAPIClient):
+    """Children class to connect to a SqLite DB using SQLAlchemy.
+
+    Attributes
+    ----------
+        db: str
+            DB to connect.
+    """
+
+    def __init__(self, db):
+        """Constructor method for this children class.
+
+        Parameters
+        ----------
+            db: str
+                DB to connect.
+        """
+        super().__init__("sqlite", db)
