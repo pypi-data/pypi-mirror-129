bbea40066701957cb5ce0c6394f639d56f8127ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:06:43 +0000

    Add package minos-microservice-common/0.3.2/minos_microservice_common-0.3.2.tar.gz

---
+++ packages/minos-microservice-common/minos_microservice_common-0.3.2.tar.gz/minos_microservice_common-0.3.2/minos/common/model/__init__.py
@@ -0,0 +1,31 @@
+from .abc import (
+    Model,
+)
+from .declarative import (
+    DeclarativeModel,
+    MinosModel,
+)
+from .dynamic import (
+    BucketModel,
+    DataTransferObject,
+    DynamicModel,
+)
+from .fields import (
+    Field,
+    ModelField,
+)
+from .serializers import (
+    AvroDataDecoder,
+    AvroDataEncoder,
+    AvroSchemaDecoder,
+    AvroSchemaEncoder,
+)
+from .types import (
+    GenericTypeProjector,
+    MissingSentinel,
+    ModelType,
+    NoneType,
+    TypeHintBuilder,
+    TypeHintComparator,
+    is_model_type,
+)
---
+++ packages/minos-microservice-common/minos_microservice_common-0.3.2.tar.gz/minos_microservice_common-0.3.2/minos/common/model/serializers/__init__.py
@@ -0,0 +1,12 @@
+from .avro_data_decoder import (
+    AvroDataDecoder,
+)
+from .avro_data_encoder import (
+    AvroDataEncoder,
+)
+from .avro_schema_decoder import (
+    AvroSchemaDecoder,
+)
+from .avro_schema_encoder import (
+    AvroSchemaEncoder,
+)
