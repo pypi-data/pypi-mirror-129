e0f9243dd08554f62d9cb141bdc9699fc2c425ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:22 +0000

    Add package tictactoe-marlene/0.1.3/tictactoe_marlene-0.1.3.tar.gz

---
+++ packages/tictactoe-marlene/tictactoe_marlene-0.1.3.tar.gz/tictactoe_marlene-0.1.3/src/tictactoe_marlene/main.py
@@ -0,0 +1,13 @@
+from src.tictactoe_marlene import Tictactoe
+
+
+def main():
+    """ Main with new game obj and call start"""
+    game = Tictactoe()
+    game.start()
+
+
+if __name__ == "__main__":
+    main()
+
+
