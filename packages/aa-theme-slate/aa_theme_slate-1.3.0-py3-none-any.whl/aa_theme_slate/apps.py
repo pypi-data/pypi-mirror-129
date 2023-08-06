66d85115f667b911ba6439e420936bf20ffea1b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:06:45 +0000

    Add package aa-theme-slate/1.3.0/aa_theme_slate-1.3.0-py3-none-any.whl

---
+++ packages/aa-theme-slate/aa_theme_slate-1.3.0-py3-none-any.whl/aa_theme_slate/apps.py
@@ -0,0 +1,19 @@
+"""
+App Config
+"""
+
+from django.apps import AppConfig
+
+from . import __version__
+
+
+class AaThemeConfig(AppConfig):
+    """
+    App config
+    """
+
+    name = "aa_theme_slate"
+    label = "aa_theme_slate"
+    verbose_name = 'Bootstrap Theme "Slate" for Alliance Auth v{version}'.format(
+        version=__version__
+    )
