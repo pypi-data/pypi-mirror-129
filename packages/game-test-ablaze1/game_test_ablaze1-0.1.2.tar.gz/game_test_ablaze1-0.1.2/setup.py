65f304e5855008339f6844e0a65adc9537c11cf4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:27:22 +0000

    Add package game-test-ablaze1/0.1.2/game_test_ablaze1-0.1.2.tar.gz

---
+++ packages/game-test-ablaze1/game_test_ablaze1-0.1.2.tar.gz/game_test_ablaze1-0.1.2/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['game_test_ablaze1']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'game-test-ablaze1',
+    'version': '0.1.2',
+    'description': '',
+    'long_description': None,
+    'author': 'Your Name',
+    'author_email': 'you@example.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
