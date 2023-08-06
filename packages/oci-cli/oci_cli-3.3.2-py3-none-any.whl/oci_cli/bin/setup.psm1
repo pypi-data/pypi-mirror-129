b71e8dbb598f0b166699f2f3e0e1d7f142696a95
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:24:36 +0000

    Add package oci-cli/3.3.2/oci_cli-3.3.2-py3-none-any.whl

---
+++ packages/oci-cli/oci_cli-3.3.2-py3-none-any.whl/oci_cli/bin/setup.psm1
@@ -0,0 +1,36 @@
+function GetOciTopLevelCommand_setup() {
+    return 'setup'
+}
+
+function GetOciSubcommands_setup() {
+    $ociSubcommands = @{
+        'setup' = 'autocomplete bootstrap config instance-principal keys oci-cli-rc repair-file-permissions'
+    }
+    return $ociSubcommands
+}
+
+function GetOciCommandsToLongParams_setup() {
+    $ociCommandsToLongParams = @{
+        'setup autocomplete' = 'help'
+        'setup bootstrap' = 'config-location help profile-name'
+        'setup config' = 'help'
+        'setup instance-principal' = 'help'
+        'setup keys' = 'help key-name output-dir overwrite passphrase passphrase-file'
+        'setup oci-cli-rc' = 'file help'
+        'setup repair-file-permissions' = 'file help'
+    }
+    return $ociCommandsToLongParams
+}
+
+function GetOciCommandsToShortParams_setup() {
+    $ociCommandsToShortParams = @{
+        'setup autocomplete' = '? h'
+        'setup bootstrap' = '? h'
+        'setup config' = '? h'
+        'setup instance-principal' = '? h'
+        'setup keys' = '? h'
+        'setup oci-cli-rc' = '? h'
+        'setup repair-file-permissions' = '? h'
+    }
+    return $ociCommandsToShortParams
+}
