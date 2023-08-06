bf19f060de42ab7e6a46e4870ff65a5705d57158
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:23:20 +0000

    Add package ts2python/0.5/ts2python-0.5-py3-none-any.whl

---
+++ packages/ts2python/ts2python-0.5-py3-none-any.whl/demo/test.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/sh
+
+python3.10 lsp-specification-3-17.py
+python lsp-specification-3-17.py
+python3.8 lsp-specification-3-17.py
+python3.7 lsp-specification-3-17.py
+python3.6 lsp-specification-3-17.py
