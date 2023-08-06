964c246c65b560993b5f89946d5f9cd2df236fa5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:22:03 +0000

    Add package blrec/1.2.2/blrec-1.2.2-py3-none-any.whl

---
+++ packages/blrec/blrec-1.2.2-py3-none-any.whl/blrec/path/__init__.py
@@ -0,0 +1,16 @@
+from .helpers import (
+    file_exists,
+    create_file,
+    danmaku_path,
+    raw_danmaku_path,
+    extra_metadata_path,
+)
+
+
+__all__ = (
+    'file_exists',
+    'create_file',
+    'danmaku_path',
+    'raw_danmaku_path',
+    'extra_metadata_path',
+)
