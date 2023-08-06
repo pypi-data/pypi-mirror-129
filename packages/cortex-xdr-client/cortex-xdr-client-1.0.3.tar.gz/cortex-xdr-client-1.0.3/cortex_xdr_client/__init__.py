b698091ec21df1961761522aee4c7f765ce30de4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:59 +0000

    Add package cortex-xdr-client/1.0.3/cortex-xdr-client-1.0.3.tar.gz

---
+++ packages/cortex-xdr-client/cortex-xdr-client-1.0.3.tar.gz/cortex-xdr-client-1.0.3/cortex_xdr_client/__init__.py
@@ -0,0 +1,4 @@
+from .client import CortexXDRClient
+from cortex_xdr_client.api.models.alerts import *
+from cortex_xdr_client.api.models.endpoints import *
+from cortex_xdr_client.api.models.incidents import *
