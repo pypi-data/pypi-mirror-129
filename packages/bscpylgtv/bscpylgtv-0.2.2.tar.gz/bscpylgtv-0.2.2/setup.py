9ec00f4115f2e5bf670ad554935d7b8dc6eade1d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:12:45 +0000

    Add package bscpylgtv/0.2.2/bscpylgtv-0.2.2.tar.gz

---
+++ packages/bscpylgtv/bscpylgtv-0.2.2.tar.gz/bscpylgtv-0.2.2/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+
+with open("README.md") as f:
+    readme = f.read()
+
+extras = {
+   "with_calibration": ["numpy>=1.17.0"]
+}
+
+setup(
+    name="bscpylgtv",
+    packages=["bscpylgtv"],
+    install_requires=["websockets==8.1", "sqlitedict"],
+    extras_require=extras,
+    python_requires=">=3.7",
+    zip_safe=True,
+    version="0.2.2",
+    description="Library to control webOS based LG TV devices.",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    author="chros",
+    author_email="chros73@gmail.com",
+    url="https://github.com/chros73/bscpylgtv",
+    keywords=["webos", "tv"],
+    classifiers=[],
+    entry_points={
+        "console_scripts": ["bscpylgtvcommand=bscpylgtv.utils:bscpylgtvcommand"]
+    },
+)
