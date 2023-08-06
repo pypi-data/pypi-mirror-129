bff0af19919b275fc187d4a62512ea8bb4a6f32e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:08:37 +0000

    Add package woodchips/0.2.3/woodchips-0.2.3.tar.gz

---
+++ packages/woodchips/woodchips-0.2.3.tar.gz/woodchips-0.2.3/setup.py
@@ -0,0 +1,36 @@
+import setuptools
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+DEV_REQUIREMENTS = [
+    'black',
+    'coveralls == 3.*',
+    'flake8',
+    'isort',
+    'mypy',
+    'pytest == 6.*',
+    'pytest-cov == 2.*',
+]
+
+setuptools.setup(
+    name='woodchips',
+    version='0.2.3',
+    description='The cutest little logger you\'ve ever seen.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='http://github.com/justintime50/woodchips',
+    author='justintime50',
+    license='MIT',
+    packages=setuptools.find_packages(),
+    package_data={'woodchips': ['py.typed']},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    extras_require={
+        'dev': DEV_REQUIREMENTS,
+    },
+    python_requires='>=3.7, <4',
+)
