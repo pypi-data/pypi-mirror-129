899e342e894a8fa908a4c8c75e7d701b19ee1c13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:29:48 +0000

    Add package snakefusion/0.1.0/snakefusion-0.1.0.tar.gz

---
+++ packages/snakefusion/snakefusion-0.1.0.tar.gz/snakefusion-0.1.0/src/lib.rs
@@ -0,0 +1,33 @@
+use pyo3::prelude::*;
+
+mod embeddings;
+use embeddings::PyEmbeddings;
+
+mod embeddings_wrap;
+use embeddings_wrap::EmbeddingsWrap;
+
+mod iter;
+use iter::{PyEmbedding, PyEmbeddingIterator};
+
+mod similarity;
+use similarity::PyWordSimilarity;
+
+mod vocab;
+use vocab::PyVocab;
+
+mod storage;
+use storage::PyStorage;
+
+/// This is a Python module for using finalfusion embeddings.
+///
+/// finalfusion is a format for word embeddings that supports words,
+/// subwords, memory-mapped matrices, and quantized matrices.
+#[pymodule]
+fn _snakefusion(_py: Python, m: &PyModule) -> PyResult<()> {
+    m.add_class::<PyEmbeddings>()?;
+    m.add_class::<PyEmbedding>()?;
+    m.add_class::<PyStorage>()?;
+    m.add_class::<PyWordSimilarity>()?;
+    m.add_class::<PyVocab>()?;
+    Ok(())
+}
