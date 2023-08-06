6a421990615feb9640b635dfa1e725362e0d2057
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:46:09 +0000

    Add package aerouter/0.1/aerouter-0.1.zip

---
+++ packages/aerouter/aerouter-0.1.zip/aerouter-0.1/setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+setuptools.setup(
+     name='aerouter',  
+     version='0.1',
+     scripts=['aerouter'] ,
+     author="fwang",
+     author_email="fwang@mowentech.com",
+     description="Place holder!",
+     long_description="Place Holder!",
+     url="https://aerouter.com",
+     packages=setuptools.find_packages(),
+     classifiers=[
+         "Development Status :: 1 - Planning",
+     ],
+ )
