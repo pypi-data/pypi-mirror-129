fd260244cea42c4b0e8689642cff45073ad6e3cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:06:44 +0000

    Add package nepali/0.5.0/nepali-0.5.0.tar.gz

---
+++ packages/nepali/nepali-0.5.0.tar.gz/nepali-0.5.0/nepali/datetime/__init__.py
@@ -0,0 +1,13 @@
+from ._datetime import nepalidate, nepalitime, nepalidatetime
+from ._formarter import NepaliDateTimeFormater
+from ._humanize import HumanizeDateTime
+
+__all__ = [
+	'nepalidate',
+	'nepalitime',
+	'nepalidatetime',
+
+	'NepaliDateTimeFormater',
+	'HumanizeDateTime',
+
+]
