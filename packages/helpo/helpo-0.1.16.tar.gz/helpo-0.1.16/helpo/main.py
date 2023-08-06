b57b2f61edd987026884f3eab46b9be68b4dbc0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:21:22 +0000

    Add package helpo/0.1.16/helpo-0.1.16.tar.gz

---
+++ packages/helpo/helpo-0.1.16.tar.gz/helpo-0.1.16/helpo/main.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3.8
+
+import helpo.cloudflarecmd as cloudflarecmd
+import helpo.jiracmd as jiracmd
+import helpo.namedotcomcmd as namedotcomcmd
+import helpo.dediboxcmd as dediboxcmd
+import helpo.remoteservercmd as remoteservercmd
+import helpo.rundeckjobscmd as rundeckjobscmd
+import typer
+
+app = typer.Typer(no_args_is_help=True)
+app.add_typer(cloudflarecmd.app, name="cloudflare", no_args_is_help=True)
+app.add_typer(namedotcomcmd.app, name="namecom", no_args_is_help=True)
+app.add_typer(jiracmd.app, name="jira", no_args_is_help=True)
+app.add_typer(dediboxcmd.app, name="dedibox", no_args_is_help=True)
+app.add_typer(remoteservercmd.app, name="remoteserver", no_args_is_help=True)
+app.add_typer(rundeckjobscmd.app, name="rundeckjobs", no_args_is_help=True)
+if __name__ == "__main__":
+    app()
